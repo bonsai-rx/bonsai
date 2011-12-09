13d1a0e5f561dfbc9e56eac1c28cb2eb43de567e
glopes <goncaloclopes@gmail.com>
2011-12-09 20:54:22 +0000

    Initial draft of video analysis framework.

---
+++ VideoAnalyzer/Source.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VideoAnalyzer
+{
+    public abstract class Source<T> : WorkflowElement
+    {
+        public event EventHandler<OutputChangedEventArgs<T>> OutputChanged;
+
+        public abstract void Start();
+
+        public abstract void Stop();
+
+        protected virtual void OnOutput(OutputChangedEventArgs<T> e)
+        {
+            var handler = OutputChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+
+    public class OutputChangedEventArgs<T> : EventArgs
+    {
+        public OutputChangedEventArgs(T output)
+        {
+            Output = output;
+        }
+
+        public T Output { get; private set; }
+    }
+}
