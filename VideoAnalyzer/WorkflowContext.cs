0ccc4fd9c29120fc91d5d46a0e88951ac2b76f7f
glopes <glopesdev@users.noreply.github.com>
2011-12-09 20:54:22 +0000

    Initial draft of video analysis framework.

---
+++ VideoAnalyzer/WorkflowContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VideoAnalyzer
+{
+    public class WorkflowContext
+        : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            return null;
+        }
+    }
+}
