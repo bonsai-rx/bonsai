44c750b2060b1d50e487c1eb73d6d0e2cd9cf97a
glopes <goncaloclopes@gmail.com>
2011-12-17 20:19:20 +0000

    Added extendable type visualizer API to support visualization of workflow data. Added TypeVisualizerDialog as the default form-based container for visualizers. Added default ObjectTextVisualizer which shows an object string representation.

---
+++ Bonsai.Design/DialogTypeVisualizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Design
+{
+    public abstract class DialogTypeVisualizer
+    {
+        public abstract void Show(object value);
+
+        public abstract void Load(IServiceProvider provider);
+
+        public abstract void Unload();
+    }
+}
---
+++ Bonsai.Design/ObjectTextVisualizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Bonsai;
+using Bonsai.Design;
+
+[assembly: TypeVisualizer(typeof(ObjectTextVisualizer), Target = typeof(object))]
+
+namespace Bonsai.Design
+{
+    public class ObjectTextVisualizer : DialogTypeVisualizer
+    {
+        Label control;
+
+        public override void Show(object value)
+        {
+            if (control.InvokeRequired)
+            {
+                control.BeginInvoke((Action<object>)Show, value);
+            }
+            else control.Text = value != null ? value.ToString() : null;
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            control = new Label();
+            control.AutoSize = true;
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(control);
+            }
+        }
+
+        public override void Unload()
+        {
+            control.Dispose();
+            control = null;
+        }
+    }
+}
