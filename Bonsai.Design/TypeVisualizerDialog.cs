64b13f52b7318ef754e80c60c0daa6c555de24e3
glopes <glopesdev@users.noreply.github.com>
2011-12-17 20:19:20 +0000

    Added extendable type visualizer API to support visualization of workflow data. Added TypeVisualizerDialog as the default form-based container for visualizers. Added default ObjectTextVisualizer which shows an object string representation.

---
+++ Bonsai.Design/TypeVisualizerDialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bonsai.Design
+{
+    public partial class TypeVisualizerDialog : Form, IDialogTypeVisualizerService
+    {
+        public TypeVisualizerDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void AddControl(Control control)
+        {
+            Controls.Add(control);
+        }
+    }
+}
