95bea05c952ee91e229f0507d1b0c3c21f07e95c
glopes <glopesdev@users.noreply.github.com>
2011-12-19 02:54:14 +0000

    Added ObservableSink for turning workflow end points into observable streams.

---
+++ Bonsai/ObservableSink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Bonsai
+{
+    public class ObservableSink<T> : Sink<T>
+    {
+        OutputObservable<T> output;
+
+        public ObservableSink()
+        {
+            output = new OutputObservable<T>();
+        }
+
+        [Browsable(false)]
+        public IObservable<T> Output
+        {
+            get { return output; }
+        }
+
+        public override void Process(T input)
+        {
+            output.OnNext(input);
+        }
+
+        public override void Load(WorkflowContext context)
+        {
+        }
+
+        public override void Unload(WorkflowContext context)
+        {
+        }
+    }
+}
