6d73cf3682709f068531296711918b6c7178912c
glopes <goncaloclopes@gmail.com>
2011-12-19 02:27:25 +0000

    Added observable Error property to Workflow to catch processing exceptions. Added first draft of error handling to workflow editor. Fixed issue with CvCameraCaptureSource thread initialization.

--- Bonsai/Workflow.cs
+++ Bonsai/Workflow.cs
@@ -11,10 +11,12 @@ namespace Bonsai
     public class Workflow : Source
     {
         IDisposable processingChain;
+        readonly OutputObservable<Exception> error;
         readonly WorkflowElementCollection components;
 
         public Workflow()
         {
+            error = new OutputObservable<Exception>();
             components = new WorkflowElementCollection();
         }
 
@@ -22,6 +24,11 @@ namespace Bonsai
         public bool Running { get; private set; }
 
         public event EventHandler RunningChanged;
+
+        public IObservable<Exception> Error
+        {
+            get { return error; }
+        }
 
         public WorkflowElementCollection Components
         {
@@ -91,6 +98,12 @@ namespace Bonsai
                 var method = component.GetType().GetMethod("Process");
                 output = Expression.Call(instance, method, output);
             }
+
+            // Route any raised exceptions through the observable error stream
+            var errorInstance = Expression.Constant(error);
+            var errorMethod = error.GetType().GetMethod("OnNext");
+            var exception = Expression.Parameter(typeof(Exception));
+            output = Expression.TryCatch(output, Expression.Catch(exception, Expression.Call(errorInstance, errorMethod, exception)));
 
             // Subscribe the compiled processing chain to the observable source
             var observer = Expression.Lambda(output, input).Compile();
