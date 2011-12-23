72c9502a743b30773d42923818f91b0d6e6eaf41
glopes <goncaloclopes@gmail.com>
2011-12-23 20:26:21 +0000

    Added new CvFileCaptureSource for processing video file frames. Refactored common capture code to CvCaptureSource.

---
+++ Bonsai.Vision/CvFileCaptureSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision
+{
+    public class CvFileCaptureSource : CvCaptureSource
+    {
+        [Editor("System.Windows.Forms.Design.FileNameEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        public string FileName { get; set; }
+
+        protected override CvCapture CreateCapture(WorkflowContext context)
+        {
+            return CvCapture.CreateFileCapture(FileName);
+        }
+    }
+}
