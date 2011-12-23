35025195c98af17981357057997f99e0bfcce99b
glopes <glopesdev@users.noreply.github.com>
2011-12-23 20:26:21 +0000

    Added new CvFileCaptureSource for processing video file frames. Refactored common capture code to CvCaptureSource.

--- Bonsai.Vision/CvCameraCaptureSource.cs
+++ Bonsai.Vision/CvCameraCaptureSource.cs
@@ -7,61 +7,13 @@ using System.Threading;
 
 namespace Bonsai.Vision
 {
-    public class CvCameraCaptureSource : Source<IplImage>
+    public class CvCameraCaptureSource : CvCaptureSource
     {
-        CvCapture capture;
-        Thread captureThread;
-        volatile bool running;
-        ManualResetEventSlim stop;
-
-        public CvCameraCaptureSource()
-        {
-            stop = new ManualResetEventSlim();
-        }
-
         public int Index { get; set; }
 
-        void CaptureNewFrame()
+        protected override CvCapture CreateCapture(WorkflowContext context)
         {
-            while (running)
-            {
-                var image = capture.QueryFrame();
-                if (image == null) break;
-                OnOutput(image);
-            }
-
-            stop.Set();
-        }
-
-        public override void Start()
-        {
-            running = true;
-            captureThread.Start();
-        }
-
-        public override void Stop()
-        {
-            running = false;
-            stop.Wait();
-        }
-
-        public override void Load(WorkflowContext context)
-        {
-            captureThread = new Thread(CaptureNewFrame);
-            capture = CvCapture.CreateCameraCapture(Index);
-
-            var width = (int)capture.GetProperty(CaptureProperty.FRAME_WIDTH);
-            var height = (int)capture.GetProperty(CaptureProperty.FRAME_HEIGHT);
-            context.AddService(typeof(CvSize), new CvSize(width, height));
-            base.Load(context);
-        }
-
-        public override void Unload(WorkflowContext context)
-        {
-            capture.Close();
-            captureThread = null;
-            context.RemoveService(typeof(CvSize));
-            base.Unload(context);
+            return CvCapture.CreateCameraCapture(Index);
         }
     }
 }
---
+++ Bonsai.Vision/CvCaptureSource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.Threading;
+
+namespace Bonsai.Vision
+{
+    public abstract class CvCaptureSource : Source<IplImage>
+    {
+        CvCapture capture;
+        Thread captureThread;
+        volatile bool running;
+        ManualResetEventSlim stop;
+
+        public CvCaptureSource()
+        {
+            stop = new ManualResetEventSlim();
+        }
+
+        void CaptureNewFrame()
+        {
+            while (running)
+            {
+                var image = capture.QueryFrame();
+                if (image == null) break;
+                OnOutput(image);
+            }
+
+            stop.Set();
+        }
+
+        public override void Start()
+        {
+            running = true;
+            captureThread.Start();
+        }
+
+        public override void Stop()
+        {
+            running = false;
+            stop.Wait();
+        }
+
+        protected abstract CvCapture CreateCapture(WorkflowContext context);
+
+        public override void Load(WorkflowContext context)
+        {
+            captureThread = new Thread(CaptureNewFrame);
+            capture = CreateCapture(context);
+
+            var width = (int)capture.GetProperty(CaptureProperty.FRAME_WIDTH);
+            var height = (int)capture.GetProperty(CaptureProperty.FRAME_HEIGHT);
+            context.AddService(typeof(CvSize), new CvSize(width, height));
+            base.Load(context);
+        }
+
+        public override void Unload(WorkflowContext context)
+        {
+            capture.Close();
+            captureThread = null;
+            context.RemoveService(typeof(CvSize));
+            base.Unload(context);
+        }
+    }
+}
