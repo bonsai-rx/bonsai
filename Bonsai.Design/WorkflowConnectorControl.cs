9d9fc9c2dbdc23fc3eedac2a52a6da026f2c9c07
glopes <goncaloclopes@gmail.com>
2011-12-23 16:05:02 +0000

    Added WorkflowConnectorControl in preparation for new workflow tree layout. Refactored WorkflowElementControl to support multiple workflows. Fixed WorkflowConnectorControl layout options.

---
+++ Bonsai.Design/WorkflowConnectorControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+
+namespace Bonsai.Design
+{
+    public partial class WorkflowConnectorControl : UserControl
+    {
+        public WorkflowConnectorControl()
+        {
+            InitializeComponent();
+            Font = new Font(FontFamily.GenericMonospace, 8);
+        }
+
+        public AnchorStyles Connections { get; set; }
+
+        private void WorkflowElementControl_Paint(object sender, PaintEventArgs e)
+        {
+            const float BorderSize = 5;
+
+            var midX = Size.Width / 2f + BorderSize;
+            var midY = Size.Height / 2f + BorderSize + 2;
+            if (Connections.HasFlag(AnchorStyles.Left)) e.Graphics.DrawLine(Pens.Black, 0, midY, midX, midY);
+            if (Connections.HasFlag(AnchorStyles.Right)) e.Graphics.DrawLine(Pens.Black, midX, midY, Size.Width, midY);
+            if (Connections.HasFlag(AnchorStyles.Top)) e.Graphics.DrawLine(Pens.Black, midX, 0, midX, midY);
+            if (Connections.HasFlag(AnchorStyles.Bottom)) e.Graphics.DrawLine(Pens.Black, midX, midY, midX, Size.Height);
+        }
+    }
+}
