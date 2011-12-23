27c1dd57f3fcc4fc8c223e88256d791f20ac8be0
glopes <glopesdev@users.noreply.github.com>
2011-12-23 16:05:02 +0000

    Added WorkflowConnectorControl in preparation for new workflow tree layout. Refactored WorkflowElementControl to support multiple workflows. Fixed WorkflowConnectorControl layout options.

--- Bonsai.Design/WorkflowElementControl.cs
+++ Bonsai.Design/WorkflowElementControl.cs
@@ -18,6 +18,7 @@ namespace Bonsai.Design
         public WorkflowElementControl()
         {
             InitializeComponent();
+            Dock = DockStyle.Fill;
             Font = new Font(FontFamily.GenericMonospace, 8);
         }
 
@@ -26,25 +27,6 @@ namespace Bonsai.Design
         public WorkflowElement Element { get; set; }
 
         public WorkflowElement ObservableElement { get; private set; }
-
-        public static Type GetWorkflowElementOutputType(WorkflowElement element)
-        {
-            if (element == null) throw new ArgumentNullException("element");
-
-            var type = element.GetType();
-            while (type != null)
-            {
-                if (type.IsGenericType)
-                {
-                    var arguments = type.GetGenericArguments();
-                    return arguments[arguments.Length - 1];
-                }
-
-                type = type.BaseType;
-            }
-
-            return null;
-        }
 
         public void SetObservableElement(WorkflowElement element, DialogTypeVisualizer visualizer, IServiceProvider provider)
         {
@@ -60,14 +42,20 @@ namespace Bonsai.Design
             var subscribeMethod = typeof(ObservableExtensions).GetMethods().First(m => m.Name == "Subscribe" && m.GetParameters().Length == 2);
             subscribeMethod = subscribeMethod.MakeGenericMethod(new[] { outputType });
 
+            WorkflowContext context = null;
+            EventHandler<LoadUnloadEventArgs> loadedHandler = (sender, e) => context = e.Context;
+            element.Loaded += loadedHandler;
+            Disposed += (sender, e) => element.Loaded -= loadedHandler;
+
             visualizerHandler = (sender, e) =>
             {
                 if (visualizerDialog == null)
                 {
-                    var workflow = (Workflow)provider.GetService(typeof(Workflow));
+                    if (context == null) return;
+                    var workflow = (Workflow)context.GetService(typeof(Workflow));
                     if (workflow == null || !workflow.Running) return;
 
-                    using (var visualizerContext = new WorkflowContext(provider))
+                    using (var visualizerContext = new WorkflowContext(context))
                     {
                         visualizerDialog = new TypeVisualizerDialog();
                         visualizerDialog.Text = Name;
@@ -117,7 +105,7 @@ namespace Bonsai.Design
         private void WorkflowElementControl_Paint(object sender, PaintEventArgs e)
         {
             const float BorderSize = 5;
-            const float ElementOffset = 25;
+            const float ElementOffset = 20;
             var text = Element != null ? Element.GetType().Name.Substring(0, 1) : string.Empty;
             var textSize = e.Graphics.MeasureString(text, Font);
 
@@ -134,8 +122,8 @@ namespace Bonsai.Design
                 e.Graphics.DrawString(text, Font, Brushes.Black, new PointF(ElementOffset + BorderSize, ElementOffset + BorderSize));
             }
 
-            var midX = ElementOffset + textSize.Width / 2f + BorderSize;
-            var midY = ElementOffset + textSize.Height / 2f + BorderSize;
+            var midX = ElementOffset + width / 2f;
+            var midY = ElementOffset + height / 2f;
             if (Connections.HasFlag(AnchorStyles.Left)) e.Graphics.DrawLine(Pens.Black, 0, midY, ElementOffset, midY);
             if (Connections.HasFlag(AnchorStyles.Right)) e.Graphics.DrawLine(Pens.Black, ElementOffset + width, midY, Size.Width, midY);
             if (Connections.HasFlag(AnchorStyles.Top)) e.Graphics.DrawLine(Pens.Black, midX, 0, midX, ElementOffset);
@@ -149,6 +137,64 @@ namespace Bonsai.Design
             {
                 handler(sender, e);
             }
+        }
+
+        public static WorkflowElementControl FromWorkflowElement(WorkflowElement element)
+        {
+            var type = element.GetType();
+            var elementControl = new WorkflowElementControl();
+            elementControl.Name = type.Name;
+            elementControl.Element = element;
+
+            if (MatchGenericType(type, typeof(Source<>))) elementControl.Connections = AnchorStyles.Right;
+            else if (MatchGenericType(type, typeof(Filter<,>)))
+            {
+                elementControl.Connections = AnchorStyles.Left | AnchorStyles.Right;
+                if (MatchGenericType(type, typeof(ParallelFilter<>))) elementControl.Connections |= AnchorStyles.Bottom;
+            }
+            else if (MatchGenericType(type, typeof(Sink<>))) elementControl.Connections = AnchorStyles.Left;
+            else throw new ArgumentException("Invalid workflow element type.", "element");
+
+            return elementControl;
+        }
+
+        public static Type GetWorkflowElementOutputType(WorkflowElement element)
+        {
+            if (element == null) throw new ArgumentNullException("element");
+
+            var type = element.GetType();
+            while (type != null)
+            {
+                if (type.IsGenericType)
+                {
+                    var arguments = type.GetGenericArguments();
+                    return arguments[arguments.Length - 1];
+                }
+
+                type = type.BaseType;
+            }
+
+            return null;
+        }
+
+        public static bool MatchGenericType(Type type, Type genericType)
+        {
+            if (!genericType.IsGenericType)
+            {
+                throw new ArgumentException("Trying to match against a non-generic type.", "genericType");
+            }
+
+            while (type != null)
+            {
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == genericType)
+                {
+                    return true;
+                }
+
+                type = type.BaseType;
+            }
+
+            return false;
         }
     }
 }
