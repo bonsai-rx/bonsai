a277468b816b6bf8c17eb41def46993d094432dc
glopes <glopesdev@users.noreply.github.com>
2011-12-26 17:22:55 +0000

    Added PrecisionAttribute to further refine display of decimal values.

---
+++ Bonsai/PrecisionAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class PrecisionAttribute : Attribute
+    {
+        public static readonly PrecisionAttribute Default = new PrecisionAttribute(0, 1);
+
+        public PrecisionAttribute(int decimalPlaces, int increment)
+            : this(decimalPlaces, (decimal)increment)
+        {
+        }
+
+        public PrecisionAttribute(int decimalPlaces, double increment)
+            : this(decimalPlaces, (decimal)increment)
+        {
+        }
+
+        public PrecisionAttribute(int decimalPlaces, decimal increment)
+        {
+            DecimalPlaces = decimalPlaces;
+            Increment = increment;
+        }
+
+        public int DecimalPlaces { get; private set; }
+
+        public decimal Increment { get; private set; }
+    }
+}
