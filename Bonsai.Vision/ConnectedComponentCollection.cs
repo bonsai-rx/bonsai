1d8520bd437a40e97643159683af11bce8473685
glopes <goncaloclopes@gmail.com>
2012-01-20 20:31:54 +0000

    Added Resize and Undistort combinators. Temporarily added Largest property to ConnectedComponentCollection.

--- Bonsai.Vision/ConnectedComponentCollection.cs
+++ Bonsai.Vision/ConnectedComponentCollection.cs
@@ -14,6 +14,26 @@ namespace Bonsai.Vision
             ImageSize = imageSize;
         }
 
+        public ConnectedComponent Largest
+        {
+            get
+            {
+                var max = 0.0;
+                ConnectedComponent largest = null;
+                foreach (var component in Items)
+                {
+                    var area = ImgProc.cvContourArea(component.Contour, CvSlice.WholeSeq, 0);
+                    if (area > max)
+                    {
+                        largest = component;
+                        max = area;
+                    }
+                }
+
+                return largest;
+            }
+        }
+
         public CvSize ImageSize { get; private set; }
     }
 }
---
+++ Bonsai.Vision/Undistort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.Runtime.InteropServices;
+
+namespace Bonsai.Vision
+{
+    public class Undistort : Projection<IplImage, IplImage>
+    {
+        CvMat cameraMatrix;
+        CvMat distortionCoefficients;
+
+        public Undistort()
+        {
+            Distortion = new double[5];
+        }
+
+        public CvPoint2D64f FocalLength { get; set; }
+
+        public CvPoint2D64f PrincipalPoint { get; set; }
+
+        public double[] Distortion { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            ImgProc.cvUndistort2(input, output, cameraMatrix, distortionCoefficients, CvMat.Null);
+            return output;
+        }
+
+        public override IDisposable Load()
+        {
+            var intrinsics = new double[] { FocalLength.X, 0, PrincipalPoint.X, 0, FocalLength.Y, PrincipalPoint.Y, 0, 0, 1 };
+            cameraMatrix = new CvMat(3, 3, CvMatDepth.CV_64F, 1);
+            Marshal.Copy(intrinsics, 0, cameraMatrix.Data, intrinsics.Length);
+
+            distortionCoefficients = new CvMat(5, 1, CvMatDepth.CV_64F, 1);
+            Marshal.Copy(Distortion, 0, distortionCoefficients.Data, Distortion.Length);
+            return base.Load();
+        }
+    }
+}
