99ad962a2938741ffcf57c47a277a1990cc8e91e
glopes <glopesdev@users.noreply.github.com>
2012-01-20 20:31:54 +0000

    Added Resize and Undistort combinators. Temporarily added Largest property to ConnectedComponentCollection.

---
+++ Bonsai.Vision/Resize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Resize : Projection<IplImage, IplImage>
+    {
+        public CvSize Size { get; set; }
+
+        public SubPixelInterpolation Interpolation { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            if (input.Size != Size)
+            {
+                var output = new IplImage(Size, input.Depth, input.NumChannels);
+                ImgProc.cvResize(input, output, Interpolation);
+                return output;
+            }
+            else return input;
+        }
+    }
+}
