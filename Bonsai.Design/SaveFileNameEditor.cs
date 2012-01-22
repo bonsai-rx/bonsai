d0c43c226fe4083f5e3c127780eaee5ee3cee41e
glopes <glopesdev@users.noreply.github.com>
2012-01-22 03:36:29 +0000

    Added a SaveFileNameEditor for editing names of output save files.

---
+++ Bonsai.Design/SaveFileNameEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing.Design;
+using System.ComponentModel;
+using System.Windows.Forms.Design;
+using System.Windows.Forms;
+
+namespace Bonsai.Design
+{
+    public class SaveFileNameEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+            if (context != null && editorService != null)
+            {
+                using (var dialog = new SaveFileDialog())
+                {
+                    var fileName = value as string;
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        dialog.FileName = fileName;
+                    }
+
+                    dialog.Filter = "All Files (*.*)|*.*";
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        return dialog.FileName;
+                    }
+                }
+            }
+
+            return base.EditValue(context, provider, value);
+        }
+    }
+}
