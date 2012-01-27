6c4ded57220847d71f1d5a241226918381f52982
glopes <goncaloclopes@gmail.com>
2012-01-27 02:42:30 +0000

    Added missing files for Delay, Throttle and DistinctUntilChanged support.

---
+++ Bonsai/Expressions/DelayBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+using System.Reflection;
+using System.Reactive.Linq;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Delay")]
+    public class DelayBuilder : CombinatorBuilder
+    {
+        static readonly MethodInfo delayMethod = typeof(Observable).GetMethods().First(m => m.Name == "Delay" &&
+                                                                                       m.GetParameters().Length == 2 &&
+                                                                                       m.GetParameters()[1].ParameterType == typeof(TimeSpan));
+
+        public TimeSpan DueTime { get; set; }
+
+        public override Expression Build()
+        {
+            var sourceType = Source.Type.GetGenericArguments()[0];
+            var dueTime = Expression.Constant(DueTime);
+            return Expression.Call(delayMethod.MakeGenericMethod(sourceType), Source, dueTime);
+        }
+    }
+}
