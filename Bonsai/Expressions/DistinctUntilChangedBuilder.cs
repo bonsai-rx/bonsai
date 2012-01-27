17990b867ace764ec640cf6811c2c4df09a989bf
glopes <glopesdev@users.noreply.github.com>
2012-01-27 02:42:30 +0000

    Added missing files for Delay, Throttle and DistinctUntilChanged support.

---
+++ Bonsai/Expressions/DistinctUntilChangedBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("DistinctUntilChanged")]
+    public class DistinctUntilChangedBuilder : CombinatorBuilder
+    {
+        static readonly MethodInfo distinctUntilChangedMethod = typeof(Observable).GetMethods().First(m => m.Name == "DistinctUntilChanged" &&
+                                                                                                      m.GetParameters().Length == 1);
+
+        public override Expression Build()
+        {
+            var sourceType = Source.Type.GetGenericArguments()[0];
+            return Expression.Call(distinctUntilChangedMethod.MakeGenericMethod(sourceType), Source);
+        }
+    }
+}
---
+++ Bonsai/Expressions/ThrottleBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Throttle")]
+    public class ThrottleBuilder : CombinatorBuilder
+    {
+        static readonly MethodInfo throttleMethod = typeof(Observable).GetMethods().First(m => m.Name == "Throttle" &&
+                                                                                          m.GetParameters().Length == 2);
+
+        public TimeSpan DueTime { get; set; }
+
+        public override Expression Build()
+        {
+            var sourceType = Source.Type.GetGenericArguments()[0];
+            var dueTime = Expression.Constant(DueTime);
+            return Expression.Call(throttleMethod.MakeGenericMethod(sourceType), Source, dueTime);
+        }
+    }
+}
