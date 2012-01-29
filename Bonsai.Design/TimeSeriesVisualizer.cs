159f41c063574888694ab9106ac188a81972abf5
glopes <goncaloclopes@gmail.com>
2012-01-29 23:38:29 +0000

    Added TimeSeriesVisualizer as default visualization for numeric data types.

---
+++ Bonsai.Design/TimeSeriesVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Design;
+
+[assembly: TypeVisualizer(typeof(TimeSeriesVisualizer), Target = typeof(int))]
+[assembly: TypeVisualizer(typeof(TimeSeriesVisualizer), Target = typeof(float))]
+[assembly: TypeVisualizer(typeof(TimeSeriesVisualizer), Target = typeof(double))]
+
+namespace Bonsai.Design
+{
+    public class TimeSeriesVisualizer : DialogTypeVisualizer
+    {
+        TimeSeriesControl control;
+
+        public override void Show(object value)
+        {
+            control.Points.AddXY(DateTime.Now, value);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            control = new TimeSeriesControl();
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(control);
+            }
+        }
+
+        public override void Unload()
+        {
+            control.Dispose();
+            control = null;
+        }
+    }
+}
