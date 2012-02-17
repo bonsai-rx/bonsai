2fb23c0b3c206fff008892d173d20856b143a88f
IntelligentSystem <glopesdev@users.noreply.github.com>
2012-02-17 12:14:11 +0000

    Added Firmata-based Bonsai.Arduino project to core Bonsai solution.

---
+++ Bonsai.Arduino/AnalogInputReceivedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Arduino
+{
+    public class AnalogInputReceivedEventArgs : EventArgs
+    {
+        public AnalogInputReceivedEventArgs(int pin, int value)
+        {
+            Pin = pin;
+            Value = value;
+        }
+
+        public int Pin { get; private set; }
+
+        public int Value { get; private set; }
+    }
+}
---
+++ Bonsai.Arduino/ArduinoAnalogInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Bonsai.Arduino
+{
+    public class ArduinoAnalogInput : Source<int>
+    {
+        IObservable<int> analogInput;
+        IDisposable connection;
+
+        [TypeConverter(typeof(SerialPortNameConverter))]
+        public string SerialPort { get; set; }
+
+        public int Pin { get; set; }
+
+        public override IDisposable Load()
+        {
+            analogInput = ObservableArduino.AnalogInput(SerialPort, Pin);
+            return base.Load();
+        }
+
+        protected override void Start()
+        {
+            connection = analogInput.Subscribe(Subject);
+        }
+
+        protected override void Stop()
+        {
+            connection.Dispose();
+        }
+    }
+}
