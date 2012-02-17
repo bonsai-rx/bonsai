d399b97aac58d63793ee0f3f2ecb3737ebef554a
IntelligentSystem <IntelligentSystem@Behavior01.champalimaud.pt>
2012-02-17 12:00:02 +0000

    Added new combinators for binary region transformation. Added Mask projection for masking out image regions. Added RangeThreshold for independently thresholding pixels by channel range.

---
+++ Bonsai.Vision.Design/RangeScalarConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Bonsai.Design;
+using OpenCV.Net;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision.Design
+{
+    public class RangeScalarConverter : TypeConverter
+    {
+        public override bool GetPropertiesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            var propertyDescriptor = context.PropertyDescriptor;
+            var valueAttributes = new Attribute[] { new RangeAttribute(0, 255), new EditorAttribute(typeof(TrackBarEditor), typeof(UITypeEditor)) };
+
+            var properties = new PropertyDescriptor[4];
+            properties[0] = new DynamicPropertyDescriptor<double>("Val0", c => ((CvScalar)c).Val0, (c, v) => { var s = (CvScalar)c; s.Val0 = (double)v; propertyDescriptor.SetValue(context.Instance, s); }, valueAttributes);
+            properties[1] = new DynamicPropertyDescriptor<double>("Val1", c => ((CvScalar)c).Val1, (c, v) => { var s = (CvScalar)c; s.Val1 = (double)v; propertyDescriptor.SetValue(context.Instance, s); }, valueAttributes);
+            properties[2] = new DynamicPropertyDescriptor<double>("Val2", c => ((CvScalar)c).Val2, (c, v) => { var s = (CvScalar)c; s.Val2 = (double)v; propertyDescriptor.SetValue(context.Instance, s); }, valueAttributes);
+            properties[3] = new DynamicPropertyDescriptor<double>("Val3", c => ((CvScalar)c).Val3, (c, v) => { var s = (CvScalar)c; s.Val3 = (double)v; propertyDescriptor.SetValue(context.Instance, s); }, valueAttributes);
+
+            return new PropertyDescriptorCollection(properties);
+        }
+    }
+}
