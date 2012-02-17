6aaef16f870d1ea92e06a526da01602c5db1a159
IntelligentSystem <IntelligentSystem@Behavior01.champalimaud.pt>
2012-02-17 12:31:10 +0000

    Added missing files.

---
+++ Bonsai/Combinator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai
+{
+    public abstract class Combinator<TSource, TResult> : LoadableElement
+    {
+        public abstract IObservable<TResult> Process(IObservable<TSource> source);
+    }
+}
---
+++ Bonsai/Expressions/QueryBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Query", Namespace = Constants.XmlNamespace)]
+    public class QueryBuilder : CombinatorBuilder
+    {
+        [Browsable(false)]
+        public LoadableElement Combinator { get; set; }
+
+        public override Expression Build()
+        {
+            var combinatorExpression = Expression.Constant(Combinator);
+            return Expression.Call(combinatorExpression, "Process", null, Source);
+        }
+    }
+}
