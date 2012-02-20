0f8ffc352e5cc489ff114c8b0152c842b1fabd59
IntelligentSystem <IntelligentSystem@Behavior01.champalimaud.pt>
2012-02-20 20:08:13 +0000

    Added Repeat combinator to built-in package.

--- Bonsai/Expressions/ExpressionBuilder.cs
+++ Bonsai/Expressions/ExpressionBuilder.cs
@@ -17,6 +17,7 @@ namespace Bonsai.Expressions
     [XmlInclude(typeof(MemberSelectorBuilder))]
     [XmlInclude(typeof(DistinctUntilChangedBuilder))]
     [XmlInclude(typeof(TimestampBuilder))]
+    [XmlInclude(typeof(RepeatBuilder))]
     [XmlInclude(typeof(TimeIntervalBuilder))]
     [XmlInclude(typeof(ThrottleBuilder))]
     [XmlInclude(typeof(SampleBuilder))]
---
+++ Bonsai/Expressions/RepeatBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Repeat", Namespace = Constants.XmlNamespace)]
+    public class RepeatBuilder : CombinatorBuilder
+    {
+        static readonly MethodInfo repeatMethod = typeof(Observable).GetMethods()
+                                                                    .First(m => m.Name == "Repeat" &&
+                                                                           m.GetParameters().Length == 1 &&
+                                                                           m.GetParameters()[0].ParameterType.IsGenericType &&
+                                                                           m.GetParameters()[0].ParameterType.GetGenericTypeDefinition() == typeof(IObservable<>));
+
+        public override Expression Build()
+        {
+            var observableType = Source.Type.GetGenericArguments()[0];
+            return Expression.Call(repeatMethod.MakeGenericMethod(observableType), Source);
+        }
+    }
+}
