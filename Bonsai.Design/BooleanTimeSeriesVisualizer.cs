bfb4363da9fdb0e5e2fae6b35156c44ffc32b8f4
glopes <goncaloclopes@gmail.com>
2012-02-26 11:42:07 +0000

    Added BooleanTimeSeriesVisualizer as the default visualizer of boolean streams.

---
+++ Bonsai.Design/BooleanTimeSeriesVisualizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Design;
+
+[assembly: TypeVisualizer(typeof(BooleanTimeSeriesVisualizer), Target = typeof(bool))]
+
+namespace Bonsai.Design
+{
+    public class BooleanTimeSeriesVisualizer : TimeSeriesVisualizer
+    {
+        object previous;
+
+        public override void Show(object value)
+        {
+            var time = DateTime.Now;
+            if (previous != null)
+            {
+                AddValue(time, previous);
+            }
+
+            AddValue(time, value);
+            previous = value;
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            base.Load(provider);
+            Chart.TimeSeries.BorderWidth = 2;
+        }
+
+        public override void Unload()
+        {
+            previous = null;
+            base.Unload();
+        }
+    }
+}
