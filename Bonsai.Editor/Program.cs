d929bcc30a3445ee651ec155b079e11b6d297bcc
IntelligentSystem <glopesdev@users.noreply.github.com>
2012-02-27 17:38:45 +0000

    Added the possibility of opening workflows directly from command line arguments.

--- Bonsai.Editor/Program.cs
+++ Bonsai.Editor/Program.cs
@@ -11,11 +11,12 @@ namespace Bonsai.Editor
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            var initialFileName = args.Length > 0 ? args[0] : null;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm { InitialFileName = initialFileName });
         }
     }
 }
