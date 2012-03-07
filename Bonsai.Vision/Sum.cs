64618e9c30e9464c38c644a770a90c613827e1d1
glopes <glopesdev@users.noreply.github.com>
2012-03-07 14:15:01 +0000

    Added Sum projection to Bonsai.Vision.

---
+++ Bonsai.Vision/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Sum : Projection<IplImage, CvScalar>
+    {
+        public override CvScalar Process(IplImage input)
+        {
+            return Core.cvSum(input);
+        }
+    }
+}
