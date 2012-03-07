bbafe576f4b695fc5366f826bbe4720ba30a4738
glopes <goncaloclopes@gmail.com>
2012-03-07 14:14:47 +0000

    Added CvScalarVisualizer to Bonsai.Vision.Design.

---
+++ Bonsai.Vision.Design/CvScalarVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Design;
+using OpenCV.Net;
+using Bonsai;
+using Bonsai.Vision.Design;
+
+[assembly: TypeVisualizer(typeof(CvScalarVisualizer), Target = typeof(CvScalar))]
+
+namespace Bonsai.Vision.Design
+{
+    public class CvScalarVisualizer : TimeSeriesVisualizer
+    {
+        public CvScalarVisualizer()
+            : base(4)
+        {
+        }
+
+        public override void Show(object value)
+        {
+            var scalar = (CvScalar)value;
+            AddValue(DateTime.Now, scalar.Val0, scalar.Val1, scalar.Val2, scalar.Val3);
+        }
+    }
+}
