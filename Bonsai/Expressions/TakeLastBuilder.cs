0ce1465c4730784a0b85ff5e90c44e612d02ddc0
IntelligentSystem <glopesdev@users.noreply.github.com>
2012-03-14 17:14:05 +0000

    Added new SubscribeWhen operator to control subscription conditioned on events. Added support for Skip, SkipLast, Take and TakeLast operators.

---
+++ Bonsai/Expressions/TakeLastBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive.Linq;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("TakeLast", Namespace = Constants.XmlNamespace)]
+    public class TakeLastBuilder : CombinatorBuilder
+    {
+        public int Count { get; set; }
+
+        protected override IObservable<TSource> Combine<TSource>(IObservable<TSource> source)
+        {
+            return source.TakeLast(Count);
+        }
+    }
+}
