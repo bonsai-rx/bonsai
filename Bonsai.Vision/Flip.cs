c0dd5f1c5605b0f69c6f7860bb7de00abf9c5e82
glopes <goncaloclopes@gmail.com>
2012-03-19 02:05:54 +0000

    Added Flip, Opening, RunningAverage and SaveImage modules to Vision package.

---
+++ Bonsai.Vision/Flip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Flip : Projection<IplImage, IplImage>
+    {
+        public FlipMode Mode { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            Core.cvFlip(input, output, Mode);
+            return output;
+        }
+    }
+}
---
+++ Bonsai.Vision/RunningAverage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class RunningAverage : Projection<IplImage, IplImage>
+    {
+        IplImage accumulator;
+
+        public double Alpha { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            if (accumulator == null)
+            {
+                accumulator = new IplImage(input.Size, 32, input.NumChannels);
+            }
+
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            ImgProc.cvRunningAvg(input, accumulator, Alpha, CvArr.Null);
+            Core.cvConvertScale(accumulator, output, 1, 0);
+            return output;
+        }
+
+        protected override void Unload()
+        {
+            if (accumulator != null)
+            {
+                accumulator.Close();
+                accumulator = null;
+            }
+            base.Unload();
+        }
+    }
+}
