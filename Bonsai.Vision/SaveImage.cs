67cdabda6c9dbf84ac7ee72e41d6f51bdebe7442
glopes <glopesdev@users.noreply.github.com>
2012-03-19 02:05:54 +0000

    Added Flip, Opening, RunningAverage and SaveImage modules to Vision package.

---
+++ Bonsai.Vision/SaveImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision
+{
+    public class SaveImage : Sink<IplImage>
+    {
+        [FileNameFilter("PNG Files|*.png|BMP Files|*.bmp|JPEG Files|*.jpg;*.jpeg")]
+        [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string FileName { get; set; }
+
+        public override void Process(IplImage input)
+        {
+            if (!string.IsNullOrEmpty(FileName))
+            {
+                HighGui.cvSaveImage(FileName, input);
+            }
+        }
+    }
+}
