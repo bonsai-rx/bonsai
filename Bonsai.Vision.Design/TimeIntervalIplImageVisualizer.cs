f74016c576988fcfb5c1139d877c5c5ba985fddc
glopes <goncaloclopes@gmail.com>
2012-03-25 15:35:39 +0100

    Added Timestamp and TimeInterval visualizers for some of the most common types.

---
+++ Bonsai.Vision.Design/TimeIntervalIplImageVisualizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using Bonsai.Design;
+using System.Reactive;
+
+[assembly: TypeVisualizer(typeof(TimeIntervalIplImageVisualizer), Target = typeof(TimeInterval<IplImage>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class TimeIntervalIplImageVisualizer : IplImageVisualizer
+    {
+        CvFont font;
+
+        public override void Show(object value)
+        {
+            var timeIntervalImage = (TimeInterval<IplImage>)value;
+            var image = timeIntervalImage.Value.Clone();
+            var interval = timeIntervalImage.Interval.TotalSeconds;
+            Core.cvPutText(image, string.Format("Interval: {0:f4}", interval), new CvPoint(10, 20), font, CvScalar.Rgb(255, 0, 0));
+            Core.cvPutText(image, string.Format("FPS: {0:f2}", 1 / interval), new CvPoint(10, 40), font, CvScalar.Rgb(255, 0, 0));
+            base.Show(image);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            font = new CvFont(1);
+            base.Load(provider);
+        }
+
+        public override void Unload()
+        {
+            font.Close();
+            base.Unload();
+        }
+    }
+}
---
+++ Bonsai.Vision.Design/TimestampedIplImageVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using Bonsai.Design;
+using System.Reactive;
+
+[assembly: TypeVisualizer(typeof(TimestampedIplImageVisualizer), Target = typeof(Timestamped<IplImage>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class TimestampedIplImageVisualizer : IplImageVisualizer
+    {
+        CvFont font;
+
+        public override void Show(object value)
+        {
+            var timestampedImage = (Timestamped<IplImage>)value;
+            var image = timestampedImage.Value.Clone();
+            Core.cvPutText(image, timestampedImage.Timestamp.ToString("dd.MM.yyyy hh:mm.ss:ffff"), new CvPoint(10, 20), font, CvScalar.Rgb(255, 0, 0));
+            base.Show(image);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            font = new CvFont(1);
+            base.Load(provider);
+        }
+
+        public override void Unload()
+        {
+            font.Close();
+            base.Unload();
+        }
+    }
+}
