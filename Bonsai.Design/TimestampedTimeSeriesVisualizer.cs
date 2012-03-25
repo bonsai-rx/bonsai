b93171ab8c08c3068e446c6e3c7cc13398213d1f
glopes <glopesdev@users.noreply.github.com>
2012-03-25 15:35:39 +0100

    Added Timestamp and TimeInterval visualizers for some of the most common types.

---
+++ Bonsai.Design/TimestampedTimeSeriesVisualizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive;
+using Bonsai;
+using Bonsai.Design;
+
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<int>), Target = typeof(Timestamped<int>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<float>), Target = typeof(Timestamped<float>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<double>), Target = typeof(Timestamped<double>))]
+
+namespace Bonsai.Design
+{
+    public class TimestampedTimeSeriesVisualizer<T> : TimeSeriesVisualizer
+    {
+        public override void Show(object value)
+        {
+            var timestamped = (Timestamped<T>)value;
+            AddValue(timestamped.Timestamp.DateTime, timestamped.Value);
+        }
+    }
+}
