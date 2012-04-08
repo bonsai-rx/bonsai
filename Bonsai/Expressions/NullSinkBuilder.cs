04e0864cbc58ac8fba471d382eb7d9e87e50fe46
glopes <goncaloclopes@gmail.com>
2012-04-08 12:23:05 +0100

    Added NullSink combinator to allow expression of arbitrary processing endpoints.

---
+++ Bonsai/Expressions/NullSinkBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Linq.Expressions;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("NullSink", Namespace = Constants.XmlNamespace)]
+    public class NullSinkBuilder : CombinatorExpressionBuilder
+    {
+        public override Expression Build()
+        {
+            return Source;
+        }
+    }
+}
