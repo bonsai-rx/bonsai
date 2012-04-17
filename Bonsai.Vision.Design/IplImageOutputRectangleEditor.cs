affc6d3973d96eb0ad9acf57c6fa95abb920b7bd
glopes <goncaloclopes@gmail.com>
2012-04-17 12:26:34 +0100

    Updated IplImageControl to use OpenGL through OpenTK for improved performance. Added IplImageRectangleEditor types.

---
+++ Bonsai.Vision.Design/IplImageOutputRectangleEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Vision.Design
+{
+    public class IplImageOutputRectangleEditor : IplImageRectangleEditor
+    {
+        public IplImageOutputRectangleEditor()
+            : base(RectangleSource.Output)
+        {
+        }
+    }
+}
