30c22d0f90f41aced956dddbe8203ae1d51ecdb4
glopes <goncaloclopes@gmail.com>
2012-04-25 02:38:22 +0100

    Added WorkflowComponentEditor allowing service access on property pages.

---
+++ Bonsai.Design/WorkflowComponentEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Bonsai.Design
+{
+    public abstract class WorkflowComponentEditor : ComponentEditor
+    {
+        public bool EditComponent(object component, IServiceProvider provider, IWin32Window owner)
+        {
+            return EditComponent(null, component, provider, owner);
+        }
+
+        public override bool EditComponent(ITypeDescriptorContext context, object component)
+        {
+            return EditComponent(context, component, null, null);
+        }
+
+        public abstract bool EditComponent(ITypeDescriptorContext context, object component, IServiceProvider provider, IWin32Window owner);
+    }
+}
