3a346b5b4d1137ef37eb5be114e0e255afe57a7e
glopes <glopesdev@users.noreply.github.com>
2012-05-08 21:01:39 +0100

    Fixed RunningAverage first frame and added Average and ImageSubtraction modules.

---
+++ Bonsai.Vision/ImageSubtraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision
+{
+    public class ImageSubtraction : Projection<IplImage, IplImage>
+    {
+        CvArr image;
+
+        public ImageSubtraction()
+        {
+            Format = LoadImageMode.Grayscale;
+        }
+
+        [FileNameFilter("PNG Files|*.png|BMP Files|*.bmp|JPEG Files|*.jpg;*.jpeg")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string FileName { get; set; }
+
+        public LoadImageMode Format { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            Core.cvAbsDiff(input, image, output);
+            return output;
+        }
+
+        public override IDisposable Load()
+        {
+            image = HighGui.cvLoadImage(FileName, Format);
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            image.Close();
+            image = null;
+            base.Unload();
+        }
+    }
+}
