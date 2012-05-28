b63f511daa0518075249d38109585e04121909d4
glopes <glopesdev@users.noreply.github.com>
2012-05-28 15:47:30 +0100

    Added support for Loop, PlaybackRate and Playing properties on FileCapture. Split CameraCapture and FileCapture base implementations.

--- Bonsai.Vision/CameraCapture.cs
+++ Bonsai.Vision/CameraCapture.cs
@@ -7,13 +7,53 @@ using System.Threading;
 
 namespace Bonsai.Vision
 {
-    public class CameraCapture : CvCaptureSource
+    public class CameraCapture : Source<IplImage>
     {
+        CvCapture capture;
+        Thread captureThread;
+        volatile bool running;
+
         public int Index { get; set; }
 
-        protected override CvCapture CreateCapture()
+        void CaptureNewFrame()
         {
-            return CvCapture.CreateCameraCapture(Index);
+            while (running)
+            {
+                var image = capture.QueryFrame();
+                if (image == null)
+                {
+                    Subject.OnCompleted();
+                    break;
+                }
+
+                Subject.OnNext(image.Clone());
+            }
+        }
+
+        protected override void Start()
+        {
+            running = true;
+            captureThread = new Thread(CaptureNewFrame);
+            captureThread.Start();
+        }
+
+        protected override void Stop()
+        {
+            running = false;
+            captureThread.Join();
+        }
+
+        public override IDisposable Load()
+        {
+            capture = CvCapture.CreateCameraCapture(Index);
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            capture.Close();
+            captureThread = null;
+            base.Unload();
         }
     }
 }
--- Bonsai.Vision/FileCapture.cs
+++ Bonsai.Vision/FileCapture.cs
@@ -5,17 +5,97 @@ using System.Text;
 using OpenCV.Net;
 using System.ComponentModel;
 using System.Drawing.Design;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Bonsai.Vision
 {
-    public class FileCapture : CvCaptureSource
+    public class FileCapture : Source<IplImage>
     {
+        CvCapture capture;
+        Thread captureThread;
+        volatile bool running;
+        Stopwatch stopwatch;
+        double captureFps;
+        IplImage image;
+
+        public FileCapture()
+        {
+            stopwatch = new Stopwatch();
+            Playing = true;
+        }
+
         [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
         public string FileName { get; set; }
 
-        protected override CvCapture CreateCapture()
+        [Range(0, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public double PlaybackRate { get; set; }
+
+        public bool Loop { get; set; }
+
+        public bool Playing { get; set; }
+
+        void CaptureNewFrame()
         {
-            return CvCapture.CreateFileCapture(FileName);
+            while (running)
+            {
+                stopwatch.Restart();
+                if (Playing || image == null)
+                {
+                    image = capture.QueryFrame();
+                    if (image == null)
+                    {
+                        if (Loop)
+                        {
+                            capture.SetProperty(CaptureProperty.POS_FRAMES, 0);
+                            image = capture.QueryFrame();
+                        }
+                        else
+                        {
+                            Subject.OnCompleted();
+                            break;
+                        }
+                    }
+                }
+
+                var targetFps = PlaybackRate > 0 ? PlaybackRate : captureFps;
+                var dueTime = Math.Max(0, (1000.0 / targetFps) - stopwatch.Elapsed.TotalMilliseconds);
+                if (dueTime > 0)
+                {
+                    Thread.Sleep(TimeSpan.FromMilliseconds(dueTime));
+                }
+
+                Subject.OnNext(image.Clone());
+            }
+        }
+
+        protected override void Start()
+        {
+            running = true;
+            captureThread = new Thread(CaptureNewFrame);
+            captureThread.Start();
+        }
+
+        protected override void Stop()
+        {
+            running = false;
+            if (captureThread != Thread.CurrentThread) captureThread.Join();
+        }
+
+        public override IDisposable Load()
+        {
+            capture = CvCapture.CreateFileCapture(FileName);
+            captureFps = capture.GetProperty(CaptureProperty.FPS);
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            capture.Close();
+            captureThread = null;
+            image = null;
+            base.Unload();
         }
     }
 }
