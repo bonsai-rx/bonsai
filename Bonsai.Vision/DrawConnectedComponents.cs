6652425174649ccbaf70e8eb32b6dc0605e432d4
glopes <glopes@localhost>
2012-06-01 19:17:32 +0100

    Added AbsoluteDifference, LoadImage and draw methods for contours. Added automatic creation of path folders in SaveImage.

---
+++ Bonsai.Vision/DrawConnectedComponents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class DrawConnectedComponents : Projection<ConnectedComponentCollection, IplImage>
+    {
+        public override IplImage Process(ConnectedComponentCollection input)
+        {
+            var output = new IplImage(input.ImageSize, 8, 1);
+            output.SetZero();
+
+            foreach (var component in input)
+            {
+                Core.cvDrawContours(output, component.Contour, CvScalar.All(255), CvScalar.All(0), 0, -1, 8, CvPoint.Zero);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Bonsai.Vision/LoadImage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Reactive.Concurrency;
+
+namespace Bonsai.Vision
+{
+    public class LoadImage : Source<IplImage>
+    {
+        IplImage image;
+        IDisposable action;
+
+        [FileNameFilter("PNG Files|*.png|BMP Files|*.bmp|JPEG Files|*.jpg;*.jpeg")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string FileName { get; set; }
+
+        public LoadImageMode Mode { get; set; }
+
+        protected override void Start()
+        {
+            action = HighResolutionScheduler.ThreadPool.Schedule(() =>
+            {
+                Subject.OnNext(image);
+                Subject.OnCompleted();
+            });
+        }
+
+        protected override void Stop()
+        {
+            action.Dispose();
+        }
+
+        public override IDisposable Load()
+        {
+            image = HighGui.cvLoadImage(FileName, Mode);
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            image.Dispose();
+            image = null;
+            base.Unload();
+        }
+    }
+}
