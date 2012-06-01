4fb2220f6637d26ba82a485fa7ed70534074addd
glopes <glopesdev@users.noreply.github.com>
2012-06-01 18:26:49 +0100

    Added FolderNameEditor to Bonsai.Design.

---
+++ Bonsai.Design/FolderNameEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing.Design;
+using System.ComponentModel;
+using System.Windows.Forms.Design;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bonsai.Design
+{
+    public class FolderNameEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+            if (context != null && editorService != null)
+            {
+                using (var dialog = new FolderBrowserDialog())
+                {
+                    var folderName = value as string;
+                    if (!string.IsNullOrEmpty(folderName))
+                    {
+                        dialog.SelectedPath = Path.GetFullPath(folderName);
+                    }
+
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        return dialog.SelectedPath;
+                    }
+                }
+            }
+
+            return base.EditValue(context, provider, value);
+        }
+    }
+}
