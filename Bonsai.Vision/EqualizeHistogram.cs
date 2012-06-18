a1236ecde08d0237f52c0c4d99876fb38dbda694
glopes <glopes@localhost>
2012-06-18 11:48:46 +0100

    Added EqualizeHistogram and IncrementalMean modules to Vision package.

---
+++ Bonsai.Vision/EqualizeHistogram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class EqualizeHistogram : Projection<IplImage, IplImage>
+    {
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            ImgProc.cvEqualizeHist(input, output);
+            return output;
+        }
+    }
+}
