2d78b72cd6f41307d8559b2454b33eeac7f0d9b7
glopes <glopesdev@users.noreply.github.com>
2012-06-26 15:29:35 +0100

    Added ColorBalance and generalized MorphologicalOperation modules.

---
+++ Bonsai.Vision/ColorBalance.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+
+namespace Bonsai.Vision
+{
+    public class ColorBalance : Projection<IplImage, IplImage>
+    {
+        IplImage channel1;
+        IplImage channel2;
+        IplImage channel3;
+        IplImage channel4;
+
+        public ColorBalance()
+        {
+            Scale = CvScalar.All(1);
+        }
+
+        [Precision(2, .01)]
+        [Range(0, int.MaxValue)]
+        [TypeConverter("Bonsai.Vision.Design.BgraScalarConverter, Bonsai.Vision.Design")]
+        public CvScalar Scale { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            channel1 = IplImageHelper.EnsureImageFormat(channel1, input.Size, 8, 1);
+            if (input.NumChannels > 1) channel2 = IplImageHelper.EnsureImageFormat(channel2, input.Size, 8, 1);
+            if (input.NumChannels > 2) channel3 = IplImageHelper.EnsureImageFormat(channel3, input.Size, 8, 1);
+            if (input.NumChannels > 3) channel4 = IplImageHelper.EnsureImageFormat(channel4, input.Size, 8, 1);
+
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            Core.cvSplit(input, channel1, channel2 ?? CvArr.Null, channel3 ?? CvArr.Null, channel4 ?? CvArr.Null);
+
+            if (channel1 != null) Core.cvConvertScale(channel1, channel1, Scale.Val0, 0);
+            if (channel2 != null) Core.cvConvertScale(channel2, channel2, Scale.Val1, 0);
+            if (channel3 != null) Core.cvConvertScale(channel3, channel3, Scale.Val2, 0);
+            if (channel4 != null) Core.cvConvertScale(channel4, channel4, Scale.Val3, 0);
+            Core.cvMerge(channel1, channel2 ?? CvArr.Null, channel3 ?? CvArr.Null, channel4 ?? CvArr.Null, output);
+            return output;
+        }
+
+        protected override void Unload()
+        {
+            if (channel1 != null)
+            {
+                channel1.Close();
+                channel1 = null;
+            }
+
+            if (channel2 != null)
+            {
+                channel2.Close();
+                channel2 = null;
+            }
+
+            if (channel3 != null)
+            {
+                channel3.Close();
+                channel3 = null;
+            }
+
+            if (channel4 != null)
+            {
+                channel4.Close();
+                channel4 = null;
+            }
+
+            base.Unload();
+        }
+    }
+}
---
+++ Bonsai.Vision/MorphologicalOperation.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class MorphologicalOperation : Projection<IplImage, IplImage>
+    {
+        CvSize size;
+        CvPoint anchor;
+        StructuringElementShape shape;
+        IplConvKernel strel;
+        bool propertyChanged;
+
+        public MorphologicalOperation()
+        {
+            Size = new CvSize(3, 3);
+            Iterations = 1;
+        }
+
+        public CvSize Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                propertyChanged = true;
+            }
+        }
+
+        public CvPoint Anchor
+        {
+            get { return anchor; }
+            set
+            {
+                anchor = value;
+                propertyChanged = true;
+            }
+        }
+
+        public StructuringElementShape Shape
+        {
+            get { return shape; }
+            set
+            {
+                shape = value;
+                propertyChanged = true;
+            }
+        }
+
+        public int Iterations { get; set; }
+
+        public MorphologicalOperator Operator { get; set; }
+
+        protected IplConvKernel StructuringElement
+        {
+            get
+            {
+                if (strel == null || propertyChanged)
+                {
+                    propertyChanged = false;
+                    if (strel != null) strel.Close();
+                    strel = new IplConvKernel(Size.Width, Size.Height, Anchor.X, Anchor.Y, Shape);
+                }
+
+                return strel;
+            }
+        }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            switch (Operator)
+            {
+                case MorphologicalOperator.Erode:
+                    ImgProc.cvErode(input, output, StructuringElement, Iterations);
+                    break;
+                case MorphologicalOperator.Dilate:
+                    ImgProc.cvDilate(input, output, StructuringElement, Iterations);
+                    break;
+                case MorphologicalOperator.Open:
+                    ImgProc.cvErode(input, output, StructuringElement, Iterations);
+                    ImgProc.cvDilate(output, output, StructuringElement, Iterations);
+                    break;
+                case MorphologicalOperator.Close:
+                    ImgProc.cvDilate(input, output, StructuringElement, Iterations);
+                    ImgProc.cvErode(output, output, StructuringElement, Iterations);
+                    break;
+                case MorphologicalOperator.TopHat:
+                    ImgProc.cvErode(input, output, StructuringElement, Iterations);
+                    ImgProc.cvDilate(output, output, StructuringElement, Iterations);
+                    Core.cvSub(input, output, output, CvArr.Null);
+                    break;
+                case MorphologicalOperator.BotHat:
+                    ImgProc.cvDilate(input, output, StructuringElement, Iterations);
+                    ImgProc.cvErode(output, output, StructuringElement, Iterations);
+                    Core.cvSub(output, input, output, CvArr.Null);
+                    break;
+                default:
+                    break;
+            }
+            return output;
+        }
+
+        protected override void Unload()
+        {
+            if (strel != null)
+            {
+                strel.Close();
+                strel = null;
+            }
+            base.Unload();
+        }
+    }
+
+    public enum MorphologicalOperator
+    {
+        Erode,
+        Dilate,
+        Open,
+        Close,
+        TopHat,
+        BotHat
+    }
+}
