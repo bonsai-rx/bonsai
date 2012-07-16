9fdedf526083360999d7c045e274640ce3ba601a
glopes <glopesdev@users.noreply.github.com>
2012-07-16 15:45:33 +0100

    Added Sobel and ConvertScale modules to Vision package.

---
+++ Bonsai.Vision/Sobel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision
+{
+    public class Sobel : Projection<IplImage, IplImage>
+    {
+        public Sobel()
+        {
+            ApertureSize = 3;
+        }
+
+        [Range(0, 6)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public int XOrder { get; set; }
+
+        [Range(0, 6)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public int YOrder { get; set; }
+
+        [Range(1, 7)]
+        [Precision(0, 2)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public int ApertureSize { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, 32, input.NumChannels);
+            ImgProc.cvSobel(input, output, XOrder, YOrder, ApertureSize);
+            return output;
+        }
+    }
+}
