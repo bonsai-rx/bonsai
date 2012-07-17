798053b6464fb344bc2a57ff81de827a9353ef0b
glopes <glopes@localhost>
2012-07-17 17:05:04 +0100

    Updated Bonsai.Scripting to use function definitions instead of inline scripts. Added PythonProjection to allow for scripted data processing.

--- Bonsai.Scripting/PythonTextWriter.cs
+++ Bonsai.Scripting/PythonTextWriter.cs
@@ -2,39 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Scripting.Hosting;
 using System.ComponentModel;
-using System.Threading.Tasks;
-using System.IO;
+using Microsoft.Scripting.Hosting;
 using System.Drawing.Design;
+using Bonsai.Expressions;
+using System.Reactive.Linq;
+using System.Linq.Expressions;
+using System.IO;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace Bonsai.Scripting
 {
     public class PythonTextWriter : PythonSink
     {
-        StreamWriter writer;
-
         [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design",
                 "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string FileName { get; set; }
 
         public bool Append { get; set; }
 
-        protected override ScriptEngine CreateEngine()
+        protected override IObservable<TSource> Combine<TSource>(IObservable<TSource> source, Action<TSource> action, ScriptScope scope)
         {
-            var engine = base.CreateEngine();
-            if (!string.IsNullOrEmpty(FileName))
-            {
-                writer = new StreamWriter(FileName, Append, Encoding.ASCII);
-                engine.Runtime.IO.SetOutput(writer.BaseStream, writer);
-            }
-            return engine;
-        }
-
-        protected override void Unload()
-        {
-            base.Unload();
-            writer.Close();
+            Task writerTask = null;
+            return Observable.Using(
+                () =>
+                {
+                    writerTask = new Task(() => { });
+                    writerTask.Start();
+                    var disposable = new CompositeDisposable();
+                    disposable.Add(Disposable.Create(writerTask.Wait));
+                    if (!string.IsNullOrEmpty(FileName))
+                    {
+                        var writer = new StreamWriter(FileName, Append, Encoding.ASCII);
+                        scope.Engine.Runtime.IO.SetOutput(writer.BaseStream, writer);
+                        disposable.Add(writer);
+                    }
+                    return disposable;
+                },
+                resource => source.Do(xs =>
+                {
+                    writerTask = writerTask.ContinueWith(task => action(xs));
+                }));
         }
     }
 }
