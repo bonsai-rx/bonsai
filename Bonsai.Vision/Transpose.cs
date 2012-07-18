50a486953ed503a64498a5b2a2de2b43d0e4833a
glopes <glopesdev@users.noreply.github.com>
2012-07-18 13:56:03 +0100

    Added Transpose module to Vision package. Fixed Crop behavior.

---
+++ Bonsai.Vision/Transpose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Transpose : Projection<IplImage, IplImage>
+    {
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(new CvSize(input.Height, input.Width), input.Depth, input.NumChannels);
+            Core.cvTranspose(input, output);
+            return output;
+        }
+    }
+}
