200a93fb96043033c04f61a427b58dbec6884c5a
glopes <glopesdev@users.noreply.github.com>
2012-08-05 20:42:45 +0100

    Added ToggleSwitch combinator to dynamically enable or disable branches.

---
+++ Bonsai.IO/ToggleSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Expressions;
+using System.Reactive.Linq;
+
+namespace Bonsai.IO
+{
+    public class ToggleSwitch : CombinatorBuilder
+    {
+        public bool Enabled { get; set; }
+
+        protected override IObservable<TSource> Combine<TSource>(IObservable<TSource> source)
+        {
+            return source.Where(xs => Enabled);
+        }
+    }
+}
