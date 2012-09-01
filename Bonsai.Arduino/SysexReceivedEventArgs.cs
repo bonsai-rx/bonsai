714695598526c01a5f745ca755e3d780387d515b
glopes <glopes@localhost>
2012-09-01 13:18:02 +0100

    Generalized Arduino class to support Firmata Sysex extended protocols. Removed preliminary support for stepper motor output.

---
+++ Bonsai.Arduino/SysexReceivedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Arduino
+{
+    public class SysexReceivedEventArgs : EventArgs
+    {
+        public SysexReceivedEventArgs(byte command, byte[] args)
+        {
+            Command = command;
+            Args = args;
+        }
+
+        public byte Command { get; private set; }
+
+        public byte[] Args { get; private set; }
+    }
+}
