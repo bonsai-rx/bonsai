55ec3babb3687c4d5c18ae2d3306e84ede49c79f
glopes <glopesdev@users.noreply.github.com>
2012-09-11 14:16:55 +0100

    Added BaudRateConverter to provide standard baud rate values for serial ports.

---
+++ Bonsai.IO/BaudRateConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.IO.Ports;
+
+namespace Bonsai.IO
+{
+    public class BaudRateConverter : Int32Converter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return false;
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                9600,
+                14400,
+                19200,
+                38400,
+                57600,
+                115200,
+                128000
+            });
+        }
+    }
+}
