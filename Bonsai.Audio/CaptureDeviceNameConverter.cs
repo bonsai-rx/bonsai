0bca49fe80c20221bf3753ff8a9af84bd8664593
glopes <goncaloclopes@gmail.com>
2012-10-07 23:32:48 +0100

    Added Bonsai.Audio and Bonsai.Dsp.Design to core Bonsai repository. They include basic support for audio capture, playback and signal visualization.

---
+++ Bonsai.Audio/CaptureDeviceNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Bonsai.Audio
+{
+    public class CaptureDeviceNameConverter : TypeConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(OpenTK.Audio.AudioCapture.AvailableDevices.ToArray());
+        }
+    }
+}
---
+++ Bonsai.Dsp.Design/CvMatControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Bonsai.Design
+{
+    public partial class CvMatControl : UserControl
+    {
+        public CvMatControl()
+        {
+            InitializeComponent();
+            Dock = DockStyle.Fill;
+        }
+
+        public Chart Chart
+        {
+            get { return chart; }
+        }
+
+        public SeriesCollection TimeSeries
+        {
+            get { return chart.Series; }
+        }
+
+        public ChartAreaCollection ChartAreas
+        {
+            get { return chart.ChartAreas; }
+        }
+    }
+}
