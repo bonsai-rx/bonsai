2f341650d8d20c3e3ce999751640c5b9ba2f02fb
glopes <glopesdev@users.noreply.github.com>
2012-10-17 00:41:45 +0100

    Added editor command help screen to Welcome dialog.

--- Bonsai.Editor/WelcomeDialog.cs
+++ Bonsai.Editor/WelcomeDialog.cs
@@ -12,15 +12,39 @@ namespace Bonsai.Editor
 {
     public partial class WelcomeDialog : Form
     {
+        Image[] screens;
+        int currentScreen;
+
         public WelcomeDialog()
         {
             InitializeComponent();
             showWelcomeDialogCheckBox.Checked = Settings.Default.ShowWelcomeDialog;
+            screens = new[] { Resources.BonsaiWelcome, Resources.BonsaiCommands };
+            UpdateStatus();
         }
 
         public bool ShowWelcomeDialog
         {
             get { return showWelcomeDialogCheckBox.Checked; }
+        }
+
+        void UpdateStatus()
+        {
+            previousButton.Enabled = currentScreen > 0;
+            nextButton.Enabled = currentScreen < screens.Length - 1;
+            pictureBox.Image = screens[currentScreen];
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            currentScreen--;
+            UpdateStatus();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            currentScreen++;
+            UpdateStatus();
         }
     }
 }
