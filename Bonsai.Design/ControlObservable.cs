cd4537d40471592c94e9f1683ea456eea38767e7
glopes <glopesdev@users.noreply.github.com>
2012-11-26 20:04:35 +0000

    Removed all references to System.Reactive.Windows.Forms to prevent memory leaks. Added replacement implementation of ControlScheduler.

---
+++ Bonsai.Design/ControlObservable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reactive.Linq;
+
+namespace Bonsai.Design
+{
+    public static class ControlObservable
+    {
+        public static IObservable<TSource> ObserveOn<TSource>(this IObservable<TSource> source, Control control)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (control == null)
+            {
+                throw new ArgumentNullException("control");
+            }
+
+            return source.ObserveOn(new ControlScheduler(control));
+        }
+    }
+}
