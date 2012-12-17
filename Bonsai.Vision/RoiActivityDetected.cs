5beb074b51170c0e1987ac2af9182bcc039eb033
IntelligentSystem <IntelligentSystem@Behavior01.champalimaud.pt>
2012-12-17 15:08:36 +0000

    Added Threshold property to RoiActivityDetected condition.

--- Bonsai.Vision/RoiActivityDetected.cs
+++ Bonsai.Vision/RoiActivityDetected.cs
@@ -9,11 +9,13 @@ namespace Bonsai.Vision
     {
         public int? Index { get; set; }
 
+        public double Threshold { get; set; }
+
         public override bool Process(RegionActivityCollection input)
         {
             if (Index.HasValue && Index >= 0 && Index < input.Count)
             {
-                return input[Index.Value].Activity.Val0 > 0;
+                return input[Index.Value].Activity.Val0 > Threshold;
             }
             else return input.Where(region => region.Activity.Val0 > 0).Count() > 0;
         }
