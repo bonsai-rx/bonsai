7561486799c0f672f397274c8713bc07556762d9
glopes <glopesdev@users.noreply.github.com>
2012-12-19 19:53:16 +0000

    Generalized CsvWriter for correct text output of non-recursive data structures. Added CsvWriter support for accurate DateTime and DateTimeOffset output format.

--- Bonsai.IO/CsvWriter.cs
+++ Bonsai.IO/CsvWriter.cs
@@ -7,9 +7,12 @@ using System.IO;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.ComponentModel;
+using System.Xml.Serialization;
+using System.Globalization;
 
 namespace Bonsai.IO
 {
+    [Description("Sinks individual elements of the input sequence to a text file.")]
     public class CsvWriter : DynamicSink
     {
         static readonly MethodInfo writeLineMethod = typeof(StreamWriter).GetMethods().First(m => m.Name == "WriteLine" && m.GetParameters().Length == 0);
@@ -17,9 +20,16 @@ namespace Bonsai.IO
                                                                                               m.GetParameters().Length == 2 &&
                                                                                               m.GetParameters()[1].ParameterType == typeof(object));
 
+        [Description("The name of the output file.")]
         [FileNameFilter("CSV (Comma delimited)|*.csv|All Files|*.*")]
         [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string FileName { get; set; }
+
+        [Description("Indicates whether to append or overwrite the specified file.")]
+        public bool Append { get; set; }
+
+        [Description("The optional suffix used to create file names.")]
+        public PathSuffix Suffix { get; set; }
 
         protected override Sink<T> CreateProcessor<T>()
         {
@@ -33,9 +43,9 @@ namespace Bonsai.IO
             Action<T> writeAction;
             CsvWriter parent;
 
-            public CsvProcessor(CsvWriter parent)
+            public CsvProcessor(CsvWriter owner)
             {
-                this.parent = parent;
+                this.parent = owner;
             }
 
             public override void Process(T input)
@@ -43,35 +53,81 @@ namespace Bonsai.IO
                 writerTask = writerTask.ContinueWith(task => writeAction(input));
             }
 
-            IEnumerable<MemberInfo> GetDataMembers(Type type)
+            class ExpressionNode
+            {
+                public Expression Expression { get; set; }
+                public ExpressionNode Parent { get; set; }
+
+                public IEnumerable<Expression> GetPath()
+                {
+                    var current = this;
+                    while (current != null)
+                    {
+                        yield return current.Expression;
+                        current = current.Parent;
+                    }
+                }
+            }
+
+            static IEnumerable<Expression> MakeMemberAccess(Expression expression)
+            {
+                var stack = new Stack<ExpressionNode>();
+                stack.Push(new ExpressionNode { Expression = expression });
+                while (stack.Count > 0)
+                {
+                    var current = stack.Pop();
+                    expression = current.Expression;
+                    if (expression.Type == typeof(string)) yield return expression;
+                    else if (expression.Type.IsPrimitive || expression.Type.IsEnum || expression.Type == typeof(string) ||
+                             expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset) ||
+                             expression.Type == typeof(TimeSpan))
+                    {
+                        yield return expression;
+                    }
+                    else
+                    {
+                        var successors = from member in GetDataMembers(expression.Type)
+                                         let memberAccess = (Expression)Expression.MakeMemberAccess(expression, member)
+                                         select memberAccess;
+                        foreach (var successor in successors)
+                        {
+                            if (current.GetPath().Any(node => node.Type == successor.Type)) continue;
+                            stack.Push(new ExpressionNode { Expression = successor, Parent = current });
+                        }
+                    }
+                }
+            }
+
+            static IEnumerable<MemberInfo> GetDataMembers(Type type)
             {
                 return Enumerable.Concat<MemberInfo>(
                     type.GetFields(BindingFlags.Instance | BindingFlags.Public),
-                    type.GetProperties(BindingFlags.Instance | BindingFlags.Public));
+                    type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                    .Where(member => member.GetCustomAttributes(typeof(XmlIgnoreAttribute), true).Length == 0);
             }
 
-            IEnumerable<Expression> MakeMemberAccess(Expression expression)
+            static Expression GetDataString(Expression expression)
             {
-                if (expression.Type.IsPrimitive || expression.Type.IsEnum || expression.Type == typeof(string))
+                if (expression.Type == typeof(string)) return expression;
+                else if (expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset))
                 {
-                    return Enumerable.Repeat(expression, 1);
+                    return Expression.Call(expression, "ToString", null, Expression.Constant("o"));
                 }
-
-                return from member in GetDataMembers(expression.Type)
-                       let memberAccess = Expression.MakeMemberAccess(expression, member)
-                       where memberAccess.Type.IsValueType
-                       from memberExpression in MakeMemberAccess(memberAccess)
-                       select memberExpression;
+                else
+                {
+                    return Expression.Call(expression, "ToString", null, Expression.Constant(CultureInfo.InvariantCulture));
+                }
             }
 
             public override IDisposable Load()
             {
-                const string ParameterName = "Value";
+                const string ParameterName = "input";
                 const string EntryFormat = "{0} ";
 
                 if (!string.IsNullOrEmpty(parent.FileName))
                 {
-                    writer = new StreamWriter(parent.FileName, false, Encoding.ASCII);
+                    var fileName = PathHelper.AppendSuffix(parent.FileName, parent.Suffix);
+                    writer = new StreamWriter(fileName, parent.Append, Encoding.ASCII);
 
                     var parameterType = typeof(T);
                     var parameter = Expression.Parameter(parameterType, ParameterName);
@@ -80,8 +136,10 @@ namespace Bonsai.IO
                     var writerConstant = Expression.Constant(writer);
 
                     var memberAccessExpressions = MakeMemberAccess(parameter).ToArray();
+                    Array.Reverse(memberAccessExpressions);
+
                     var writeParameters = from memberAccess in memberAccessExpressions
-                                          let memberString = Expression.Call(memberAccess, "ToString", null)
+                                          let memberString = GetDataString(memberAccess)
                                           select Expression.Call(writerConstant, writeMethod, formatConstant, memberString);
                     var writeLineExpression = Expression.Call(writerConstant, writeLineMethod);
                     var bodyExpressions = writeParameters.Concat(Enumerable.Repeat(writeLineExpression, 1)).ToArray();
@@ -106,8 +164,10 @@ namespace Bonsai.IO
 
             protected override void Unload()
             {
-                writerTask.Wait();
-                writer.Close();
+                var closingWriter = writer;
+                writerTask.ContinueWith(task => closingWriter.Close());
+                writerTask = null;
+                writer = null;
                 base.Unload();
             }
         }
