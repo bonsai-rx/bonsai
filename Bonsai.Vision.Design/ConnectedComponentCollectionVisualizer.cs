0d8d757068b891940ec733ad8d9e71a29ab9b8c8
glopes <glopesdev@users.noreply.github.com>
2012-12-19 19:51:02 +0000

    Updated LargestBinaryRegion to return a single connected component. Added ConnectedComponentVisualizer and abstracted out component drawing methods.

--- Bonsai.Vision.Design/ConnectedComponentCollectionVisualizer.cs
+++ Bonsai.Vision.Design/ConnectedComponentCollectionVisualizer.cs
@@ -22,21 +22,7 @@ namespace Bonsai.Vision.Design
 
             foreach (var component in connectedComponents)
             {
-                var centroid = component.Centroid;
-                var orientation = component.Orientation;
-                var minorAxisOrientation = orientation + Math.PI / 2.0;
-                var halfMajorAxis = component.MajorAxisLength * 0.5;
-                var halfMinorAxis = component.MinorAxisLength * 0.5;
-                var major1 = new CvPoint((int)(centroid.X + halfMajorAxis * Math.Cos(orientation)), (int)(centroid.Y + halfMajorAxis * Math.Sin(orientation)));
-                var major2 = new CvPoint((int)(centroid.X - halfMajorAxis * Math.Cos(orientation)), (int)(centroid.Y - halfMajorAxis * Math.Sin(orientation)));
-                var minor1 = new CvPoint((int)(centroid.X + halfMinorAxis * Math.Cos(minorAxisOrientation)), (int)(centroid.Y + halfMinorAxis * Math.Sin(minorAxisOrientation)));
-                var minor2 = new CvPoint((int)(centroid.X - halfMinorAxis * Math.Cos(minorAxisOrientation)), (int)(centroid.Y - halfMinorAxis * Math.Sin(minorAxisOrientation)));
-
-                Core.cvDrawContours(output, component.Contour, CvScalar.All(255), CvScalar.All(0), 0, -1, 8, CvPoint.Zero);
-                Core.cvDrawContours(output, component.Contour, CvScalar.Rgb(255, 0, 0), CvScalar.Rgb(0, 0, 255), 0, 1, 8, CvPoint.Zero);
-                Core.cvLine(output, major1, major2, CvScalar.Rgb(0, 0, 255), 1, 8, 0);
-                Core.cvLine(output, minor1, minor2, CvScalar.Rgb(255, 0, 0), 1, 8, 0);
-                Core.cvCircle(output, new CvPoint(centroid), 2, CvScalar.Rgb(255, 0, 0), -1, 8, 0);
+                DrawingHelper.DrawConnectedComponent(output, component);
             }
 
             base.Show(output);
---
+++ Bonsai.Vision.Design/ConnectedComponentVisualizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using Bonsai.Design;
+using Bonsai.Vision;
+
+[assembly: TypeVisualizer(typeof(ConnectedComponentVisualizer), Target = typeof(ConnectedComponent))]
+
+namespace Bonsai.Vision.Design
+{
+    public class ConnectedComponentVisualizer : IplImageVisualizer
+    {
+        public override void Show(object value)
+        {
+            var connectedComponent = (ConnectedComponent)value;
+            var boundingBox = connectedComponent.Contour.Rect;
+            var boundingBoxCenter = new CvPoint2D32f(0.5f * boundingBox.Width, 0.5f * boundingBox.Height);
+            var output = new IplImage(new CvSize(boundingBox.Width, boundingBox.Height), 8, 3);
+            output.SetZero();
+
+            DrawingHelper.DrawConnectedComponent(output, connectedComponent, boundingBoxCenter - connectedComponent.Centroid);
+            base.Show(output);
+        }
+    }
+}
