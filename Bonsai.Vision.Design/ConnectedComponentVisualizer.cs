72075c54832a8f718b27e6ac1fcf96beb98616d1
glopes <goncaloclopes@gmail.com>
2012-12-22 02:28:39 +0000

    Fixed ConnectedComponentVisualizer in the case of invalid contour.

--- Bonsai.Vision.Design/ConnectedComponentVisualizer.cs
+++ Bonsai.Vision.Design/ConnectedComponentVisualizer.cs
@@ -17,12 +17,16 @@ namespace Bonsai.Vision.Design
         public override void Show(object value)
         {
             var connectedComponent = (ConnectedComponent)value;
-            var boundingBox = connectedComponent.Contour.Rect;
-            var boundingBoxCenter = new CvPoint2D32f(0.5f * boundingBox.Width, 0.5f * boundingBox.Height);
+            var validContour = connectedComponent.Contour != null && !connectedComponent.Contour.IsInvalid;
+            var boundingBox = validContour ? connectedComponent.Contour.Rect : new CvRect(0, 0, 1, 1);
             var output = new IplImage(new CvSize(boundingBox.Width, boundingBox.Height), 8, 3);
             output.SetZero();
 
-            DrawingHelper.DrawConnectedComponent(output, connectedComponent, boundingBoxCenter - connectedComponent.Centroid);
+            if (validContour)
+            {
+                var boundingBoxCenter = new CvPoint2D32f(0.5f * boundingBox.Width, 0.5f * boundingBox.Height);
+                DrawingHelper.DrawConnectedComponent(output, connectedComponent, boundingBoxCenter - connectedComponent.Centroid);
+            }
             base.Show(output);
         }
     }
