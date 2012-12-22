42be7c3d4b9b17c80dca2fc93ca3489a8a222fd7
glopes <goncaloclopes@gmail.com>
2012-12-22 02:29:33 +0000

    Added SortBinaryRegions module to sort detected components by area.

---
+++ Bonsai.Vision/SortBinaryRegions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Vision
+{
+    public class SortBinaryRegions : Transform<ConnectedComponentCollection, ConnectedComponentCollection>
+    {
+        public override ConnectedComponentCollection Process(ConnectedComponentCollection input)
+        {
+            var components = new List<ConnectedComponent>(input);
+            components.Sort((xs, ys) => xs.Area.CompareTo(ys.Area));
+            return new ConnectedComponentCollection(components, input.ImageSize);
+        }
+    }
+}
