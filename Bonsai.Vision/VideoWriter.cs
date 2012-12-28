8b31c2701abc532e13086619de3428505fbbf92a
glopes <goncaloclopes@gmail.com>
2012-12-28 17:24:51 +0000

    Added Bonsai.IO.FileSink to refactor file stream writer asynchronous pattern. Refactored VideoWriter to use the new FileSink.

--- Bonsai.Vision/VideoWriter.cs
+++ Bonsai.Vision/VideoWriter.cs
@@ -6,18 +6,14 @@ using OpenCV.Net;
 using System.Threading.Tasks;
 using Bonsai.IO;
 using System.ComponentModel;
+using System.Reactive.Disposables;
 
 namespace Bonsai.Vision
 {
-    public class VideoWriter : Sink<IplImage>
+    public class VideoWriter : FileSink<IplImage, VideoWriterDisposable>
     {
-        Task writerTask;
-        CvVideoWriter writer;
         CvSize writerFrameSize;
-        static readonly object syncRoot = new object();
-
-        [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
-        public string FileName { get; set; }
+        static readonly object SyncRoot = new object();
 
         public string FourCC { get; set; }
 
@@ -25,70 +21,37 @@ namespace Bonsai.Vision
 
         public CvSize FrameSize { get; set; }
 
-        public PathSuffix Suffix { get; set; }
-
         public SubPixelInterpolation ResizeInterpolation { get; set; }
 
-        public override void Process(IplImage input)
+        protected override VideoWriterDisposable CreateWriter(string fileName, IplImage input)
         {
-            if (writerTask == null) return;
-
-            var runningWriter = writer;
-            writerTask = writerTask.ContinueWith(task =>
+            var frameSize = FrameSize.Width > 0 && FrameSize.Height > 0 ? FrameSize : input.Size;
+            var fourCCText = FourCC;
+            var fourCC = fourCCText.Length != 4 ? 0 : CvVideoWriter.FourCC(fourCCText[0], fourCCText[1], fourCCText[2], fourCCText[3]);
+            writerFrameSize = frameSize;
+            lock (SyncRoot)
             {
-                if (runningWriter == null)
+                var writer = new CvVideoWriter(FileName, fourCC, FrameRate, frameSize, input.NumChannels > 1);
+                return new VideoWriterDisposable(writer, Disposable.Create(() =>
                 {
-                    PathHelper.EnsureDirectory(FileName);
-                    var fileName = PathHelper.AppendSuffix(FileName, Suffix);
-                    var frameSize = FrameSize.Width > 0 && FrameSize.Height > 0 ? FrameSize : input.Size;
-                    var fourCCText = FourCC;
-                    var fourCC = fourCCText.Length != 4 ? 0 : CvVideoWriter.FourCC(fourCCText[0], fourCCText[1], fourCCText[2], fourCCText[3]);
-                    writerFrameSize = frameSize;
-                    lock (syncRoot)
+                    lock (SyncRoot)
                     {
-                        runningWriter = writer = new CvVideoWriter(FileName, fourCC, FrameRate, frameSize, input.NumChannels > 1);
+                        writer.Close();
                     }
-                }
-
-                if (input.Width != writerFrameSize.Width || input.Height != writerFrameSize.Height)
-                {
-                    var resized = new IplImage(new CvSize(writerFrameSize.Width, writerFrameSize.Height), input.Depth, input.NumChannels);
-                    ImgProc.cvResize(input, resized, ResizeInterpolation);
-                    input = resized;
-                }
-
-                runningWriter.WriteFrame(input);
-            });
+                }));
+            }
         }
 
-        public override IDisposable Load()
+        protected override void Write(VideoWriterDisposable writer, IplImage input)
         {
-            if (!string.IsNullOrEmpty(FileName))
+            if (input.Width != writerFrameSize.Width || input.Height != writerFrameSize.Height)
             {
-                writerTask = new Task(() => { });
-                writerTask.Start();
+                var resized = new IplImage(new CvSize(writerFrameSize.Width, writerFrameSize.Height), input.Depth, input.NumChannels);
+                ImgProc.cvResize(input, resized, ResizeInterpolation);
+                input = resized;
             }
 
-            return base.Load();
-        }
-
-        protected override void Unload()
-        {
-            var closingWriter = writer;
-            writerTask.ContinueWith(task =>
-            {
-                if (closingWriter != null)
-                {
-                    lock (syncRoot)
-                    {
-                        closingWriter.Close();
-                    }
-                }
-            });
-
-            writerTask = null;
-            writer = null;
-            base.Unload();
+            writer.Writer.WriteFrame(input);
         }
     }
 }
