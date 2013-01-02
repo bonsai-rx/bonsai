dfa07a6b3c3a0b3b65fdae99a2bdcebdc9e61ee5
glopes <glopes@localhost>
2013-01-02 11:46:29 +0000

    Added Add and MergeBinaryRegions modules.

---
+++ Bonsai.Vision/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Add : Transform<IplImage, IplImage, IplImage>
+    {
+        public override IplImage Process(IplImage first, IplImage second)
+        {
+            var output = new IplImage(first.Size, first.Depth, first.NumChannels);
+            Core.cvAdd(first, second, output, CvArr.Null);
+            return output;
+        }
+    }
+}
---
+++ Bonsai.Vision/MergeBinaryRegions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Vision
+{
+    public class MergeBinaryRegions : Transform<ConnectedComponentCollection, ConnectedComponentCollection, ConnectedComponentCollection>
+    {
+        public override ConnectedComponentCollection Process(ConnectedComponentCollection first, ConnectedComponentCollection second)
+        {
+            var output = new ConnectedComponentCollection(first.Concat(second).ToList(), first.ImageSize);
+            return output;
+        }
+    }
+}
