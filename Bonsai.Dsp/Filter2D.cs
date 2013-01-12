9da6cbad541ca8f3f81a2776271541458320b800
glopes <goncaloclopes@gmail.com>
2013-01-12 17:59:53 +0000

    Added Filter2D module for both Dsp and Vision packages.

---
+++ Bonsai.Dsp/Filter2D.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace Bonsai.Dsp
+{
+    public abstract class Filter2D<TArray> : Transform<TArray, TArray> where TArray : CvArr
+    {
+        CvMat kernel;
+        float[,] currentKernel;
+
+        public Filter2D()
+        {
+            Anchor = new CvPoint(-1, -1);
+        }
+
+        public CvPoint Anchor { get; set; }
+
+        [XmlIgnore]
+        [TypeConverter(typeof(MultidimensionalArrayConverter))]
+        public float[,] Kernel { get; set; }
+
+        [Browsable(false)]
+        [XmlElement("Kernel")]
+        public string KernelXml
+        {
+            get { return ArrayConvert.ToString(Kernel); }
+            set { Kernel = ArrayConvert.ToArray<float>(value); }
+        }
+
+        protected abstract TArray CreateOutput(TArray input);
+
+        public override TArray Process(TArray input)
+        {
+            if (Kernel != currentKernel)
+            {
+                currentKernel = Kernel;
+                UnloadKernel();
+                if (currentKernel != null && currentKernel.Length > 0)
+                {
+                    var rows = currentKernel.GetLength(0);
+                    var columns = currentKernel.GetLength(1);
+                    var kernelHandle = GCHandle.Alloc(currentKernel, GCHandleType.Pinned);
+                    try
+                    {
+                        using (var kernelHeader = new CvMat(rows, columns, CvMatDepth.CV_32F, 1, kernelHandle.AddrOfPinnedObject()))
+                        {
+                            kernel = kernelHeader.Clone();
+                        }
+
+                    }
+                    finally { kernelHandle.Free(); }
+                }
+            }
+
+            if (kernel == null) return input;
+            else
+            {
+                var output = CreateOutput(input);
+                ImgProc.cvFilter2D(input, output, kernel, Anchor);
+                return output;
+            }
+        }
+
+        void UnloadKernel()
+        {
+            if (kernel != null)
+            {
+                kernel.Dispose();
+                kernel = null;
+            }
+        }
+
+        protected override void Unload()
+        {
+            UnloadKernel();
+            base.Unload();
+        }
+    }
+
+    public class Filter2D : Filter2D<CvMat>
+    {
+        protected override CvMat CreateOutput(CvMat input)
+        {
+            return new CvMat(input.Rows, input.Cols, input.Depth, input.NumChannels);
+        }
+    }
+}
---
+++ Bonsai.Vision/Filter2D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using Bonsai.Dsp;
+
+namespace Bonsai.Vision
+{
+    public class Filter2D : Filter2D<IplImage>
+    {
+        protected override IplImage CreateOutput(IplImage input)
+        {
+            return new IplImage(input.Size, input.Depth, input.NumChannels);
+        }
+    }
+}
