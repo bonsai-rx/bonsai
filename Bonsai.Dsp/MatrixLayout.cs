4c3ba5e7215e67b16570b2f99ea206bbb8c34e42
glopes <goncaloclopes@gmail.com>
2013-01-12 19:00:00 +0000

    Added options for reading and writing row-major and column-major matrix data.

---
+++ Bonsai.Dsp/MatrixLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Dsp
+{
+    public enum MatrixLayout
+    {
+        RowMajor,
+        ColumnMajor
+    }
+}
--- Bonsai.Dsp/MatrixWriter.cs
+++ Bonsai.Dsp/MatrixWriter.cs
@@ -13,6 +13,8 @@ namespace Bonsai.Dsp
 {
     public class MatrixWriter : FileSink<CvMat, BinaryWriter>
     {
+        public MatrixLayout Layout { get; set; }
+
         protected override BinaryWriter CreateWriter(string fileName, CvMat input)
         {
             var stream = new FileStream(fileName, FileMode.Create);
@@ -23,7 +25,24 @@ namespace Bonsai.Dsp
         protected override void Write(BinaryWriter writer, CvMat input)
         {
             var data = new byte[input.Step * input.Rows];
-            Marshal.Copy(input.Data, data, 0, data.Length);
+            var dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            try
+            {
+                CvMat dataHeader;
+                switch (Layout)
+                {
+                    case MatrixLayout.ColumnMajor:
+                        dataHeader = new CvMat(input.Cols, input.Rows, input.Depth, input.NumChannels, dataHandle.AddrOfPinnedObject());
+                        Core.cvTranspose(input, dataHeader);
+                        break;
+                    default:
+                    case MatrixLayout.RowMajor:
+                        dataHeader = new CvMat(input.Rows, input.Cols, input.Depth, input.NumChannels, dataHandle.AddrOfPinnedObject());
+                        Core.cvCopy(input, dataHeader);
+                        break;
+                }
+            }
+            finally { dataHandle.Free(); }
             writer.Write(data);
         }
     }
