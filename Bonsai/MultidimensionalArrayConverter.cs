f19b76ddb19f669db26a88df58b20c428cca2011
glopes <glopesdev@users.noreply.github.com>
2013-01-12 17:57:19 +0000

    Added serialization and editing support classes for rectangular arrays.

---
+++ Bonsai/MultidimensionalArrayConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Bonsai
+{
+    public class MultidimensionalArrayConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string)) return true;
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var valueString = value as string;
+            if (valueString != null)
+            {
+                return ArrayConvert.ToArray(
+                    valueString,
+                    context.PropertyDescriptor.PropertyType.GetElementType(),
+                    culture);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (value != null && destinationType == typeof(string))
+            {
+                return ArrayConvert.ToString((Array)value, culture);
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
