f5472fedaafa474af4d6cf20b3c991fa62bcd5cc
glopes <goncaloclopes@gmail.com>
2013-01-16 05:41:20 +0000

    Added ParseInt method to Bonsai.IO for string conversion.

---
+++ Bonsai.IO/ParseInt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.IO
+{
+    public class ParseInt : Transform<string, int>
+    {
+        public override int Process(string input)
+        {
+            return int.Parse(input);
+        }
+    }
+}
