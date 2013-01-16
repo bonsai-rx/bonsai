8175673f103fbdd96f2a2f1dd8fdfdb125f0a54c
glopes <glopes@localhost>
2013-01-16 13:05:29 +0000

    Updated ParseInt module to output a buffered sample array.

--- Bonsai.IO/ParseInt.cs
+++ Bonsai.IO/ParseInt.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenCV.Net;
 
 namespace Bonsai.IO
 {
-    public class ParseInt : Transform<string, int>
+    public class ParseInt : Transform<string, CvMat>
     {
-        public override int Process(string input)
+        public override CvMat Process(string input)
         {
-            return int.Parse(input);
+            var output = new CvMat(1, 1, CvMatDepth.CV_32S, 1);
+            Core.cvSet2D(output, 1, 1, CvScalar.Real(int.Parse(input)));
+            return output;
         }
     }
 }
