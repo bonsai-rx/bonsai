36b69bee985b10a8f944323fe6dbc9fcc67d342a
IntelligentSystem <glopesdev@users.noreply.github.com>
2013-01-25 21:57:00 +0000

    Added Accumulator module to Vision package for image integration over time.

---
+++ Bonsai.Vision/Accumulator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Accumulator : Transform<IplImage, IplImage>
+    {
+        IplImage sum;
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            sum = IplImageHelper.EnsureImageFormat(sum, input.Size, 32, input.NumChannels);
+            ImgProc.cvAcc(input, sum, CvArr.Null);
+            Core.cvConvert(sum, output);
+            return output;
+        }
+
+        protected override void Unload()
+        {
+            if (sum != null)
+            {
+                sum.Dispose();
+                sum = null;
+            }
+            base.Unload();
+        }
+    }
+}
