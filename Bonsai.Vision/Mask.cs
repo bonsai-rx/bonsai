ce8f0dda01401f9e15f8f8cef7f37f7abf5453d9
glopes <glopesdev@users.noreply.github.com>
2013-02-19 14:14:26 +0000

    Refactored Mask to MaskImage and reimplemented dynamic Mask module.

--- Bonsai.Vision/Mask.cs
+++ Bonsai.Vision/Mask.cs
@@ -8,39 +8,14 @@ using System.Drawing.Design;
 
 namespace Bonsai.Vision
 {
-    public class Mask : Transform<IplImage, IplImage>
+    public class Mask : Transform<IplImage, IplImage, IplImage>
     {
-        CvArr mask;
-
-        [FileNameFilter("PNG Files|*.png|BMP Files|*.bmp|JPEG Files|*.jpg;*.jpeg")]
-        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
-        public string FileName { get; set; }
-
-        public override IplImage Process(IplImage input)
+        public override IplImage Process(IplImage first, IplImage second)
         {
-            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            var output = new IplImage(first.Size, first.Depth, first.NumChannels);
             output.SetZero();
-            Core.cvCopy(input, output, mask);
+            Core.cvCopy(first, output, second ?? CvArr.Null);
             return output;
-        }
-
-        public override IDisposable Load()
-        {
-            if (string.IsNullOrEmpty(FileName))
-            {
-                mask = CvArr.Null;
-            }
-            else mask = HighGui.cvLoadImage(FileName, LoadImageMode.Grayscale);
-            return base.Load();
-        }
-
-        protected override void Unload()
-        {
-            if (mask != null && mask != CvArr.Null)
-            {
-                mask.Close();
-            }
-            base.Unload();
         }
     }
 }
---
+++ Bonsai.Vision/MaskImage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace Bonsai.Vision
+{
+    public class MaskImage : Transform<IplImage, IplImage>
+    {
+        CvArr mask;
+
+        [FileNameFilter("PNG Files|*.png|BMP Files|*.bmp|JPEG Files|*.jpg;*.jpeg")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string FileName { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            output.SetZero();
+            Core.cvCopy(input, output, mask);
+            return output;
+        }
+
+        public override IDisposable Load()
+        {
+            if (string.IsNullOrEmpty(FileName))
+            {
+                mask = CvArr.Null;
+            }
+            else mask = HighGui.cvLoadImage(FileName, LoadImageMode.Grayscale);
+            return base.Load();
+        }
+
+        protected override void Unload()
+        {
+            if (mask != null && mask != CvArr.Null)
+            {
+                mask.Close();
+            }
+            base.Unload();
+        }
+    }
+}
