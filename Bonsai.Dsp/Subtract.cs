aff5a953ee054b6a839b97caf573a69e7c0a8acc
glopes <goncaloclopes@gmail.com>
2013-02-21 03:35:01 +0000

    Added Subtract module to Dsp package.

---
+++ Bonsai.Dsp/Subtract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Dsp
+{
+    public class Subtract : Transform<CvMat, CvMat, CvMat>
+    {
+        public override CvMat Process(CvMat first, CvMat second)
+        {
+            var output = new CvMat(first.Rows, first.Cols, first.Depth, first.NumChannels);
+            Core.cvSub(first, second, output, CvArr.Null);
+            return output;
+        }
+    }
+}
