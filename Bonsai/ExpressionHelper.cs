4740609bef717e2adb20d1a4a2e8672fb4bb391b
glopes <glopesdev@users.noreply.github.com>
2013-02-24 12:14:57 +0000

    Added preliminary support for indexed properties in MemberSelector.

--- Bonsai/ExpressionHelper.cs
+++ Bonsai/ExpressionHelper.cs
@@ -9,6 +9,9 @@ namespace Bonsai
     public static class ExpressionHelper
     {
         public const string MemberSeparator = ".";
+        public const string IndexBegin = "[";
+        public const string IndexEnd = "]";
+        public const string IndexParameterSeparator = ",";
 
         public static Expression MemberAccess(Expression expression, string memberPath)
         {
@@ -16,7 +19,26 @@ namespace Bonsai
             {
                 foreach (var memberName in memberPath.Split(new[] { MemberSeparator }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    expression = Expression.PropertyOrField(expression, memberName);
+                    var indexBegin = memberName.IndexOf(IndexBegin);
+                    if (indexBegin >= 0)
+                    {
+                        var indexEnd = memberName.IndexOf(IndexEnd);
+                        if (indexEnd < 0) throw new ArgumentException("Member path has badly formatted index accessor.", "memberPath");
+                        var propertyName = memberName.Substring(0, indexBegin);
+                        var propertyInfo = expression.Type.GetProperty(propertyName);
+                        if (propertyInfo == null) throw new ArgumentException("Member path has reference to non-existent indexed property.", "memberPath");
+                        var parameterInfo = propertyInfo.GetIndexParameters();
+                        var indexParameters = memberName
+                            .Substring(indexBegin + 1, indexEnd - indexBegin - 1)
+                            .Split(new[] { IndexParameterSeparator }, StringSplitOptions.RemoveEmptyEntries);
+                        var arguments = (from indexParameter in parameterInfo.Zip(indexParameters, (xs, ys) => Tuple.Create(xs, ys))
+                                         let parameterType = indexParameter.Item1.ParameterType
+                                         let parameter = Convert.ChangeType(indexParameter.Item2, parameterType)
+                                         select Expression.Constant(parameter))
+                                        .ToArray();
+                        expression = Expression.Property(expression, propertyName, arguments);
+                    }
+                    else expression = Expression.PropertyOrField(expression, memberName);
                 }
             }
 
