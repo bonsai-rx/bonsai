589c60823fb9e80fb20083fb7a7f698c0db6f990
glopes <glopesdev@users.noreply.github.com>
2013-03-05 04:13:00 +0000

    Added preliminary implementations of useful image visualizer mashups.

---
+++ Bonsai.Vision.Design/ConnectedComponentCollectionMashupVisualizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Design;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+
+[assembly: TypeVisualizer(typeof(ConnectedComponentCollectionMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, ConnectedComponentCollectionVisualizer>))]
+[assembly: TypeVisualizer(typeof(ConnectedComponentCollectionMashupVisualizer), Target = typeof(VisualizerMashup<ContoursVisualizer, ConnectedComponentCollectionVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class ConnectedComponentCollectionMashupVisualizer : DialogTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            var image = visualizer.VisualizerImage;
+            var connectedComponents = (ConnectedComponentCollection)value;
+            if (image != null)
+            {
+                foreach (var component in connectedComponents)
+                {
+                    DrawingHelper.DrawConnectedComponent(image, component);
+                }
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
---
+++ Bonsai.Vision.Design/ContoursMashupVisualizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Design;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+
+[assembly: TypeVisualizer(typeof(ContoursMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, ContoursVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class ContoursMashupVisualizer : DialogTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            var contours = (Contours)value;
+            var image = visualizer.VisualizerImage;
+            if (image != null && !contours.FirstContour.IsInvalid)
+            {
+                Core.cvDrawContours(image, contours.FirstContour, CvScalar.All(255), CvScalar.All(128), 1, 1, 8, CvPoint.Zero);
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
