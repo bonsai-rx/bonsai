c0cb54e81663a8a6c387bad2a293c8aa1b6533f9
glopes <goncaloclopes@gmail.com>
2013-03-10 16:58:44 +0000

    Added Skeletonization module implemented using a medial axis transform. Added DistanceTransform and Laplace modules to vision package.

---
+++ Bonsai.Vision/DistanceTransform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class DistanceTransform : Transform<IplImage, IplImage>
+    {
+        public DistanceTransform()
+        {
+            DistanceType = OpenCV.Net.DistanceType.L2;
+        }
+
+        public DistanceType DistanceType { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, 32, 1);
+            ImgProc.cvDistTransform(input, output, DistanceType, 3, null, CvArr.Null, DistanceLabel.ConnectedComponent);
+            return output;
+        }
+    }
+}
---
+++ Bonsai.Vision/Laplace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class Laplace : Transform<IplImage, IplImage>
+    {
+        public Laplace()
+        {
+            ApertureSize = 3;
+        }
+
+        public int ApertureSize { get; set; }
+
+        public override IplImage Process(IplImage input)
+        {
+            var output = new IplImage(input.Size, input.Depth, input.NumChannels);
+            ImgProc.cvLaplace(input, output, ApertureSize);
+            return output;
+        }
+    }
+}
