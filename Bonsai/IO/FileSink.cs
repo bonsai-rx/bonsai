65b7d8e17128ff35a983abf3125cea2d2b024a95
glopes <glopes@localhost>
2013-03-18 17:07:54 +0000

    Added Buffered property to FileSink to enable non-buffered write mode.

--- Bonsai/IO/FileSink.cs
+++ Bonsai/IO/FileSink.cs
@@ -12,10 +12,17 @@ namespace Bonsai.IO
         Task writerTask;
         TWriter writer;
 
+        protected FileSink()
+        {
+            Buffered = true;
+        }
+
         [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string FileName { get; set; }
 
         public PathSuffix Suffix { get; set; }
+
+        public bool Buffered { get; set; }
 
         protected abstract TWriter CreateWriter(string fileName, TSource input);
 
@@ -23,25 +30,29 @@ namespace Bonsai.IO
 
         public override void Process(TSource input)
         {
-            if (writerTask == null) return;
-
             var runningWriter = writer;
-            writerTask = writerTask.ContinueWith(task =>
+            Action writeTask = () =>
             {
                 if (runningWriter == null)
                 {
-                    PathHelper.EnsureDirectory(FileName);
-                    var fileName = PathHelper.AppendSuffix(FileName, Suffix);
+                    var fileName = FileName;
+                    if (string.IsNullOrEmpty(fileName)) return;
+
+                    PathHelper.EnsureDirectory(fileName);
+                    fileName = PathHelper.AppendSuffix(fileName, Suffix);
                     runningWriter = writer = CreateWriter(fileName, input);
                 }
 
                 Write(runningWriter, input);
-            });
+            };
+
+            if (writerTask == null) writeTask();
+            else writerTask = writerTask.ContinueWith(task => writeTask());
         }
 
         public override IDisposable Load()
         {
-            if (!string.IsNullOrEmpty(FileName))
+            if (Buffered)
             {
                 writerTask = new Task(() => { });
                 writerTask.Start();
@@ -53,14 +64,16 @@ namespace Bonsai.IO
         protected override void Unload()
         {
             var closingWriter = writer;
-            writerTask.ContinueWith(task =>
+            Action closingTask = () =>
             {
                 if (closingWriter != null)
                 {
                     closingWriter.Dispose();
                 }
-            });
+            };
 
+            if (writerTask == null) closingTask();
+            else writerTask.ContinueWith(task => closingTask());
             writerTask = null;
             writer = null;
             base.Unload();
