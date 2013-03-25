e0487f77c9f8710eaaea3d85f090e4a4eca9464e
glopes <glopesdev@users.noreply.github.com>
2013-03-25 01:07:51 +0000

    Updated VideoPlayerControl to allow jumping directly to a specific frame.

---
+++ Bonsai.Vision.Design/SeekEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Vision.Design
+{
+    public class SeekEventArgs : EventArgs
+    {
+        public SeekEventArgs(int frameNumber)
+        {
+            FrameNumber = frameNumber;
+        }
+
+        public int FrameNumber { get; private set; }
+    }
+}
