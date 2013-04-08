ace639c9898fe662e0bb8532177a29a129f3677e
glopes <glopesdev@users.noreply.github.com>
2013-04-08 11:52:29 +0100

    Updated Grayscale module to pass-through already grayscale input images.

--- Bonsai.Vision/Grayscale.cs
+++ Bonsai.Vision/Grayscale.cs
@@ -12,9 +12,16 @@ namespace Bonsai.Vision
     {
         public override IplImage Process(IplImage input)
         {
-            var output = new IplImage(input.Size, 8, 1);
-            ImgProc.cvCvtColor(input, output, ColorConversion.BGR2GRAY);
-            return output;
+            if (input.NumChannels == 1)
+            {
+                return input;
+            }
+            else
+            {
+                var output = new IplImage(input.Size, 8, 1);
+                ImgProc.cvCvtColor(input, output, ColorConversion.BGR2GRAY);
+                return output;
+            }
         }
     }
 }
