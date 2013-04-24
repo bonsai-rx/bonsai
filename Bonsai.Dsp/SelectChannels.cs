209960267bad680ee1a4cc23f539948d09218032
glopes <glopesdev@users.noreply.github.com>
2013-04-24 18:36:48 +0100

    Added SelectChannels module to Dsp package to allow sampling only specific rows.

---
+++ Bonsai.Dsp/SelectChannels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Dsp
+{
+    public class SelectChannels : Transform<CvMat, CvMat>
+    {
+        public SelectChannels()
+        {
+            Length = 1;
+        }
+
+        public int StartIndex { get; set; }
+
+        public int Length { get; set; }
+
+        public override CvMat Process(CvMat input)
+        {
+            return input.GetSubRect(new CvRect(0, StartIndex, input.Cols, Length));
+        }
+    }
+}
