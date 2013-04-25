54bfea324d16231803b83b0cd4df8cb290148e30
glopes <glopesdev@users.noreply.github.com>
2013-04-25 10:27:44 +0100

    Added CountNonZero module to Dsp and Vision packages.

---
+++ Bonsai.Dsp/CountNonZero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Dsp
+{
+    public class CountNonZero : Transform<CvMat, int>
+    {
+        public override int Process(CvMat input)
+        {
+            return Core.cvCountNonZero(input);
+        }
+    }
+}
---
+++ Bonsai.Vision/CountNonZero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCV.Net;
+
+namespace Bonsai.Vision
+{
+    public class CountNonZero : Transform<IplImage, int>
+    {
+        public override int Process(IplImage input)
+        {
+            return Core.cvCountNonZero(input);
+        }
+    }
+}
