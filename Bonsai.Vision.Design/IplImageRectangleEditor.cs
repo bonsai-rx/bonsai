743e6bbb74348edc4a2b15f1143322d726308442
glopes <glopes@localhost>
2013-05-02 11:47:56 +0100

    Fixed image quadrangle, rectangle and roi editor initialization sequence.

--- Bonsai.Vision.Design/IplImageRectangleEditor.cs
+++ Bonsai.Vision.Design/IplImageRectangleEditor.cs
@@ -79,7 +79,7 @@ namespace Bonsai.Vision.Design
                     }
 
                     IDisposable subscription = null;
-                    imageControl.HandleCreated += delegate { subscription = source.Subscribe(image => imageControl.Image = (IplImage)image); };
+                    imageControl.Load += delegate { subscription = source.Subscribe(image => imageControl.Image = (IplImage)image); };
                     imageControl.HandleDestroyed += delegate { subscription.Dispose(); };
                     editorService.ShowDialog(visualizerDialog);
 
