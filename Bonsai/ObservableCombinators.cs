28b82231cc953329e93357485da7090afe3e2f80
glopes <glopes@localhost>
2013-08-15 12:18:20 +0100

    Added ReconnectableObservable, Multicast and PublishReconnectable combinators.

--- Bonsai/ObservableCombinators.cs
+++ Bonsai/ObservableCombinators.cs
@@ -6,6 +6,7 @@ using System.Reactive.Linq;
 using System.Reactive.Concurrency;
 using System.Threading;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
 
 namespace Bonsai
 {
@@ -63,6 +64,16 @@ namespace Bonsai
         {
             return source.Window(openSampler, window => closeSampler)
                          .SelectMany(window => window.Take(1));
+        }
+
+        public static IConnectableObservable<TResult> Multicast<TSource, TResult>(this IObservable<TSource> source, Func<ISubject<TSource, TResult>> subjectSelector)
+        {
+            return new ReconnectableObservable<TSource, TResult>(source, subjectSelector);
+        }
+
+        public static IConnectableObservable<TSource> PublishReconnectable<TSource>(this IObservable<TSource> source)
+        {
+            return Multicast(source, () => new Subject<TSource>());
         }
     }
 }
---
+++ Bonsai/ReconnectableObservable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+using System.Text;
+using System.Reactive.Disposables;
+
+namespace Bonsai
+{
+    class ReconnectableObservable<TSource, TResult> : IConnectableObservable<TResult>
+    {
+        readonly object gate;
+        readonly IObservable<TSource> observableSource;
+        readonly Func<ISubject<TSource, TResult>> subjectFactory;
+        IConnectableObservable<TResult> connectableSource;
+
+        public ReconnectableObservable(IObservable<TSource> source, Func<ISubject<TSource, TResult>> subjectSelector)
+        {
+            subjectFactory = subjectSelector;
+            observableSource = source.AsObservable();
+            gate = new object();
+        }
+
+        public IDisposable Connect()
+        {
+            lock (gate)
+            {
+                EnsureConnectableSource();
+                var connection = connectableSource.Connect();
+                return Disposable.Create(() =>
+                {
+                    lock (gate)
+                    {
+                        using (connection)
+                        {
+                            connectableSource = null;
+                        }
+                    }
+                });
+            }
+        }
+
+        public IDisposable Subscribe(IObserver<TResult> observer)
+        {
+            lock (gate)
+            {
+                EnsureConnectableSource();
+                return connectableSource.Subscribe(observer);
+            }
+        }
+
+        private void EnsureConnectableSource()
+        {
+            if (connectableSource == null)
+            {
+                var subject = subjectFactory();
+                connectableSource = observableSource.Multicast(subject);
+            }
+        }
+    }
+}
