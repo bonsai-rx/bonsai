6a483c03761c317719c85c9d08f987626122c24e
glopes <glopes@localhost>
2013-08-28 04:07:17 +0100

    Added first draft of NuGet package manager dialog.

---
+++ Bonsai.Design/PackageDetails.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NuGet;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace Bonsai.Design
+{
+    public partial class PackageDetails : UserControl
+    {
+        static readonly Uri NugetPackageRepository = new Uri("https://packages.nuget.org/api/v2");
+        const string NoDependenciesText = "No Dependencies";
+        const string DependencyWarningText = "Each item above may have sub-dependencies subject to additional license agreements.";
+
+        public PackageDetails()
+        {
+            InitializeComponent();
+            SetPackage(null);
+        }
+
+        public void SetPackage(IPackage package)
+        {
+            detailsLayoutPanel.Visible = package != null;
+            if (package == null) return;
+
+            createdByLabel.Text = string.Join(CultureInfo.CurrentCulture.TextInfo.ListSeparator, package.Authors);
+            idLinkLabel.Text = package.Id;
+            var packageUri = new Uri(NugetPackageRepository, package.Id + "/" + package.Version.ToString());
+            SetLinkLabelUri(idLinkLabel, packageUri, false);
+            versionLabel.Text = package.Version.ToString();
+            lastPublishedLabel.Text = package.Published.HasValue ? package.Published.Value.Date.ToShortDateString() : "(unpublished)";
+            downloadsLabel.Text = package.DownloadCount.ToString();
+            SetLinkLabelUri(licenseLinkLabel, package.LicenseUrl, true);
+            SetLinkLabelUri(projectLinkLabel, package.ProjectUrl, true);
+            SetLinkLabelUri(reportAbuseLinkLabel, package.ReportAbuseUrl, false);
+            descriptionLabel.Text = package.Description;
+            tagsLabel.Text = package.Tags;
+            dependenciesTextBox.Lines = (from dependencySet in package.DependencySets
+                                         from dependency in dependencySet.Dependencies
+                                         select PackageDependencyString(dependency)).ToArray();
+            if (dependenciesTextBox.Lines.Length > 0)
+            {
+                dependenciesTextBox.Visible = true;
+                dependencyWarningLabel.Text = DependencyWarningText;
+            }
+            else
+            {
+                dependenciesTextBox.Visible = false;
+                dependencyWarningLabel.Text = NoDependenciesText;
+            }
+        }
+
+        static void SetLinkLabelUri(LinkLabel linkLabel, Uri uri, bool hideEmptyLink)
+        {
+            linkLabel.Links[0].Description = uri != null && uri.IsAbsoluteUri ? uri.AbsoluteUri : null;
+            linkLabel.Links[0].LinkData = uri;
+            linkLabel.Visible = !hideEmptyLink || linkLabel.Links[0].LinkData != null;
+        }
+
+        static string PackageDependencyString(PackageDependency dependency)
+        {
+            var versionSpecText = VersionSpecString(dependency.VersionSpec);
+            return dependency.Id + versionSpecText;
+        }
+
+        static string VersionSpecString(IVersionSpec versionSpec)
+        {
+            if (versionSpec == null) return string.Empty;
+            var versionText = new StringBuilder();
+            if (versionSpec.MinVersion != null)
+            {
+                if (versionSpec.MaxVersion == versionSpec.MinVersion)
+                {
+                    versionText.Append("= ");
+                }
+                else versionText.Append(versionSpec.IsMinInclusive ? "≥ " : "> ");
+                versionText.Append(versionSpec.MinVersion);
+            }
+
+            if (versionSpec.MaxVersion != null && versionSpec.MaxVersion != versionSpec.MinVersion)
+            {
+                if (versionSpec.MinVersion != null)
+                {
+                    versionText.Append(" && ");
+                }
+
+                versionText.Append(versionSpec.IsMaxInclusive ? "≤ " : "< ");
+                versionText.Append(versionSpec.MaxVersion);
+            }
+
+            if (versionText.Length > 0)
+            {
+                return string.Format(" ({0})", versionText);
+            }
+            else return string.Empty;
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var linkUri = (Uri)e.Link.LinkData;
+            if (linkUri != null)
+            {
+                Process.Start(linkUri.AbsoluteUri);
+            }
+        }
+    }
+}
---
+++ Bonsai.Design/PackageManagerDialog.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Bonsai.Design
+{
+    partial class PackageManagerDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.packageView = new Bonsai.Design.PackageView();
+            this.packageIcons = new System.Windows.Forms.ImageList(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.releaseFilterComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sortComboBox = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.packageDetails = new Bonsai.Design.PackageDetails();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.97351F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.02649F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 315F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.treeView2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 361);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.packageView, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(138, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(327, 355);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // packageView
+            // 
+            this.packageView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.packageView.DrawMode = System.Windows.Forms.TreeViewDrawMode.OwnerDrawText;
+            this.packageView.FullRowSelect = true;
+            this.packageView.ImageIndex = 0;
+            this.packageView.ImageList = this.packageIcons;
+            this.packageView.ItemHeight = 64;
+            this.packageView.Location = new System.Drawing.Point(3, 33);
+            this.packageView.Name = "packageView";
+            this.packageView.SelectedImageIndex = 0;
+            this.packageView.ShowRootLines = false;
+            this.packageView.Size = new System.Drawing.Size(321, 299);
+            this.packageView.TabIndex = 0;
+            this.packageView.DrawNode += new System.Windows.Forms.DrawTreeNodeEventHandler(this.packageView_DrawNode);
+            this.packageView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.packageView_AfterSelect);
+            // 
+            // packageIcons
+            // 
+            this.packageIcons.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.packageIcons.ImageSize = new System.Drawing.Size(32, 32);
+            this.packageIcons.TransparentColor = System.Drawing.Color.Violet;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.releaseFilterComboBox);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.sortComboBox);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(321, 24);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // releaseFilterComboBox
+            // 
+            this.releaseFilterComboBox.FormattingEnabled = true;
+            this.releaseFilterComboBox.Items.AddRange(new object[] {
+            "Stable Only",
+            "Include Prerelease"});
+            this.releaseFilterComboBox.Location = new System.Drawing.Point(3, 3);
+            this.releaseFilterComboBox.Name = "releaseFilterComboBox";
+            this.releaseFilterComboBox.Size = new System.Drawing.Size(121, 21);
+            this.releaseFilterComboBox.TabIndex = 0;
+            this.releaseFilterComboBox.SelectedIndexChanged += new System.EventHandler(this.filterComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(130, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sort by:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sortComboBox
+            // 
+            this.sortComboBox.FormattingEnabled = true;
+            this.sortComboBox.Items.AddRange(new object[] {
+            "Most Downloads",
+            "Published Date",
+            "Name: Ascending",
+            "Name: Descending"});
+            this.sortComboBox.Location = new System.Drawing.Point(179, 3);
+            this.sortComboBox.Name = "sortComboBox";
+            this.sortComboBox.Size = new System.Drawing.Size(121, 21);
+            this.sortComboBox.TabIndex = 2;
+            this.sortComboBox.SelectedIndexChanged += new System.EventHandler(this.filterComboBox_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.flowLayoutPanel2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.packageDetails, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(471, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(310, 355);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.comboBox3);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(304, 24);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(3, 3);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(147, 21);
+            this.comboBox3.TabIndex = 0;
+            // 
+            // packageDetails
+            // 
+            this.packageDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.packageDetails.Location = new System.Drawing.Point(3, 33);
+            this.packageDetails.Name = "packageDetails";
+            this.packageDetails.Size = new System.Drawing.Size(304, 319);
+            this.packageDetails.TabIndex = 1;
+            // 
+            // treeView2
+            // 
+            this.treeView2.BackColor = System.Drawing.SystemColors.Control;
+            this.treeView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(3, 3);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(129, 355);
+            this.treeView2.TabIndex = 4;
+            // 
+            // PackageManagerDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PackageManagerDialog";
+            this.ShowIcon = false;
+            this.Text = "PackageManagerDialog";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ImageList packageIcons;
+        private PackageView packageView;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox releaseFilterComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox sortComboBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private PackageDetails packageDetails;
+    }
+}
