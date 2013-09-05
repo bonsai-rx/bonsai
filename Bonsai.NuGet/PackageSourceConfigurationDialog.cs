0e9baf0b70fe04fad6d616449c1241cd97f26a9d
glopes <glopes@localhost>
2013-09-06 00:19:40 +0100

    Added first draft of package source configuration dialog.

---
+++ Bonsai.NuGet/PackageSourceConfigurationDialog.cs
@@ -0,0 +1,37 @@
+﻿using NuGet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bonsai.NuGet
+{
+    public partial class PackageSourceConfigurationDialog : Form
+    {
+        public PackageSourceConfigurationDialog()
+        {
+            InitializeComponent();
+        }
+
+        public PackageSourceProvider SourceProvider { get; set; }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var provider = SourceProvider;
+            if (provider != null)
+            {
+                foreach (var packageSource in provider.LoadPackageSources())
+                {
+                    var item = packageSourceListView.Items.Add(packageSource.Name, packageSource.Source, 0);
+                    item.Checked = packageSource.IsEnabled;
+                }
+            }
+
+            base.OnLoad(e);
+        }
+    }
+}
