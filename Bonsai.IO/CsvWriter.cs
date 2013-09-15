c8af36e62707a7e96847ebd13ec91fff7b1206a2
glopes <glopesdev@users.noreply.github.com>
2013-09-15 04:53:15 +0100

    Updated CsvWriter to cache code generation and create file asynchronously.

--- Bonsai.IO/CsvWriter.cs
+++ Bonsai.IO/CsvWriter.cs
@@ -11,12 +11,13 @@ using System.Xml.Serialization;
 using System.Globalization;
 using System.Reactive.Linq;
 using System.Reactive.Disposables;
+using Bonsai.Expressions;
 
 namespace Bonsai.IO
 {
     [WorkflowElementCategory(ElementCategory.Sink)]
     [Description("Sinks individual elements of the input sequence to a text file.")]
-    public class CsvWriter : Combinator
+    public class CsvWriter : CombinatorExpressionBuilder
     {
         static readonly MethodInfo writeLineMethod = typeof(StreamWriter).GetMethods().First(m => m.Name == "WriteLine" && m.GetParameters().Length == 0);
         static readonly MethodInfo writeMethod = typeof(StreamWriter).GetMethods().First(m => m.Name == "Write" &&
@@ -41,165 +42,159 @@ namespace Bonsai.IO
         [Editor("Bonsai.Design.MemberSelectorEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string Selector { get; set; }
 
-        class CsvProcessor<T>
+        class ExpressionNode
         {
-            Task writerTask;
-            StreamWriter writer;
-            Action<T> writeAction;
-            CsvWriter parent;
+            public Expression Expression { get; set; }
+            public ExpressionNode Parent { get; set; }
 
-            public CsvProcessor(CsvWriter owner)
+            public IEnumerable<Expression> GetPath()
             {
-                this.parent = owner;
-            }
-
-            public void Process(T input)
-            {
-                if (writerTask != null)
+                var current = this;
+                while (current != null)
                 {
-                    writerTask = writerTask.ContinueWith(task => writeAction(input));
+                    yield return current.Expression;
+                    current = current.Parent;
                 }
-            }
-
-            class ExpressionNode
-            {
-                public Expression Expression { get; set; }
-                public ExpressionNode Parent { get; set; }
-
-                public IEnumerable<Expression> GetPath()
-                {
-                    var current = this;
-                    while (current != null)
-                    {
-                        yield return current.Expression;
-                        current = current.Parent;
-                    }
-                }
-            }
-
-            static IEnumerable<Expression> MakeMemberAccess(Expression expression)
-            {
-                var stack = new Stack<ExpressionNode>();
-                stack.Push(new ExpressionNode { Expression = expression });
-                while (stack.Count > 0)
-                {
-                    var current = stack.Pop();
-                    expression = current.Expression;
-                    if (expression.Type == typeof(string)) yield return expression;
-                    else if (expression.Type.IsPrimitive || expression.Type.IsEnum || expression.Type == typeof(string) ||
-                             expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset) ||
-                             expression.Type == typeof(TimeSpan))
-                    {
-                        yield return expression;
-                    }
-                    else
-                    {
-                        var successors = from member in GetDataMembers(expression.Type)
-                                         let memberAccess = (Expression)Expression.MakeMemberAccess(expression, member)
-                                         select memberAccess;
-                        foreach (var successor in successors)
-                        {
-                            if (current.GetPath().Any(node => node.Type == successor.Type)) continue;
-                            stack.Push(new ExpressionNode { Expression = successor, Parent = current });
-                        }
-                    }
-                }
-            }
-
-            static IEnumerable<MemberInfo> GetDataMembers(Type type)
-            {
-                return Enumerable.Concat<MemberInfo>(
-                    type.GetFields(BindingFlags.Instance | BindingFlags.Public),
-                    type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
-                    .Where(member => !member.IsDefined(typeof(XmlIgnoreAttribute), true));
-            }
-
-            static Expression GetDataString(Expression expression)
-            {
-                if (expression.Type == typeof(string)) return expression;
-                else if (expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset))
-                {
-                    return Expression.Call(expression, "ToString", null, Expression.Constant("o"));
-                }
-                else if (expression.Type == typeof(IntPtr) || expression.Type == typeof(TimeSpan))
-                {
-                    return Expression.Call(expression, "ToString", null);
-                }
-                else
-                {
-                    return Expression.Call(expression, "ToString", null, Expression.Constant(CultureInfo.InvariantCulture));
-                }
-            }
-
-            public IDisposable Load()
-            {
-                const string ParameterName = "input";
-                const string EntryFormat = "{0} ";
-
-                if (!string.IsNullOrEmpty(parent.FileName))
-                {
-                    var fileName = PathHelper.AppendSuffix(parent.FileName, parent.Suffix);
-                    writer = new StreamWriter(fileName, parent.Append, Encoding.ASCII);
-
-                    var parameterType = typeof(T);
-                    var parameter = Expression.Parameter(parameterType, ParameterName);
-                    var memberExpression = ExpressionHelper.MemberAccess(parameter, parent.Selector);
-
-                    var formatConstant = Expression.Constant(EntryFormat);
-                    var writerConstant = Expression.Constant(writer);
-
-                    var memberAccessExpressions = MakeMemberAccess(memberExpression).ToArray();
-                    Array.Reverse(memberAccessExpressions);
-
-                    var writeParameters = from memberAccess in memberAccessExpressions
-                                          let memberString = GetDataString(memberAccess)
-                                          select Expression.Call(writerConstant, writeMethod, formatConstant, memberString);
-                    var writeLineExpression = Expression.Call(writerConstant, writeLineMethod);
-                    var bodyExpressions = writeParameters.Concat(Enumerable.Repeat(writeLineExpression, 1)).ToArray();
-
-                    var body = Expression.Block(bodyExpressions);
-                    writeAction = Expression.Lambda<Action<T>>(body, parameter).Compile();
-
-                    var header = string.Empty;
-                    if (parent.IncludeHeader)
-                    {
-                        foreach (var memberAccess in memberAccessExpressions)
-                        {
-                            var accessExpression = memberAccess.ToString();
-                            header += string.Format(EntryFormat, accessExpression.Remove(accessExpression.IndexOf(ParameterName), ParameterName.Length)).TrimStart('.');
-                        }
-                        header = header.Trim();
-                    }
-
-                    writerTask = new Task(() => { if (!string.IsNullOrEmpty(header)) writer.WriteLine(header); });
-                    writerTask.Start();
-                }
-
-                return Disposable.Create(Unload);
-            }
-
-            protected void Unload()
-            {
-                var closingWriter = writer;
-                if (closingWriter != null)
-                {
-                    writerTask.ContinueWith(task => closingWriter.Close());
-                }
-
-                writerTask = null;
-                writer = null;
             }
         }
 
-        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        static IEnumerable<Expression> MakeMemberAccess(Expression expression)
         {
-            return Observable.Defer(() =>
+            var stack = new Stack<ExpressionNode>();
+            stack.Push(new ExpressionNode { Expression = expression });
+            while (stack.Count > 0)
             {
-                var processor = new CsvProcessor<TSource>(this);
-                return Observable.Using(
-                    () => processor.Load(),
-                    resource => source.Do(input => processor.Process(input)));
-            });
+                var current = stack.Pop();
+                expression = current.Expression;
+                if (expression.Type == typeof(string)) yield return expression;
+                else if (expression.Type.IsPrimitive || expression.Type.IsEnum || expression.Type == typeof(string) ||
+                         expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset) ||
+                         expression.Type == typeof(TimeSpan))
+                {
+                    yield return expression;
+                }
+                else
+                {
+                    var successors = from member in GetDataMembers(expression.Type)
+                                     let memberAccess = (Expression)Expression.MakeMemberAccess(expression, member)
+                                     select memberAccess;
+                    foreach (var successor in successors)
+                    {
+                        if (current.GetPath().Any(node => node.Type == successor.Type)) continue;
+                        stack.Push(new ExpressionNode { Expression = successor, Parent = current });
+                    }
+                }
+            }
+        }
+
+        static IEnumerable<MemberInfo> GetDataMembers(Type type)
+        {
+            return Enumerable.Concat<MemberInfo>(
+                type.GetFields(BindingFlags.Instance | BindingFlags.Public),
+                type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                .Where(member => !member.IsDefined(typeof(XmlIgnoreAttribute), true));
+        }
+
+        static Expression GetDataString(Expression expression)
+        {
+            if (expression.Type == typeof(string)) return expression;
+            else if (expression.Type == typeof(DateTime) || expression.Type == typeof(DateTimeOffset))
+            {
+                return Expression.Call(expression, "ToString", null, Expression.Constant("o"));
+            }
+            else if (expression.Type == typeof(IntPtr) || expression.Type == typeof(TimeSpan))
+            {
+                return Expression.Call(expression, "ToString", null);
+            }
+            else
+            {
+                return Expression.Call(expression, "ToString", null, Expression.Constant(CultureInfo.InvariantCulture));
+            }
+        }
+
+        public override Expression Build()
+        {
+            const string ParameterName = "input";
+            const string EntryFormat = "{0} ";
+
+            var parameterType = Source.Type.GetGenericArguments()[0];
+            var inputParameter = Expression.Parameter(parameterType, ParameterName);
+            var writerParameter = Expression.Parameter(typeof(StreamWriter));
+            var memberExpression = ExpressionHelper.MemberAccess(inputParameter, Selector);
+            var formatConstant = Expression.Constant(EntryFormat);
+
+            var memberAccessExpressions = MakeMemberAccess(memberExpression).ToArray();
+            Array.Reverse(memberAccessExpressions);
+
+            var writeParameters = from memberAccess in memberAccessExpressions
+                                  let memberString = GetDataString(memberAccess)
+                                  select Expression.Call(writerParameter, writeMethod, formatConstant, memberString);
+            var writeLineExpression = Expression.Call(writerParameter, writeLineMethod);
+            var bodyExpressions = writeParameters.Concat(Enumerable.Repeat(writeLineExpression, 1)).ToArray();
+
+            var body = Expression.Block(bodyExpressions);
+            var writeAction = Expression.Lambda(body, inputParameter, writerParameter);
+
+            var header = string.Empty;
+            if (IncludeHeader)
+            {
+                foreach (var memberAccess in memberAccessExpressions)
+                {
+                    var accessExpression = memberAccess.ToString();
+                    header += string.Format(
+                        EntryFormat,
+                        accessExpression.Remove(accessExpression.IndexOf(ParameterName), ParameterName.Length))
+                        .TrimStart('.');
+                }
+                header = header.Trim();
+            }
+
+            var csvWriter = Expression.Constant(this);
+            var headerExpression = Expression.Constant(header);
+            return Expression.Call(csvWriter, "Process", new[] { parameterType }, Source, headerExpression, writeAction);
+        }
+
+        IObservable<TSource> Process<TSource>(IObservable<TSource> source, string header, Action<TSource, StreamWriter> writeAction)
+        {
+            return Observable.Using(
+                () =>
+                {
+                    if (string.IsNullOrEmpty(FileName))
+                    {
+                        throw new InvalidOperationException("A valid filename must be specified.");
+                    }
+
+                    var disposable = new WriterDisposable();
+                    disposable.WriterTask = new Task(() =>
+                    {
+                        var fileName = PathHelper.AppendSuffix(FileName, Suffix);
+                        var writer = new StreamWriter(fileName, Append, Encoding.ASCII);
+                        if (!string.IsNullOrEmpty(header)) writer.WriteLine(header);
+                        disposable.Writer = writer;
+                    });
+                    disposable.WriterTask.Start();
+                    return disposable;
+                },
+                disposable => source.Do(input =>
+                {
+                    disposable.WriterTask = disposable.WriterTask.ContinueWith(task => writeAction(input, disposable.Writer));
+                }));
+        }
+
+        class WriterDisposable : IDisposable
+        {
+            public Task WriterTask { get; set; }
+
+            public StreamWriter Writer { get; set; }
+
+            public void Dispose()
+            {
+                var closingWriter = Writer;
+                if (closingWriter != null)
+                {
+                    WriterTask.ContinueWith(task => closingWriter.Close());
+                }
+            }
         }
     }
 }
