6721ca9c39d23c47357841e81e717292bad2577e
glopes <glopesdev@users.noreply.github.com>
2013-09-21 15:21:07 +0100

    Added nuspec, assembly metadata and automatic builds for Player package.

--- Bonsai.Player/Properties/AssemblyInfo.cs
+++ Bonsai.Player/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai.Player")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bonsai - Player")]
+[assembly: AssemblyDescription("A command line player for Bonsai workflows.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.Player")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo C. Lopes 2011-2013")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
