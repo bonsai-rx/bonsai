cbba7a1865950de13a498aee57cd0c2f96bb3173
glopes <glopesdev@users.noreply.github.com>
2013-09-22 04:44:08 +0100

    Added package restore feature to editor bootstrapper.

--- Bonsai.Editor/PackageHelper.cs
+++ Bonsai.Editor/PackageHelper.cs
@@ -6,49 +6,85 @@ using System.IO;
 using System.Reflection;
 using Bonsai.Design;
 using Bonsai.Expressions;
+using Bonsai.Configuration;
+using NuGet;
+using System.Threading.Tasks;
+using Bonsai.NuGet;
+using PackageReference = Bonsai.Configuration.PackageReference;
 
 namespace Bonsai.Editor
 {
-    class PackageHelper
+    static class PackageHelper
     {
-        const string DefaultProbingPath = "Packages";
-
-        static AssemblyName GetAssemblyName(string fileName)
+        internal static void RunPackageOperation(EventLogger logger, Func<Task> operationFactory)
         {
-            try { return AssemblyName.GetAssemblyName(fileName); }
-            catch (BadImageFormatException)
+            using (var dialog = new PackageOperationDialog())
             {
-                return null;
+                dialog.RegisterEventLogger(logger);
+                var operation = operationFactory();
+                operation.ContinueWith(task => dialog.BeginInvoke((Action)dialog.Close));
+                dialog.ShowDialog();
             }
         }
 
-        static IEnumerable<string> GetPackageFiles(string path)
+        static SemanticVersion ParseVersion(string version)
         {
-            path = Path.GetFullPath(path);
-            if (Directory.Exists(path) &&
-                !string.Equals(path.TrimEnd('\\'),
-                               Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory).TrimEnd('\\'),
-                               StringComparison.InvariantCultureIgnoreCase))
-            {
-                var bonsaiAssemblyName = typeof(ExpressionBuilder).Assembly.GetName();
-                var bonsaiDesignAssemblyName = typeof(DialogTypeVisualizer).Assembly.GetName();
-                return from fileName in Directory.GetFiles(path, "*.dll")
-                       let assemblyName = GetAssemblyName(fileName)
-                       where assemblyName != null &&
-                             assemblyName.FullName != bonsaiAssemblyName.FullName &&
-                             assemblyName.FullName != bonsaiDesignAssemblyName.FullName
-                       select fileName;
-            }
-
-            return Enumerable.Empty<string>();
+            if (string.IsNullOrEmpty(version)) return null;
+            return SemanticVersion.Parse(version);
         }
 
-        public static IEnumerable<string> GetPackageFiles()
+        internal static Task<IPackage> StartInstallPackage(this IPackageManager packageManager, string packageId, SemanticVersion version)
         {
-            IEnumerable<string> packageFiles = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
-            packageFiles = packageFiles.Concat(GetPackageFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, DefaultProbingPath)));
-            packageFiles = packageFiles.Concat(GetPackageFiles(Environment.CurrentDirectory));
-            return packageFiles;
+            return Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    packageManager.Logger.Log(MessageLevel.Info, "Checking for latest version of {0}.", packageId);
+                    var package = packageManager.SourceRepository.FindPackage(packageId);
+                    packageManager.InstallPackage(package, false, true);
+                    return package;
+                }
+                catch (Exception ex)
+                {
+                    packageManager.Logger.Log(MessageLevel.Error, ex.Message);
+                    return null;
+                }
+            });
+        }
+
+        internal static IEnumerable<PackageReference> GetMissingPackages(IEnumerable<PackageReference> packages, IPackageRepository repository)
+        {
+            return from package in packages
+                   let version = ParseVersion(package.Version)
+                   where !repository.Exists(package.Id, version)
+                   select package;
+        }
+
+        internal static IEnumerable<Task<IPackage>> StartRestorePackages(this IPackageManager packageManager, IEnumerable<PackageReference> packages)
+        {
+            return packages.Select(package => StartRestorePackage(packageManager, package));
+        }
+
+        internal static Task<IPackage> StartRestorePackage(this IPackageManager packageManager, PackageReference package)
+        {
+            return StartRestorePackage(packageManager, package.Id, ParseVersion(package.Version));
+        }
+
+        internal static Task<IPackage> StartRestorePackage(this IPackageManager packageManager, string id, SemanticVersion version)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    packageManager.Logger.Log(MessageLevel.Info, "Restoring '{0}' package.", id);
+                    return packageManager.SourceRepository.FindPackage(id, version);
+                }
+                catch (Exception ex)
+                {
+                    packageManager.Logger.Log(MessageLevel.Error, ex.Message);
+                    return null;
+                }
+            });
         }
     }
 }
