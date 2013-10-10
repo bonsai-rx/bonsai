47e9626013410f3bb3f0abfd157388d23bd1f2a2
glopes <glopes@localhost>
2013-10-10 17:55:52 +0100

    Added Sum operator to Dsp package.

---
+++ Bonsai.Dsp/Sum.cs
@@ -0,0 +1,17 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Bonsai.Dsp
+{
+    public class Sum : Transform<Mat, Scalar>
+    {
+        public override IObservable<Scalar> Process(IObservable<Mat> source)
+        {
+            return Observable.Select(source, input => CV.Sum(input));
+        }
+    }
+}
