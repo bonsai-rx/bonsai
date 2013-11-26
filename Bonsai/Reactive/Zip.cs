a3edb388dd2551675760d2ddc155d1ca74c5d4d0
glopes <glopesdev@users.noreply.github.com>
2013-11-26 01:20:22 +0000

    Added more general overloads to CombineLatest and Zip.

--- Bonsai/Reactive/Zip.cs
+++ Bonsai/Reactive/Zip.cs
@@ -15,61 +15,65 @@ namespace Bonsai.Reactive
     [Description("Combines values from the input sequences whenever all of the sequences have produced an element.")]
     public class Zip
     {
-        public IObservable<Tuple<TSource, TOther>> Process<TSource, TOther>(IObservable<TSource> source, IObservable<TOther> second)
+        public IObservable<Tuple<TSource1, TSource2>> Process<TSource1, TSource2>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2)
         {
-            return source.Zip(second, (xs, ys) => Tuple.Create(xs, ys));
+            return source1.Zip(source2, (xs, ys) => Tuple.Create(xs, ys));
         }
 
-        public IObservable<Tuple<TSource, TSource, TSource>> Process<TSource>(
-            IObservable<TSource> source,
-            IObservable<TSource> source2,
-            IObservable<TSource> source3)
+        public IObservable<Tuple<TSource1, TSource2, TSource3>> Process<TSource1, TSource2, TSource3>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2,
+            IObservable<TSource3> source3)
         {
-            return source.Zip(source2, source3, (s1, s2, s3) => Tuple.Create(s1, s2, s3));
+            return source1.Zip(source2, source3, (s1, s2, s3) => Tuple.Create(s1, s2, s3));
         }
 
-        public IObservable<Tuple<TSource, TSource, TSource, TSource>> Process<TSource>(
-            IObservable<TSource> source,
-            IObservable<TSource> source2,
-            IObservable<TSource> source3,
-            IObservable<TSource> source4)
+        public IObservable<Tuple<TSource1, TSource2, TSource3, TSource4>> Process<TSource1, TSource2, TSource3, TSource4>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2,
+            IObservable<TSource3> source3,
+            IObservable<TSource4> source4)
         {
-            return source.Zip(source2, source3, source4, (s1, s2, s3, s4) => Tuple.Create(s1, s2, s3, s4));
+            return source1.Zip(source2, source3, source4, (s1, s2, s3, s4) => Tuple.Create(s1, s2, s3, s4));
         }
 
-        public IObservable<Tuple<TSource, TSource, TSource, TSource, TSource>> Process<TSource>(
-            IObservable<TSource> source,
-            IObservable<TSource> source2,
-            IObservable<TSource> source3,
-            IObservable<TSource> source4,
-            IObservable<TSource> source5)
+        public IObservable<Tuple<TSource1, TSource2, TSource3, TSource4, TSource5>>
+            Process<TSource1, TSource2, TSource3, TSource4, TSource5>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2,
+            IObservable<TSource3> source3,
+            IObservable<TSource4> source4,
+            IObservable<TSource5> source5)
         {
-            return source.Zip(source2, source3, source4, source5,
+            return source1.Zip(source2, source3, source4, source5,
                 (s1, s2, s3, s4, s5) => Tuple.Create(s1, s2, s3, s4, s5));
         }
 
-        public IObservable<Tuple<TSource, TSource, TSource, TSource, TSource, TSource>> Process<TSource>(
-            IObservable<TSource> source,
-            IObservable<TSource> source2,
-            IObservable<TSource> source3,
-            IObservable<TSource> source4,
-            IObservable<TSource> source5,
-            IObservable<TSource> source6)
+        public IObservable<Tuple<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6>> Process<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2,
+            IObservable<TSource3> source3,
+            IObservable<TSource4> source4,
+            IObservable<TSource5> source5,
+            IObservable<TSource6> source6)
         {
-            return source.Zip(source2, source3, source4, source5, source6,
+            return source1.Zip(source2, source3, source4, source5, source6,
                 (s1, s2, s3, s4, s5, s6) => Tuple.Create(s1, s2, s3, s4, s5, s6));
         }
 
-        public IObservable<Tuple<TSource, TSource, TSource, TSource, TSource, TSource, TSource>> Process<TSource>(
-            IObservable<TSource> source,
-            IObservable<TSource> source2,
-            IObservable<TSource> source3,
-            IObservable<TSource> source4,
-            IObservable<TSource> source5,
-            IObservable<TSource> source6,
-            IObservable<TSource> source7)
+        public IObservable<Tuple<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7>>
+            Process<TSource1, TSource2, TSource3, TSource4, TSource5, TSource6, TSource7>(
+            IObservable<TSource1> source1,
+            IObservable<TSource2> source2,
+            IObservable<TSource3> source3,
+            IObservable<TSource4> source4,
+            IObservable<TSource5> source5,
+            IObservable<TSource6> source6,
+            IObservable<TSource7> source7)
         {
-            return source.Zip(source2, source3, source4, source5, source6, source7,
+            return source1.Zip(source2, source3, source4, source5, source6, source7,
                 (s1, s2, s3, s4, s5, s6, s7) => Tuple.Create(s1, s2, s3, s4, s5, s6, s7));
         }
 
