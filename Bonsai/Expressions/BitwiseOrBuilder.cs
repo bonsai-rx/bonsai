3d86680d6482a38f7f63be93ab63e2307c4fc9b7
glopes <glopes@localhost>
2013-12-02 20:56:22 +0000

    Added first draft of generic support for arithmetic operators.

---
+++ Bonsai/Expressions/BitwiseOrBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("BitwiseOr", Namespace = Constants.XmlNamespace)]
+    public class BitwiseOrBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.Or(left, right);
+        }
+    }
+}
---
+++ Bonsai/Expressions/DivideBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Divide", Namespace = Constants.XmlNamespace)]
+    public class DivideBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.Divide(left, right);
+        }
+    }
+}
