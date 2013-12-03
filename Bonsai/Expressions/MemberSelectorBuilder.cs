6ee3f1a2051a05775d6e2b2dc5df62d7f834e60e
glopes <glopesdev@users.noreply.github.com>
2013-12-03 16:23:24 +0000

    Reinstated MemberSelector in order to explicitly project input data type.

---
+++ Bonsai/Expressions/MemberSelectorBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Reactive.Linq;
+using System.ComponentModel;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("MemberSelector", Namespace = Constants.XmlNamespace)]
+    [Description("Selects inner properties of elements of the sequence.")]
+    public class MemberSelectorBuilder : SelectBuilder
+    {
+        [Description("The inner properties that will be selected for each element of the sequence.")]
+        [Editor("Bonsai.Design.MemberSelectorEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
+        public string Selector { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
+        {
+            return ExpressionHelper.MemberAccess(expression, Selector);
+        }
+    }
+}
