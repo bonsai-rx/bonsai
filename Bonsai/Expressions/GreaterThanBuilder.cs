8fcb1206e18e8e8fd416753cc7c2f337d535099e
glopes <glopesdev@users.noreply.github.com>
2013-12-03 13:04:01 +0000

    Added support for generic comparison and logical operators.

---
+++ Bonsai/Expressions/GreaterThanBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("GreaterThan", Namespace = Constants.XmlNamespace)]
+    public class GreaterThanBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.GreaterThan(left, right);
+        }
+    }
+}
---
+++ Bonsai/Expressions/LogicalAndBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("LogicalAnd", Namespace = Constants.XmlNamespace)]
+    public class LogicalAndBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.AndAlso(left, right);
+        }
+    }
+}
