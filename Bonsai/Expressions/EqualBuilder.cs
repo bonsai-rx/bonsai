bdb408b20f2588722cdb67eefd3f1251a8534153
glopes <glopes@localhost>
2013-12-03 13:04:01 +0000

    Added support for generic comparison and logical operators.

---
+++ Bonsai/Expressions/EqualBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Equal", Namespace = Constants.XmlNamespace)]
+    public class EqualBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.Equal(left, right);
+        }
+    }
+}
---
+++ Bonsai/Expressions/NotEqualBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("NotEqual", Namespace = Constants.XmlNamespace)]
+    public class NotEqualBuilder : BinaryOperatorBuilder
+    {
+        protected override Expression BuildSelector(Expression left, Expression right)
+        {
+            return Expression.NotEqual(left, right);
+        }
+    }
+}
