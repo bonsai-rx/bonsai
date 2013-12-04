03ec5baa802b9dc80e8b841598a7731df52b4c11
glopes <glopes@localhost>
2013-12-04 11:43:27 +0000

    Updated edge drawing so that extra property mapping edges are colored in red.

---
+++ Bonsai.Design/TypeDescriptorContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Design
+{
+    class TypeDescriptorContext : ITypeDescriptorContext
+    {
+        public TypeDescriptorContext(object instance, PropertyDescriptor propertyDescriptor)
+        {
+            Instance = instance;
+            PropertyDescriptor = propertyDescriptor;
+        }
+
+        public virtual IContainer Container
+        {
+            get { return null; }
+        }
+
+        public object Instance { get; private set; }
+
+        public virtual void OnComponentChanged()
+        {
+        }
+
+        public virtual bool OnComponentChanging()
+        {
+            return false;
+        }
+
+        public PropertyDescriptor PropertyDescriptor { get; private set; }
+
+        public virtual object GetService(Type serviceType)
+        {
+            return null;
+        }
+    }
+}
