b7dacaf4619c157a93071e8abbee3379822c213b
glopes <glopesdev@users.noreply.github.com>
2013-12-16 00:33:50 +0000

    Generalized branch multicast behavior to allow sharing with Replay.

---
+++ Bonsai/Expressions/MulticastBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Expressions
+{
+    public abstract class MulticastBuilder : MulticastExpressionBuilder
+    {
+        internal MulticastBuilder()
+        {
+        }
+
+        internal override Expression BuildMulticast(Expression source, LambdaExpression selector)
+        {
+            var sourceType = source.Type.GetGenericArguments()[0];
+            var resultType = selector.ReturnType.GetGenericArguments()[0];
+            var builder = Expression.Constant(this);
+            return Expression.Call(builder, "Multicast", new[] { sourceType, resultType }, source, selector);
+        }
+
+        internal abstract IObservable<TResult> Multicast<TSource, TResult>(IObservable<TSource> source, Func<IObservable<TSource>, IObservable<TResult>> selector);
+    }
+}
---
+++ Bonsai/Expressions/ReplayBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    [XmlType("Replay", Namespace = Constants.XmlNamespace)]
+    public class ReplayBuilder : MulticastBuilder
+    {
+        internal override IObservable<TResult> Multicast<TSource, TResult>(IObservable<TSource> source, Func<IObservable<TSource>, IObservable<TResult>> selector)
+        {
+            return selector(source.Replay(Scheduler.Immediate).RefCount());
+        }
+    }
+}
