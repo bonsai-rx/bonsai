55789064db3e2a6414b749028bd3a95b12fe2dd3
glopes <glopes@localhost>
2013-12-16 00:47:35 +0000

    Added DoubleProperty specifying double precision floating point values.

---
+++ Bonsai/Expressions/Properties/DoubleProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Expressions
+{
+    [DisplayName("Double")]
+    public class DoubleProperty : WorkflowProperty<double>
+    {
+    }
+}
