f9aa1482a8d4aec73db5804b32d6f2782d6b86f1
glopes <glopes@localhost>
2013-12-25 06:25:04 +0000

    Added summary documentation to basic workflow element types.

--- Bonsai/Transform.cs
+++ Bonsai/Transform.cs
@@ -5,6 +5,12 @@ using System.Text;
 
 namespace Bonsai
 {
+    /// <summary>
+    /// Represents an operation on observable sequences which transforms every element of the source
+    /// sequence into an element in the result sequence.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the result sequence.</typeparam>
     [WorkflowElementCategory(ElementCategory.Transform)]
     public abstract class Transform<TSource, TResult> : Combinator<TSource, TResult>
     {
