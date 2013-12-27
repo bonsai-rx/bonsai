2e9bf6b6c4f5fdefeae4a61bed683e069ee25802
glopes <glopesdev@users.noreply.github.com>
2013-12-27 23:36:11 +0000

    Added preliminary support for setting capture properties in CameraCapture.

--- Bonsai.Vision/CameraCapture.cs
+++ Bonsai.Vision/CameraCapture.cs
@@ -6,6 +6,7 @@ using OpenCV.Net;
 using System.Threading;
 using System.Reactive.Linq;
 using System.ComponentModel;
+using System.Reactive.Disposables;
 
 namespace Bonsai.Vision
 {
@@ -13,15 +14,29 @@ namespace Bonsai.Vision
     public class CameraCapture : Source<IplImage>
     {
         IObservable<IplImage> source;
+        readonly CapturePropertyCollection captureProperties = new CapturePropertyCollection();
 
         public CameraCapture()
         {
             source = Observable
                 .Using(
-                    () => Capture.CreateCameraCapture(Index),
+                    () =>
+                    {
+                        var capture = Capture.CreateCameraCapture(Index);
+                        foreach (var setting in captureProperties)
+                        {
+                            capture.SetProperty(setting.Property, setting.Value);
+                        }
+                        captureProperties.Capture = capture;
+                        return Disposable.Create(() =>
+                        {
+                            captureProperties.Capture = null;
+                            capture.Close();
+                        });
+                    },
                     capture => ObservableCombinators.GenerateWithThread<IplImage>(observer =>
                     {
-                        var image = capture.QueryFrame();
+                        var image = captureProperties.Capture.QueryFrame();
                         if (image == null)
                         {
                             observer.OnCompleted();
@@ -34,6 +49,12 @@ namespace Bonsai.Vision
 
         [Description("The index of the camera from which to acquire images.")]
         public int Index { get; set; }
+
+        [Description("Specifies the set of capture properties assigned to the camera.")]
+        public CapturePropertyCollection CaptureProperties
+        {
+            get { return captureProperties; }
+        }
 
         public override IObservable<IplImage> Generate()
         {
---
+++ Bonsai.Vision/CapturePropertyCollection.cs
@@ -0,0 +1,17 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [TypeConverter("Bonsai.Vision.Design.CapturePropertyCollectionConverter, Bonsai.Vision.Design")]
+    public class CapturePropertyCollection : Collection<CapturePropertyAssignment>
+    {
+        public Capture Capture { get; internal set; }
+    }
+}
