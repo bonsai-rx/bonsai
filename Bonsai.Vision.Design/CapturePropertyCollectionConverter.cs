be9538e0b1c61aee04b518e48b20d7c8e1986ad2
glopes <glopes@localhost>
2013-12-27 23:36:11 +0000

    Added preliminary support for setting capture properties in CameraCapture.

---
+++ Bonsai.Vision.Design/CapturePropertyCollectionConverter.cs
@@ -0,0 +1,46 @@
+﻿using Bonsai.Design;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision.Design
+{
+    public class CapturePropertyCollectionConverter : CollectionConverter
+    {
+        public override bool GetPropertiesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            var propertyCollection = (CapturePropertyCollection)value;
+            if (propertyCollection != null && propertyCollection.Count > 0)
+            {
+                var properties = (from property in propertyCollection
+                                  select new DynamicPropertyDescriptor(
+                                      property.Property.ToString(),
+                                      typeof(double),
+                                      component => property.Value,
+                                      (component, propval) =>
+                                      {
+                                          property.Value = (double)propval;
+                                          var capture = propertyCollection.Capture;
+                                          if (capture != null)
+                                          {
+                                              capture.SetProperty(property.Property, property.Value);
+                                          }
+                                      },
+                                      new EditorAttribute(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))))
+                                 .ToArray();
+                return new PropertyDescriptorCollection(properties);
+            }
+
+            return base.GetProperties(context, value, attributes);
+        }
+    }
+}
---
+++ Bonsai.Vision/CapturePropertyAssignment.cs
@@ -0,0 +1,24 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    public class CapturePropertyAssignment
+    {
+        public CaptureProperty Property { get; set; }
+
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public double Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} = {1}", Property, Value);
+        }
+    }
+}
