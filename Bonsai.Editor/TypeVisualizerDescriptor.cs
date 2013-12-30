da92b21d1af354adf76a0a0bccfc211a60bb82d4
glopes <glopes@localhost>
2013-12-30 13:49:50 +0000

    Added missing files.

---
+++ Bonsai.Editor/TypeVisualizerDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Editor
+{
+    [Serializable]
+    public class TypeVisualizerDescriptor
+    {
+        public string VisualizerTypeName;
+        public string TargetTypeName;
+
+        public TypeVisualizerDescriptor(TypeVisualizerAttribute typeVisualizer)
+        {
+            TargetTypeName = typeVisualizer.TargetTypeName;
+            VisualizerTypeName = typeVisualizer.VisualizerTypeName;
+        }
+    }
+}
---
+++ Bonsai.Editor/WorkflowElementDescriptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Editor
+{
+    [Serializable]
+    [DebuggerDisplay("Name = {Name}, AssemblyName = {AssemblyName}")]
+    public struct WorkflowElementDescriptor
+    {
+        public string Name { get; set; }
+
+        public string Namespace { get; set; }
+
+        public string AssemblyQualifiedName { get; set; }
+
+        public string Description { get; set; }
+
+        public ElementCategory[] ElementTypes { get; set; }
+    }
+}
