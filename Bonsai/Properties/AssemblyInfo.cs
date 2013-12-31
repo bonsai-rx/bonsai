b2a3218c81b0bca931f4f91648a93ab470467793
glopes <glopesdev@users.noreply.github.com>
2013-12-31 12:35:27 +0000

    Updated Bonsai package description.

--- Bonsai/Properties/AssemblyInfo.cs
+++ Bonsai/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bonsai")]
-[assembly: AssemblyDescription("A visual framework for data stream processing built on top of Rx for .NET.")]
+[assembly: AssemblyDescription("A visual programming framework for data stream processing built on top of Rx for .NET.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai")]
