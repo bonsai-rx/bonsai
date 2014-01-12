8be09ae412ee84b12e94613db9cff01756cc7503
glopes <glopes@localhost>
2014-01-12 18:45:48 +0000

    Added first draft of generic module unification across Vision and Dsp packages.

---
+++ Bonsai.Dsp/Abs.cs
@@ -0,0 +1,23 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace Bonsai.Dsp
+{
+    public class Abs : ArrayTransform
+    {
+        protected override IObservable<TArray> Process<TArray>(IObservable<TArray> source, Func<TArray, TArray> outputFactory)
+        {
+            return source.Select(input =>
+            {
+                var output = outputFactory(input);
+                CV.Abs(input, output);
+                return output;
+            });
+        }
+    }
+}
