7d3facdff98b49c3980dbe317ea87a4cd2588638
glopes <glopesdev@users.noreply.github.com>
2014-01-14 14:30:13 +0000

    Unified ConvertScale across Dsp and Vision packages.

--- Bonsai.Dsp/ConvertScale.cs
+++ Bonsai.Dsp/ConvertScale.cs
@@ -8,8 +8,10 @@ using System.ComponentModel;
 
 namespace Bonsai.Dsp
 {
-    [Description("Converts the input matrix into the specified bit depth, with optional linear transformation.")]
-    public class ConvertScale : Transform<Mat, Mat>
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Converts the input array into the specified bit depth, with optional linear transformation.")]
+    public class ConvertScale : ArrayTransform
     {
         public ConvertScale()
         {
@@ -17,23 +19,52 @@ namespace Bonsai.Dsp
             Scale = 1;
         }
 
-        [Description("The target bit depth of individual matrix elements.")]
+        [TypeConverter(typeof(DepthConverter))]
+        [Description("The target bit depth of individual array elements.")]
         public Depth Depth { get; set; }
 
-        [Description("The optional scale factor to apply to individual matrix elements.")]
+        [Description("The optional scale factor to apply to individual array elements.")]
         public double Scale { get; set; }
 
-        [Description("The optional value to be added to individual matrix elements.")]
+        [Description("The optional value to be added to individual array elements.")]
         public double Shift { get; set; }
 
-        public override IObservable<Mat> Process(IObservable<Mat> source)
+        public override IObservable<TArray> Process<TArray>(IObservable<TArray> source)
         {
+            var outputFactory = ArrFactory<TArray>.TemplateSizeChannelFactory;
             return source.Select(input =>
             {
-                var output = new Mat(input.Rows, input.Cols, Depth, input.Channels);
+                var output = outputFactory(input, Depth);
                 CV.ConvertScale(input, output, Scale, Shift);
                 return output;
             });
+        }
+
+        class DepthConverter : EnumConverter
+        {
+            public DepthConverter()
+                : base(typeof(Depth))
+            {
+            }
+
+            public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                return new StandardValuesCollection(new[]
+                {
+                    Depth.U8,
+                    Depth.S8,
+                    Depth.U16,
+                    Depth.S16,
+                    Depth.S32,
+                    Depth.F32,
+                    Depth.F64
+                });
+            }
+
+            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+            {
+                return true;
+            }
         }
     }
 }
