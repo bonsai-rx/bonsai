c660cee401e30efee1a8bdc2bb7a2daa04c8acd6
glopes <glopesdev@users.noreply.github.com>
2014-01-15 01:01:53 +0000

    Added missing file.

---
+++ Bonsai.Dsp/ArrHelper.cs
@@ -0,0 +1,73 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    static class ArrHelper
+    {
+        public static Mat EnsureFormat(Mat output, Size size, Depth depth, int channels)
+        {
+            if (output == null || output.Size != size || output.Depth != depth || output.Channels != channels)
+            {
+                if (output != null) output.Close();
+                return new Mat(size, depth, channels);
+            }
+
+            return output;
+        }
+
+        public static IplImage EnsureFormat(IplImage output, Size size, IplDepth depth, int channels)
+        {
+            if (output == null || output.Size != size || output.Depth != depth || output.Channels != channels)
+            {
+                if (output != null) output.Close();
+                return new IplImage(size, depth, channels);
+            }
+
+            return output;
+        }
+
+        public static IplImage EnsureColorCopy(IplImage output, IplImage image)
+        {
+            output = EnsureFormat(output, image.Size, image.Depth, 3);
+            if (image.Channels == 1) CV.CvtColor(image, output, ColorConversion.Gray2Bgr);
+            else CV.Copy(image, output);
+            return output;
+        }
+
+        public static Depth FromIplDepth(IplDepth depth)
+        {
+            const int ConversionOffset = (int)
+                (Depth.U8 +
+                ((int)Depth.U16 << 4) +
+                ((int)Depth.F32 << 8) +
+                ((int)Depth.F64 << 16) +
+                ((int)Depth.S8 << 20) +
+                ((int)Depth.S16 << 24) +
+                ((int)Depth.S32 << 28));
+            var depthShift = (((int)depth & 0xF0) >> 2) + (unchecked((int)depth & 0x80000000) != 0 ? 20 : 0);
+            return (Depth)((ConversionOffset >> depthShift) & 15);
+        }
+
+        public static IplDepth FromDepth(Depth depth)
+        {
+            switch (depth)
+            {
+                case Depth.U8: return IplDepth.U8;
+                case Depth.S8: return IplDepth.S8;
+                case Depth.U16: return IplDepth.U16;
+                case Depth.S16: return IplDepth.S16;
+                case Depth.S32: return IplDepth.S32;
+                case Depth.F32: return IplDepth.F32;
+                case Depth.F64: return IplDepth.F64;
+                case Depth.UserType:
+                default:
+                    throw new ArgumentException("Invalid depth type.", "depth");
+            }
+        }
+    }
+}
