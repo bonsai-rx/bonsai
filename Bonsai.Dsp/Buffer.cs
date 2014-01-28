9ac43b5d392a911d6ecd73cc3b2b07c1a9ab232c
glopes <glopes@localhost>
2014-01-28 19:27:28 +0000

    Added buffer module to Dsp package for packing numeric signals into matrix form.

---
+++ Bonsai.Dsp/Buffer.cs
@@ -0,0 +1,47 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class Buffer
+    {
+        public int Count { get; set; }
+
+        public IObservable<Mat> Process(IObservable<byte> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+
+        public IObservable<Mat> Process(IObservable<short> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+
+        public IObservable<Mat> Process(IObservable<ushort> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+
+        public IObservable<Mat> Process(IObservable<int> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+
+        public IObservable<Mat> Process(IObservable<float> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+
+        public IObservable<Mat> Process(IObservable<double> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
+        }
+    }
+}
