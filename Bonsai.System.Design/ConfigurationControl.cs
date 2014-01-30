aea3d88f7984682b75e74282c68c84bc30cf8b28
glopes <glopes@localhost>
2014-01-30 02:54:31 +0000

    Refactored serial configuration control into more general ConfigurationControl.

---
+++ Bonsai.System.Design/ConfigurationControl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.Design;
+using System.Collections.ObjectModel;
+using System.Windows.Forms.Design;
+using System.IO.Ports;
+using Bonsai.Design;
+
+namespace Bonsai.IO.Design
+{
+    public abstract partial class ConfigurationControl : UserControl
+    {
+        ConfigurationEditorService editorService;
+
+        public ConfigurationControl()
+        {
+            InitializeComponent();
+            editorService = new ConfigurationEditorService(this);
+
+            SuspendLayout();
+            foreach (var portName in GetConfigurationNames())
+            {
+                configurationNameListbox.Items.Add(portName);
+            }
+
+            configurationNameListbox.Height = configurationNameListbox.ItemHeight * (configurationNameListbox.Items.Count + 0);
+            Height = configurationNameListbox.PreferredHeight + configurationManagerButton.Height;
+            ResumeLayout();
+        }
+
+        public object SelectedValue
+        {
+            get { return configurationNameListbox.SelectedItem; }
+            set { configurationNameListbox.SelectedItem = value; }
+        }
+
+        public event EventHandler SelectedValueChanged;
+
+        private void OnSelectedValueChanged(EventArgs e)
+        {
+            var handler = SelectedValueChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected abstract IEnumerable<string> GetConfigurationNames();
+
+        protected abstract object LoadConfiguration();
+
+        protected abstract void SaveConfiguration(object configuration);
+
+        protected abstract CollectionEditor CreateConfigurationEditor(Type type);
+
+        private void configurationManagerButton_Click(object sender, EventArgs e)
+        {
+            var configuration = LoadConfiguration();
+            if (configuration == null)
+            {
+                throw new InvalidOperationException("Failed to load configuration.");
+            }
+
+            var configurationManager = CreateConfigurationEditor(configuration.GetType());
+            configurationManager.EditValue(editorService, editorService, configuration);
+            if (editorService.DialogResult == DialogResult.OK)
+            {
+                SaveConfiguration(configuration);
+            }
+        }
+
+        private void configurationNameListbox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            OnSelectedValueChanged(e);
+        }
+
+        class FlatButton : Button
+        {
+            public override void NotifyDefault(bool value)
+            {
+                base.NotifyDefault(false);
+            }
+        }
+    }
+}
