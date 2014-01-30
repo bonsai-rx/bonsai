2e2825aa399e61e68a58dec4e60335868a1d7c7e
glopes <glopesdev@users.noreply.github.com>
2014-01-30 23:48:40 +0000

    Extended node editor action to launch modal editor of default property.

---
+++ Bonsai.Editor/GraphView/WorkflowGraphViewEditorService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace Bonsai.Design
+{
+    class WorkflowGraphViewEditorService : IWindowsFormsEditorService, IServiceProvider
+    {
+        Control ownerControl;
+        IServiceProvider serviceProvider;
+
+        internal WorkflowGraphViewEditorService(Control owner, IServiceProvider provider)
+        {
+            ownerControl = owner;
+            serviceProvider = provider;
+        }
+
+        public void CloseDropDown()
+        {
+        }
+
+        public void DropDownControl(Control control)
+        {
+        }
+
+        public DialogResult ShowDialog(Form dialog)
+        {
+            return dialog.ShowDialog(ownerControl);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(IWindowsFormsEditorService))
+            {
+                return this;
+            }
+
+            if (serviceProvider != null)
+            {
+                return serviceProvider.GetService(serviceType);
+            }
+
+            return null;
+        }
+    }
+}
