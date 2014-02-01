e7ac927b24e5ad45e815ecf2db6b474d3934c275
glopes <glopes@localhost>
2014-02-01 17:10:14 +0000

    Generalized binary operator to allow any type to specify constant operands.

--- Bonsai.Core/Expressions/BinaryOperatorBuilder.cs
+++ Bonsai.Core/Expressions/BinaryOperatorBuilder.cs
@@ -16,49 +16,12 @@ namespace Bonsai.Expressions
     [TypeDescriptionProvider(typeof(BinaryOperatorTypeDescriptionProvider))]
     public abstract class BinaryOperatorBuilder : SelectBuilder
     {
-        Func<object> getter;
-        Action<object> setter;
-        WorkflowProperty property;
-        static readonly MethodInfo initializePropertyMethod = typeof(BinaryOperatorBuilder).GetMethod("InitializeProperty", BindingFlags.Instance | BindingFlags.NonPublic);
-
         /// <summary>
         /// Gets or sets the value which will be paired with elements of the observable
         /// sequence in case the sequence itself is not composed of paired elements.
         /// </summary>
         [Browsable(false)]
-        public object Value
-        {
-            get
-            {
-                if (property == null) return null;
-                return getter();
-            }
-            set
-            {
-                if (property == null && value != null)
-                {
-                    initializePropertyMethod.MakeGenericMethod(value.GetType()).Invoke(this, null);
-                }
-
-                if (property != null)
-                {
-                    setter(value);
-                }
-            }
-        }
-
-        internal WorkflowProperty Property
-        {
-            get { return property; }
-        }
-
-        void InitializeProperty<TValue>()
-        {
-            var typedProperty = new WorkflowProperty<TValue>();
-            getter = () => typedProperty.Value;
-            setter = value => typedProperty.Value = (TValue)value;
-            property = typedProperty;
-        }
+        public WorkflowProperty Operand { get; set; }
 
         /// <summary>
         /// When overridden in a derived class, returns the expression that applies a binary
@@ -85,9 +48,7 @@ namespace Bonsai.Expressions
             Expression left, right;
             if (expression.Type.IsGenericType && expression.Type.GetGenericTypeDefinition() == typeof(Tuple<,>))
             {
-                getter = null;
-                setter = null;
-                property = null;
+                Operand = null;
                 left = ExpressionHelper.MemberAccess(expression, "Item1");
                 right = ExpressionHelper.MemberAccess(expression, "Item2");
                 if (left.Type != right.Type && left.Type.IsPrimitive && right.Type.IsPrimitive)
@@ -97,26 +58,52 @@ namespace Bonsai.Expressions
                     else right = Expression.Convert(right, left.Type);
                 }
             }
-            else if (expression.Type.IsPrimitive || expression.Type == typeof(string))
+            else
             {
-                if (property == null || property.GetType().GetGenericArguments()[0] != expression.Type)
+                var operand = Operand;
+                if (operand == null || operand.PropertyType != expression.Type)
                 {
-                    initializePropertyMethod.MakeGenericMethod(expression.Type).Invoke(this, null);
+                    var propertyType = GetPropertyType(expression.Type);
+                    Operand = operand = (WorkflowProperty)Activator.CreateInstance(propertyType);
                 }
 
                 left = expression;
-                var valueProperty = Expression.Constant(property);
-                right = Expression.Property(valueProperty, "Value");
-            }
-            else
-            {
-                getter = null;
-                setter = null;
-                property = null;
-                throw new InvalidOperationException("The input to this binary operator must be an element pair.");
+                var operandExpression = Expression.Constant(operand);
+                right = Expression.Property(operandExpression, "Value");
             }
 
             return BuildSelector(left, right);
+        }
+
+        Type GetPropertyType(Type expressionType)
+        {
+            if (expressionType == typeof(DateTimeOffset)) return typeof(DateTimeOffsetProperty);
+            if (expressionType == typeof(TimeSpan)) return typeof(TimeSpanProperty);
+
+            var typeCode = Type.GetTypeCode(expressionType);
+            switch (typeCode)
+            {
+                case TypeCode.Boolean: return typeof(BooleanProperty);
+                case TypeCode.DateTime: return typeof(DateTimeProperty);
+                case TypeCode.Double: return typeof(DoubleProperty);
+                case TypeCode.Int32: return typeof(IntProperty);
+                case TypeCode.Single: return typeof(FloatProperty);
+                case TypeCode.String: return typeof(StringProperty);
+                case TypeCode.Byte:
+                case TypeCode.Char:
+                case TypeCode.DBNull:
+                case TypeCode.Decimal:
+                case TypeCode.Empty:
+                case TypeCode.Int16:
+                case TypeCode.Int64:
+                case TypeCode.Object:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                default:
+                    return typeof(WorkflowProperty<>).MakeGenericType(expressionType);
+            }
         }
     }
 }
