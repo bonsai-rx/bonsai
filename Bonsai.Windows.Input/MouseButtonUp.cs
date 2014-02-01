0964ebdbd8cfb94d2c95b608de02b86d1c2dcdb5
glopes <glopes@localhost>
2014-02-01 16:57:53 +0000

    Added windows input module to handle basic low-level keyboard and mouse input.

---
+++ Bonsai.Windows.Input/MouseButtonUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonsai.Windows.Input
+{
+    public class MouseButtonUp : Source<MouseButtons>
+    {
+        public override IObservable<MouseButtons> Generate()
+        {
+            return InterceptMouse.Instance.MouseButtonUp;
+        }
+    }
+}
---
+++ Bonsai.Windows.Input/MouseWheel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Windows.Input
+{
+    public class MouseWheel : Source<int>
+    {
+        public override IObservable<int> Generate()
+        {
+            return InterceptMouse.Instance.MouseWheel;
+        }
+    }
+}
