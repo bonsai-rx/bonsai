ec442779a8412cfce651919232636c272dad3d24
glopes <glopesdev@users.noreply.github.com>
2014-02-12 05:05:42 +0000

    Added first draft of function generator module for Dsp package.

---
+++ Bonsai.Dsp/FunctionGenerator.cs
@@ -0,0 +1,94 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Design;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    public class FunctionGenerator : Source<Mat>
+    {
+        public FunctionGenerator()
+        {
+            PlaybackRate = 100;
+            BufferLength = 441;
+            Frequency = 1;
+        }
+
+        public int BufferLength { get; set; }
+
+        [Range(1, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
+        public int Frequency { get; set; }
+
+        public FunctionWaveform Waveform { get; set; }
+
+        public int PlaybackRate { get; set; }
+
+        public override IObservable<Mat> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var stopwatch = new Stopwatch();
+                var buffer = new double[BufferLength];
+                return ObservableCombinators.GenerateWithThread<Mat>(observer =>
+                {
+                    stopwatch.Restart();
+                    var frequency = Frequency;
+                    var period = buffer.Length / frequency;
+                    var indexScale = 1.0 / buffer.Length;
+                    var waveform = Waveform;
+                    switch (waveform)
+                    {
+                        default:
+                        case FunctionWaveform.Sine:
+                            indexScale = 2 * Math.PI * indexScale;
+                            for (int i = 0; i < buffer.Length; i++)
+                            {
+                                buffer[i] = Math.Sin(frequency * i * indexScale);
+                            }
+                            break;
+                        case FunctionWaveform.Triangular:
+                            for (int i = 0; i < buffer.Length; i++)
+                            {
+                                var t = frequency * (i + period / 4) * indexScale;
+                                buffer[i] = 1 - (4 * Math.Abs((t % 1) - 0.5) - 1);
+                            }
+                            break;
+                        case FunctionWaveform.Square:
+                        case FunctionWaveform.Sawtooth:
+                            for (int i = 0; i < buffer.Length; i++)
+                            {
+                                var t = frequency * (i + period / 2) * indexScale;
+                                buffer[i] = 2 * (t % 1) - 1;
+                                if (waveform == FunctionWaveform.Square)
+                                {
+                                    buffer[i] = Math.Sign(buffer[i]);
+                                }
+                            }
+                            break;
+                    }
+
+                    observer.OnNext(Mat.FromArray(buffer));
+                    var playbackRate = PlaybackRate;
+                    if (playbackRate <= 0)
+                    {
+                        throw new InvalidOperationException("Playback rate must be a positive integer.");
+                    }
+
+                    var dueTime = Math.Max(0, (1000.0 / playbackRate) - stopwatch.Elapsed.TotalMilliseconds);
+                    if (dueTime > 0)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(dueTime));
+                    }
+                });
+            });
+        }
+    }
+}
