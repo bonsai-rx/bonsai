8367fafc461f85188cd85fcaf76939f1308024ca
glopes <glopesdev@users.noreply.github.com>
2014-02-17 19:40:52 +0000

    Updated Undistort module to allow for online editing of distortion parameters.

--- Bonsai.Vision/Undistort.cs
+++ Bonsai.Vision/Undistort.cs
@@ -10,30 +10,99 @@ namespace Bonsai.Vision
 {
     public class Undistort : Transform<IplImage, IplImage>
     {
+        Point2d focalLength;
+        Point2d principalPoint;
+        Point3d radialDistortion;
+        Point2d tangentialDistortion;
+        Mat intrinsics;
+        Mat distortion;
+
         public Undistort()
         {
-            Distortion = new double[5];
+            UpdateIntrinsics();
+            UpdateDistortion();
         }
 
-        public Point2d FocalLength { get; set; }
+        public Point2d FocalLength
+        {
+            get { return focalLength; }
+            set
+            {
+                focalLength = value;
+                UpdateIntrinsics();
+            }
+        }
 
-        public Point2d PrincipalPoint { get; set; }
+        public Point2d PrincipalPoint
+        {
+            get { return principalPoint; }
+            set
+            {
+                principalPoint = value;
+                UpdateIntrinsics();
+            }
+        }
 
-        public double[] Distortion { get; set; }
+        public Point3d RadialDistortion
+        {
+            get { return radialDistortion; }
+            set
+            {
+                radialDistortion = value;
+                UpdateDistortion();
+            }
+        }
+
+        public Point2d TangentialDistortion
+        {
+            get { return tangentialDistortion; }
+            set
+            {
+                tangentialDistortion = value;
+                UpdateDistortion();
+            }
+        }
+
+        void UpdateIntrinsics()
+        {
+            intrinsics = Mat.FromArray(new double[,]
+            {
+                {focalLength.X, 0, principalPoint.X},
+                {0, focalLength.Y, principalPoint.Y},
+                {0, 0, 1}
+            });
+        }
+
+        void UpdateDistortion()
+        {
+            distortion = Mat.FromArray(new[]
+            {
+                radialDistortion.X,
+                radialDistortion.Y,
+                tangentialDistortion.X,
+                tangentialDistortion.Y,
+                radialDistortion.Z
+            });
+        }
 
         public override IObservable<IplImage> Process(IObservable<IplImage> source)
         {
             return Observable.Defer(() =>
             {
-                var intrinsics = new double[] { FocalLength.X, 0, PrincipalPoint.X, 0, FocalLength.Y, PrincipalPoint.Y, 0, 0, 1 };
-                var cameraMatrix = new Mat(3, 3, Depth.F64, 1);
-                Marshal.Copy(intrinsics, 0, cameraMatrix.Data, intrinsics.Length);
-
-                var distortionCoefficients = new Mat(5, 1, Depth.F64, 1);
-                Marshal.Copy(Distortion, 0, distortionCoefficients.Data, Distortion.Length);
-
+                Mat cameraMatrix = null;
+                Mat distortionCoefficients = null;
                 return source.Select(input =>
                 {
+                    if (cameraMatrix != intrinsics)
+                    {
+                        cameraMatrix = intrinsics;
+                    }
+
+                    if (distortionCoefficients != distortion)
+                    {
+                        distortionCoefficients = distortion;
+                    }
+
                     var output = new IplImage(input.Size, input.Depth, input.Channels);
                     CV.Undistort2(input, output, cameraMatrix, distortionCoefficients);
                     return output;
