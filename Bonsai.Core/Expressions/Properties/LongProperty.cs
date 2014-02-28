dc4e919df4c92a4c60b114da35f70ec749faf145
glopes <glopesdev@users.noreply.github.com>
2014-02-28 12:10:15 +0000

    Added LongProperty to allow specification of 64-bit integer properties.

---
+++ Bonsai.Core/Expressions/Properties/LongProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions
+{
+    /// <summary>
+    /// Represents a workflow property containing a 64-bit signed integer.
+    /// </summary>
+    [DisplayName("Long")]
+    [XmlType(Namespace = Constants.XmlNamespace)]
+    [Description("Represents a workflow property containing a 64-bit signed integer.")]
+    public class LongProperty : WorkflowProperty<long>
+    {
+    }
+}
