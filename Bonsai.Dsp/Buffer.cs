ec24ffd1863373cc9ec41f0e37c09cfd47cde4be
glopes <glopes@localhost>
2014-02-28 12:45:06 +0000

    Generalized Buffer to create multichannel arrays from sequences of tuple values.

--- Bonsai.Dsp/Buffer.cs
+++ Bonsai.Dsp/Buffer.cs
@@ -14,6 +14,8 @@ namespace Bonsai.Dsp
     {
         public int Count { get; set; }
 
+        #region One Channel
+
         public IObservable<Mat> Process(IObservable<byte> source)
         {
             return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
@@ -43,5 +45,298 @@ namespace Bonsai.Dsp
         {
             return source.Buffer(Count).Select(buffer => Mat.FromArray(buffer.ToArray()));
         }
+
+        #endregion
+
+        #region Two Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue>> source)
+        {
+            var output = new TValue[2, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
+
+        #region Three Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue, TValue>> source)
+        {
+            var output = new TValue[3, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+                output[2, i] = tuple.Item3;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
+
+        #region Four Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue, TValue, TValue>> source)
+        {
+            var output = new TValue[4, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+                output[2, i] = tuple.Item3;
+                output[3, i] = tuple.Item4;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte, byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short, short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort, ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int, int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float, float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double, double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
+
+        #region Five Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue, TValue, TValue, TValue>> source)
+        {
+            var output = new TValue[5, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+                output[2, i] = tuple.Item3;
+                output[3, i] = tuple.Item4;
+                output[4, i] = tuple.Item5;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte, byte, byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short, short, short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort, ushort, ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int, int, int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float, float, float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double, double, double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
+
+        #region Six Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue, TValue, TValue, TValue, TValue>> source)
+        {
+            var output = new TValue[6, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+                output[2, i] = tuple.Item3;
+                output[3, i] = tuple.Item4;
+                output[4, i] = tuple.Item5;
+                output[5, i] = tuple.Item6;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte, byte, byte, byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short, short, short, short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort, ushort, ushort, ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int, int, int, int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float, float, float, float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double, double, double, double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
+
+        #region Seven Channels
+
+        TValue[,] ToArray<TValue>(IList<Tuple<TValue, TValue, TValue, TValue, TValue, TValue, TValue>> source)
+        {
+            var output = new TValue[7, source.Count];
+            for (int i = 0; i < source.Count; i++)
+            {
+                var tuple = source[i];
+                output[0, i] = tuple.Item1;
+                output[1, i] = tuple.Item2;
+                output[2, i] = tuple.Item3;
+                output[3, i] = tuple.Item4;
+                output[4, i] = tuple.Item5;
+                output[5, i] = tuple.Item6;
+                output[6, i] = tuple.Item7;
+            }
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<byte, byte, byte, byte, byte, byte, byte>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<short, short, short, short, short, short, short>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<ushort, ushort, ushort, ushort, ushort, ushort, ushort>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<int, int, int, int, int, int, int>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<float, float, float, float, float, float, float>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        public IObservable<Mat> Process(IObservable<Tuple<double, double, double, double, double, double, double>> source)
+        {
+            return source.Buffer(Count).Select(buffer => Mat.FromArray(ToArray(buffer)));
+        }
+
+        #endregion
     }
 }
