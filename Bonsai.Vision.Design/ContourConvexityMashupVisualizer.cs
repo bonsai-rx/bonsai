6a25dbe8d3411392d3dd7d207107f4d87f7616dc
glopes <glopesdev@users.noreply.github.com>
2014-03-02 02:29:08 +0000

    Added support for convexity defect analysis of connected component contours.

---
+++ Bonsai.Vision.Design/ContourConvexityMashupVisualizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Design;
+using Bonsai;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+
+[assembly: TypeVisualizer(typeof(ContourConvexityMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, ContourConvexityVisualizer>))]
+[assembly: TypeVisualizer(typeof(ContourConvexityMashupVisualizer), Target = typeof(VisualizerMashup<ContoursVisualizer, ContourConvexityVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class ContourConvexityMashupVisualizer : MashupTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            var image = visualizer.VisualizerImage;
+            var contourConvexity = (ContourConvexity)value;
+            var contour = contourConvexity.Contour;
+
+            if (image != null && contour != null)
+            {
+                CV.DrawContours(image, contourConvexity.ConvexHull, Scalar.Rgb(0, 255, 0), Scalar.All(0), 0);
+                DrawingHelper.DrawConvexityDefects(image, contourConvexity.ConvexityDefects, Scalar.Rgb(204, 0, 204));
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
