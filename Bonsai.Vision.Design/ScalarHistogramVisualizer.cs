8c69b3f8ac8a5d39b00b626470521915d728b4a8
glopes <glopesdev@users.noreply.github.com>
2014-03-03 04:36:02 +0000

    Added support for computing image histograms.

---
+++ Bonsai.Vision.Design/ScalarHistogramVisualizer.cs
@@ -0,0 +1,121 @@
+﻿using Bonsai;
+using Bonsai.Design;
+using Bonsai.Design.Visualizers;
+using Bonsai.Vision;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+[assembly: TypeVisualizer(typeof(ScalarHistogramVisualizer), Target = typeof(ScalarHistogram))]
+
+namespace Bonsai.Vision.Design
+{
+    public class ScalarHistogramVisualizer : DialogTypeVisualizer
+    {
+        static readonly TimeSpan TargetElapsedTime = TimeSpan.FromSeconds(1.0 / 30);
+
+        CurveItem val0;
+        CurveItem val1;
+        CurveItem val2;
+        CurveItem val3;
+        ChartControl chart;
+        DateTimeOffset updateTime;
+
+        CurveItem CreateHistogramCurve(Color color)
+        {
+            var series = new LineItem(string.Empty, new PointPairList(), color, SymbolType.None);
+            series.Line.IsAntiAlias = true;
+            series.Line.IsOptimizedDraw = true;
+            series.Label.IsVisible = false;
+            chart.GraphPane.CurveList.Add(series);
+            return series;
+        }
+
+        void UpdateHistogramValues(Histogram histogram, CurveItem values)
+        {
+            values.Clear();
+            for (int i = 0; i < 256; i++)
+            {
+                values.AddPoint(i, histogram.QueryValue(i));
+            }
+        }
+
+        void UpdateCurveItem(Histogram histogram, Color color, ref CurveItem values)
+        {
+            if (histogram == null)
+            {
+                chart.GraphPane.CurveList.Remove(values);
+                values = null;
+            }
+            else
+            {
+                if (values == null)
+                {
+                    values = CreateHistogramCurve(color);
+                }
+
+                UpdateHistogramValues(histogram, values);
+            }
+        }
+
+        void UpdateValues(ScalarHistogram value)
+        {
+            if (value == null)
+            {
+                chart.GraphPane.CurveList.Clear();
+                val0 = val1 = val2 = val3 = null;
+                return;
+            }
+
+            UpdateCurveItem(value.Val0, Color.Blue, ref val0);
+            UpdateCurveItem(value.Val1, Color.Green, ref val1);
+            UpdateCurveItem(value.Val2, Color.Red, ref val2);
+            UpdateCurveItem(value.Val3, Color.Magenta, ref val3);
+        }
+
+        public override void Show(object value)
+        {
+            var time = DateTimeOffset.Now;
+            if ((time - updateTime) > TargetElapsedTime)
+            {
+                var histogram = (ScalarHistogram)value;
+                UpdateValues(histogram);
+                chart.Invalidate();
+                updateTime = time;
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            chart = new ChartControl();
+            chart.Dock = DockStyle.Fill;
+            chart.GraphPane.XAxis.Type = AxisType.Linear;
+            chart.GraphPane.XAxis.Title.Text = "Intensity";
+            chart.GraphPane.XAxis.Title.IsVisible = true;
+            chart.GraphPane.XAxis.MinorTic.IsAllTics = false;
+
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(chart);
+            }
+        }
+
+        public override void Unload()
+        {
+            chart.Dispose();
+            chart = null;
+            val0 = null;
+            val1 = null;
+            val2 = null;
+            val3 = null;
+        }
+    }
+}
---
+++ Bonsai.Vision/ImageHistogram.cs
@@ -0,0 +1,42 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    public class ImageHistogram : Transform<IplImage, ScalarHistogram>
+    {
+        static Histogram ComputeChannelHistogram(IplImage channel)
+        {
+            var histogram = new Histogram(1, new[] { 256 }, HistogramType.Array, new[] { new[] { 0, 256f } });
+            histogram.CalcArrHist(new[] { channel });
+            histogram.Normalize(1);
+            return histogram;
+        }
+
+        public override IObservable<ScalarHistogram> Process(IObservable<IplImage> source)
+        {
+            return source.Select(input =>
+            {
+                var c0 = input.Channels > 1 ? new IplImage(input.Size, input.Depth, 1) : input;
+                var c1 = input.Channels > 1 ? new IplImage(input.Size, input.Depth, 1) : null;
+                var c2 = input.Channels > 2 ? new IplImage(input.Size, input.Depth, 1) : null;
+                var c3 = input.Channels > 3 ? new IplImage(input.Size, input.Depth, 1) : null;
+                if (input.Channels > 1)
+                {
+                    CV.Split(input, c0, c1, c2, c3);
+                }
+
+                var h0 = ComputeChannelHistogram(c0);
+                var h1 = c1 != null ? ComputeChannelHistogram(c1) : null;
+                var h2 = c2 != null ? ComputeChannelHistogram(c2) : null;
+                var h3 = c3 != null ? ComputeChannelHistogram(c3) : null;
+                return new ScalarHistogram(h0, h1, h2, h3);
+            });
+        }
+    }
+}
