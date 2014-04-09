45f3ce28c8f78754604f6afd31191f27d1ba0afe
glopes <glopes@localhost>
2014-04-09 01:24:19 +0100

    Added Exp, Log and DiscreteFourierTransform modules to Dsp package.

---
+++ Bonsai.Dsp/DiscreteFourierTransform.cs
@@ -0,0 +1,34 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    public class DiscreteFourierTransform : ArrayTransform
+    {
+        public DiscreteTransformFlags OperationFlags { get; set; }
+
+        public override IObservable<TArray> Process<TArray>(IObservable<TArray> source)
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateSizeFactory;
+            var inputFactory = ArrFactory<TArray>.TemplateSizeChannelFactory;
+            return source.Select(input =>
+            {
+                var output = outputFactory(input, Depth.F32, 2);
+                if (input.ElementType != output.ElementType)
+                {
+                    var temp = inputFactory(input, Depth.F32);
+                    CV.Convert(input, temp);
+                    input = temp;
+                }
+
+                CV.DFT(input, output, OperationFlags, 0);
+                return output;
+            });
+        }
+    }
+}
---
+++ Bonsai.Dsp/Exp.cs
@@ -0,0 +1,24 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace Bonsai.Dsp
+{
+    public class Exp : ArrayTransform
+    {
+        public override IObservable<TArray> Process<TArray>(IObservable<TArray> source)
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateFactory;
+            return source.Select(input =>
+            {
+                var output = outputFactory(input);
+                CV.Exp(input, output);
+                return output;
+            });
+        }
+    }
+}
