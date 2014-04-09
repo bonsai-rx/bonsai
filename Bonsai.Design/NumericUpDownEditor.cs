d4c2676c4d85bff22ecf23bab7a73efb10d3a1ab
glopes <glopes@localhost>
2014-04-09 19:08:13 +0100

    Updated NumericUpDownEditor to set range of edited value and allow cancellation.

--- Bonsai.Design/NumericUpDownEditor.cs
+++ Bonsai.Design/NumericUpDownEditor.cs
@@ -16,6 +16,15 @@ namespace Bonsai.Design
             return UITypeEditorEditStyle.DropDown;
         }
 
+        class PreviewNumericUpDown : NumericUpDown
+        {
+            protected override bool ProcessDialogKey(Keys keyData)
+            {
+                if (keyData == Keys.Escape) OnPreviewKeyDown(new PreviewKeyDownEventArgs(keyData));
+                return base.ProcessDialogKey(keyData);
+            }
+        }
+
         public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
         {
             var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
@@ -31,7 +40,7 @@ namespace Bonsai.Design
                     decimalPlaces = precision.DecimalPlaces;
                 }
 
-                var numericUpDown = new NumericUpDown();
+                var numericUpDown = new PreviewNumericUpDown();
                 numericUpDown.Minimum = range.Minimum;
                 numericUpDown.Maximum = range.Maximum;
                 numericUpDown.DecimalPlaces = decimalPlaces;
@@ -40,10 +49,19 @@ namespace Bonsai.Design
                     numericUpDown.Increment = precision.Increment;
                 }
 
-                numericUpDown.Value = Convert.ToDecimal(value);
-                numericUpDown.ValueChanged += (sender, e) => propertyDescriptor.SetValue(context.Instance, Convert.ChangeType(numericUpDown.Value, propertyDescriptor.PropertyType));
+                var changed = false;
+                var cancelled = false;
+                numericUpDown.Value = Math.Max(numericUpDown.Minimum, Math.Min(Convert.ToDecimal(value), numericUpDown.Maximum));
+                numericUpDown.PreviewKeyDown += (sender, e) => cancelled = e.KeyCode == Keys.Escape;
+                numericUpDown.ValueChanged += (sender, e) =>
+                {
+                    changed = true;
+                    propertyDescriptor.SetValue(context.Instance, Convert.ChangeType(numericUpDown.Value, propertyDescriptor.PropertyType));
+                };
                 editorService.DropDownControl(numericUpDown);
-                return Convert.ChangeType(numericUpDown.Value, propertyDescriptor.PropertyType);
+
+                if (cancelled && changed) propertyDescriptor.SetValue(context.Instance, value);
+                return cancelled ? value : Convert.ChangeType(numericUpDown.Value, propertyDescriptor.PropertyType);
             }
 
             return base.EditValue(context, provider, value);
