f0beeff6e3ffd2890200a560a0c5d4e0a4ebf104
glopes <glopesdev@users.noreply.github.com>
2014-04-09 01:24:19 +0100

    Added Exp, Log and DiscreteFourierTransform modules to Dsp package.

---
+++ Bonsai.Dsp/Log.cs
@@ -0,0 +1,24 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace Bonsai.Dsp
+{
+    public class Log : ArrayTransform
+    {
+        public override IObservable<TArray> Process<TArray>(IObservable<TArray> source)
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateFactory;
+            return source.Select(input =>
+            {
+                var output = outputFactory(input);
+                CV.Log(input, output);
+                return output;
+            });
+        }
+    }
+}
