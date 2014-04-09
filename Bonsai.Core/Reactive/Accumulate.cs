4bf0bbf5cef8c7133c7fe4b5c2c047a2bbc4581e
glopes <glopes@localhost>
2014-04-09 22:31:48 +0100

    Added Accumulate combinator to Core package.

---
+++ Bonsai.Core/Reactive/Accumulate.cs
@@ -0,0 +1,46 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Reactive
+{
+    /// <summary>
+    /// Represents a combinator that computes the cumulative sum of an observable sequence
+    /// and returns each intermediate result.
+    /// </summary>
+    [XmlType("Accumulate", Namespace = Constants.XmlNamespace)]
+    [Description("Computes the cumulative sum of an observable sequence and returns each intermediate result.")]
+    public class AccumulateBuilder : SingleArgumentExpressionBuilder
+    {
+        static readonly MethodInfo scanMethod = typeof(Observable).GetMethods()
+                                                                  .Single(m => m.Name == "Scan" &&
+                                                                          m.GetParameters().Length == 2);
+
+        /// <summary>
+        /// Generates an <see cref="Expression"/> node from a collection of input arguments.
+        /// The result can be chained with other builders in a workflow.
+        /// </summary>
+        /// <param name="arguments">
+        /// A collection of <see cref="Expression"/> nodes that represents the input arguments.
+        /// </param>
+        /// <returns>An <see cref="Expression"/> tree node.</returns>
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            var source = arguments.Single();
+            var parameterType = source.Type.GetGenericArguments()[0];
+            var accumulatorParameter = Expression.Parameter(parameterType);
+            var currentParameter = Expression.Parameter(parameterType);
+            var accumulatorBody = Expression.Add(accumulatorParameter, currentParameter);
+            var accumulator = Expression.Lambda(accumulatorBody, accumulatorParameter, currentParameter);
+            return Expression.Call(scanMethod.MakeGenericMethod(parameterType), source, accumulator);
+        }
+    }
+}
