9d9a91576536bf71947fd7af64cc04a6eb3e1ac9
glopes <glopes@localhost>
2014-04-09 19:11:24 +0100

    Updated Smooth module with working default parameters and editor attributes.

--- Bonsai.Vision/Smooth.cs
+++ Bonsai.Vision/Smooth.cs
@@ -4,19 +4,39 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using OpenCV.Net;
+using System.ComponentModel;
+using System.Drawing.Design;
 
 namespace Bonsai.Vision
 {
     public class Smooth : Transform<IplImage, IplImage>
     {
+        public Smooth()
+        {
+            Size1 = 3;
+            SmoothType = SmoothMethod.Gaussian;
+        }
+
         public SmoothMethod SmoothType { get; set; }
 
+        [Precision(0, 2)]
+        [Range(1, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
         public int Size1 { get; set; }
 
+        [Precision(0, 2)]
+        [Range(1, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
         public int Size2 { get; set; }
 
+        [Precision(2, 0.1)]
+        [Range(0, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
         public double Sigma1 { get; set; }
 
+        [Precision(2, 0.1)]
+        [Range(0, int.MaxValue)]
+        [Editor(DesignTypes.NumericUpDownEditor, typeof(UITypeEditor))]
         public double Sigma2 { get; set; }
 
         public override IObservable<IplImage> Process(IObservable<IplImage> source)
