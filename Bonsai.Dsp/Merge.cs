d701b1de52a01305c0928c8f03f3c8bfb1502226
glopes <glopesdev@users.noreply.github.com>
2014-04-09 01:08:38 +0100

    Moved Merge and Split into Dsp package for handling both Mat and IplImage.

---
+++ Bonsai.Dsp/Merge.cs
@@ -0,0 +1,54 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class Merge
+    {
+        static TArray Process<TArray>(
+            TArray item1,
+            TArray item2,
+            TArray item3,
+            TArray item4,
+            Func<TArray, int, TArray> outputFactory)
+            where TArray : Arr
+        {
+            var template = item1 ?? item2 ?? item3 ?? item4;
+            if (template == null) return null;
+
+            var channels = 0;
+            if (item1 != null) channels++;
+            if (item2 != null) channels++;
+            if (item3 != null) channels++;
+            if (item4 != null) channels++;
+            var output = outputFactory(template, channels);
+            CV.Merge(item1, item2, item3, item4, output);
+            return output;
+        }
+
+        public IObservable<TArray> Process<TArray>(IObservable<Tuple<TArray, TArray>> source) where TArray : Arr
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateSizeDepthFactory;
+            return source.Select(input => Process(input.Item1, input.Item2, null, null, outputFactory));
+        }
+
+        public IObservable<TArray> Process<TArray>(IObservable<Tuple<TArray, TArray, TArray>> source) where TArray : Arr
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateSizeDepthFactory;
+            return source.Select(input => Process(input.Item1, input.Item2, input.Item3, null, outputFactory));
+        }
+
+        public IObservable<TArray> Process<TArray>(IObservable<Tuple<TArray, TArray, TArray, TArray>> source) where TArray : Arr
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateSizeDepthFactory;
+            return source.Select(input => Process(input.Item1, input.Item2, input.Item3, input.Item4, outputFactory));
+        }
+    }
+}
