c50b27393931d326dc14b7b6115871ff41fa9eb7
glopes <glopes@localhost>
2014-04-09 01:11:26 +0100

    Added submatrix module to Dsp package.

---
+++ Bonsai.Dsp/Submatrix.cs
@@ -0,0 +1,30 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    public class Submatrix : Transform<Mat, Mat>
+    {
+        public int StartRow { get; set; }
+
+        public int? EndRow { get; set; }
+
+        public int StartCol { get; set; }
+
+        public int? EndCol { get; set; }
+
+        public override IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return source.Select(input =>
+            {
+                return input.GetRows(StartRow, EndRow.GetValueOrDefault(input.Rows))
+                            .GetCols(StartCol, EndCol.GetValueOrDefault(input.Cols));
+            });
+        }
+    }
+}
