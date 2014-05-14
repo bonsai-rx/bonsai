040aee641b9e1cfe73566345f35a9882f8dd9b14
glopes <glopesdev@users.noreply.github.com>
2014-05-14 01:42:53 +0100

    Updated version numbers for modified packages.

--- Bonsai/Properties/AssemblyInfo.cs
+++ Bonsai/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
