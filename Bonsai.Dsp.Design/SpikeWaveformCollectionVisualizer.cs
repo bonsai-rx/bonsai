0d4b8c2324ac8f2d012471f8bab5037940c1ff85
glopes <glopesdev@users.noreply.github.com>
2014-07-30 01:48:26 +0100

    Added first draft of spike detection combinator.

---
+++ Bonsai.Dsp.Design/SpikeWaveformCollectionVisualizer.cs
@@ -0,0 +1,48 @@
+﻿using Bonsai;
+using Bonsai.Dsp;
+using Bonsai.Dsp.Design;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(SpikeWaveformCollectionVisualizer), Target = typeof(SpikeWaveformCollection))]
+
+namespace Bonsai.Dsp.Design
+{
+    public class SpikeWaveformCollectionVisualizer : MatVisualizer
+    {
+        public SpikeWaveformCollectionVisualizer()
+        {
+            OverlayChannels = false;
+        }
+
+        public override void Show(object value)
+        {
+            var spikes = (SpikeWaveformCollection)value;
+            var bufferSize = spikes.BufferSize;
+            double[] samples = null;
+            foreach (var spike in spikes)
+            {
+                if (samples == null)
+                {
+                    samples = new double[spike.Waveform.Cols];
+                }
+
+                using (var sampleHeader = Mat.CreateMatHeader(samples))
+                {
+                    CV.Convert(spike.Waveform, sampleHeader);
+                }
+
+                Graph.UpdateWaveform(spike.ChannelIndex, samples, bufferSize.Height, samples.Length);
+            }
+
+            if (spikes.Count > 0)
+            {
+                InvalidateGraph();
+            }
+        }
+    }
+}
---
+++ Bonsai.Dsp/DetectSpikes.cs
@@ -0,0 +1,100 @@
+﻿using Bonsai.Dsp;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    public class DetectSpikes : Combinator<Mat, SpikeWaveformCollection>
+    {
+        readonly Delay delay = new Delay();
+
+        public int Delay
+        {
+            get { return delay.Count; }
+            set { delay.Count = value; }
+        }
+
+        public int Length { get; set; }
+
+        public int Threshold { get; set; }
+
+        public override IObservable<SpikeWaveformCollection> Process(IObservable<Mat> source)
+        {
+            return Observable.Defer(() =>
+            {
+                byte[] triggerBuffer = null;
+                bool[] activeChannels = null;
+                SampleBuffer[] activeSpikes = null;
+                return source.Publish(ps => ps.Zip(delay.Process(ps), (input, delayed) =>
+                {
+                    var spikes = new SpikeWaveformCollection(input.Size);
+                    if (activeSpikes == null)
+                    {
+                        triggerBuffer = new byte[input.Cols];
+                        activeChannels = new bool[input.Rows];
+                        activeSpikes = new SampleBuffer[input.Rows];
+                    }
+
+                    for (int i = 0; i < activeSpikes.Length; i++)
+                    {
+                        using (var channel = input.GetRow(i))
+                        using (var delayedChannel = delayed.GetRow(i))
+                        {
+                            if (activeSpikes[i] != null)
+                            {
+                                var buffer = activeSpikes[i];
+                                buffer.Update(delayedChannel, 0);
+                                if (buffer.Completed)
+                                {
+                                    spikes.Add(new SpikeWaveform
+                                    {
+                                        ChannelIndex = i,
+                                        SampleIndex = buffer.SampleIndex,
+                                        Waveform = buffer.Samples
+                                    });
+                                    activeSpikes[i] = null;
+                                }
+                                else continue;
+                            }
+
+                            var threshold = Threshold;
+                            using (var triggerHeader = Mat.CreateMatHeader(triggerBuffer))
+                            {
+                                CV.Threshold(channel, triggerHeader, threshold, 1, threshold < 0 ? ThresholdTypes.BinaryInv : ThresholdTypes.Binary);
+                            }
+
+                            for (int j = 0; j < triggerBuffer.Length; j++)
+                            {
+                                var triggerHigh = triggerBuffer[j] > 0;
+                                if (triggerHigh && !activeChannels[i])
+                                {
+                                    var buffer = new SampleBuffer(channel, Length, j);
+                                    buffer.Update(delayedChannel, j);
+                                    if (buffer.Completed)
+                                    {
+                                        spikes.Add(new SpikeWaveform
+                                        {
+                                            ChannelIndex = i,
+                                            SampleIndex = buffer.SampleIndex,
+                                            Waveform = buffer.Samples
+                                        });
+                                    }
+                                    else activeSpikes[i] = buffer;
+                                }
+
+                                activeChannels[i] = triggerHigh;
+                            }
+                        }
+                    }
+
+                    return spikes;
+                }));
+            });
+        }
+    }
+}
