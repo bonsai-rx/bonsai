98493f01ba93a0518f36ea26d93e1d32220fd64a
glopes <glopes@localhost>
2014-08-01 03:48:48 +0100

    Added first release of Bonsai.Video 2.0.0 and Bonsai.Video.Design 2.0.0.

---
+++ Bonsai.Video/ScreenCaptureStream.cs
@@ -0,0 +1,55 @@
+﻿using AForge.Video;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonsai.Video
+{
+    [Description("Produces a sequence of images captured from the desktop screen.")]
+    public class ScreenCaptureStream : VideoCapture
+    {
+        int frameInterval;
+
+        [Description("The rectangle region of the screen to capture.")]
+        [Editor("Bonsai.Vision.Design.IplImageOutputRectangleEditor, Bonsai.Vision.Design", typeof(UITypeEditor))]
+        public Rect CaptureRegion { get; set; }
+
+        [Description("The interval between each screen grab, in milliseconds.")]
+        public int FrameInterval
+        {
+            get { return frameInterval; }
+            set
+            {
+                frameInterval = value;
+                var videoSource = (AForge.Video.ScreenCaptureStream)VideoSource;
+                if (videoSource != null)
+                {
+                    videoSource.FrameInterval = frameInterval;
+                }
+            }
+        }
+
+        protected override IVideoSource CreateVideoSource()
+        {
+            Rectangle region;
+            var captureRegion = CaptureRegion;
+            if (captureRegion.Width == 0 || captureRegion.Height == 0)
+            {
+                region = Rectangle.Empty;
+                foreach (var screen in Screen.AllScreens)
+                {
+                    region = Rectangle.Union(region, screen.Bounds);
+                }
+            }
+            else region = new Rectangle(captureRegion.X, captureRegion.Y, captureRegion.Width, captureRegion.Height);
+            return new AForge.Video.ScreenCaptureStream(region, frameInterval);
+        }
+    }
+}
---
+++ Bonsai.Video/VideoCapture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Video;
+using System.Drawing;
+using OpenCV.Net;
+using System.Drawing.Imaging;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.ComponentModel;
+
+namespace Bonsai.Video
+{
+    public abstract class VideoCapture : Source<IplImage>
+    {
+        IVideoSource videoSource;
+        IObservable<IplImage> source;
+
+        public VideoCapture()
+        {
+            source = Observable.Using(
+                () =>
+                {
+                    Load();
+                    return Disposable.Create(Unload);
+                },
+                resource => Observable.FromEventPattern<NewFrameEventArgs>(
+                    handler => videoSource.NewFrame += new NewFrameEventHandler(handler),
+                    handler => videoSource.NewFrame -= new NewFrameEventHandler(handler))
+                    .Select(evt =>
+                    {
+                        var bitmap = evt.EventArgs.Frame;
+                        var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                        try
+                        {
+                            var image = new IplImage(new OpenCV.Net.Size(bitmap.Width, bitmap.Height), IplDepth.U8, 3, bitmapData.Scan0);
+                            var output = new IplImage(image.Size, image.Depth, image.Channels);
+                            CV.Copy(image, output);
+                            return output;
+                        }
+                        finally { bitmap.UnlockBits(bitmapData); }
+                    }))
+                    .PublishReconnectable()
+                    .RefCount();
+        }
+
+        protected abstract IVideoSource CreateVideoSource();
+
+        [Browsable(false)]
+        public IVideoSource VideoSource
+        {
+            get { return videoSource; }
+        }
+
+        private void Load()
+        {
+            videoSource = CreateVideoSource();
+            videoSource.Start();
+        }
+
+        private void Unload()
+        {
+            videoSource.SignalToStop();
+            videoSource = null;
+        }
+
+        public override IObservable<IplImage> Generate()
+        {
+            return source;
+        }
+    }
+}
