d338a554fb2e1b03245fd6b754880c9f13950f6e
glopes <glopesdev@users.noreply.github.com>
2015-02-22 21:32:26 +0000

    Added new SpikeThresholdEditor to allow individual channel threshold selection.

--- Bonsai.Dsp.Design/SpikeThresholdEditor.cs
+++ Bonsai.Dsp.Design/SpikeThresholdEditor.cs
@@ -1,23 +1,19 @@
 ﻿using Bonsai.Design;
-using Bonsai.Expressions;
-using OpenCV.Net;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing.Design;
 using System.Linq;
-using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Windows.Forms.Design;
 
 namespace Bonsai.Dsp.Design
 {
-    public class SelectChannelEditor : DataSourceTypeEditor
+    public class SpikeThresholdEditor : DataSourceTypeEditor
     {
-        public SelectChannelEditor()
-            : base(DataSource.Input)
+        public SpikeThresholdEditor()
+            : base(DataSource.Output)
         {
         }
 
@@ -37,14 +33,16 @@ namespace Bonsai.Dsp.Design
             if (editorService != null)
             {
                 IDisposable subscription = null;
+                var propertyDescriptor = context.PropertyDescriptor;
                 var source = GetDataSource(context, provider);
                 var chart = new ChartPanel(source, provider);
-                chart.ClientSize = new System.Drawing.Size(320, 180);
-                var matVisualizer = new MatVisualizer();
-                matVisualizer.OverlayChannels = false;
-                matVisualizer.SelectedChannels = (int[])value;
-                matVisualizer.Load(chart);
-                var visualizerObservable = matVisualizer.Visualize(source.Output, chart);
+                chart.ClientSize = new System.Drawing.Size(320, 320);
+                var spikeVisualizer = new SpikeWaveformCollectionVisualizer<WaveformThresholdPicker>();
+                spikeVisualizer.Load(chart);
+                var thresholdPicker = spikeVisualizer.Graph;
+                thresholdPicker.Threshold = (double[])value;
+                thresholdPicker.ThresholdChanged += (sender, e) => propertyDescriptor.SetValue(context.Instance, thresholdPicker.Threshold);
+                var visualizerObservable = spikeVisualizer.Visualize(source.Output, chart);
                 chart.HandleCreated += delegate { subscription = visualizerObservable.Subscribe(); };
                 chart.Leave += delegate { editorService.CloseDropDown(); subscription.Dispose(); };
                 try
@@ -54,10 +52,10 @@ namespace Bonsai.Dsp.Design
                 finally
                 {
                     chart.Dispose();
-                    matVisualizer.Unload();
+                    spikeVisualizer.Unload();
                 }
 
-                return matVisualizer.SelectedChannels;
+                return thresholdPicker.Threshold;
             }
 
             return base.EditValue(context, provider, value);
---
+++ Bonsai.Dsp.Design/WaveformThresholdPicker.cs
@@ -0,0 +1,91 @@
+﻿using Bonsai.Design;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Bonsai.Dsp.Design
+{
+    class WaveformThresholdPicker : WaveformView
+    {
+        static readonly TimeSpan PickerRefreshInterval = TimeSpan.FromMilliseconds(30);
+        IDisposable thresholdNotifications;
+
+        public WaveformThresholdPicker()
+        {
+            Chart.IsEnableZoom = false;
+            InitializeReactiveEvents();
+        }
+
+        public double[] Threshold { get; set; }
+
+        public event EventHandler ThresholdChanged;
+
+        protected virtual void OnThresholdChanged(EventArgs e)
+        {
+            var handler = ThresholdChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private void InitializeReactiveEvents()
+        {
+            var scheduler = new ControlScheduler(this);
+            var thresholdDrag = (from mouseDown in Chart.MouseDown
+                                 where mouseDown.Button == MouseButtons.Left
+                                 let pane = Chart.MasterPane.FindChartRect(mouseDown.Location)
+                                 where pane != null
+                                 let firstThreshold = GetLocationValue(pane, mouseDown.Location)
+                                 select Observable.Return(firstThreshold).Concat(
+                                        (from mouseMove in Chart.MouseMove.TakeUntil(Chart.MouseUp)
+                                                                    .Sample(PickerRefreshInterval, scheduler)
+                                         select GetLocationValue(pane, mouseMove.Location)))
+                                         .Select(threshold => new { pane, threshold }))
+                                         .Merge();
+            thresholdNotifications = thresholdDrag.Subscribe(xs => ProcessThreshold(xs.pane, xs.threshold));
+        }
+
+        private double GetLocationValue(GraphPane pane, PointF point)
+        {
+            double x, y;
+            pane.ReverseTransform(point, out x, out y);
+            return y;
+        }
+
+        private void ProcessThreshold(GraphPane pane, double threshold)
+        {
+            var channelIndex = (int)pane.Tag;
+            var channelCount = Chart.ChannelCount;
+            var thresholdValues = Threshold ?? new double[channelCount];
+            if (thresholdValues.Length != channelCount) Array.Resize(ref thresholdValues, channelCount);
+            thresholdValues[channelIndex] = threshold;
+            Threshold = thresholdValues;
+
+            var thresholdLine = pane.GraphObjList.FirstOrDefault();
+            if (thresholdLine == null)
+            {
+                thresholdLine = new LineObj(Color.Red, pane.XAxis.Scale.Min, threshold, pane.XAxis.Scale.Max, threshold);
+                pane.GraphObjList.Add(thresholdLine);
+            }
+            else thresholdLine.Location.Y = threshold;
+            OnThresholdChanged(EventArgs.Empty);
+        }
+
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            if (thresholdNotifications != null)
+            {
+                thresholdNotifications.Dispose();
+                thresholdNotifications = null;
+            }
+            base.OnHandleDestroyed(e);
+        }
+    }
+}
