1ee064be2e8d977417d47fabbe7501f5a845dc3d
glopes <glopes@localhost>
2015-02-26 00:05:09 +0000

    Added CsvReader source module for reading and parsing text files.

--- Bonsai.Core/Expressions/ParseBuilder.cs
+++ Bonsai.Core/Expressions/ParseBuilder.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reactive;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -33,87 +31,6 @@ namespace Bonsai.Expressions
         [Description("The parse pattern to match, including conversion specifications for output data types.")]
         public string Pattern { get; set; }
 
-        static Expression BuildParser(Expression expression, string pattern)
-        {
-            var regexPattern = string.Empty;
-            pattern = pattern.Replace("%%", "%");
-            var tokens = pattern.Split(new[] { '%' }, StringSplitOptions.None);
-            var tokenTypes = new Type[tokens.Length - 1];
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                var token = tokens[i];
-                if (i > 0)
-                {
-                    Type tokenType;
-                    switch (token[0])
-                    {
-                        case 'i': tokenType = typeof(int); break;
-                        case 'f': tokenType = typeof(float); break;
-                        case 'b': tokenType = typeof(bool); break;
-                        case 'c': tokenType = typeof(char); break;
-                        case 's':
-                        default:
-                            tokenType = typeof(string);
-                            break;
-                    }
-
-                    tokenTypes[i - 1] = tokenType;
-                    token = token.Substring(1);
-                    regexPattern += "(.*)";
-                }
-
-                regexPattern += token;
-            }
-
-            var regex = new Regex(regexPattern, RegexOptions.Singleline);
-            var regexExpression = Expression.Constant(regex);
-            var matchVariable = Expression.Variable(typeof(Match));
-            var matchExpression = Expression.Call(regexExpression, "Match", null, expression);
-            var matchAssignment = Expression.Assign(matchVariable, matchExpression);
-            var matchResult = Expression.Property(matchVariable, "Success");
-            var invariantCulture = Expression.Constant(CultureInfo.InvariantCulture);
-
-            var inputValidation = Expression.IfThen(
-                Expression.Equal(expression, Expression.Constant(null, typeof(string))),
-                Expression.Throw(Expression.Constant(
-                    new InvalidOperationException("The input string cannot be null."))));
-
-            int groupIndex = 1;
-            var groupParsers = Array.ConvertAll(tokenTypes, tokenType =>
-            {
-                var groupExpression = Expression.Property(matchVariable, "Groups");
-                var groupIndexer = Expression.Property(groupExpression, "Item", Expression.Constant(groupIndex++));
-                var groupValueExpression = Expression.Property(groupIndexer, "Value");
-                if (tokenType == typeof(string)) return (Expression)groupValueExpression;
-                if (tokenType == typeof(int) || tokenType == typeof(float))
-                {
-                    return Expression.Call(tokenType, "Parse", null, groupValueExpression, invariantCulture);
-                }
-                else return Expression.Call(tokenType, "Parse", null, groupValueExpression);
-            });
-
-            var matchValidation = Expression.IfThen(
-                Expression.Not(matchResult),
-                Expression.Throw(Expression.Constant(
-                    new InvalidOperationException("The input string failed to match the parse pattern."))));
-
-            Expression resultExpression;
-            switch (tokenTypes.Length)
-            {
-                case 0: resultExpression = Expression.Constant(Unit.Default); break;
-                case 1: resultExpression = groupParsers[0]; break;
-                default:
-                    resultExpression = (Expression)Expression.Call(typeof(Tuple), "Create", tokenTypes, groupParsers);
-                    break;
-            }
-
-            return Expression.Block(new[] { matchVariable },
-                inputValidation,
-                matchAssignment,
-                matchValidation,
-                resultExpression);
-        }
-
         /// <summary>
         /// Returns the expression that applies a pattern matching operation on
         /// the specified input parameter to the selector result.
@@ -132,7 +49,7 @@ namespace Bonsai.Expressions
 
             var pattern = Pattern;
             if (string.IsNullOrEmpty(pattern)) return Expression.Constant(Unit.Default);
-            else return BuildParser(expression, pattern);
+            else return ExpressionHelper.Parse(expression, pattern);
         }
 
         class PatternConverter : StringConverter
---
+++ Bonsai.System/IO/CsvReader.cs
@@ -0,0 +1,67 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [WorkflowElementCategory(ElementCategory.Source)]
+    [Description("Sources individual lines of a text file as an observable sequence.")]
+    public class CsvReader : CombinatorExpressionBuilder
+    {
+        public CsvReader()
+            : base(minArguments: 0, maxArguments: 0)
+        {
+        }
+
+        [Description("The name of the CSV file.")]
+        [FileNameFilter("CSV (Comma delimited)|*.csv|All Files|*.*")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
+        public string FileName { get; set; }
+
+        [TypeConverter("Bonsai.Expressions.ParseBuilder+PatternConverter, Bonsai.Core")]
+        [Description("The parse pattern to match, including conversion specifications for output data types.")]
+        public string Pattern { get; set; }
+
+        protected override Expression BuildCombinator(IEnumerable<Expression> arguments)
+        {
+            var pattern = Pattern;
+            var parameter = Expression.Parameter(typeof(string));
+            var parseBody = !string.IsNullOrEmpty(pattern) ? ExpressionHelper.Parse(parameter, pattern) : parameter;
+            var parser = Expression.Lambda(parseBody, parameter);
+            var combinatorExpression = Expression.Constant(this);
+            return Expression.Call(combinatorExpression, "Generate", new[] { parseBody.Type }, parser);
+        }
+
+        IObservable<TResult> Generate<TResult>(Func<string, TResult> parser)
+        {
+            return Observable.Create<TResult>(async (observer, token) =>
+            {
+                try
+                {
+                    using (var reader = new StreamReader(FileName))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            if (token.IsCancellationRequested) break;
+                            var line = await reader.ReadLineAsync();
+                            observer.OnNext(parser(line));
+                        }
+                    }
+
+                    observer.OnCompleted();
+                }
+                catch (Exception ex)
+                {
+                    observer.OnError(ex);
+                }
+            });
+        }
+    }
+}
