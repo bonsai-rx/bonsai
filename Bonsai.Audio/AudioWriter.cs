9f8ec817431faf625353b10a85777d637c2065e5
glopes <glopesdev@users.noreply.github.com>
2015-02-28 13:12:11 +0000

    Added support for writing audio signals into an uncompressed WAV file.

---
+++ Bonsai.Audio/AudioWriter.cs
@@ -0,0 +1,55 @@
+﻿using Bonsai.IO;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Audio
+{
+    [Description("Writes the incoming sample sequence into an uncompressed RIFF/WAV file.")]
+    public class AudioWriter : FileSink<Mat, RiffWriter>
+    {
+        [Description("The sampling frequency (Hz) of the audio sequence.")]
+        public int SamplingFrequency { get; set; }
+
+        protected override RiffWriter CreateWriter(string fileName, Mat input)
+        {
+            var stream = new FileStream(fileName, FileMode.Create);
+            var bitsPerSample = input.Depth == Depth.U8 ? 8 : 16;
+            return new RiffWriter(stream, input.Rows, SamplingFrequency, bitsPerSample);
+        }
+
+        protected override void Write(RiffWriter writer, Mat input)
+        {
+            var dataDepth = input.Depth == Depth.U8 ? Depth.U8 : Depth.S16;
+            var elementSize = input.Depth == Depth.U8 ? 1 : 2;
+            var step = elementSize * input.Cols;
+            var data = new byte[step * input.Rows];
+            var dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            try
+            {
+                var dataAddr = dataHandle.AddrOfPinnedObject();
+                using (var dataHeader = new Mat(input.Cols, input.Rows, dataDepth, input.Channels,
+                                                dataAddr, elementSize * input.Rows))
+                {
+                    if (input.Depth != dataDepth)
+                    {
+                        using (var conversion = new Mat(input.Size, dataDepth, input.Channels))
+                        {
+                            CV.Convert(input, conversion);
+                            CV.Transpose(conversion, dataHeader);
+                        }
+                    }
+                    else CV.Transpose(input, dataHeader);
+                }
+            }
+            finally { dataHandle.Free(); }
+            writer.Write(data);
+        }
+    }
+}
---
+++ Bonsai.Audio/RiffWriter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Audio
+{
+    public class RiffWriter : IDisposable
+    {
+        bool disposed;
+        long dataSize;
+        WaveFormatEx format;
+        readonly BinaryWriter writer;
+        static readonly byte[] riffId = Encoding.ASCII.GetBytes("RIFF");
+        static readonly byte[] waveId = Encoding.ASCII.GetBytes("WAVE");
+        static readonly byte[] fmtId = Encoding.ASCII.GetBytes("fmt ");
+        static readonly byte[] dataId = Encoding.ASCII.GetBytes("data");
+        
+        public RiffWriter(Stream stream, int channels, int samplesPerSecond, int bitsPerSample)
+        {
+            format.wFormatTag = 1;
+            format.nChannels = (ushort)channels;
+            format.nSamplesPerSec = (uint)samplesPerSecond;
+            format.nAvgBytesPerSec = (uint)(channels * samplesPerSecond * bitsPerSample / 8);
+            format.nBlockAlign = (ushort)(channels * bitsPerSample / 8);
+            format.wBitsPerSample = (ushort)bitsPerSample;
+
+            writer = new BinaryWriter(stream);
+            WriteRiffHeader(writer, format, 0);
+        }
+
+        static void WriteRiffHeader(BinaryWriter writer, WaveFormatEx format, uint dataSize)
+        {
+            var formatSize = (uint)Marshal.SizeOf(typeof(WaveFormatEx));
+            var totalSize = (uint)
+            (
+                formatSize + dataSize +
+                waveId.Length + fmtId.Length + dataId.Length +
+                4 + 4
+            );
+
+            writer.Write(riffId);
+            writer.Write(totalSize);
+            writer.Write(waveId);
+            writer.Write(fmtId);
+            writer.Write(formatSize);
+
+            writer.Write(format.wFormatTag);
+            writer.Write(format.nChannels);
+            writer.Write(format.nSamplesPerSec);
+            writer.Write(format.nAvgBytesPerSec);
+            writer.Write(format.nBlockAlign);
+            writer.Write(format.wBitsPerSample);
+            writer.Write(dataId);
+            writer.Write(dataSize);
+        }
+
+        public void Write(byte[] buffer)
+        {
+            writer.Write(buffer);
+            dataSize += buffer.LongLength;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    var position = writer.BaseStream.Position;
+                    writer.BaseStream.Seek(0, SeekOrigin.Begin);
+                    WriteRiffHeader(writer, format, (uint)dataSize);
+                    writer.BaseStream.Seek(position, SeekOrigin.Begin);
+                    writer.Close();
+                    disposed = true;
+                }
+            }
+        }
+
+        public void Close()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        void IDisposable.Dispose()
+        {
+            Close();
+        }
+
+        #region WaveFormatEx Structure
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct WaveFormatEx
+        {
+            public ushort wFormatTag;
+            public ushort nChannels;
+            public uint nSamplesPerSec;
+            public uint nAvgBytesPerSec;
+            public ushort nBlockAlign;
+            public ushort wBitsPerSample;
+        }
+
+        #endregion
+    }
+}
