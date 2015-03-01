55b0d04e4a043fccc69879e8f166ec63acc385b2
glopes <glopesdev@users.noreply.github.com>
2015-03-01 01:04:49 +0000

    Performance and reliability improvements to MatrixReader replay loop.

--- Bonsai.Dsp/MatrixReader.cs
+++ Bonsai.Dsp/MatrixReader.cs
@@ -10,6 +10,7 @@ using System.Reactive.Linq;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bonsai.Dsp
 {
@@ -37,53 +38,61 @@ namespace Bonsai.Dsp
 
         public override IObservable<Mat> Generate()
         {
-            var stopwatch = new Stopwatch();
-            return Observable.Using(
-                () => new BinaryReader(new FileStream(FileName, FileMode.Open, FileAccess.Read)),
-                reader => ObservableCombinators.GenerateWithThread<Mat>(observer =>
+            return Observable.Create<Mat>((observer, cancellationToken) =>
+            {
+                return Task.Factory.StartNew(() =>
                 {
-                    stopwatch.Restart();
-                    var channelCount = ChannelCount;
-                    var bufferLength = BufferLength;
-                    var output = new Mat(channelCount, bufferLength, Depth, 1);
-                    var depthSize = output.Step / bufferLength;
-                    var buffer = new byte[bufferLength * channelCount * depthSize];
-                    var bytesRead = reader.Read(buffer, 0, buffer.Length);
-                    if (bytesRead == 0)
+                    using (var reader = new BinaryReader(new FileStream(FileName, FileMode.Open, FileAccess.Read)))
+                    using (var sampleSignal = new ManualResetEvent(false))
                     {
-                        observer.OnCompleted();
-                        return;
-                    }
-                    else
-                    {
-                        Mat bufferHeader;
-                        var bufferHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-                        try
+                        var stopwatch = new Stopwatch();
+                        var channelCount = ChannelCount;
+                        var bufferLength = BufferLength;
+
+                        while (!cancellationToken.IsCancellationRequested)
                         {
-                            switch (Layout)
+                            stopwatch.Restart();
+                            var output = new Mat(channelCount, bufferLength, Depth, 1);
+                            var depthSize = output.Step / bufferLength;
+                            var buffer = new byte[bufferLength * channelCount * depthSize];
+                            var bytesRead = reader.Read(buffer, 0, buffer.Length);
+                            if (bytesRead < buffer.Length) break;
+
+                            Mat bufferHeader;
+                            var bufferHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                            try
                             {
-                                case MatrixLayout.ColumnMajor:
-                                    bufferHeader = new Mat(bufferLength, channelCount, Depth, 1, bufferHandle.AddrOfPinnedObject());
-                                    CV.Transpose(bufferHeader, output);
-                                    break;
-                                default:
-                                case MatrixLayout.RowMajor:
-                                    bufferHeader = new Mat(channelCount, bufferLength, Depth, 1, bufferHandle.AddrOfPinnedObject());
-                                    CV.Copy(bufferHeader, output);
-                                    break;
+                                switch (Layout)
+                                {
+                                    case MatrixLayout.ColumnMajor:
+                                        bufferHeader = new Mat(bufferLength, channelCount, Depth, 1, bufferHandle.AddrOfPinnedObject());
+                                        CV.Transpose(bufferHeader, output);
+                                        break;
+                                    default:
+                                    case MatrixLayout.RowMajor:
+                                        bufferHeader = new Mat(channelCount, bufferLength, Depth, 1, bufferHandle.AddrOfPinnedObject());
+                                        CV.Copy(bufferHeader, output);
+                                        break;
+                                }
+                            }
+                            finally { bufferHandle.Free(); }
+
+                            observer.OnNext(output);
+                            var sampleInterval = 1000.0 / Frequency;
+                            var dueTime = Math.Max(0, (sampleInterval * BufferLength) - stopwatch.Elapsed.TotalMilliseconds);
+                            if (dueTime > 0)
+                            {
+                                sampleSignal.WaitOne(TimeSpan.FromMilliseconds(dueTime));
                             }
                         }
-                        finally { bufferHandle.Free(); }
 
-                        observer.OnNext(output);
-                        var sampleInterval = 1000.0 / Frequency;
-                        var dueTime = Math.Max(0, (sampleInterval * BufferLength) - stopwatch.Elapsed.TotalMilliseconds);
-                        if (dueTime > 0)
-                        {
-                            Thread.Sleep(TimeSpan.FromMilliseconds(dueTime));
-                        }
+                        observer.OnCompleted();
                     }
-                }));
+                },
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+            });
         }
     }
 }
