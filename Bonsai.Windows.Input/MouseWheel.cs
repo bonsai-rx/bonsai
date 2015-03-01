dec73c0aacca4012f2a59907e0627d8fd9627739
glopes <glopesdev@users.noreply.github.com>
2015-03-01 01:30:26 +0000

    Added editor documentation for modules in the windows input package.

--- Bonsai.Windows.Input/MouseWheel.cs
+++ Bonsai.Windows.Input/MouseWheel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bonsai.Windows.Input
 {
+    [Description("Produces a sequence of values whenever the mouse wheel moves.")]
     public class MouseWheel : Source<int>
     {
         public override IObservable<int> Generate()
