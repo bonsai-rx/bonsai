9150bcf9b9fc090ccc742f2aeabc0eeca4a730e3
glopes <glopes@localhost>
2015-03-02 22:54:45 +0000

    Fixed #238. Added basic error handling operators.

---
+++ Bonsai.Core/Reactive/Retry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Reactive
+{
+    /// <summary>
+    /// Represents a combinator that repeats an observable sequence
+    /// until it successfully terminates.
+    /// </summary>
+    [XmlType(Namespace = Constants.XmlNamespace)]
+    [Description("Repeats the observable sequence until it successfully terminates.")]
+    public class Retry : Combinator
+    {
+        /// <summary>
+        /// Repeats the observable sequence until it successfully terminates.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">The observable sequence to repeat until it successfully terminates.</param>
+        /// <returns>
+        /// The observable sequence producing the elements of the given sequence repeatedly
+        /// until it terminates successfully.
+        /// </returns>
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Retry();
+        }
+    }
+}
---
+++ Bonsai.Core/Reactive/RetryCount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Reactive
+{
+    /// <summary>
+    /// Represents a combinator that repeats an observable sequence the specified
+    /// number of times or until it successfully terminates.
+    /// </summary>
+    [XmlType(Namespace = Constants.XmlNamespace)]
+    [Description("Repeats the observable sequence the specified number of times or until it successfully terminates.")]
+    public class RetryCount : Combinator
+    {
+        /// <summary>
+        /// Gets or sets the number of times to repeat the sequence.
+        /// </summary>
+        [Description("The number of times to repeat the sequence.")]
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Repeats the observable sequence the specified number of times
+        /// or until it successfully terminates.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">The observable sequence to repeat until it successfully terminates.</param>
+        /// <returns>
+        /// The observable sequence producing the elements of the given sequence repeatedly
+        /// until it terminates successfully.
+        /// </returns>
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Retry(Count);
+        }
+    }
+}
