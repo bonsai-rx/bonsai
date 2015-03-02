e6d7060ad5086a21f8cb2c59dce54158164c2b5d
glopes <glopesdev@users.noreply.github.com>
2015-03-02 22:10:23 +0000

    Fixed #240. Added Depth property to ConvertToArray for array type specification.

--- Bonsai.Dsp/ConvertToArray.cs
+++ Bonsai.Dsp/ConvertToArray.cs
@@ -1,8 +1,10 @@
-﻿using OpenCV.Net;
+﻿using Bonsai.Expressions;
+using OpenCV.Net;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reactive.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -10,18 +12,61 @@ using System.Threading.Tasks;
 
 namespace Bonsai.Dsp
 {
-    [Combinator]
     [WorkflowElementCategory(ElementCategory.Transform)]
     [Description("Converts the input buffer into a managed array of bytes.")]
-    public class ConvertToArray
+    public class ConvertToArray : SelectBuilder
     {
-        public IObservable<byte[]> Process<TArray>(IObservable<TArray> source) where TArray : Arr
+        [TypeConverter(typeof(DepthConverter))]
+        [Description("The target bit depth of individual array elements.")]
+        public Depth? Depth { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
         {
-            return source.Select(input =>
+            var depth = Depth;
+            var arrayType = expression.Type;
+            if (depth.HasValue)
             {
-                var inputHeader = input.GetMat();
-                return ArrHelper.ToArray(inputHeader);
-            });
+                var elementType = GetElementType(depth.Value);
+                var depthExpression = Expression.Constant(depth.Value);
+                return Expression.Call(typeof(ConvertToArray), "Process", new[] { arrayType, elementType }, expression, depthExpression);
+            }
+            else return Expression.Call(typeof(ConvertToArray), "Process", new[] { arrayType }, expression);
+        }
+
+        static Type GetElementType(Depth depth)
+        {
+            switch (depth)
+            {
+                case OpenCV.Net.Depth.U16: return typeof(ushort);
+                case OpenCV.Net.Depth.S16: return typeof(short);
+                case OpenCV.Net.Depth.S32: return typeof(int);
+                case OpenCV.Net.Depth.F32: return typeof(float);
+                case OpenCV.Net.Depth.F64: return typeof(double);
+                case OpenCV.Net.Depth.U8:
+                case OpenCV.Net.Depth.S8:
+                case OpenCV.Net.Depth.UserType:
+                default:
+                    return typeof(byte);
+            }
+        }
+
+        static byte[] Process<TArray>(TArray input) where TArray : Arr
+        {
+            var inputHeader = input.GetMat();
+            return ArrHelper.ToArray(inputHeader);
+        }
+
+        static TElement[] Process<TArray, TElement>(TArray input, Depth depth)
+            where TArray : Arr
+            where TElement : struct
+        {
+            var inputHeader = input.GetMat();
+            var output = new TElement[inputHeader.Rows * inputHeader.Cols];
+            using (var outputHeader = Mat.CreateMatHeader(output, inputHeader.Rows, inputHeader.Cols, depth, 1))
+            {
+                CV.Convert(inputHeader, outputHeader);
+            }
+            return output;
         }
     }
 }
