24852cbeab8adf56debf3a03c606b72bf9346b45
glopes <glopes@localhost>
2015-03-06 16:24:54 +0000

    Fixed #163. Added CropPolygon and MaskPolygon to allow creating polygonal masks.

---
+++ Bonsai.Vision/MaskPolygon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [Description("Masks a non-rectangular region of interest bounded by a set of polygonal contours.")]
+    public class MaskPolygon : CropPolygon
+    {
+        public MaskPolygon()
+            : base(false)
+        {
+        }
+    }
+}
