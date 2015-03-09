5a8d85a1c1fd0cb910c7c7a5d96e2a9bc3db3805
glopes <glopesdev@users.noreply.github.com>
2015-03-09 21:17:19 +0000

    Added MatchTemplate module to Dsp package.

---
+++ Bonsai.Dsp/MatchTemplate.cs
@@ -0,0 +1,39 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Compares a template against overlapped regions in the input array.")]
+    public class MatchTemplate
+    {
+        [Description("Specifies the method used to compare the template with array regions.")]
+        public TemplateMatchingMethod MatchingMethod { get; set; }
+
+        public IObservable<TArray> Process<TArray, TTemplate>(IObservable<Tuple<TArray, TTemplate>> source)
+            where TArray : Arr
+            where TTemplate : Arr
+        {
+            var outputFactory = ArrFactory<TArray>.DefaultFactory;
+            return source.Select(input =>
+            {
+                var image = input.Item1;
+                var template = input.Item2;
+                var outputSize = image.Size;
+                var templateSize = template.Size;
+                outputSize.Width -= templateSize.Width - 1;
+                outputSize.Height -= templateSize.Height - 1;
+                var output = outputFactory(outputSize, Depth.F32, 1);
+                CV.MatchTemplate(image, template, output, MatchingMethod);
+                return output;
+            });
+        }
+    }
+}
