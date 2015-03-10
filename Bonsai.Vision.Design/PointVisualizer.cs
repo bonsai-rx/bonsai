eb33851e925ee240d57f0c327acff819b64cf37c
glopes <glopesdev@users.noreply.github.com>
2015-03-10 12:41:49 +0000

    Added PointMashupVisualizer to allow superimposing detected points on images.

---
+++ Bonsai.Vision.Design/PointVisualizer.cs
@@ -0,0 +1,20 @@
+﻿using Bonsai;
+using Bonsai.Design;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(PointVisualizer), Target = typeof(Point))]
+[assembly: TypeVisualizer(typeof(PointVisualizer), Target = typeof(Point2d))]
+[assembly: TypeVisualizer(typeof(PointVisualizer), Target = typeof(Point2f))]
+
+namespace Bonsai.Vision.Design
+{
+    public class PointVisualizer : ObjectTextVisualizer
+    {
+    }
+}
