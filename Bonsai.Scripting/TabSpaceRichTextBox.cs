8c439b2f952cb8b9af1ba91746a5200b84418cb1
glopes <glopesdev@users.noreply.github.com>
2015-03-17 18:17:49 +0000

    Updated python script editor to insert spaces instead of tabs.

---
+++ Bonsai.Scripting/TabSpaceRichTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonsai.Scripting
+{
+    class TabSpaceRichTextBox : RichTextBox
+    {
+        protected override bool ProcessCmdKey(ref Message m, Keys keyData)
+        {
+            if (keyData == Keys.Tab)
+            {
+                SelectionLength = 0;
+                SelectedText = new string(' ', 2);
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref m, keyData);
+        }
+    }
+}
