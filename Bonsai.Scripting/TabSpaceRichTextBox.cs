b62b35cb4f4f07f3305bb6bab2926aec312d9087
glopes <glopesdev@users.noreply.github.com>
2015-03-18 21:09:14 +0000

    Updated python script editor to improve Tab indent/unindent strategies.

--- Bonsai.Scripting/TabSpaceRichTextBox.cs
+++ Bonsai.Scripting/TabSpaceRichTextBox.cs
@@ -9,12 +9,33 @@ namespace Bonsai.Scripting
 {
     class TabSpaceRichTextBox : RichTextBox
     {
+        const string Tab = "  ";
+
         protected override bool ProcessCmdKey(ref Message m, Keys keyData)
         {
             if (keyData == Keys.Tab)
             {
                 SelectionLength = 0;
-                SelectedText = new string(' ', 2);
+                var selectionStart = SelectionStart;
+                SelectionStart = GetFirstCharIndexOfCurrentLine();
+                SelectedText = Tab;
+                SelectionStart = selectionStart + Tab.Length;
+                return true;
+            }
+
+            if (keyData == (Keys.Shift | Keys.Tab))
+            {
+                var lineIndex = GetLineFromCharIndex(SelectionStart);
+                var line = Lines[lineIndex];
+                if (!string.IsNullOrEmpty(line) && line.StartsWith(Tab))
+                {
+                    var selectionStart = SelectionStart;
+                    SelectionStart = GetFirstCharIndexOfCurrentLine();
+                    SelectionLength = Tab.Length;
+                    SelectedText = string.Empty;
+                    SelectionStart = Math.Max(0, selectionStart - 2);
+                }
+
                 return true;
             }
 
