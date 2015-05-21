1e655beb21fa43720877a266957b8152dde22432
glopes <glopesdev@users.noreply.github.com>
2015-05-21 01:56:33 +0100

    Added error checking to shader compilation and uniform variable binding steps.

---
+++ Bonsai.Shaders/ShaderException.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Serializable]
+    public class ShaderException : GraphicsException
+    {
+        public ShaderException()
+        {
+        }
+
+        public ShaderException(string message)
+            : base(message)
+        {
+        }
+    }
+}
