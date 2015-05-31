4dd7078868c98859af76bf6cd5e081b790947fe2
glopes <glopes@localhost>
2015-05-31 13:15:02 +0100

    Added connected component layering method to improve workflow graphical layout.

--- Bonsai.Editor/GraphView/GraphNodeGrouping.cs
+++ Bonsai.Editor/GraphView/GraphNodeGrouping.cs
@@ -11,24 +11,33 @@ namespace Bonsai.Design
         public GraphNodeGrouping(int layer)
         {
             Key = layer;
+            UpdateItems = true;
         }
 
         public int Key { get; private set; }
 
+        public bool UpdateItems { get; set; }
+
         protected override void InsertItem(int index, GraphNode item)
         {
             base.InsertItem(index, item);
-            item.LayerIndex = index;
-            for (int i = index + 1; i < Count; i++)
+            if (UpdateItems)
             {
-                Items[i].LayerIndex = i;
+                item.LayerIndex = index;
+                for (int i = index + 1; i < Count; i++)
+                {
+                    Items[i].LayerIndex = i;
+                }
             }
         }
 
         protected override void SetItem(int index, GraphNode item)
         {
             base.SetItem(index, item);
-            item.LayerIndex = index;
+            if (UpdateItems)
+            {
+                item.LayerIndex = index;
+            }
         }
 
         protected override void RemoveItem(int index)
