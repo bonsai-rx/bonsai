3bcbe77f1f62e72548a5fcfd2a331788bec7ae81
glopes <glopes@localhost>
2015-05-31 17:56:14 +0100

    Fixed #330. Added some basic GLSL type checking, if available, to UpdateUniform.

--- Bonsai.Shaders/UpdateUniform.cs
+++ Bonsai.Shaders/UpdateUniform.cs
@@ -19,7 +19,7 @@ namespace Bonsai.Shaders
         [Editor("Bonsai.Shaders.Design.ShaderConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
         public string ShaderName { get; set; }
 
-        IObservable<TSource> Process<TSource>(IObservable<TSource> source, Action<int, TSource> update)
+        IObservable<TSource> Process<TSource>(IObservable<TSource> source, Action<int, TSource> update, ActiveUniformType type)
         {
             return Observable.Defer(() =>
             {
@@ -43,6 +43,24 @@ namespace Bonsai.Shaders
                                     name,
                                     ShaderName));
                             }
+
+                            int uniformIndex;
+                            GL.GetUniformIndices(shader.Program, 1, new[] { name }, out uniformIndex);
+                            if (uniformIndex >= 0)
+                            {
+                                int uniformSize;
+                                ActiveUniformType uniformType;
+                                GL.GetActiveUniform(shader.Program, uniformIndex, out uniformSize, out uniformType);
+                                if (uniformType != type)
+                                {
+                                    throw new InvalidOperationException(string.Format(
+                                        "Expected a {2} uniform, but the variable \"{0}\" in shader program \"{1}\" has type {3}.",
+                                        name,
+                                        ShaderName,
+                                        type,
+                                        uniformType));
+                                }
+                            }
                         });
                     }),
                     (input, shader) =>
@@ -58,62 +76,74 @@ namespace Bonsai.Shaders
 
         public IObservable<int> Process(IObservable<int> source)
         {
-            return Process(source, (location, input) => GL.Uniform1(location, input));
+            return Process(source, (location, input) => GL.Uniform1(location, input),
+                           ActiveUniformType.Int);
         }
 
         public IObservable<float> Process(IObservable<float> source)
         {
-            return Process(source, (location, input) => GL.Uniform1(location, input));
+            return Process(source, (location, input) => GL.Uniform1(location, input),
+                           ActiveUniformType.Float);
         }
 
         public IObservable<double> Process(IObservable<double> source)
         {
-            return Process(source, (location, input) => GL.Uniform1(location, input));
+            return Process(source, (location, input) => GL.Uniform1(location, input),
+                           ActiveUniformType.Double);
         }
 
         public IObservable<Tuple<int, int>> Process(IObservable<Tuple<int, int>> source)
         {
-            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2));
+            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2),
+                           ActiveUniformType.IntVec2);
         }
 
         public IObservable<Tuple<float, float>> Process(IObservable<Tuple<float, float>> source)
         {
-            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2));
+            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2),
+                           ActiveUniformType.FloatVec2);
         }
 
         public IObservable<Tuple<double, double>> Process(IObservable<Tuple<double, double>> source)
         {
-            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2));
+            return Process(source, (location, input) => GL.Uniform2(location, input.Item1, input.Item2),
+                           ActiveUniformType.DoubleVec2);
         }
 
         public IObservable<Tuple<int, int, int>> Process(IObservable<Tuple<int, int, int>> source)
         {
-            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3));
+            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3),
+                           ActiveUniformType.IntVec3);
         }
 
         public IObservable<Tuple<float, float, float>> Process(IObservable<Tuple<float, float, float>> source)
         {
-            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3));
+            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3),
+                           ActiveUniformType.FloatVec3);
         }
 
         public IObservable<Tuple<double, double, double>> Process(IObservable<Tuple<double, double, double>> source)
         {
-            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3));
+            return Process(source, (location, input) => GL.Uniform3(location, input.Item1, input.Item2, input.Item3),
+                           ActiveUniformType.DoubleVec3);
         }
 
         public IObservable<Tuple<int, int, int, int>> Process(IObservable<Tuple<int, int, int, int>> source)
         {
-            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4));
+            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4),
+                           ActiveUniformType.IntVec4);
         }
 
         public IObservable<Tuple<float, float, float, float>> Process(IObservable<Tuple<float, float, float, float>> source)
         {
-            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4));
+            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4),
+                           ActiveUniformType.FloatVec4);
         }
 
         public IObservable<Tuple<double, double, double, double>> Process(IObservable<Tuple<double, double, double, double>> source)
         {
-            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4));
+            return Process(source, (location, input) => GL.Uniform4(location, input.Item1, input.Item2, input.Item3, input.Item4),
+                           ActiveUniformType.DoubleVec4);
         }
     }
 }
