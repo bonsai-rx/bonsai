c7923e0634e4d3137a66e32a52309a80c5fc6d2b
glopes <glopesdev@users.noreply.github.com>
2015-06-25 14:49:28 +0100

    Added True and False as built-in keywords for python script editor.

--- Bonsai.Scripting/PythonScriptEditorDialog.cs
+++ Bonsai.Scripting/PythonScriptEditorDialog.cs
@@ -39,7 +39,7 @@ namespace Bonsai.Scripting
             scintilla.Lexer = Lexer.Python;
 
             scintilla.SetKeywords(0, "and del from not while as elif global or with assert else if pass yield break except import print class exec in raise continue finally is return def for lambda try");
-            scintilla.SetKeywords(1, "None abs divmod input open staticmethod all enumerate int ord str any eval isinstance pow sum basestring execfile issubclass print super bin file iter property tuple bool filter len range type bytearray float list raw_input unichr callable format locals reduce unicode chr frozenset long reload vars classmethod getattr map repr xrange cmp globals max reversed zip compile hasattr memoryview round __import__ complex hash min set  delattr help next setattr  dict hex object slice dir id oct sorted");
+            scintilla.SetKeywords(1, "None True False abs divmod input open staticmethod all enumerate int ord str any eval isinstance pow sum basestring execfile issubclass print super bin file iter property tuple bool filter len range type bytearray float list raw_input unichr callable format locals reduce unicode chr frozenset long reload vars classmethod getattr map repr xrange cmp globals max reversed zip compile hasattr memoryview round __import__ complex hash min set  delattr help next setattr  dict hex object slice dir id oct sorted");
         }
 
         public string Script { get; set; }
