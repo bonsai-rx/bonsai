bb1abdaec0381a9fa066b8c660fba7df636d9f6b
glopes <glopesdev@users.noreply.github.com>
2015-07-01 18:11:35 +0100

    Added all available keyboard and mouse event sources for shader window.

---
+++ Bonsai.Shaders/KeyPress.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Produces a sequence of characters whenever a key is pressed while the shader window has focus.")]
+    [Editor("Bonsai.Shaders.Design.ShaderConfigurationComponentEditor, Bonsai.Shaders.Design", typeof(ComponentEditor))]
+    public class KeyPress : Source<char>
+    {
+        public override IObservable<char> Generate()
+        {
+            return ShaderManager.WindowSource.SelectMany(window => Observable.FromEventPattern<KeyPressEventArgs>(
+                handler => window.KeyPress += handler,
+                handler => window.KeyPress -= handler)
+                .Select(evt => evt.EventArgs.KeyChar));
+        }
+    }
+}
---
+++ Bonsai.Shaders/MouseEnter.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Produces a sequence of events whenever the mouse cursor enters the shader window bounds.")]
+    [Editor("Bonsai.Shaders.Design.ShaderConfigurationComponentEditor, Bonsai.Shaders.Design", typeof(ComponentEditor))]
+    public class MouseEnter : Source<Unit>
+    {
+        public override IObservable<Unit> Generate()
+        {
+            return ShaderManager.WindowSource.SelectMany(window => Observable.FromEventPattern<EventArgs>(
+                handler => window.MouseEnter += handler,
+                handler => window.MouseEnter -= handler)
+                .Select(evt => Unit.Default));
+        }
+    }
+}
