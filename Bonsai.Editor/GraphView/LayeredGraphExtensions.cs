1ac507cc15275b1500d730b8ac7dbf6e11369f86
glopes <glopesdev@users.noreply.github.com>
2015-07-12 01:01:10 +0100

    Generalized layout edge sorting to any comparable edge type.

--- Bonsai.Editor/GraphView/LayeredGraphExtensions.cs
+++ Bonsai.Editor/GraphView/LayeredGraphExtensions.cs
@@ -366,13 +366,9 @@ namespace Bonsai.Design
                 var layer = layers[i];
                 if (i > 0)
                 {
-                    //TODO: ExpressionBuilderArgument should implement IComparable so ordering can be generic
                     var nodeOrder = from node in layer
                                     from edge in node.Successors
-                                    let label = edge.Label as ExpressionBuilderArgument
-                                    where label != null
-                                    let successor = edge.Node
-                                    orderby node.BuildDependency, successor.LayerIndex, label.Index
+                                    orderby node.BuildDependency, edge.Node.LayerIndex, edge.Label
                                     group node by node into g
                                     select g.Key;
                     var sortedLayer = new GraphNodeGrouping(layer.Key);
