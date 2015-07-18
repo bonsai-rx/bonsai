0bcfd1a51abfbdc1ee6234af569ca3353769dfdc
glopes <glopes@localhost>
2015-07-18 20:01:57 +0100

    Added missing documentation strings to property and input mapping builders.

--- Bonsai.Core/Expressions/InputMappingBuilder.cs
+++ Bonsai.Core/Expressions/InputMappingBuilder.cs
@@ -10,11 +10,21 @@ using System.Xml.Serialization;
 
 namespace Bonsai.Expressions
 {
+    /// <summary>
+    /// Represents an expression builder that selects inner properties of elements of the sequence
+    /// and assigns their values to properties of a workflow element.
+    /// </summary>
     [XmlType("InputMapping", Namespace = Constants.XmlNamespace)]
+    [Description("Selects inner properties of elements of the sequence and assigns their values to properties of a workflow element.")]
     public class InputMappingBuilder : PropertyMappingBuilder
     {
         readonly MemberSelectorBuilder selector = new MemberSelectorBuilder();
 
+        /// <summary>
+        /// Gets or sets a string used to select the input element member to project
+        /// as output of the sequence.
+        /// </summary>
+        [Description("The inner properties that will be selected for each element of the sequence.")]
         [Editor("Bonsai.Design.MemberSelectorEditor, Bonsai.Design", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string Selector
         {
