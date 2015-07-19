eee6c6930f98f175583c2ba7a8d93be71755bb3c
glopes <glopes@localhost>
2015-07-19 12:35:05 +0100

    Added LogPolar transform and refactored common polar transform properties.

--- Bonsai.Vision/LogPolar.cs
+++ Bonsai.Vision/LogPolar.cs
@@ -9,15 +9,15 @@ using System.Threading.Tasks;
 
 namespace Bonsai.Vision
 {
-    [Description("Performs forward or inverse linear-polar image transform. This transform \"emulates\" human foveal vision.")]
-    public class LinearPolar : PolarTransform
+    [Description("Performs forward or inverse log-polar image transform. This transform \"emulates\" human foveal vision.")]
+    public class LogPolar : PolarTransform
     {
         public override IObservable<IplImage> Process(IObservable<IplImage> source)
         {
             return source.Select(input =>
             {
                 var output = new IplImage(input.Size, input.Depth, input.Channels);
-                CV.LinearPolar(input, output, Center, Magnitude, Flags);
+                CV.LogPolar(input, output, Center, Magnitude, Flags);
                 return output;
             });
         }
