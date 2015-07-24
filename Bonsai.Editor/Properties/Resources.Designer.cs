5c444f313db6813af1770a062ce9faae23db6690
glopes <glopes@localhost>
2015-07-24 19:11:10 +0100

    Added automatic workflow file update and warning dialog.

--- Bonsai.Editor/Properties/Resources.Designer.cs
+++ Bonsai.Editor/Properties/Resources.Designer.cs
@@ -243,5 +243,23 @@ namespace Bonsai.Editor.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This workflow file has been created with an older version of Bonsai. Further modifications will be incompatible with previous versions..
+        /// </summary>
+        internal static string UpdateWorkflow_Warning {
+            get {
+                return ResourceManager.GetString("UpdateWorkflow_Warning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Update Workflow.
+        /// </summary>
+        internal static string UpdateWorkflow_Warning_Caption {
+            get {
+                return ResourceManager.GetString("UpdateWorkflow_Warning_Caption", resourceCulture);
+            }
+        }
     }
 }
