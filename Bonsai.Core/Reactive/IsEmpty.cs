2e85cb835e5708d64a47c584f9e11de134ba334a
glopes <glopes@localhost>
2015-08-10 02:35:43 +0100

    Added support for IsEmpty reactive operator.

---
+++ Bonsai.Core/Reactive/IsEmpty.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Reactive
+{
+    /// <summary>
+    /// Represents a combinator that determines whether the observable
+    /// sequence is empty.
+    /// </summary>
+    [Combinator]
+    [XmlType(Namespace = Constants.XmlNamespace)]
+    [Description("Determines whether the observable sequence is empty.")]
+    public class IsEmpty
+    {
+        /// <summary>
+        /// Determines whether the observable sequence is empty.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">The source sequence to check.</param>
+        /// <returns>
+        /// An observable sequence containing a single element determining whether
+        /// the source sequence is empty.
+        /// </returns>
+        public IObservable<bool> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.IsEmpty();
+        }
+    }
+}
