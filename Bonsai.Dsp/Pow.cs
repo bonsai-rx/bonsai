53fa77e5091a7afea1efc3ab0557422a163f6ab6
glopes <glopesdev@users.noreply.github.com>
2015-08-25 09:07:57 +0100

    Added Pow module to Dsp package.

---
+++ Bonsai.Dsp/Pow.cs
@@ -0,0 +1,28 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.ComponentModel;
+
+namespace Bonsai.Dsp
+{
+    [Description("Raises every input array element to a power.")]
+    public class Pow : ArrayTransform
+    {
+        public double Power { get; set; }
+
+        public override IObservable<TArray> Process<TArray>(IObservable<TArray> source)
+        {
+            var outputFactory = ArrFactory<TArray>.TemplateFactory;
+            return source.Select(input =>
+            {
+                var output = outputFactory(input);
+                CV.Pow(input, output, Power);
+                return output;
+            });
+        }
+    }
+}
