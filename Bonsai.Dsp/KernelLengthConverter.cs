6ae558f8c0a3fa953d5cc870a3d6ee61705831a2
glopes <glopes@localhost>
2015-09-15 10:03:51 +0100

    Added missing description and type converters to Decimate module.

---
+++ Bonsai.Dsp/KernelLengthConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Dsp
+{
+    class KernelLengthConverter : Int32Converter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            var kernelLength = (int)base.ConvertFrom(context, culture, value);
+            if (kernelLength % 2 != 0)
+            {
+                throw new ArgumentOutOfRangeException("value", "The length of the filter kernel must be an even number.");
+            }
+
+            return kernelLength;
+        }
+    }
+}
