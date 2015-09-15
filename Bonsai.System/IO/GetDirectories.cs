483fb9fbc7ee2a41e1d3935a19435d05e10b2516
glopes <glopesdev@users.noreply.github.com>
2015-09-15 09:33:39 +0100

    Fixed #382. Added support for searching files and subdirectories in a path.

---
+++ Bonsai.System/IO/GetDirectories.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [DefaultProperty("Path")]
+    [Description("Returns the names of the subdirectories that match the specified search pattern.")]
+    public class GetDirectories : Source<string[]>
+    {
+        [Description("The path to search.")]
+        [Editor("Bonsai.Design.FolderNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string Path { get; set; }
+
+        [Description("The search string used to match against the names of subdirectories in the path.")]
+        public string SearchPattern { get; set; }
+
+        [Description("Specifies whether the search should include all subdirectories.")]
+        public SearchOption SearchOption { get; set; }
+
+        public override IObservable<string[]> Generate()
+        {
+            return Observable.Return(Directory.GetDirectories(Path, SearchPattern, SearchOption));
+        }
+    }
+}
