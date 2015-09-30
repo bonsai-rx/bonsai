e87c6c75b133e974e6126cbac42c5eb5fc7409aa
glopes <glopesdev@users.noreply.github.com>
2015-09-30 17:59:09 +0100

    Updated element loader to include only public types.

--- Bonsai/WorkflowElementLoader.cs
+++ Bonsai/WorkflowElementLoader.cs
@@ -50,8 +50,8 @@ namespace Bonsai
             for (int i = 0; i < types.Length; i++)
             {
                 var type = types[i];
-                if (!type.IsAbstract && !type.ContainsGenericParameters && IsWorkflowElement(type) &&
-                    !type.IsDefined(typeof(ObsoleteAttribute)))
+                if (type.IsPublic && !type.IsAbstract && !type.ContainsGenericParameters &&
+                    IsWorkflowElement(type) && !type.IsDefined(typeof(ObsoleteAttribute)))
                 {
                     var descriptionAttribute = (DescriptionAttribute)TypeDescriptor.GetAttributes(type)[typeof(DescriptionAttribute)];
                     yield return new WorkflowElementDescriptor
