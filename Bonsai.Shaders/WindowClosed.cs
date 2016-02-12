975de8b2681b8188a4c0474ddda53edc932a637d
glopes <glopes@localhost>
2016-02-12 01:43:30 +0000

    Added shader event source to signal window closure.

---
+++ Bonsai.Shaders/WindowClosed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Produces a sequence of events whenever the shader window is closed.")]
+    [Editor("Bonsai.Shaders.Design.ShaderConfigurationComponentEditor, Bonsai.Shaders.Design", typeof(ComponentEditor))]
+    public class WindowClosed : Source<EventPattern<EventArgs>>
+    {
+        public override IObservable<EventPattern<EventArgs>> Generate()
+        {
+            return ShaderManager.WindowSource.SelectMany(window => window.WindowClosed().Take(1));
+        }
+    }
+}
