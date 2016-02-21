06a600ba7c65afb09db786113b6602ff729fe622
glopes <glopesdev@users.noreply.github.com>
2016-02-21 18:21:59 +0000

    Added combinator to allow easy scheduling of notifications in the render loop.

---
+++ Bonsai.Shaders/SampleOnUpdateFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Samples the latest notifications of the input sequence in the render loop.")]
+    public class SampleOnUpdateFrame : Combinator
+    {
+        static readonly UpdateFrame updateFrame = new UpdateFrame();
+
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            var update = updateFrame.Generate();
+            return source.CombineLatest(update, (x, evt) => x).Sample(update);
+        }
+    }
+}
