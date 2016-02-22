1a80ffc88f73b2438f5a99905c7b6314e8bfb20e
glopes <glopesdev@users.noreply.github.com>
2016-02-22 17:34:53 +0000

    Added combinators to allow creating basic shader pipeline transformations.

---
+++ Bonsai.Shaders/CreateVector3.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    public class CreateVector3 : Source<Vector3>
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public override IObservable<Vector3> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Vector3(X, Y, Z)));
+        }
+
+        public IObservable<Vector3> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Vector3(X, Y, Z));
+        }
+    }
+}
