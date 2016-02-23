2b218e0b1cf5303e002cf2ec18a89c59297713b5
glopes <glopesdev@users.noreply.github.com>
2016-02-23 01:55:06 +0000

    Added user documentation strings to transformation constructors.

--- Bonsai.Shaders/CreateVector3.cs
+++ Bonsai.Shaders/CreateVector3.cs
@@ -1,6 +1,7 @@
 ﻿using OpenTK;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -8,12 +9,16 @@ using System.Threading.Tasks;
 
 namespace Bonsai.Shaders
 {
+    [Description("Creates a 3D vector element.")]
     public class CreateVector3 : Source<Vector3>
     {
+        [Description("The x-component of the vector.")]
         public float X { get; set; }
 
+        [Description("The y-component of the vector.")]
         public float Y { get; set; }
 
+        [Description("The z-component of the vector.")]
         public float Z { get; set; }
 
         public override IObservable<Vector3> Generate()
