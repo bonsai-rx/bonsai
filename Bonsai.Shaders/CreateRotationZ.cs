e5f982fd1a3b11af5b13379593bf5e08a7cc0d92
glopes <glopes@localhost>
2016-02-23 19:23:57 +0000

    Added combinators for constructing euler angle rotation matrices.

---
+++ Bonsai.Shaders/CreateRotationZ.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Creates a rotation matrix for a rotation around the z-axis.")]
+    public class CreateRotationZ : Source<Matrix4>
+    {
+        [Description("The angle describing the magnitude of the rotation about the z-axis.")]
+        public float Angle { get; set; }
+
+        public override IObservable<Matrix4> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(Matrix4.CreateRotationZ(Angle)));
+        }
+
+        public IObservable<Matrix4> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => Matrix4.CreateRotationZ(Angle));
+        }
+    }
+}
