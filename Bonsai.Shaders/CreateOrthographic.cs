554f7bba1023513fa0f944ed7bb7b8082674310f
glopes <glopesdev@users.noreply.github.com>
2016-03-09 11:46:40 +0000

    Added combinator to allow constructing orthographic projection matrices.

---
+++ Bonsai.Shaders/CreateOrthographic.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Creates an orthographic projection matrix.")]
+    public class CreateOrthographic : Source<Matrix4>
+    {
+        [Description("The width of the projection volume.")]
+        public float Width { get; set; }
+
+        [Description("The height of the projection volume.")]
+        public float Height { get; set; }
+
+        [Description("The distance to the near clip plane.")]
+        public float NearClip { get; set; }
+
+        [Description("The distance to the far clip plane.")]
+        public float FarClip { get; set; }
+
+        public override IObservable<Matrix4> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(Matrix4.CreateOrthographic(Width, Height, NearClip, FarClip)));
+        }
+
+        public IObservable<Matrix4> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(x => Matrix4.CreateOrthographic(Width, Height, NearClip, FarClip));
+        }
+    }
+}
