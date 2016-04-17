b22eaa5a4e4f54bf1827f300a5da64159958331d
glopes <glopes@localhost>
2016-04-17 15:46:49 +0100

    Added support for drawing instanced vertex data.

---
+++ Bonsai.Shaders/InstanceAttributeMapping.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    public class InstanceAttributeMapping : VertexAttributeMapping
+    {
+        public InstanceAttributeMapping()
+        {
+            Divisor = 1;
+        }
+
+        public int Divisor { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("InstanceAttribute({0}{1})", Type, Size);
+        }
+    }
+}
---
+++ Bonsai.Shaders/MeshInstanced.cs
@@ -0,0 +1,65 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    class MeshInstanced : IDisposable
+    {
+        int vbo;
+        int vao;
+        Mesh instanceMesh;
+
+        internal MeshInstanced(Mesh mesh)
+        {
+            instanceMesh = mesh;
+            GL.GenBuffers(1, out vbo);
+            GL.GenVertexArrays(1, out vao);
+        }
+
+        public int InstanceCount { get; set; }
+
+        public Mesh InstanceMesh
+        {
+            get { return instanceMesh; }
+        }
+
+        public int VertexBuffer
+        {
+            get { return vbo; }
+        }
+
+        public int VertexArray
+        {
+            get { return vao; }
+        }
+
+        public void Draw()
+        {
+            var eao = instanceMesh.ElementArray;
+            var vertexCount = instanceMesh.VertexCount;
+            var instanceCount = InstanceCount;
+            if (vertexCount > 0 && instanceCount > 0)
+            {
+                GL.BindVertexArray(vao);
+                if (eao > 0)
+                {
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, eao);
+                    GL.DrawElementsInstanced(instanceMesh.DrawMode, vertexCount, DrawElementsType.UnsignedShort, IntPtr.Zero, instanceCount);
+                    GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+                }
+                else GL.DrawArraysInstanced(instanceMesh.DrawMode, 0, vertexCount, instanceCount);
+                GL.BindVertexArray(0);
+            }
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteVertexArrays(1, ref vao);
+            GL.DeleteBuffers(1, ref vbo);
+        }
+    }
+}
