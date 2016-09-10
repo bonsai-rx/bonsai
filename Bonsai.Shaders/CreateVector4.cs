f81061e58c4dfc6de9048365aa4fe0de9a3f6aaa
glopes <glopesdev@users.noreply.github.com>
2016-09-10 13:29:44 +0100

    Added support for more vector constructors.

---
+++ Bonsai.Shaders/CreateVector4.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Creates a 4D vector element.")]
+    public class CreateVector4 : Source<Vector4>
+    {
+        [Description("The x-component of the vector.")]
+        public float X { get; set; }
+
+        [Description("The y-component of the vector.")]
+        public float Y { get; set; }
+
+        [Description("The z-component of the vector.")]
+        public float Z { get; set; }
+
+        [Description("The w-component of the vector.")]
+        public float W { get; set; }
+
+        public override IObservable<Vector4> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Vector4(X, Y, Z, W)));
+        }
+
+        public IObservable<Vector4> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Vector4(X, Y, Z, W));
+        }
+    }
+}
