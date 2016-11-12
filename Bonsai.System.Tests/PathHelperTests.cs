1f7d6eacd13ed9608345a10f8e760fbfa820f8a3
glopes <glopesdev@users.noreply.github.com>
2016-11-12 11:24:30 +0000

    Fixed #541. Added PathHelper tests and corrected edge cases.

---
+++ Bonsai.System.Tests/PathHelperTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bonsai.IO;
+using System.IO;
+
+namespace Bonsai.System.Tests
+{
+    [TestClass]
+    public class PathHelperTests
+    {
+        const string BasePath = "Folder\\Subfolder";
+        const string Extension = ".txt";
+        const string Suffix = "_suf";
+
+        [TestMethod]
+        public void AppendSuffix_EmptyPathEmptySuffix_ReturnsEmptyPath()
+        {
+            var path = PathHelper.AppendSuffix(string.Empty, string.Empty);
+            Assert.AreEqual(string.Empty, path);
+        }
+
+        [TestMethod]
+        public void AppendSuffix_EmptyPath_ReturnsSuffix()
+        {
+            var path = PathHelper.AppendSuffix(string.Empty, Suffix);
+            Assert.AreEqual(Suffix, path);
+        }
+
+        [TestMethod]
+        public void AppendSuffix_EmptySuffix_ReturnsPath()
+        {
+            var path = PathHelper.AppendSuffix(BasePath, string.Empty);
+            Assert.AreEqual(BasePath, path);
+        }
+
+        [TestMethod]
+        public void AppendSuffix_PathWithoutExtension_ReturnsPathAndSuffix()
+        {
+            var path = PathHelper.AppendSuffix(BasePath, Suffix);
+            Assert.AreEqual(BasePath + Suffix, path);
+        }
+
+        [TestMethod]
+        public void AppendSuffix_ExtensionWithEmptyFileName_ReturnsSuffixAndExtension()
+        {
+            var path = PathHelper.AppendSuffix(Extension, Suffix);
+            Assert.AreEqual(Suffix + Extension, path);
+        }
+
+        [TestMethod]
+        public void AppendSuffix_PathWithExtension_ReturnsSuffixBeforeExtension()
+        {
+            var basePath = Path.ChangeExtension(BasePath, Extension);
+            var path = PathHelper.AppendSuffix(basePath, Suffix);
+            Assert.AreEqual(Path.GetExtension(basePath), Path.GetExtension(path));
+            Assert.AreEqual(Path.GetFileNameWithoutExtension(basePath) + Suffix,
+                            Path.GetFileNameWithoutExtension(path));
+        }
+    }
+}
