b8b3f3791702bf873e51ef793be6a37bfb85b00b
glopes <glopes@localhost>
2016-11-12 13:06:34 +0000

    Added AudioManager class to allow sharing audio context across playback nodes.

---
+++ Bonsai.Audio/AudioContextConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Audio
+{
+    public class AudioContextConfiguration
+    {
+        [Description("The name of the output device used for playback.")]
+        [TypeConverter(typeof(PlaybackDeviceNameConverter))]
+        public string DeviceName { get; set; }
+
+        [Description("The playback frequency (Hz) used by the output device.")]
+        public int Frequency { get; set; }
+
+        [Description("The refresh frequency (Hz) used by the output device.")]
+        public int Refresh { get; set; }
+    }
+}
---
+++ Bonsai.Audio/AudioContextConfigurationCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Audio
+{
+    [XmlRoot("AudioContextSettings")]
+    public class AudioContextConfigurationCollection : KeyedCollection<string, AudioContextConfiguration>
+    {
+        protected override string GetKeyForItem(AudioContextConfiguration item)
+        {
+            return item.DeviceName;
+        }
+    }
+}
