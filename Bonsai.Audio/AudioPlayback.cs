47d808d7f8b69c070e65520422e75022717f35df
glopes <glopes@localhost>
2016-11-12 17:39:33 +0000

    Added support for creating and configuring AL audio sources.

--- Bonsai.Audio/AudioPlayback.cs
+++ Bonsai.Audio/AudioPlayback.cs
@@ -16,6 +16,8 @@ namespace Bonsai.Audio
     [Description("Plays the sequence of buffered samples to the specified audio output device.")]
     public class AudioPlayback : Sink<Mat>
     {
+        readonly CreateSource createSource = new CreateSource();
+
         public AudioPlayback()
         {
             Frequency = 44100;
@@ -23,86 +25,68 @@ namespace Bonsai.Audio
 
         [Description("The name of the output device used for playback.")]
         [TypeConverter(typeof(PlaybackDeviceNameConverter))]
-        public string DeviceName { get; set; }
+        public string DeviceName
+        {
+            get { return createSource.DeviceName; }
+            set { createSource.DeviceName = value; }
+        }
 
         [Description("The playback frequency (Hz) to use for input buffers.")]
         public int Frequency { get; set; }
 
-        static void ClearBuffers(int source, int input)
-        {
-            int[] freeBuffers;
-            if (input == 0)
-            {
-                int processedBuffers;
-                AL.GetSource(source, ALGetSourcei.BuffersProcessed, out processedBuffers);
-                if (processedBuffers == 0)
-                    return;
-
-                freeBuffers = AL.SourceUnqueueBuffers(source, processedBuffers);
-            }
-            else
-            {
-                freeBuffers = AL.SourceUnqueueBuffers(source, input);
-            }
-
-            AL.DeleteBuffers(freeBuffers);
-        }
-
         public override IObservable<Mat> Process(IObservable<Mat> source)
         {
-            return Observable.Using(
-                () => AudioManager.ReserveContext(DeviceName),
-                context =>
+            return source.Publish(ps => Process(createSource.Generate().TakeUntil(ps.LastAsync()), ps));
+        }
+
+        public IObservable<Mat> Process(IObservable<Mat> dataSource, IObservable<AudioSource> audioSource)
+        {
+            return Process(audioSource, dataSource);
+        }
+
+        public IObservable<Mat> Process(IObservable<AudioSource> audioSource, IObservable<Mat> dataSource)
+        {
+            return audioSource.SelectMany(source =>
+                dataSource.Do(input =>
                 {
-                    var sourceId = AL.GenSource();
-                    return source.Do(input =>
+                    var transpose = input.Rows < input.Cols;
+                    var channels = transpose ? input.Rows : input.Cols;
+                    if (channels > 2)
                     {
-                        var transpose = input.Rows < input.Cols;
-                        var channels = transpose ? input.Rows : input.Cols;
-                        if (channels > 2)
-                        {
-                            throw new InvalidOperationException("Unsupported number of channels for the specified output format.");
-                        }
+                        throw new InvalidOperationException("Unsupported number of channels for the specified output format.");
+                    }
 
-                        var format = channels == 2 ? ALFormat.Stereo16 : ALFormat.Mono16;
-                        var convertDepth = input.Depth != Depth.S16;
-                        if (convertDepth || transpose)
-                        {
-                            // Convert if needed
-                            if (convertDepth)
-                            {
-                                var temp = new Mat(input.Rows, input.Cols, Depth.S16, 1);
-                                CV.Convert(input, temp);
-                                input = temp;
-                            }
-
-                            // Transpose multichannel to column order
-                            if (transpose)
-                            {
-                                var temp = new Mat(input.Cols, input.Rows, input.Depth, 1);
-                                CV.Transpose(input, temp);
-                                input = temp;
-                            }
-                        }
-
-                        var buffer = AL.GenBuffer();
-                        AL.BufferData(buffer, format, input.Data, input.Rows * input.Step, Frequency);
-                        AL.SourceQueueBuffer(sourceId, buffer);
-
-                        ClearBuffers(sourceId, 0);
-                        if (AL.GetSourceState(sourceId) != ALSourceState.Playing)
-                        {
-                            AL.SourcePlay(sourceId);
-                        }
-                    }).Finally(() =>
+                    var format = channels == 2 ? ALFormat.Stereo16 : ALFormat.Mono16;
+                    var convertDepth = input.Depth != Depth.S16;
+                    if (convertDepth || transpose)
                     {
-                        int queuedBuffers;
-                        AL.GetSource(sourceId, ALGetSourcei.BuffersQueued, out queuedBuffers);
-                        ClearBuffers(sourceId, queuedBuffers);
+                        // Convert if needed
+                        if (convertDepth)
+                        {
+                            var temp = new Mat(input.Rows, input.Cols, Depth.S16, 1);
+                            CV.Convert(input, temp);
+                            input = temp;
+                        }
 
-                        AL.DeleteSource(sourceId);
-                    });
-                });
+                        // Transpose multichannel to column order
+                        if (transpose)
+                        {
+                            var temp = new Mat(input.Cols, input.Rows, input.Depth, 1);
+                            CV.Transpose(input, temp);
+                            input = temp;
+                        }
+                    }
+
+                    var buffer = AL.GenBuffer();
+                    AL.BufferData(buffer, format, input.Data, input.Rows * input.Step, Frequency);
+                    AL.SourceQueueBuffer(source.Id, buffer);
+
+                    source.ClearBuffers(0);
+                    if (AL.GetSourceState(source.Id) != ALSourceState.Playing)
+                    {
+                        AL.SourcePlay(source.Id);
+                    }
+                }));
         }
     }
 }
---
+++ Bonsai.Audio/CreateSource.cs
@@ -0,0 +1,49 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Audio
+{
+    public class CreateSource : Source<AudioSource>
+    {
+        [Description("The name of the output device used for playback.")]
+        [TypeConverter(typeof(PlaybackDeviceNameConverter))]
+        public string DeviceName { get; set; }
+
+        [TypeConverter("OpenCV.Net.NumericAggregateConverter, OpenCV.Net")]
+        public Vector3 Direction { get; set; }
+
+        [TypeConverter("OpenCV.Net.NumericAggregateConverter, OpenCV.Net")]
+        public Vector3 Position { get; set; }
+
+        [TypeConverter("OpenCV.Net.NumericAggregateConverter, OpenCV.Net")]
+        public Vector3 Velocity { get; set; }
+
+        public bool Looping { get; set; }
+
+        public bool Relative { get; set; }
+
+        public override IObservable<AudioSource> Generate()
+        {
+            return Observable.Using(
+                () => AudioManager.ReserveContext(DeviceName),
+                context =>
+                {
+                    var source = new AudioSource();
+                    source.Direction = Direction;
+                    source.Position = Position;
+                    source.Velocity = Velocity;
+                    source.Looping = Looping;
+                    source.Relative = Relative;
+                    return Observable.Return(source)
+                                     .Concat(Observable.Never(source))
+                                     .Finally(source.Dispose);
+                });
+        }
+    }
+}
