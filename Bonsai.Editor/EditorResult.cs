a2f59362b75fb2f46b97d60f959818bddc031890
glopes <glopes@localhost>
2016-12-12 09:19:28 +0000

    Refactored editor interface to allow multiple results and access to active path.

---
+++ Bonsai.Editor/EditorResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Editor
+{
+    public enum EditorResult
+    {
+        Exit,
+        ManagePackages
+    }
+}
