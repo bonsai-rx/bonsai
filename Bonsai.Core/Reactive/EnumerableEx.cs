d2c480a75bb491cb84465fcfdab16162f5930a43
glopes <glopes@localhost>
2016-12-20 04:39:09 +0000

    Updated combinator overloads with variable arguments to avoid ambiguous calls.

---
+++ Bonsai.Core/Reactive/EnumerableEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Reactive
+{
+    static class EnumerableEx
+    {
+        internal static IEnumerable<TSource> Concat<TSource>(TSource first, TSource second, params TSource[] remainder)
+        {
+            yield return first;
+            yield return second;
+            for (int i = 0; i < remainder.Length; i++)
+            {
+                yield return remainder[i];
+            }
+        }
+    }
+}
