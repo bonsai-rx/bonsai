eace4c4373e51ed9673ded39574886a10d7cd8e5
glopes <glopesdev@users.noreply.github.com>
2016-12-26 12:27:42 +0000

    Updated package configuration manager to cache gallery packages.

---
+++ Bonsai/GalleryPackagePathResolver.cs
@@ -0,0 +1,22 @@
+﻿using NuGet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai
+{
+    class GalleryPackagePathResolver : DefaultPackagePathResolver
+    {
+        public GalleryPackagePathResolver(string path)
+            : base(path)
+        {
+        }
+
+        public override string GetPackageDirectory(string packageId, SemanticVersion version)
+        {
+            return string.Empty;
+        }
+    }
+}
