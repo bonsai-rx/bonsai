3891edf2b8e07a514fdfb42b60397502b8a1680e
glopes <glopes@localhost>
2016-12-29 17:23:09 +0000

    Added mashup visualizers for image keypoint collections.

---
+++ Bonsai.Vision.Design/KeyPointCollectionMashupVisualizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai;
+using Bonsai.Vision.Design;
+using Bonsai.Vision;
+using OpenCV.Net;
+using Bonsai.Design;
+
+[assembly: TypeVisualizer(typeof(KeyPointCollectionMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, KeyPointCollectionVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class KeyPointCollectionMashupVisualizer : MashupTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            var keyPoints = (KeyPointCollection)value;
+            KeyPointCollectionVisualizer.Draw(visualizer.VisualizerImage, keyPoints);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
