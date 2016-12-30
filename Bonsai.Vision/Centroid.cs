8a358faf00c1fe10a9c868a515ba0e7ef915eb02
glopes <glopesdev@users.noreply.github.com>
2016-12-30 01:11:02 +0000

    Added support for computing the centroid over several point set representations.

---
+++ Bonsai.Vision/Centroid.cs
@@ -0,0 +1,117 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    public class Centroid : Transform<Point[], Point2f>
+    {
+        static readonly Point2f InvalidCentroid = new Point2f(float.NaN, float.NaN);
+
+        public override IObservable<Point2f> Process(IObservable<Point[]> source)
+        {
+            return source.Select(input =>
+            {
+                if (input.Length == 0) return InvalidCentroid;
+                var sum = Point2f.Zero;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    sum.X += input[i].X;
+                    sum.Y += input[i].Y;
+                }
+
+                return new Point2f(sum.X / input.Length, sum.Y / input.Length);
+            });
+        }
+
+        public IObservable<Point2f> Process(IObservable<IEnumerable<Point>> source)
+        {
+            return source.Select(input =>
+            {
+                var count = 0;
+                var sum = Point2f.Zero;
+                foreach (var point in input)
+                {
+                    sum.X += point.X;
+                    sum.Y += point.Y;
+                    count++;
+                }
+
+                if (count == 0) return InvalidCentroid;
+                else return new Point2f(sum.X / count, sum.Y / count);
+            });
+        }
+
+        public IObservable<Point2f> Process(IObservable<Point2f[]> source)
+        {
+            return source.Select(input =>
+            {
+                if (input.Length == 0) return InvalidCentroid;
+                var sum = Point2f.Zero;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    sum.X += input[i].X;
+                    sum.Y += input[i].Y;
+                }
+
+                return new Point2f(sum.X / input.Length, sum.Y / input.Length);
+            });
+        }
+
+        public IObservable<Point2f> Process(IObservable<IEnumerable<Point2f>> source)
+        {
+            return source.Select(input =>
+            {
+                var count = 0;
+                var sum = Point2f.Zero;
+                foreach (var point in input)
+                {
+                    sum.X += point.X;
+                    sum.Y += point.Y;
+                    count++;
+                }
+
+                if (count == 0) return InvalidCentroid;
+                else return new Point2f(sum.X / count, sum.Y / count);
+            });
+        }
+
+        static Point2f FromMoments(Moments moments)
+        {
+            if (moments.M00 > 0)
+            {
+                var x = moments.M10 / moments.M00;
+                var y = moments.M01 / moments.M00;
+                return new Point2f((float)x, (float)y);
+            }
+            else return InvalidCentroid;
+        }
+
+        public IObservable<Point2f> Process(IObservable<IplImage> source)
+        {
+            return source.Select(input =>
+            {
+                var moments = new Moments(input);
+                return FromMoments(moments);
+            });
+        }
+
+        public IObservable<Point2f> Process(IObservable<Contour> source)
+        {
+            return source.Select(input =>
+            {
+                var moments = new Moments(input);
+                return FromMoments(moments);
+            });
+        }
+
+        public IObservable<Point2f> Process(IObservable<ConnectedComponent> source)
+        {
+            return source.Select(input => input.Centroid);
+        }
+    }
+}
