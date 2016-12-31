dd610beb747e0ba995de4394281916206beb0f2b
glopes <glopes@localhost>
2016-12-31 00:27:21 +0000

    Updated circle and line segment marshalling directives.

--- Bonsai.Vision/LineSegment.cs
+++ Bonsai.Vision/LineSegment.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bonsai.Vision
 {
+    [StructLayout(LayoutKind.Sequential)]
     public struct LineSegment
     {
         public Point Start;
