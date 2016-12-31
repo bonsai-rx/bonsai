04a88365e7661bd95b59533b0a24604832bd7447
glopes <glopes@localhost>
2016-12-31 00:27:02 +0000

    Added visualizers for bounding box rectangles.

---
+++ Bonsai.Vision.Design/RectMashupVisualizer.cs
@@ -0,0 +1,34 @@
+﻿using Bonsai;
+using Bonsai.Design;
+using Bonsai.Vision.Design;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(RectMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, RectVisualizer>))]
+[assembly: TypeVisualizer(typeof(RectMashupVisualizer), Target = typeof(VisualizerMashup<ContoursVisualizer, RectVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class RectMashupVisualizer : MashupTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            RectVisualizer.Draw(visualizer.VisualizerImage, value);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
