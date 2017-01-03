811bdace4cb0c53120c833bf2a5d18e88d67d403
glopes <glopesdev@users.noreply.github.com>
2017-01-03 12:33:13 +0000

    Added support for image texture bindings.

---
+++ Bonsai.Shaders/Configuration/ImageTextureBindingConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders.Configuration
+{
+    public class ImageTextureBindingConfiguration : TextureBindingConfiguration
+    {
+        [Description("The type of access that will be performed on the image.")]
+        public TextureAccess Access { get; set; }
+
+        [Description("The format that the elements of the image will be treated as for the purposes of formatted stores.")]
+        public SizedInternalFormat Format { get; set; }
+
+        public override void Bind(Texture texture)
+        {
+            GL.BindImageTexture(
+                (int)(TextureSlot - TextureUnit.Texture0),
+                texture.Id,
+                0, false, 0,
+                Access,
+                Format);
+        }
+
+        public override void Unbind(Texture texture)
+        {
+            GL.BindImageTexture(
+                (int)(TextureSlot - TextureUnit.Texture0),
+                0,
+                0, false, 0,
+                Access,
+                Format);
+        }
+    }
+}
