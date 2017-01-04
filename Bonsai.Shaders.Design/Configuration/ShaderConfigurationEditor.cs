69bbee21c9e1ed0bf0676458e97c2b0496d221eb
glopes <glopesdev@users.noreply.github.com>
2017-01-04 13:14:49 +0000

    Added missing shader configuration editor source file.

---
+++ Bonsai.Shaders.Design/Configuration/ShaderConfigurationEditor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace Bonsai.Shaders.Configuration.Design
+{
+    class ShaderConfigurationEditor : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.DropDown;
+        }
+
+        protected virtual ShaderConfigurationControl CreateEditorControl()
+        {
+            return new ShaderConfigurationControl();
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+            if (editorService != null)
+            {
+                var configurationControl = CreateEditorControl();
+                configurationControl.SelectedValue = value;
+                configurationControl.SelectedValueChanged += delegate { editorService.CloseDropDown(); };
+                editorService.DropDownControl(configurationControl);
+                return configurationControl.SelectedValue;
+            }
+
+            return base.EditValue(context, provider, value);
+        }
+    }
+}
