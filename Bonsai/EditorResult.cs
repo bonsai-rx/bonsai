893a6a91db967d321655b38b696f9ee54448635a
glopes <glopes@localhost>
2017-01-06 01:04:48 +0000

    Internalized EditorResult to avoid loading editor assembly during bootstrap.

---
+++ Bonsai/EditorResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai
+{
+    enum EditorResult
+    {
+        Exit,
+        ManagePackages,
+        ExportPackage,
+        OpenGallery
+    }
+}
