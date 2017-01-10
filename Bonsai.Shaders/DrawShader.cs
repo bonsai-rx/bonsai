8e1f7ea75a3798d1fc8703faa2bed21e34812a68
glopes <glopesdev@users.noreply.github.com>
2017-01-10 18:16:09 +0000

    Added DrawShader as an obsolete type for backward compatibility.

---
+++ Bonsai.Shaders/DrawShader.cs
@@ -0,0 +1,39 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Obsolete]
+    [Description("Issues a shader draw call. This type is obsolete, please use DrawMesh instead.")]
+    public class DrawShader : Sink
+    {
+        readonly DrawMesh drawMesh = new DrawMesh();
+
+        [Description("The name of the material shader program.")]
+        [Editor("Bonsai.Shaders.Configuration.Design.MaterialConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
+        public string ShaderName
+        {
+            get { return drawMesh.MaterialName; }
+            set { drawMesh.MaterialName = value; }
+        }
+
+        [Description("The name of the mesh geometry to draw.")]
+        [Editor("Bonsai.Shaders.Configuration.Design.MeshConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
+        public string MeshName
+        {
+            get { return drawMesh.MeshName; }
+            set { drawMesh.MeshName = value; }
+        }
+
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return drawMesh.Process(source);
+        }
+    }
+}
