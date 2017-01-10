d758e121799dfa813de1ef9bcc3bf06405e7b66f
glopes <glopes@localhost>
2017-01-10 00:02:25 +0000

    Updated bootstrap to allow installing a gallery package from the command line.

---
+++ Bonsai/PropertyAssignment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai
+{
+    struct PropertyAssignment
+    {
+        public string Name;
+        public string Value;
+        const string PropertyAssignmentSeparator = "=";
+
+        public static PropertyAssignment Parse(string property)
+        {
+            if (string.IsNullOrEmpty(property))
+            {
+                throw new ArgumentNullException("property");
+            }
+
+            var assignment = property.Split(new[] { PropertyAssignmentSeparator }, 2, StringSplitOptions.None);
+            if (assignment.Length != 2)
+            {
+                throw new ArgumentException("Invalid property assignment.", "property");
+            }
+
+            PropertyAssignment result;
+            result.Name = assignment[0];
+            result.Value = assignment[1];
+            return result;
+        }
+    }
+}
