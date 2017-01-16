83c8948dd279dfa45fe836806b3f6ac7317d7ec0
glopes <glopesdev@users.noreply.github.com>
2017-01-16 16:32:50 +0000

    Added support for retrieving the state of the global desktop mouse cursor.

---
+++ Bonsai.Shaders/Input/MouseCursor.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders.Input
+{
+    [Description("Retrieves the state of the mouse cursor. The position is defined in absolute desktop points, with the origin placed at the top-left corner of the display.")]
+    public class MouseCursor : Source<MouseState>
+    {
+        public override IObservable<MouseState> Generate()
+        {
+            return ShaderManager.WindowSource.SelectMany(window => Observable.FromEventPattern<FrameEventArgs>(
+                handler => window.UpdateFrame += handler,
+                handler => window.UpdateFrame -= handler)
+                .Select(evt => OpenTK.Input.Mouse.GetCursorState())
+                .TakeUntil(window.WindowClosed()));
+        }
+
+        public IObservable<MouseState> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => OpenTK.Input.Mouse.GetCursorState());
+        }
+    }
+}
