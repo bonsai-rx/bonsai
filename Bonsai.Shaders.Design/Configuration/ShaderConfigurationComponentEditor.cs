8d74e04293530229a0046ee35549c3a1052e412c
glopes <glopesdev@users.noreply.github.com>
2017-01-24 14:13:54 +0000

    Added new viewport effect shader example.

--- Bonsai.Shaders.Design/Configuration/ShaderConfigurationComponentEditor.cs
+++ Bonsai.Shaders.Design/Configuration/ShaderConfigurationComponentEditor.cs
@@ -108,6 +108,21 @@ void main()
 
                 new GlslScriptExample
                 {
+                    Name = "Viewport Effect",
+                    Type = ShaderType.FragmentShader,
+                    Source = @"#version 400
+in vec2 texCoord;
+out vec4 fragColor;
+
+void main()
+{
+  fragColor = vec4(texCoord, 0.0, 1.0);
+}
+"
+                },
+
+                new GlslScriptExample
+                {
                     Name = "Diffuse Texture",
                     Type = ShaderType.FragmentShader,
                     Source = @"#version 400
