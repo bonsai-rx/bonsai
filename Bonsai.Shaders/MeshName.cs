522d2c2b3ce614eeec53a73d680a9769d4432810
glopes <glopesdev@users.noreply.github.com>
2017-02-26 12:34:01 +0000

    Generalized DrawMeshAggregate to allow specifying instanced rendering.

--- Bonsai.Shaders/MeshName.cs
+++ Bonsai.Shaders/MeshName.cs
@@ -12,8 +12,13 @@ namespace Bonsai.Shaders
     public class MeshName
     {
         [XmlText]
-        [Description("The name of the mesh geometry to draw.")]
+        [Description("The name of the mesh geometry to aggregate.")]
         [Editor("Bonsai.Shaders.Configuration.Design.MeshConfigurationEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
         public string Name { get; set; }
+
+        [DefaultValue(0)]
+        [XmlAttribute("divisor")]
+        [Description("Optionally specifies the number of instances populated by each buffer item in case of instanced rendering.")]
+        public int Divisor { get; set; }
     }
 }
