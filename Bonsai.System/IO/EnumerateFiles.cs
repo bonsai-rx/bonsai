91957617f0765a41aa08450f5f507c1f7075f37e
glopes <glopesdev@users.noreply.github.com>
2017-08-10 13:33:44 +0100

    Fixed #579. Added support for enumerating files and directories in a path.

---
+++ Bonsai.System/IO/EnumerateFiles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [DefaultProperty("Path")]
+    [Description("Returns a sequence of the file names that match the specified search pattern.")]
+    public class EnumerateFiles : Source<string>
+    {
+        public EnumerateFiles()
+        {
+            Path = ".";
+            SearchPattern = "*";
+        }
+
+        [Description("The path to search.")]
+        [Editor("Bonsai.Design.FolderNameEditor, Bonsai.Design", typeof(UITypeEditor))]
+        public string Path { get; set; }
+
+        [Description("The search string used to match against the names of files in the path.")]
+        public string SearchPattern { get; set; }
+
+        [Description("Specifies whether the search should include all subdirectories.")]
+        public SearchOption SearchOption { get; set; }
+
+        public override IObservable<string> Generate()
+        {
+            return Directory.EnumerateFiles(Path, SearchPattern, SearchOption).ToObservable();
+        }
+    }
+}
