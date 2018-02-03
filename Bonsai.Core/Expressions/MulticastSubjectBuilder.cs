125467ab49ef4941752e6829b5daf01f164c294d
glopes <glopes@localhost>
2018-02-03 15:13:03 +0000

    Updated multicast subject name converter to list only contravariant variables.

--- Bonsai.Core/Expressions/MulticastSubjectBuilder.cs
+++ Bonsai.Core/Expressions/MulticastSubjectBuilder.cs
@@ -18,8 +18,10 @@ namespace Bonsai.Expressions
     [WorkflowElementCategory(ElementCategory.Sink)]
     [XmlType("MulticastSubject", Namespace = Constants.XmlNamespace)]
     [Description("Pushes a sequence of values into a shared subject.")]
+    [TypeDescriptionProvider(typeof(MulticastSubjectTypeDescriptionProvider))]
     public class MulticastSubjectBuilder : SingleArgumentExpressionBuilder, IRequireSubject
     {
+        Type observableType;
         IBuildContext buildContext;
 
         /// <summary>
@@ -46,6 +48,7 @@ namespace Bonsai.Expressions
         /// <returns>An <see cref="Expression"/> tree node.</returns>
         public override Expression Build(IEnumerable<Expression> arguments)
         {
+            observableType = null;
             if (buildContext == null)
             {
                 throw new InvalidOperationException("No valid build context was provided.");
@@ -53,15 +56,63 @@ namespace Bonsai.Expressions
 
             var name = Name;
             var source = arguments.First();
+            observableType = source.Type.GetGenericArguments()[0];
             if (string.IsNullOrEmpty(name)) return source;
             var subjectExpression = buildContext.GetVariable(name);
-            var parameterType = source.Type.GetGenericArguments()[0];
-            return Expression.Call(typeof(MulticastSubjectBuilder), "Process", new[] { parameterType }, source, subjectExpression);
+            return Expression.Call(typeof(MulticastSubjectBuilder), "Process", new[] { observableType }, source, subjectExpression);
         }
 
         static IObservable<TSource> Process<TSource>(IObservable<TSource> source, IObserver<TSource> subject)
         {
             return source.Do(subject);
+        }
+
+        class MulticastSubjectTypeDescriptionProvider : TypeDescriptionProvider
+        {
+            readonly MulticastSubjectTypeDescriptor typeDescriptor = new MulticastSubjectTypeDescriptor(null);
+
+            public override ICustomTypeDescriptor GetTypeDescriptor(Type objectType, object instance)
+            {
+                return instance != null ? new MulticastSubjectTypeDescriptor(instance) : typeDescriptor;
+            }
+        }
+
+        class MulticastSubjectTypeDescriptor : CustomTypeDescriptor
+        {
+            MulticastSubjectBuilder builder;
+            static readonly ICustomTypeDescriptor baseDescriptor = TypeDescriptor.GetProvider(typeof(MulticastSubjectBuilder))
+                                                                                 .GetTypeDescriptor(typeof(MulticastSubjectBuilder));
+
+            public MulticastSubjectTypeDescriptor(object instance)
+                : base(baseDescriptor)
+            {
+                builder = (MulticastSubjectBuilder)instance;
+            }
+
+            public override PropertyDescriptorCollection GetProperties()
+            {
+                return GetProperties(null);
+            }
+
+            public override PropertyDescriptorCollection GetProperties(Attribute[] attributes)
+            {
+                var baseProperties = base.GetProperties(attributes);
+                if (builder == null) return baseProperties;
+
+                var properties = from property in baseProperties.Cast<PropertyDescriptor>()
+                                 let propertyAttributes = GetPropertyAttributes(property)
+                                 select new ExtendedPropertyDescriptor(property, propertyAttributes);
+                return new PropertyDescriptorCollection(properties.ToArray());
+            }
+
+            Attribute[] GetPropertyAttributes(PropertyDescriptor descriptor)
+            {
+                var attributes = descriptor.Attributes;
+                var result = new Attribute[attributes.Count + 1];
+                attributes.CopyTo(result, 0);
+                result[result.Length - 1] = new SubjectTargetAttribute(builder.observableType);
+                return result;
+            }
         }
     }
 }
---
+++ Bonsai.Core/Expressions/SubjectTargetAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Expressions
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    sealed class SubjectTargetAttribute : Attribute
+    {
+        public SubjectTargetAttribute(Type targetType)
+            : this(targetType != null ? targetType.AssemblyQualifiedName : null)
+        {
+        }
+
+        public SubjectTargetAttribute(string targetTypeName)
+        {
+            TargetTypeName = targetTypeName;
+        }
+
+        public string TargetTypeName { get; private set; }
+
+        public Type TargetType
+        {
+            get { return Type.GetType(TargetTypeName); }
+        }
+    }
+}
