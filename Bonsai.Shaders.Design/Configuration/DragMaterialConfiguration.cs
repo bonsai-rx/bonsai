a1cc245ea559b9f1920f0de79bfa941e3c102c03
glopes <glopes@localhost>
2018-02-04 18:43:02 +0000

    Added drag and drop support for resource files in shader configuration window.

---
+++ Bonsai.Shaders.Design/Configuration/DragMaterialConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders.Configuration.Design
+{
+    class DragMaterialConfiguration : DragResourceConfiguration
+    {
+        public DragMaterialConfiguration(CollectionEditorControl editor)
+            : base(editor)
+        {
+        }
+
+        protected override bool IsResourceAllowed(string fileName)
+        {
+            return Path.GetExtension(fileName) == ".mtl";
+        }
+
+        protected override object CreateResourceConfiguration(string fileName)
+        {
+            var material = new MaterialConfiguration();
+            material.Name = Path.GetFileNameWithoutExtension(fileName);
+            MtlReader.ReadMaterial(material, fileName);
+            return material;
+        }
+    }
+}
---
+++ Bonsai.Shaders.Design/Configuration/DragTextureConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Bonsai.Design;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders.Configuration.Design
+{
+    class DragTextureConfiguration : DragResourceConfiguration
+    {
+        static readonly string[] AllowedExtensions = new[] { ".png", ".bmp", ".jpg", ".jpeg", ".tif" };
+
+        public DragTextureConfiguration(CollectionEditorControl editor)
+            : base(editor)
+        {
+        }
+
+        protected override bool IsResourceAllowed(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            return Array.Exists(AllowedExtensions, extension.Equals);
+        }
+
+        protected override object CreateResourceConfiguration(string fileName)
+        {
+            var configuration = new ImageTexture();
+            configuration.FileName = PathConvert.GetProjectPath(fileName);
+            configuration.Name = Path.GetFileNameWithoutExtension(fileName);
+            return configuration;
+        }
+    }
+}
