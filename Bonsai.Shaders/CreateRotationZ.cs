b6f02869460a13ad9a08afa88d08909cf14acb8c
glopes <glopes@localhost>
2018-02-06 00:36:05 +0000

    Added sliders for rotation matrix constructor operators.

--- Bonsai.Shaders/CreateRotationZ.cs
+++ Bonsai.Shaders/CreateRotationZ.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing.Design;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Bonsai.Shaders
     [Description("Creates a rotation matrix for a rotation around the z-axis.")]
     public class CreateRotationZ : Source<Matrix4>
     {
+        [Range(-Math.PI, Math.PI)]
+        [Editor(DesignTypes.SliderEditor, typeof(UITypeEditor))]
         [Description("The angle describing the magnitude of the rotation about the z-axis.")]
         public float Angle { get; set; }
 
