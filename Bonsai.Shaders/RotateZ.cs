e60c86a81ec736a862065feb70df0e0272c3d7cb
glopes <glopes@localhost>
2018-02-08 03:32:11 +0000

    Added composable matrix transform operators.

---
+++ Bonsai.Shaders/RotateZ.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Applies a rotation around the z-axis.")]
+    public class RotateZ : MatrixTransform
+    {
+        [Range(-Math.PI, Math.PI)]
+        [Editor(DesignTypes.SliderEditor, typeof(UITypeEditor))]
+        [Description("The angle describing the magnitude of the rotation about the z-axis.")]
+        public float Angle { get; set; }
+
+        protected override void CreateTransform(out Matrix4 result)
+        {
+            Matrix4.CreateRotationZ(Angle, out result);
+        }
+    }
+}
---
+++ Bonsai.Shaders/TransformRelative.cs
@@ -0,0 +1,58 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Applies a relative model matrix transform specifying position, rotation and scale.")]
+    public class TransformRelative : MatrixTransform
+    {
+        Vector3 position;
+        Quaternion rotation;
+        Vector3 scale;
+
+        public TransformRelative()
+        {
+            Rotation = Quaternion.Identity;
+            Scale = Vector3.One;
+        }
+
+        [TypeConverter("OpenCV.Net.NumericAggregateConverter, OpenCV.Net")]
+        [Description("The relative position of the model, in the local coordinate frame.")]
+        public Vector3 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        [TypeConverter(typeof(QuaternionConverter))]
+        [Description("The quaternion representing the relative rotation of the model, in the local coordinate frame.")]
+        public Quaternion Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        [Description("The relative scale vector applied to the model, in the local coordinate frame.")]
+        [TypeConverter("OpenCV.Net.NumericAggregateConverter, OpenCV.Net")]
+        public Vector3 Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        protected override void CreateTransform(out Matrix4 result)
+        {
+            Matrix4 temp;
+            Matrix4.CreateScale(ref scale, out result);
+            Matrix4.CreateFromQuaternion(ref rotation, out temp);
+            Matrix4.Mult(ref result, ref temp, out result);
+            Matrix4.CreateTranslation(ref position, out temp);
+            Matrix4.Mult(ref result, ref temp, out result);
+        }
+    }
+}
