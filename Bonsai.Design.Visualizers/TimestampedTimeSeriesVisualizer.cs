700dfdd362dbab5e655513cf53a244025ce04d91
glopes <glopes@localhost>
2018-03-04 17:34:21 -0500

    Fixed #629. Added missing timestamped primitive type visualizers.

--- Bonsai.Design.Visualizers/TimestampedTimeSeriesVisualizer.cs
+++ Bonsai.Design.Visualizers/TimestampedTimeSeriesVisualizer.cs
@@ -7,6 +7,11 @@ using Bonsai;
 using Bonsai.Design.Visualizers;
 
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<int>), Target = typeof(Timestamped<int>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<uint>), Target = typeof(Timestamped<uint>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<long>), Target = typeof(Timestamped<long>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<ulong>), Target = typeof(Timestamped<ulong>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<short>), Target = typeof(Timestamped<short>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<ushort>), Target = typeof(Timestamped<ushort>))]
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<float>), Target = typeof(Timestamped<float>))]
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<double>), Target = typeof(Timestamped<double>))]
 
