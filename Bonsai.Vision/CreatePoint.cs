aaed6007e3ed3b4a7dc8d3bfc7e3718d2a2b6c47
glopes <glopes@localhost>
2018-03-18 15:37:11 +0000

    Added constructors for integer and single-precision point data types.

---
+++ Bonsai.Vision/CreatePoint.cs
@@ -0,0 +1,31 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [Description("Creates an integral 2D point value.")]
+    public class CreatePoint : Source<Point>
+    {
+        [Description("The x-component of the point.")]
+        public int X { get; set; }
+
+        [Description("The y-component of the point.")]
+        public int Y { get; set; }
+
+        public override IObservable<Point> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Point(X, Y)));
+        }
+
+        public IObservable<Point> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Point(X, Y));
+        }
+    }
+}
---
+++ Bonsai.Vision/CreatePoint2f.cs
@@ -0,0 +1,31 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [Description("Creates a single-precision 2D point value.")]
+    public class CreatePoint2f : Source<Point2f>
+    {
+        [Description("The x-component of the point.")]
+        public float X { get; set; }
+
+        [Description("The y-component of the point.")]
+        public float Y { get; set; }
+
+        public override IObservable<Point2f> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Point2f(X, Y)));
+        }
+
+        public IObservable<Point2f> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Point2f(X, Y));
+        }
+    }
+}
