33b1cab46fba05ab1bd054f63fe4bd786ffdadbe
glopes <glopes@localhost>
2018-05-02 22:28:24 +0100

    Added support for storing recently used files in editor settings.

---
+++ Bonsai.Editor/RecentlyUsedFile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Editor
+{
+    class RecentlyUsedFile
+    {
+        public RecentlyUsedFile(DateTimeOffset timestamp, string fileName)
+        {
+            Timestamp = timestamp;
+            FileName = fileName;
+        }
+
+        public DateTimeOffset Timestamp { get; private set; }
+
+        public string FileName { get; private set; }
+    }
+
+    class RecentlyUsedFileCollection : IEnumerable<RecentlyUsedFile>
+    {
+        readonly SortedList<DateTimeOffset, RecentlyUsedFile> files = new SortedList<DateTimeOffset, RecentlyUsedFile>();
+
+        public RecentlyUsedFileCollection(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; private set; }
+
+        public int Count
+        {
+            get { return files.Count; }
+        }
+
+        public void Add(string fileName)
+        {
+            var timestamp = DateTimeOffset.Now;
+            Add(timestamp, fileName);
+        }
+
+        public void Add(DateTimeOffset timestamp, string fileName)
+        {
+            var item = files.Values.FirstOrDefault(value => value.FileName == fileName);
+            if (item != null)
+            {
+                files.Remove(item.Timestamp);
+            }
+
+            item = new RecentlyUsedFile(timestamp, fileName);
+            while (files.Count >= Capacity)
+            {
+                files.RemoveAt(files.Count - 1);
+            }
+
+            files.Add(timestamp, item);
+        }
+
+        public IEnumerator<RecentlyUsedFile> GetEnumerator()
+        {
+            return files.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
