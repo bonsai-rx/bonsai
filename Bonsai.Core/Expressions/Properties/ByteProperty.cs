9bd78201ca86b71152a784051bfb6dea912891b3
glopes <glopes@localhost>
2018-06-02 11:15:30 +0100

    Added ByteProperty to allow easier construction of 8-bit integer values.

---
+++ Bonsai.Core/Expressions/Properties/ByteProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Expressions.Properties
+{
+    /// <summary>
+    /// Represents a workflow property containing an 8-bit unsigned integer.
+    /// </summary>
+    [DisplayName("Byte")]
+    [XmlType(Namespace = Constants.XmlNamespace)]
+    [Description("Represents a workflow property containing an 8-bit unsigned integer.")]
+    public class ByteProperty : WorkflowProperty<byte>
+    {
+    }
+}
