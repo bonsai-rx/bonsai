1742d2beebf168618eb6cea19763ac40747b5011
glopes <glopesdev@users.noreply.github.com>
2018-06-04 00:21:37 +0100

    Added operators for creating and updating Matrix2 and Matrix3 uniform values.

---
+++ Bonsai.Shaders/CreateMatrix2.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Bonsai.Shaders
+{
+    [Description("Creates a 2x2 matrix.")]
+    public class CreateMatrix2 : Source<Matrix2>
+    {
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("The top row of the matrix.")]
+        public Vector2 Row0 { get; set; }
+
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("The bottom row of the matrix.")]
+        public Vector2 Row1 { get; set; }
+
+        public override IObservable<Matrix2> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Matrix2(Row0, Row1)));
+        }
+
+        public IObservable<Matrix2> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Matrix2(Row0, Row1));
+        }
+    }
+}
---
+++ Bonsai.Shaders/CreateMatrix3.cs
@@ -0,0 +1,36 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Bonsai.Shaders
+{
+    [Description("Creates a 3x3 matrix containing 3D rotation and scale.")]
+    public class CreateMatrix3 : Source<Matrix3>
+    {
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("The top row of the matrix.")]
+        public Vector3 Row0 { get; set; }
+
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("The second row of the matrix.")]
+        public Vector3 Row1 { get; set; }
+
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("The bottom row of the matrix.")]
+        public Vector3 Row2 { get; set; }
+
+        public override IObservable<Matrix3> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Matrix3(Row0, Row1, Row2)));
+        }
+
+        public IObservable<Matrix3> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new Matrix3(Row0, Row1, Row2));
+        }
+    }
+}
