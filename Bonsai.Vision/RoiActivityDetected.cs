7f41b0bf34154cbea196fede135200811d88f8a6
glopes <glopesdev@users.noreply.github.com>
2018-06-21 08:35:42 +0100

    Ensure RoiActivityDetected threshold is applied even when no index is specified.

--- Bonsai.Vision/RoiActivityDetected.cs
+++ Bonsai.Vision/RoiActivityDetected.cs
@@ -24,7 +24,7 @@ namespace Bonsai.Vision
                 {
                     return input[Index.Value].Activity.Val0 > Threshold;
                 }
-                else return input.Where(region => region.Activity.Val0 > 0).Count() > 0;
+                else return input.Where(region => region.Activity.Val0 > Threshold).Count() > 0;
             });
         }
     }
