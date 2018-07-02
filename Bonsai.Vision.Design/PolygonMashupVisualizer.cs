f84e3c721632c355b4b82dbf0d7d0e0b0c3e0d60
glopes <glopes@localhost>
2018-07-02 23:36:25 +0100

    Added visualizers for polygon regions.

---
+++ Bonsai.Vision.Design/PolygonMashupVisualizer.cs
@@ -0,0 +1,33 @@
+﻿using Bonsai;
+using Bonsai.Design;
+using Bonsai.Vision.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: TypeVisualizer(typeof(PolygonMashupVisualizer), Target = typeof(VisualizerMashup<IplImageVisualizer, PolygonVisualizer>))]
+[assembly: TypeVisualizer(typeof(PolygonMashupVisualizer), Target = typeof(VisualizerMashup<ContoursVisualizer, PolygonVisualizer>))]
+
+namespace Bonsai.Vision.Design
+{
+    public class PolygonMashupVisualizer : MashupTypeVisualizer
+    {
+        IplImageVisualizer visualizer;
+
+        public override void Show(object value)
+        {
+            PolygonVisualizer.Draw(visualizer.VisualizerImage, value);
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            visualizer = (IplImageVisualizer)provider.GetService(typeof(DialogMashupVisualizer));
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
