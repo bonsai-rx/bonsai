1e400973791c532d0f17579e022c8065310b8099
glopes <glopes@localhost>
2018-07-06 01:56:23 +0100

    Fixed #633. Ensure graph edges override Equals to prevent unexpected duplicates.

--- Bonsai.Core/Dag/Edge.cs
+++ Bonsai.Core/Dag/Edge.cs
@@ -63,6 +63,30 @@ namespace Bonsai.Dag
         public Node<TNodeValue, TEdgeLabel> Target { get; private set; }
 
         /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="obj">An object to compare with this instance.</param>
+        /// <returns>
+        /// <b>true</b> if <paramref name="obj"/> is an instance of <see cref="Edge{TNodeValue, TEdgeLabel}"/>
+        /// and its index equals the index value of this instance; otherwise, false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            var edge = obj as Edge<TNodeValue, TEdgeLabel>;
+            if (edge == null) return false;
+            return Target == edge.Target && EqualityComparer<TEdgeLabel>.Default.Equals(Label, edge.Label);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return Target.GetHashCode() ^ EqualityComparer<TEdgeLabel>.Default.GetHashCode(Label);
+        }
+
+        /// <summary>
         /// Returns a string that represents this <see cref="T:Bonsai.Dag.Edge`2{T,U}"/> instance.
         /// </summary>
         /// <returns>
