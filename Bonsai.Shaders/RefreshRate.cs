934200e1c8e1c45b6a8a55f5013b1430a0ea4fec
glopes <glopes@localhost>
2018-07-16 14:00:09 +0100

    Added support for retrieving the target refresh rate of the shader window.

---
+++ Bonsai.Shaders/RefreshRate.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Gets the target refresh rate of the shader window.")]
+    public class RefreshRate : Transform<INativeWindow, double>
+    {
+        public override IObservable<double> Process(IObservable<INativeWindow> source)
+        {
+            return source.Select(input => ((ShaderWindow)input).RefreshRate);
+        }
+
+        public IObservable<double> Process<TEventArgs>(IObservable<EventPattern<INativeWindow, TEventArgs>> source)
+        {
+            return source.Select(input => ((ShaderWindow)input.Sender).RefreshRate);
+        }
+    }
+}
