047a0cd074f040bf59dee375cae79e7b406186a1
glopes <glopes@localhost>
2018-07-31 22:27:04 +0100

    Added support for path manipulation operators.

---
+++ Bonsai.System/IO/GetFileNameWithoutExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [Description("Returns the file name of the specified path string without the extension.")]
+    public class GetFileNameWithoutExtension : Transform<string, string>
+    {
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return source.Select(Path.GetFileNameWithoutExtension);
+        }
+    }
+}
---
+++ Bonsai.System/IO/GetFullPath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [Description("Returns the absolute path for the specified path string.")]
+    public class GetFullPath : Transform<string, string>
+    {
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            return source.Select(Path.GetFullPath);
+        }
+    }
+}
