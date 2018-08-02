b05c213f62af54a77b427548e82ffb16d374c26b
glopes <glopes@localhost>
2018-08-02 09:11:54 +0100

    Added composable resource loader architecture.

---
+++ Bonsai.Shaders/LoadResources.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    [Description("Loads a collection of resources into the resource manager.")]
+    public class LoadResources : Combinator<ResourceConfigurationCollection, IDisposable>
+    {
+        public override IObservable<IDisposable> Process(IObservable<ResourceConfigurationCollection> source)
+        {
+            return source.Select(input => input.ResourceManager.Load(input));
+        }
+
+        public IObservable<IDisposable> Process(IObservable<Tuple<IEnumerable<IResourceConfiguration>, ResourceManager>> source)
+        {
+            return source.Select(input => input.Item2.Load(input.Item1));
+        }
+    }
+}
---
+++ Bonsai.Shaders/ResourceConfigurationCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    public class ResourceConfigurationCollection : IEnumerable<IResourceConfiguration>
+    {
+        readonly ResourceManager manager;
+        readonly IEnumerable<IResourceConfiguration> resources;
+
+        public ResourceConfigurationCollection(ResourceManager resourceManager, IEnumerable<IResourceConfiguration> collection)
+        {
+            if (resourceManager == null)
+            {
+                throw new ArgumentNullException("resourceManager");
+            }
+
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection");
+            }
+
+            manager = resourceManager;
+            resources = collection;
+        }
+
+        public ResourceManager ResourceManager
+        {
+            get { return manager; }
+        }
+
+        public ResourceConfigurationCollection Add(IResourceConfiguration resource)
+        {
+            return AddRange(Enumerable.Repeat(resource, 1));
+        }
+
+        public ResourceConfigurationCollection AddRange(IEnumerable<IResourceConfiguration> collection)
+        {
+            return new ResourceConfigurationCollection(manager, resources.Concat(collection));
+        }
+
+        public IEnumerator<IResourceConfiguration> GetEnumerator()
+        {
+            return resources.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
