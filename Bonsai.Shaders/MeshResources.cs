8e31ff4afcf6784faf63a46a6e3dc1ee1cae0b8c
glopes <glopesdev@users.noreply.github.com>
2018-08-02 09:29:31 +0100

    Added resource loaders for meshes, textures and shaders.

---
+++ Bonsai.Shaders/MeshResources.cs
@@ -0,0 +1,34 @@
+﻿using Bonsai.Shaders.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Shaders
+{
+    [DefaultProperty("Meshes")]
+    [Description("Creates a collection of mesh resources to be loaded into the resource manager.")]
+    public class MeshResources : ResourceLoader
+    {
+        readonly MeshConfigurationCollection meshes = new MeshConfigurationCollection();
+
+        [XmlArrayItem(typeof(TexturedQuad))]
+        [XmlArrayItem(typeof(TexturedModel))]
+        [XmlArrayItem(typeof(MeshConfiguration))]
+        [Editor("Bonsai.Shaders.Configuration.Design.MeshConfigurationCollectionEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
+        [Description("The collection of mesh resources to be loaded into the resource manager.")]
+        public MeshConfigurationCollection Meshes
+        {
+            get { return meshes; }
+        }
+
+        protected override IEnumerable<IResourceConfiguration> GetResources()
+        {
+            return meshes;
+        }
+    }
+}
---
+++ Bonsai.Shaders/TextureResources.cs
@@ -0,0 +1,33 @@
+﻿using Bonsai.Shaders.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Shaders
+{
+    [DefaultProperty("Textures")]
+    [Description("Creates a collection of texture resources to be loaded into the resource manager.")]
+    public class TextureResources : ResourceLoader
+    {
+        readonly TextureConfigurationCollection textures = new TextureConfigurationCollection();
+
+        [XmlArrayItem(typeof(Texture2D))]
+        [XmlArrayItem(typeof(ImageTexture))]
+        [Editor("Bonsai.Shaders.Configuration.Design.TextureConfigurationCollectionEditor, Bonsai.Shaders.Design", typeof(UITypeEditor))]
+        [Description("The collection of texture resources to be loaded into the resource manager.")]
+        public TextureConfigurationCollection Textures
+        {
+            get { return textures; }
+        }
+
+        protected override IEnumerable<IResourceConfiguration> GetResources()
+        {
+            return textures;
+        }
+    }
+}
