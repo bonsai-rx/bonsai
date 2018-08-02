2ae42f287f8adc334fd66d5f3569a00f56bfdadc
glopes <glopes@localhost>
2018-08-02 09:25:33 +0100

    Added generic collection editor architecture for resource loaders.

---
+++ Bonsai.Shaders.Design/Configuration/CollectionEditorDialog.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonsai.Shaders.Configuration.Design
+{
+    partial class CollectionEditorDialog : Form
+    {
+        int initialHeight;
+        int initialCollectionEditorHeight;
+        EditorSite editorSite;
+
+        public CollectionEditorDialog()
+        {
+            InitializeComponent();
+            editorSite = new EditorSite(this);
+            propertyGrid.Site = editorSite;
+        }
+
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public CollectionEditorControl EditorControl
+        {
+            get { return collectionEditorControl; }
+        }
+
+        public Type CollectionItemType
+        {
+            get { return collectionEditorControl.CollectionItemType; }
+            set { collectionEditorControl.CollectionItemType = value; }
+        }
+
+        public Type[] NewItemTypes
+        {
+            get { return collectionEditorControl.NewItemTypes; }
+            set { collectionEditorControl.NewItemTypes = value; }
+        }
+
+        public IEnumerable Items
+        {
+            get { return collectionEditorControl.Items; }
+            set { collectionEditorControl.Items = value; }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            initialHeight = Height;
+            initialCollectionEditorHeight = propertyGrid.Height;
+            OnResize(EventArgs.Empty);
+            base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            if (initialHeight > 0)
+            {
+                var expansion = Height - initialHeight;
+                collectionEditorControl.Height = initialCollectionEditorHeight + expansion;
+            }
+            base.OnResize(e);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            propertyGrid.SelectedObject = null;
+            base.OnFormClosed(e);
+        }
+
+        void collectionEditorControl_SelectedItemChanged(object sender, EventArgs e)
+        {
+            var collectionEditor = (CollectionEditorControl)sender;
+            propertyGrid.SelectedObjects = collectionEditor.SelectedItems.Cast<object>().ToArray();
+        }
+
+        void propertyGrid_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+        {
+            collectionEditorControl.Refresh();
+        }
+
+        class EditorSite : ISite
+        {
+            CollectionEditorDialog siteDialog;
+
+            public EditorSite(CollectionEditorDialog dialog)
+            {
+                siteDialog = dialog;
+            }
+
+            public IComponent Component
+            {
+                get { return null; }
+            }
+
+            public IContainer Container
+            {
+                get { return null; }
+            }
+
+            public bool DesignMode
+            {
+                get { return false; }
+            }
+
+            public string Name { get; set; }
+
+            public object GetService(Type serviceType)
+            {
+                if (siteDialog.ServiceProvider != null)
+                {
+                    return siteDialog.ServiceProvider.GetService(serviceType);
+                }
+
+                return null;
+            }
+        }
+    }
+}
