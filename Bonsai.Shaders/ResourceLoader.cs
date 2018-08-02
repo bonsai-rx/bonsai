1aca07d1124557109350b2f84799c9611feb5696
glopes <glopesdev@users.noreply.github.com>
2018-08-02 09:11:54 +0100

    Added composable resource loader architecture.

---
+++ Bonsai.Shaders/ResourceLoader.cs
@@ -0,0 +1,36 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Shaders
+{
+    public abstract class ResourceLoader : Transform<ResourceConfigurationCollection, ResourceConfigurationCollection>
+    {
+        protected abstract IEnumerable<IResourceConfiguration> GetResources();
+
+        public IObservable<ResourceConfigurationCollection> Process(IObservable<INativeWindow> source)
+        {
+            return source.Select(input => new ResourceConfigurationCollection(((ShaderWindow)input).ResourceManager, GetResources()));
+        }
+
+        public IObservable<ResourceConfigurationCollection> Process<TEventArgs>(IObservable<EventPattern<INativeWindow, TEventArgs>> source)
+        {
+            return source.Select(input => new ResourceConfigurationCollection(((ShaderWindow)input.Sender).ResourceManager, GetResources()));
+        }
+
+        public IObservable<ResourceConfigurationCollection> Process(IObservable<ResourceManager> source)
+        {
+            return source.Select(input => new ResourceConfigurationCollection(input, GetResources()));
+        }
+
+        public override IObservable<ResourceConfigurationCollection> Process(IObservable<ResourceConfigurationCollection> source)
+        {
+            return source.Select(input => input.AddRange(GetResources()));
+        }
+    }
+}
