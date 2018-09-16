58cf0485f8adec9a2545e30c99638f8cb900d5ae
glopes <glopes@localhost>
2018-09-16 11:12:34 +0100

    Added numeric up down editors for integer based workflow properties.

--- Bonsai.Core/Expressions/Properties/ByteProperty.cs
+++ Bonsai.Core/Expressions/Properties/ByteProperty.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -14,7 +15,65 @@ namespace Bonsai.Expressions
     [DisplayName("Byte")]
     [XmlType(Namespace = Constants.XmlNamespace)]
     [Description("Represents a workflow property containing an 8-bit unsigned integer.")]
-    public class ByteProperty : WorkflowProperty<byte>
+    public class ByteProperty : WorkflowProperty
     {
+        byte value;
+        event Action<byte> ValueChanged;
+
+        /// <summary>
+        /// Gets or sets the value of the property.
+        /// </summary>
+        [Range(byte.MinValue, byte.MaxValue)]
+        [Description("The value of the property.")]
+        [Editor(DesignTypes.NumericUpDownEditor, DesignTypes.UITypeEditor)]
+        public byte Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnValueChanged(value);
+            }
+        }
+
+        internal override Type PropertyType
+        {
+            get { return typeof(byte); }
+        }
+
+        void OnValueChanged(byte value)
+        {
+            var handler = ValueChanged;
+            if (handler != null)
+            {
+                handler(value);
+            }
+        }
+
+        /// <summary>
+        /// Generates an observable sequence that produces a value whenever the
+        /// workflow property changes, starting with the initial property value.
+        /// </summary>
+        /// <returns>An observable sequence of property values.</returns>
+        public virtual IObservable<byte> Generate()
+        {
+            return Observable
+                .Defer(() => Observable.Return(value))
+                .Concat(Observable.FromEvent<byte>(
+                    handler => ValueChanged += handler,
+                    handler => ValueChanged -= handler));
+        }
+
+        /// <summary>
+        /// Generates an observable sequence that produces a value whenever the
+        /// source sequence emits a new element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">The source sequence used to generate new values.</param>
+        /// <returns>An observable sequence of property values.</returns>
+        public IObservable<byte> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(x => value);
+        }
     }
 }
