935d13df3a461fdf29f0fa642a207f451a829a67
glopes <glopesdev@users.noreply.github.com>
2018-09-16 10:59:30 +0100

    Ensure property mappings are disconnected rather than rerouted when disabled.

---
+++ Bonsai.Core/Expressions/DisconnectExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Expressions
+{
+    class DisconnectExpression : Expression
+    {
+        internal static readonly DisconnectExpression Instance = new DisconnectExpression();
+
+        private DisconnectExpression()
+        {
+        }
+
+        public override ExpressionType NodeType
+        {
+            get { return ExpressionType.Extension; }
+        }
+    }
+}
