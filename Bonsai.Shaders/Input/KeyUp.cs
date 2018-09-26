9005f8677e2520c5c18ee0e452f2d36caa152d5d
glopes <glopesdev@users.noreply.github.com>
2018-09-26 10:43:10 +0100

    Added optional filter properties for shader window keyboard and mouse events.

--- Bonsai.Shaders/Input/KeyUp.cs
+++ Bonsai.Shaders/Input/KeyUp.cs
@@ -15,11 +15,24 @@ namespace Bonsai.Shaders.Input
     [Editor("Bonsai.Shaders.Configuration.Design.ShaderConfigurationComponentEditor, Bonsai.Shaders.Design", typeof(ComponentEditor))]
     public class KeyUp : Source<EventPattern<INativeWindow, KeyboardKeyEventArgs>>
     {
+        [Description("The optional key to use as a filter.")]
+        public Key? Key { get; set; }
+
+        [Description("The optional key modifiers to use as a filter.")]
+        public KeyModifiers? Modifiers { get; set; }
+
         public override IObservable<EventPattern<INativeWindow, KeyboardKeyEventArgs>> Generate()
         {
             return ShaderManager.WindowSource.SelectMany(window => window.EventPattern<KeyboardKeyEventArgs>(
                 handler => window.KeyUp += handler,
-                handler => window.KeyUp -= handler));
+                handler => window.KeyUp -= handler))
+                .Where(evt =>
+                {
+                    var args = evt.EventArgs;
+                    var key = Key.GetValueOrDefault(args.Key);
+                    var modifiers = Modifiers.GetValueOrDefault(args.Modifiers);
+                    return args.Key == key && args.Modifiers == modifiers;
+                });
         }
     }
 }
