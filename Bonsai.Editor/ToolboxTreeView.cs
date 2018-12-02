a78ea1ec2cae71a3d9b88b13508a945a317a604b
glopes <glopes@localhost>
2018-12-02 15:54:20 +0000

    Ensure disabled tree view nodes keep the parent background color.

---
+++ Bonsai.Editor/ToolboxTreeView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonsai.Editor
+{
+    class ToolboxTreeView : TreeView
+    {
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+            foreach (TreeNode node in Nodes)
+            {
+                SetNodeEnabled(node);
+            }
+        }
+
+        void SetNodeEnabled(TreeNode node)
+        {
+            node.BackColor = Enabled ? Color.Empty : BackColor;
+            foreach (TreeNode child in node.Nodes)
+            {
+                SetNodeEnabled(child);
+            }
+        }
+    }
+}
