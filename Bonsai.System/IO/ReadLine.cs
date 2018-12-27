916d01aee8d0baf4802447d595d517dea900a5e8
glopes <glopes@localhost>
2018-12-27 16:55:11 +0000

    Added support for reading and writing lines from the standard IO stream.

---
+++ Bonsai.System/IO/ReadLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO
+{
+    [Description("Reads a line of characters asynchronously from the input stream.")]
+    public class ReadLine : Source<string>
+    {
+        IObservable<string> Generate(TextReader reader)
+        {
+            return Observable
+                .FromAsync(reader.ReadLineAsync)
+                .Repeat()
+                .TakeWhile(text => text != null);
+        }
+
+        public override IObservable<string> Generate()
+        {
+            return Observable.Using(
+                () => new StreamReader(Console.OpenStandardInput()),
+                reader => Generate(reader));
+        }
+
+        public IObservable<string> Generate(IObservable<TextReader> source)
+        {
+            return source.SelectMany(reader => Generate(reader));
+        }
+    }
+}
