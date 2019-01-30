fefe41b84e81a21959ce62c1c89d0a12a7d54374
glopes <glopes@localhost>
2019-01-30 14:10:52 +0000

    Added support for calibrating camera intrinsics from chessboard pattern views.

---
+++ Bonsai.Vision/CameraCalibration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    public struct CameraCalibration
+    {
+        public Intrinsics Intrinsics;
+        public double ReprojectionError;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{{Intrinsics: {0}, ReprojectionError: {1}}}",
+                Intrinsics,
+                ReprojectionError);
+        }
+    }
+}
