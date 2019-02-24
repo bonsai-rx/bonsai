fd48d8019fa5e3eaad5cb5691a5b321a5df5cf12
glopes <glopesdev@users.noreply.github.com>
2019-02-24 23:58:15 +0000

    Updated phong shading texture example to use both ambient and diffuse channels.

--- Bonsai.Shaders.Design/Configuration/ShaderConfigurationComponentEditor.cs
+++ Bonsai.Shaders.Design/Configuration/ShaderConfigurationComponentEditor.cs
@@ -176,7 +176,7 @@ uniform vec3 Ka;
 uniform vec3 Kd;
 uniform vec3 Ks;
 uniform float Ns = 1.0;
-uniform sampler2D mapKd;
+uniform sampler2D tex;
 uniform vec3 light;
 in vec3 position;
 in vec2 texCoord;
@@ -188,12 +188,13 @@ void main()
   vec3 L = normalize(light - position);
   vec3 R = normalize(-reflect(L, normal));
   vec3 V = normalize(-position);
+  vec4 texel = texture(tex, texCoord);
 
-  vec3 Iamb = Ka;
-  vec3 Idiff = Kd * texture(mapKd, texCoord).rgb * max(dot(normal, L), 0.0);
+  vec3 Iamb = Ka * texel.rgb;
+  vec3 Idiff = Kd * texel.rgb * max(dot(normal, L), 0.0);
   vec3 Ispec = Ks * pow(max(dot(R, V), 0.0), Ns);
 
-  fragColor = vec4(Iamb + Idiff + Ispec, 1.0);
+  fragColor = vec4(Iamb + Idiff + Ispec, texel.a);
 }
 "
                 }
