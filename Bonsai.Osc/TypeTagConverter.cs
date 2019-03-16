40c49c2833bac2fc5bf785ba230e4152f206098d
glopes <glopesdev@users.noreply.github.com>
2019-03-16 19:32:30 +0000

    Added Parse operator to allow retrieving content from generic messages.

---
+++ Bonsai.Osc/TypeTagConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Bonsai.Osc
+{
+    class TypeTagConverter : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(new[]
+            {
+                Osc.TypeTag.Int32,
+                Osc.TypeTag.Float,
+                Osc.TypeTag.String,
+                Osc.TypeTag.Blob,
+                Osc.TypeTag.Int64,
+                Osc.TypeTag.TimeTag,
+                Osc.TypeTag.Double,
+                Osc.TypeTag.Char
+            });
+        }
+    }
+}
