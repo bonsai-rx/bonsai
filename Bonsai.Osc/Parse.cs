9bc195ff8040ec72abf34229668aca6c345e1ee8
glopes <glopes@localhost>
2019-03-16 19:32:30 +0000

    Added Parse operator to allow retrieving content from generic messages.

---
+++ Bonsai.Osc/Parse.cs
@@ -0,0 +1,46 @@
+﻿using Bonsai.Expressions;
+using Bonsai.Osc.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Osc
+{
+    [Description("Retrieves data contents from an OSC message.")]
+    public class Parse : SelectBuilder
+    {
+        [TypeConverter(typeof(TypeTagConverter))]
+        [Description("The OSC type tag specifying the contents of the message.")]
+        public string TypeTag { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
+        {
+            var readerParameter = Expression.Parameter(typeof(BinaryReader));
+            var builder = Expression.Constant(this);
+            if (string.IsNullOrEmpty(TypeTag))
+            {
+                return expression;
+            }
+            else
+            {
+                var parseMessage = MessageParser.Content(TypeTag, readerParameter);
+                var messageParser = Expression.Lambda(parseMessage, readerParameter);
+                return Expression.Call(builder, "Process", new[] { messageParser.ReturnType }, expression, messageParser);
+            }
+        }
+
+        TResult Process<TResult>(Message message, Func<BinaryReader, TResult> messageReader)
+        {
+            var contents = message.GetStream();
+            using (var reader = new BigEndianReader(contents))
+            {
+                return messageReader(reader);
+            }
+        }
+    }
+}
--- Bonsai.Osc/ReceiveMessage.cs
+++ Bonsai.Osc/ReceiveMessage.cs
@@ -78,28 +78,5 @@ namespace Bonsai.Osc
                 }
             });
         }
-
-        class TypeTagConverter : StringConverter
-        {
-            public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
-            {
-                return true;
-            }
-
-            public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
-            {
-                return new StandardValuesCollection(new[]
-                {
-                    Osc.TypeTag.Int32,
-                    Osc.TypeTag.Float,
-                    Osc.TypeTag.String,
-                    Osc.TypeTag.Blob,
-                    Osc.TypeTag.Int64,
-                    Osc.TypeTag.TimeTag,
-                    Osc.TypeTag.Double,
-                    Osc.TypeTag.Char
-                });
-            }
-        }
     }
 }
