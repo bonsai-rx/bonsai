c24eb78cb31dc0f08278b580d5ad3fbdd48b71a8
glopes <glopesdev@users.noreply.github.com>
2019-03-28 09:02:49 +0000

    Added support for serializing list types.

---
+++ Bonsai.System.Design/DataMemberSelectorEditor.cs
@@ -0,0 +1,24 @@
+﻿using Bonsai.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.IO.Design
+{
+    class DataMemberSelectorEditor : MemberSelectorEditor
+    {
+        public DataMemberSelectorEditor()
+            : base(GetDataElementType, true)
+        {
+        }
+
+        static Type GetDataElementType(Expression expression)
+        {
+            var parameterType = expression.Type.GetGenericArguments()[0];
+            return ExpressionHelper.GetGenericTypeBindings(typeof(IList<>), parameterType).FirstOrDefault() ?? parameterType;
+        }
+    }
+}
