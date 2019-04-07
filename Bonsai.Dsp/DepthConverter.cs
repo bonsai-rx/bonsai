c3aa66b8c51291c9cbb734725ff16ed0a9f6a88d
glopes <glopes@localhost>
2019-04-07 19:47:31 +0100

    Avoid excluding the default null value.

--- Bonsai.Dsp/DepthConverter.cs
+++ Bonsai.Dsp/DepthConverter.cs
@@ -8,30 +8,17 @@ using System.Threading.Tasks;
 
 namespace Bonsai.Dsp
 {
-    class DepthConverter : EnumConverter
+    class DepthConverter : NullableConverter
     {
         public DepthConverter()
-            : base(typeof(Depth))
+            : base(typeof(Depth?))
         {
         }
 
         public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
         {
-            return new StandardValuesCollection(new[]
-            {
-                Depth.U8,
-                Depth.S8,
-                Depth.U16,
-                Depth.S16,
-                Depth.S32,
-                Depth.F32,
-                Depth.F64
-            });
-        }
-
-        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
-        {
-            return true;
+            var values = base.GetStandardValues(context);
+            return new StandardValuesCollection(values.Cast<object>().Where(value => (Depth?)value != Depth.UserType).ToArray());
         }
     }
 }
