988de06a5e89c95217c151b37539edf63bc11c2e
glopes <glopesdev@users.noreply.github.com>
2019-04-08 03:03:55 +0100

    Added common integer SampleRate property to all Audio operators.

--- Bonsai.Audio/AudioWriter.cs
+++ Bonsai.Audio/AudioWriter.cs
@@ -14,20 +14,44 @@ namespace Bonsai.Audio
     [Description("Writes the incoming sample sequence into an uncompressed RIFF/WAV file.")]
     public class AudioWriter : FileSink<Mat, RiffWriter>
     {
-        [Description("The sampling frequency (Hz) of the audio sequence.")]
-        public int SamplingFrequency { get; set; }
+        public AudioWriter()
+        {
+            SampleRate = 44100;
+        }
+
+        [Description("The sample rate of the input signal, in Hz.")]
+        public int SampleRate { get; set; }
+
+        [Browsable(false)]
+        public double? SamplingFrequency
+        {
+            get { return null; }
+            set
+            {
+                if (value != null)
+                {
+                    SampleRate = (int)value.Value;
+                }
+            }
+        }
+
+        [Browsable(false)]
+        public bool SamplingFrequencySpecified
+        {
+            get { return SamplingFrequency.HasValue; }
+        }
 
         protected override RiffWriter CreateWriter(string fileName, Mat input)
         {
-            var samplingFrequency = SamplingFrequency;
-            if (samplingFrequency <= 0)
+            var sampleRate = SampleRate;
+            if (sampleRate <= 0)
             {
-                throw new InvalidOperationException("Sampling frequency must be a positive integer in Hz.");
+                throw new InvalidOperationException("Sample rate must be a positive integer.");
             }
 
             var stream = new FileStream(fileName, FileMode.Create);
             var bitsPerSample = input.Depth == Depth.U8 ? 8 : 16;
-            return new RiffWriter(stream, input.Rows, samplingFrequency, bitsPerSample);
+            return new RiffWriter(stream, input.Rows, sampleRate, bitsPerSample);
         }
 
         protected override void Write(RiffWriter writer, Mat input)
