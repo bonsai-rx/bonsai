1b1569832a3006c2003e79eda0202580419fcca1
glopes <glopes@localhost>
2019-04-25 16:18:27 +0100

    Added keyboard support for closing the script editor dialog.

--- Bonsai.Scripting/PythonScriptEditorDialog.cs
+++ Bonsai.Scripting/PythonScriptEditorDialog.cs
@@ -51,6 +51,25 @@ namespace Bonsai.Scripting
             base.OnLoad(e);
         }
 
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && !e.Handled)
+            {
+                Close();
+                e.Handled = true;
+            }
+
+            base.OnKeyDown(e);
+        }
+
+        private void scintilla_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && e.Modifiers == Keys.Control)
+            {
+                okButton.PerformClick();
+            }
+        }
+
         private void scintilla_TextChanged(object sender, EventArgs e)
         {
             Script = scintilla.Text;
