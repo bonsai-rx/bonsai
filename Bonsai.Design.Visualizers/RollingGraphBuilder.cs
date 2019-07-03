707bb64d1b87ebed6107dbfa3c5a601584fc00e2
glopes <glopesdev@users.noreply.github.com>
2019-07-03 11:34:02 +0100

    Added first draft of rolling graph visualizer operator.

---
+++ Bonsai.Design.Visualizers/RollingGraphBuilder.cs
@@ -0,0 +1,100 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Design.Visualizers
+{
+    [DefaultProperty("ElementSelector")]
+    [TypeVisualizer(typeof(RollingGraphVisualizer))]
+    [Description("A visualizer that plots each element of the sequence as a rolling graph.")]
+    public class RollingGraphBuilder : SingleArgumentExpressionBuilder
+    {
+        [Editor(typeof(MemberSelectorEditor), typeof(UITypeEditor))]
+        [Description("The inner property that will be used as index for the graph.")]
+        public string IndexSelector { get; set; }
+
+        [Editor(typeof(MultiMemberSelectorEditor), typeof(UITypeEditor))]
+        [Description("The inner properties that will be displayed in the graph.")]
+        public string ElementSelector { get; set; }
+
+        internal VisualizerController Controller { get; set; }
+
+        internal class VisualizerController
+        {
+            internal int NumSeries;
+            internal Type IndexType;
+            internal string IndexLabel;
+            internal Action<object, RollingGraphView> Show;
+        }
+
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            var source = arguments.First();
+            var parameterType = source.Type.GetGenericArguments()[0];
+            var valueParameter = Expression.Parameter(typeof(object));
+            var viewParameter = Expression.Parameter(typeof(RollingGraphView));
+            var elementVariable = Expression.Variable(parameterType);
+            Controller = new VisualizerController();
+
+            Expression selectedX;
+            var selectorX = IndexSelector;
+            if (!string.IsNullOrEmpty(selectorX))
+            {
+                selectedX = SelectMembers(elementVariable, selectorX).First();
+                Controller.IndexLabel = selectorX;
+            }
+            else
+            {
+                selectedX = Expression.Property(null, typeof(DateTime), "Now");
+                Controller.IndexLabel = "Time";
+            }
+
+            if (selectedX.Type == typeof(DateTimeOffset)) selectedX = Expression.Property(selectedX, "DateTime");
+            if (selectedX.Type == typeof(DateTime))
+            {
+                selectedX = Expression.Convert(selectedX, typeof(ZedGraph.XDate));
+            }
+            Controller.IndexType = selectedX.Type;
+            selectedX = Expression.Convert(selectedX, typeof(double));
+
+            Expression showBody;
+            var selectedMembers = SelectMembers(elementVariable, ElementSelector)
+                .Select(x => x.Type.IsArray ? x : Expression.Convert(x, typeof(object))).ToArray();
+            if (selectedMembers.Length == 1 && selectedMembers[0].Type.IsArray)
+            {
+                var selectedValues = Expression.Convert(selectedMembers[0], typeof(Array));
+                showBody = Expression.Block(new[] { elementVariable },
+                    Expression.Assign(elementVariable, Expression.Convert(valueParameter, parameterType)),
+                    Expression.Call(typeof(RollingGraphBuilder), "ShowArrayValues", null, viewParameter, selectedX, selectedValues));
+            }
+            else
+            {
+                var selectedValues = Expression.NewArrayInit(typeof(object), selectedMembers);
+                showBody = Expression.Block(new[] { elementVariable },
+                    Expression.Assign(elementVariable, Expression.Convert(valueParameter, parameterType)),
+                    Expression.Call(viewParameter, "AddValues", null, selectedX, selectedValues));
+            }
+
+            Controller.NumSeries = selectedMembers.Length;
+            Controller.Show = Expression.Lambda<Action<object, RollingGraphView>>(showBody, valueParameter, viewParameter).Compile();
+            return Expression.Call(typeof(RollingGraphBuilder), "Process", new[] { parameterType }, source);
+        }
+
+        static void ShowArrayValues(RollingGraphView view, double index, Array values)
+        {
+            if (values.Length != view.Graph.NumSeries) view.Graph.NumSeries = values.Length;
+            view.Graph.AddValues(index, values);
+        }
+
+        static IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source;
+        }
+    }
+}
---
+++ Bonsai.Design.Visualizers/RollingGraphVisualizer.cs
@@ -0,0 +1,86 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZedGraph;
+
+namespace Bonsai.Design.Visualizers
+{
+    public class RollingGraphVisualizer : DialogTypeVisualizer
+    {
+        RollingGraphView view;
+        RollingGraphBuilder.VisualizerController controller;
+        static readonly TimeSpan TargetElapsedTime = TimeSpan.FromSeconds(1.0 / 30);
+        DateTimeOffset updateTime;
+
+        public RollingGraphVisualizer()
+        {
+            AutoScale = true;
+            Capacity = 640;
+            Max = 1;
+        }
+
+        public int Capacity { get; set; }
+
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+
+        public bool AutoScale { get; set; }
+
+        public override void Show(object value)
+        {
+            var time = DateTime.Now;
+            controller.Show(value, view);
+            if ((time - updateTime) > TargetElapsedTime)
+            {
+                view.Graph.Invalidate();
+                updateTime = time;
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            var context = (ITypeVisualizerContext)provider.GetService(typeof(ITypeVisualizerContext));
+            var lineChartBuilder = (RollingGraphBuilder)ExpressionBuilder.GetVisualizerElement(context.Source).Builder;
+            controller = lineChartBuilder.Controller;
+
+            view = new RollingGraphView();
+            view.Capacity = Capacity;
+            view.AutoScale = AutoScale;
+            if (!AutoScale)
+            {
+                view.Min = Min;
+                view.Max = Max;
+            }
+
+            view.HandleDestroyed += delegate
+            {
+                Min = view.Min;
+                Max = view.Max;
+                AutoScale = view.AutoScale;
+                Capacity = view.Capacity;
+            };
+
+            view.NumSeries = controller.NumSeries;
+            view.Dock = System.Windows.Forms.DockStyle.Fill;
+            GraphHelper.FormatOrdinalAxis(view.Graph.GraphPane.XAxis, controller.IndexType);
+            GraphHelper.SetAxisLabel(view.Graph.GraphPane.XAxis, controller.IndexLabel);
+
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(view);
+            }
+        }
+
+        public override void Unload()
+        {
+            view.Dispose();
+            view = null;
+            controller = null;
+        }
+    }
+}
