895affc763973e65f15c1c9d3fa0c66e98c227f6
glopes <glopes@localhost>
2019-08-11 02:10:03 +0100

    Added support for specifying recommended prefixes for XML type namespaces.

--- Bonsai.Core/Properties/AssemblyInfo.cs
+++ Bonsai.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Bonsai;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.4.0.0")]
 [assembly: AssemblyInformationalVersion("2.4.0")]
+[assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Reactive", "rx")]
---
+++ Bonsai.Core/XmlNamespacePrefixAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai
+{
+    /// <summary>
+    /// Specifies a recommended prefix to associate with a XML namespace identifier
+    /// when serializing a workflow file.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class XmlNamespacePrefixAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlNamespacePrefixAttribute"/> class
+        /// with the specified namespace and prefix.
+        /// </summary>
+        /// <param name="xmlNamespace">The XML namespace identifier.</param>
+        /// <param name="prefix">The recommended prefix for the namespace.</param>
+        public XmlNamespacePrefixAttribute(string xmlNamespace, string prefix)
+        {
+            XmlNamespace = xmlNamespace;
+            Prefix = prefix;
+        }
+
+        /// <summary>
+        /// Gets or sets the XML namespace identifier.
+        /// </summary>
+        public string XmlNamespace { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recommended prefix for the namespace.
+        /// </summary>
+        public string Prefix { get; set; }
+    }
+}
