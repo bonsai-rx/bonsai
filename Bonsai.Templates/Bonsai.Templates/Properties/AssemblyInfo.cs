54d11f36141f0a43dc991b04d48159594439eb0c
glopes <glopesdev@users.noreply.github.com>
2019-09-01 21:32:31 +0100

    Updated project and item template copyright strings.

--- Bonsai.Templates/Bonsai.Templates/Properties/AssemblyInfo.cs
+++ Bonsai.Templates/Bonsai.Templates/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.Templates")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2018")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
