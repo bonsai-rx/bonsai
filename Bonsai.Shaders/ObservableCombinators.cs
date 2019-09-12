29d134836c06ce803b9fc0123afbb73c800c58f4
glopes <glopes@localhost>
2019-09-12 13:07:39 +0100

    Ensure window resource is obtained before subscribing to the main sequence.

--- Bonsai.Shaders/ObservableCombinators.cs
+++ Bonsai.Shaders/ObservableCombinators.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
@@ -21,10 +22,21 @@ namespace Bonsai.Shaders
             IObservable<TSource2> second,
             Func<TSource1, TSource2, TResult> resultSelector)
         {
-            return first.Publish(ps1 => second.Publish(ps2 =>
-                ps1.CombineLatest(ps2, resultSelector)
-                   .TakeUntil(ps1.LastOrDefaultAsync())
-                   .TakeUntil(ps2.LastOrDefaultAsync())));
+            return Observable.Create<TResult>(observer =>
+            {
+                var disposable1 = new SingleAssignmentDisposable();
+                var disposable2 = new SingleAssignmentDisposable();
+                disposable1.Disposable = second.SubscribeSafe(Observer.Create<TSource2>(
+                    x2 =>
+                    {
+                        disposable2.Disposable = first
+                            .Select(x1 => resultSelector(x1, x2))
+                            .SubscribeSafe(observer);
+                    },
+                    observer.OnError,
+                    observer.OnCompleted));
+                return new CompositeDisposable(disposable2, disposable1);
+            });
         }
     }
 }
