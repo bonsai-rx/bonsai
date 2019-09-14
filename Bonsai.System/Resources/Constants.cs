b689f3a083c6bb5477ff5e1b08f9ec7a0be2419a
glopes <glopes@localhost>
2019-09-14 12:07:29 +0100

    Added explicit XmlTypeAttribute to avoid name clash in base class serialization.

---
+++ Bonsai.System/Resources/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Resources
+{
+    static class Constants
+    {
+        public const string XmlNamespace = "clr-namespace:Bonsai.Resources;assembly=Bonsai.System";
+    }
+}
--- Bonsai.System/Resources/ResourceLoader.cs
+++ Bonsai.System/Resources/ResourceLoader.cs
@@ -5,9 +5,11 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Bonsai.Resources
 {
+    [XmlType(Namespace = Constants.XmlNamespace)]
     public abstract class ResourceLoader : Transform<ResourceConfigurationCollection, ResourceConfigurationCollection>
     {
         protected internal abstract IEnumerable<IResourceConfiguration> GetResources();
