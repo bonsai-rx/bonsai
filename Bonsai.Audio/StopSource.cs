54f8659253ad2296d5c1058e0e92bb204e9fc367
glopes <glopesdev@users.noreply.github.com>
2019-09-22 18:43:28 +0100

    Added support for updating the playback state of multiple audio sources.

---
+++ Bonsai.Audio/StopSource.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Audio.OpenAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Audio
+{
+    [Description("Stops the specified set of audio sources.")]
+    public class StopSource : UpdateSourceState
+    {
+        internal override void Update(int[] sources)
+        {
+            AL.SourceStop(sources.Length, sources);
+        }
+    }
+}
