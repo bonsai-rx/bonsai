fc5a071a1db80a6df66261fdbce1dfdd2e9f75cb
glopes <glopesdev@users.noreply.github.com>
2019-09-23 19:49:15 +0100

    Added support for sending and receiving raw firmata sysex messages.

---
+++ Bonsai.Arduino/SendSysex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Arduino
+{
+    [DefaultProperty("Feature")]
+    [Description("Sends a sequence of system exclusive messages to the specified Arduino.")]
+    public class SendSysex : Sink<byte[]>
+    {
+        [Editor("Bonsai.Arduino.Design.ArduinoConfigurationEditor, Bonsai.Arduino.Design", typeof(UITypeEditor))]
+        [Description("The name of the serial port used to communicate with the Arduino.")]
+        public string PortName { get; set; }
+
+        [Description("The feature ID used to identify the system exclusive message payload.")]
+        public int Feature { get; set; }
+
+        public override IObservable<byte[]> Process(IObservable<byte[]> source)
+        {
+            return Observable.Using(
+                () => ArduinoManager.ReserveConnection(PortName),
+                connection => source.Do(input =>
+                {
+                    lock (connection.Arduino)
+                    {
+                        connection.Arduino.SendSysex((byte)Feature, input);
+                    }
+                }));
+        }
+    }
+}
