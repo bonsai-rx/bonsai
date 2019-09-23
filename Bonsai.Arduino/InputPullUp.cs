e608efa5a701bca3ad2fc3c79e3c6a9b6a28de6d
glopes <glopesdev@users.noreply.github.com>
2019-09-23 19:44:37 +0100

    Added support for reading digital inputs using internal pull-up resistor.

---
+++ Bonsai.Arduino/InputPullUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Arduino
+{
+    [DefaultProperty("Pin")]
+    [Description("Produces a sequence of digital state transitions from the specified Arduino input pin in pull-up mode.")]
+    public class InputPullUp : Source<bool>
+    {
+        [Editor("Bonsai.Arduino.Design.ArduinoConfigurationEditor, Bonsai.Arduino.Design", typeof(UITypeEditor))]
+        [Description("The name of the serial port used to communicate with the Arduino.")]
+        public string PortName { get; set; }
+
+        [Description("The digital input pull-up pin number from which to take readings.")]
+        public int Pin { get; set; }
+
+        public override IObservable<bool> Generate()
+        {
+            return ObservableArduino.DigitalInput(PortName, Pin, PinMode.InputPullUp);
+        }
+    }
+}
