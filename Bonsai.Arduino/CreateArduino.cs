34c575922c2324a388f2e75d93e377581e0594b6
glopes <glopes@localhost>
2019-09-23 19:50:05 +0100

    Added support for explicitly creating and configuring a firmata connection.

---
+++ Bonsai.Arduino/CreateArduino.cs
@@ -0,0 +1,43 @@
+﻿using Bonsai.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Arduino
+{
+    [Description("Creates a connection to an Arduino board using the Firmata protocol.")]
+    public class CreateArduino : Source<Arduino>
+    {
+        public CreateArduino()
+        {
+            BaudRate = Arduino.DefaultBaudRate;
+            SamplingInterval = Arduino.DefaultSamplingInterval;
+        }
+
+        [TypeConverter(typeof(SerialPortNameConverter))]
+        [Description("The name of the serial port connection.")]
+        public string PortName { get; set; }
+
+        [TypeConverter(typeof(BaudRateConverter))]
+        [Description("The baud rate used by the serial port connection.")]
+        public int BaudRate { get; set; }
+
+        [Description("The interval (ms) controlling how often analog and I2C data are sampled and transmitted.")]
+        public int SamplingInterval { get; set; }
+
+        public override IObservable<Arduino> Generate()
+        {
+            return Observable.Using(
+                () => ArduinoManager.ReserveConnection(PortName, BaudRate, SamplingInterval),
+                resource =>
+                {
+                    return Observable.Return(resource.Arduino)
+                                     .Concat(Observable.Never(resource.Arduino));
+                });
+        }
+    }
+}
