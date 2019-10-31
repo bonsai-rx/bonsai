06aa883692a50f4e9ac9d5a44c9db24a180841af
glopes <glopes@localhost>
2019-10-31 17:22:57 +0000

    Added unit tests for top-level prefix reuse and edge case causing prefix clash.

---
+++ Bonsai.Core.Tests/TestTypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Core.Tests
+{
+    [XmlInclude(typeof(PolyType))]
+    [XmlInclude(typeof(MorphicType))]
+    [XmlInclude(typeof(ExtraTypes.ExtraType))]
+    [XmlType(Namespace = XmlNamespace)]
+    public abstract class PolymorphicType
+    {
+        public const string XmlNamespace = "clr-namespace:Bonsai.Core.Tests;assembly=Bonsai.Core.Tests";
+
+        public PolymorphicType Extension { get; set; }
+    }
+
+    [XmlType(Namespace = XmlNamespace)]
+    public class PolyType : PolymorphicType
+    {
+    }
+
+    [XmlType(Namespace = XmlNamespace)]
+    public class MorphicType : PolymorphicType
+    {
+    }
+
+    namespace ExtraTypes
+    {
+        [XmlType(Namespace = "clr-namespace:Bonsai.Core.Tests.ExtraTypes;assembly=Bonsai.Core.Tests")]
+        public class ExtraType : PolymorphicType
+        {
+        }
+    }
+}
