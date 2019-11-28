45786900c64b4852cd2e05f0648788d846e40f9f
glopes <glopes@localhost>
2019-11-28 16:11:02 +0000

    Updated all package projects to the new csproj format.

--- Bonsai.Shaders/Properties/AssemblyInfo.cs
+++ Bonsai.Shaders/Properties/AssemblyInfo.cs
@@ -1,40 +1,8 @@
 ﻿using Bonsai;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai - Shaders Library")]
-[assembly: AssemblyDescription("Bonsai Shaders Library containing modules for dynamic control of shader primitives.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gonçalo Lopes")]
-[assembly: AssemblyProduct("Bonsai.Shaders")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2011-2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4a274c68-1951-423e-8885-9b355a795fc8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.20.0.0")]
-[assembly: AssemblyInformationalVersion("0.20.0")]
 [assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Shaders", "gl")]
 [assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Shaders.Input", "gli")]
 [assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Shaders")]
--- Bonsai.Windows.Input/Properties/AssemblyInfo.cs
+++ Bonsai.Windows.Input/Properties/AssemblyInfo.cs
@@ -1,39 +1,7 @@
 ﻿using Bonsai;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai - Windows Input Library")]
-[assembly: AssemblyDescription("Bonsai Windows Library containing reactive interfaces to the standard operating system input devices.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gonçalo Lopes")]
-[assembly: AssemblyProduct("Bonsai.Windows.Input")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2011-2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5dc867e6-5c0c-4811-8074-5b35faf19ed7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
 [assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Windows.Input", "wie")]
 [assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Hid")]
