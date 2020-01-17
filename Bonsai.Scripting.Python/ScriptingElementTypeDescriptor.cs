f5b252ab07c92ced44c2714b8a64c7b43de0995c
glopes <glopes@localhost>
2020-01-17 22:02:24 +0000

    Move python scripting operators to dedicated package.

---
+++ Bonsai.Scripting.Python/ScriptingElementTypeDescriptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Scripting
+{
+    class ScriptingElementTypeDescriptor : CustomTypeDescriptor
+    {
+        readonly IScriptingElement element;
+        readonly string defaultDescription;
+
+        public ScriptingElementTypeDescriptor(object instance, string description)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            element = instance as IScriptingElement;
+            defaultDescription = description;
+        }
+
+        public override AttributeCollection GetAttributes()
+        {
+            var description = defaultDescription;
+            if (element != null && !string.IsNullOrEmpty(element.Description))
+            {
+                description = element.Description;
+            }
+
+            return new AttributeCollection(new DescriptionAttribute(description));
+        }
+    }
+}
