dc2a91ee9ac469c74fee4df6acb7fae46032ce8b
glopesdev <glopesdev@users.noreply.github.com>
2020-04-05 10:16:50 +0100

    Add operators to sample and update keyboard state

---
+++ Bonsai.Windows.Input/KeyState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Bonsai.Windows.Input
+{
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Determines whether a key is up or down at the time of notification.")]
+    public class KeyState : Combinator<bool>
+    {
+        [Description("The target key to be observed.")]
+        public Keys Filter { get; set; }
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys key);
+
+        public override IObservable<bool> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => (GetAsyncKeyState(Filter) & 0x8000) != 0);
+        }
+    }
+}
---
+++ Bonsai.Windows.Input/SendKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+namespace Bonsai.Windows.Input
+{
+    [Description("Sends one or more keystrokes to the active window as if typed at the keyboard.")]
+    public class SendKeys : Sink
+    {
+        [Description("The string expression specifying the keystrokes to send.")]
+        public string Keys { get; set; }
+
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Do(input => System.Windows.Forms.SendKeys.SendWait(Keys));
+        }
+    }
+}
