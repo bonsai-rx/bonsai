ec49be74bc0d43b00a61d3326567acc037dfb684
glopesdev <glopesdev@users.noreply.github.com>
2020-04-05 14:27:37 +0100

    Add operator to update cursor position

---
+++ Bonsai.Windows.Input/SetCursorPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace Bonsai.Windows.Input
+{
+    [Description("Sets the current position of the mouse cursor.")]
+    public class SetCursorPosition : Sink<Point>
+    {
+        public override IObservable<Point> Process(IObservable<Point> source)
+        {
+            return source.Do(input => Cursor.Position = input);
+        }
+    }
+}
