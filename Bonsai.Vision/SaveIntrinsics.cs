314c109a41b48a2b81893fb9d83567c6f3842afb
glopesdev <glopesdev@users.noreply.github.com>
2020-04-07 18:39:34 +0100

    Save reprojection error for camera calibration

--- Bonsai.Vision/SaveIntrinsics.cs
+++ Bonsai.Vision/SaveIntrinsics.cs
@@ -24,48 +24,61 @@ namespace Bonsai.Vision
 
         public override IObservable<Intrinsics> Process(IObservable<Intrinsics> source)
         {
-            return source.Do(intrinsics =>
+            return source.Do(WriteIntrinsics);
+        }
+
+        public IObservable<CameraCalibration> Process(IObservable<CameraCalibration> source)
+        {
+            return source.Do(calibration => WriteIntrinsics(calibration.Intrinsics, calibration.ReprojectionError));
+        }
+
+        private void WriteIntrinsics(Intrinsics intrinsics)
+        {
+            WriteIntrinsics(intrinsics, null);
+        }
+
+        private void WriteIntrinsics(Intrinsics intrinsics, double? reprojectionError)
+        {
+            var fileName = FileName;
+            if (!string.IsNullOrEmpty(fileName))
             {
-                var fileName = FileName;
-                if (!string.IsNullOrEmpty(fileName))
+                PathHelper.EnsureDirectory(fileName);
+                fileName = PathHelper.AppendSuffix(fileName, Suffix);
+                using (var storage = new MemStorage())
+                using (var fileStorage = new FileStorage(fileName, storage, StorageFlags.FormatYaml | StorageFlags.Write))
                 {
-                    PathHelper.EnsureDirectory(fileName);
-                    fileName = PathHelper.AppendSuffix(fileName, Suffix);
-                    using (var storage = new MemStorage())
-                    using (var fileStorage = new FileStorage(fileName, storage, StorageFlags.FormatYaml | StorageFlags.Write))
+                    var imageSize = intrinsics.ImageSize;
+                    if (imageSize.HasValue)
                     {
-                        var imageSize = intrinsics.ImageSize;
-                        if (imageSize.HasValue)
-                        {
-                            fileStorage.WriteInt("image_width", imageSize.Value.Width);
-                            fileStorage.WriteInt("image_height", imageSize.Value.Height);
-                        }
-
-                        var focalLength = intrinsics.FocalLength;
-                        var principalPoint = intrinsics.PrincipalPoint;
-                        var cameraMatrix = Mat.FromArray(new double[,]
-                        {
-                            {focalLength.X, 0, principalPoint.X},
-                            {0, focalLength.Y, principalPoint.Y},
-                            {0, 0, 1}
-                        });
-
-                        var radialDistortion = intrinsics.RadialDistortion;
-                        var tangentialDistortion = intrinsics.TangentialDistortion;
-                        var distortionCoefficients = Mat.FromArray(new double[,]
-                        {
-                            { radialDistortion.X },
-                            { radialDistortion.Y },
-                            { tangentialDistortion.X },
-                            { tangentialDistortion.Y },
-                            { radialDistortion.Z }
-                        });
-
-                        fileStorage.Write("camera_matrix", cameraMatrix);
-                        fileStorage.Write("distortion_coefficients", distortionCoefficients);
+                        fileStorage.WriteInt("image_width", imageSize.Value.Width);
+                        fileStorage.WriteInt("image_height", imageSize.Value.Height);
                     }
+
+                    var focalLength = intrinsics.FocalLength;
+                    var principalPoint = intrinsics.PrincipalPoint;
+                    var cameraMatrix = Mat.FromArray(new double[,]
+                    {
+                        {focalLength.X, 0, principalPoint.X},
+                        {0, focalLength.Y, principalPoint.Y},
+                        {0, 0, 1}
+                    });
+
+                    var radialDistortion = intrinsics.RadialDistortion;
+                    var tangentialDistortion = intrinsics.TangentialDistortion;
+                    var distortionCoefficients = Mat.FromArray(new double[,]
+                    {
+                        { radialDistortion.X },
+                        { radialDistortion.Y },
+                        { tangentialDistortion.X },
+                        { tangentialDistortion.Y },
+                        { radialDistortion.Z }
+                    });
+
+                    fileStorage.Write("camera_matrix", cameraMatrix);
+                    fileStorage.Write("distortion_coefficients", distortionCoefficients);
+                    if (reprojectionError.HasValue) fileStorage.WriteReal("reprojection_error", reprojectionError.Value);
                 }
-            });
+            }
         }
     }
 }
