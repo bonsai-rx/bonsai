f25226173ad13674c05dc69106814b6c34971e13
glopesdev <glopesdev@users.noreply.github.com>
2020-04-07 19:04:40 +0100

    Add support for serializing camera extrinsics

---
+++ Bonsai.Vision/LoadExtrinsics.cs
@@ -0,0 +1,74 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [DefaultProperty("FileName")]
+    [Description("Loads a set of camera extrinsics from a YML file.")]
+    public class LoadExtrinsics : Source<Extrinsics>
+    {
+        [Description("The name of the camera extrinsics file.")]
+        [FileNameFilter("YML Files (*.yml)|*.yml|All Files|*.*")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string FileName { get; set; }
+
+        Extrinsics CreateExtrinsics()
+        {
+            var fileName = FileName;
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new InvalidOperationException("A valid camera extrinsics file path was not specified.");
+            }
+
+            Extrinsics extrinsics;
+            using (var storage = new MemStorage())
+            using (var fileStorage = new FileStorage(fileName, storage, StorageFlags.FormatYaml))
+            {
+                if (fileStorage.IsInvalid)
+                {
+                    throw new InvalidOperationException("The specified camera extrinsics file does not exist.");
+                }
+
+                using (var root = fileStorage.GetRootFileNode())
+                using (var rotation = fileStorage.GetFileNode(root, "rotation"))
+                using (var translation = fileStorage.GetFileNode(root, "translation"))
+                {
+                    if (rotation != null)
+                    {
+                        extrinsics.Rotation.X = fileStorage.ReadReal(rotation, "x");
+                        extrinsics.Rotation.Y = fileStorage.ReadReal(rotation, "y");
+                        extrinsics.Rotation.Z = fileStorage.ReadReal(rotation, "z");
+                    }
+                    else extrinsics.Rotation = Point3d.Zero;
+
+                    if (translation != null)
+                    {
+                        extrinsics.Translation.X = fileStorage.ReadReal(rotation, "x");
+                        extrinsics.Translation.Y = fileStorage.ReadReal(rotation, "y");
+                        extrinsics.Translation.Z = fileStorage.ReadReal(rotation, "z");
+                    }
+                    else extrinsics.Translation = Point3d.Zero;
+                }
+            }
+
+            return extrinsics;
+        }
+
+        public override IObservable<Extrinsics> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(CreateExtrinsics()));
+        }
+
+        public IObservable<Extrinsics> Generate<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(x => CreateExtrinsics());
+        }
+    }
+}
---
+++ Bonsai.Vision/SaveExtrinsics.cs
@@ -0,0 +1,53 @@
+﻿using Bonsai.IO;
+using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Vision
+{
+    [DefaultProperty("FileName")]
+    [Description("Saves a set of camera extrinsics to a YML file.")]
+    public class SaveExtrinsics : Sink<Extrinsics>
+    {
+        [FileNameFilter("YML Files (*.yml)|*.yml|All Files|*.*")]
+        [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        [Description("The name of the output camera extrinsics file.")]
+        public string FileName { get; set; }
+
+        [Description("The optional suffix used to generate file names.")]
+        public PathSuffix Suffix { get; set; }
+
+        public override IObservable<Extrinsics> Process(IObservable<Extrinsics> source)
+        {
+            return source.Do(extrinsics =>
+            {
+                var fileName = FileName;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    PathHelper.EnsureDirectory(fileName);
+                    fileName = PathHelper.AppendSuffix(fileName, Suffix);
+                    using (var storage = new MemStorage())
+                    using (var fileStorage = new FileStorage(fileName, storage, StorageFlags.FormatYaml | StorageFlags.Write))
+                    {
+                        fileStorage.StartWriteStruct("rotation", StructStorageFlags.Map);
+                        fileStorage.WriteReal("x", extrinsics.Rotation.X);
+                        fileStorage.WriteReal("y", extrinsics.Rotation.Y);
+                        fileStorage.WriteReal("z", extrinsics.Rotation.Z);
+                        fileStorage.EndWriteStruct();
+
+                        fileStorage.StartWriteStruct("translation", StructStorageFlags.Map);
+                        fileStorage.WriteReal("x", extrinsics.Translation.X);
+                        fileStorage.WriteReal("y", extrinsics.Translation.Y);
+                        fileStorage.WriteReal("z", extrinsics.Translation.Z);
+                        fileStorage.EndWriteStruct();
+                    }
+                }
+            });
+        }
+    }
+}
