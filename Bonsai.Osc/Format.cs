ee148196374005d8654248fda48a5ae724e07505
glopesdev <glopesdev@users.noreply.github.com>
2020-04-11 01:08:44 +0100

    Added operator to format OSC messages

---
+++ Bonsai.Osc/Format.cs
@@ -0,0 +1,54 @@
+﻿using Bonsai.Expressions;
+using Bonsai.Osc.IO;
+using Bonsai.Osc.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Osc
+{
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Formats input data as an Open Sound Control message.")]
+    public class Format : SelectBuilder
+    {
+        public Format()
+        {
+            Address = MessageBuilder.AddressSeparator;
+        }
+
+        [Description("The OSC address on which to broadcast the input data.")]
+        public string Address { get; set; }
+
+        protected override Expression BuildSelector(Expression expression)
+        {
+            if (expression.Type == typeof(Message))
+            {
+                return expression;
+            }
+
+            var inputParameter = Expression.Parameter(expression.Type);
+            var writerParameter = Expression.Parameter(typeof(BigEndianWriter));
+            var buildMessage = MessageBuilder.Message(Address, inputParameter, writerParameter);
+            var messageBuilder = Expression.Lambda(buildMessage, inputParameter, writerParameter);
+            return Expression.Call(typeof(Format), nameof(BuildMessage), new[] { expression.Type }, expression, messageBuilder);
+        }
+
+        static Message BuildMessage<TSource>(TSource source, Action<TSource, BigEndianWriter> messageBuilder)
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new BigEndianWriter(stream))
+            {
+                messageBuilder(source, writer);
+                var buffer = stream.ToArray();
+                return new Message(buffer);
+            }
+        }
+    }
+}
