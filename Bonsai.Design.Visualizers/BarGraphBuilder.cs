2fe66512242249c2062f53d13c89269d4db98fd6
glopesdev <glopesdev@users.noreply.github.com>
2020-05-07 12:04:19 +0100

    Add bar graph visualizer operator

---
+++ Bonsai.Design.Visualizers/BarGraphBuilder.cs
@@ -0,0 +1,94 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using ZedGraph;
+
+namespace Bonsai.Design.Visualizers
+{
+    [DefaultProperty(nameof(ValueSelector))]
+    [TypeVisualizer(typeof(BarGraphVisualizer))]
+    [Description("A visualizer that plots each element of the sequence as a bar graph.")]
+    public class BarGraphBuilder : SingleArgumentExpressionBuilder
+    {
+        [Editor("Bonsai.Design.MemberSelectorEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        [Description("The inner property that will be used as index for the graph.")]
+        public string IndexSelector { get; set; }
+
+        [Editor("Bonsai.Design.MultiMemberSelectorEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        [Description("The inner properties that will be displayed in the graph.")]
+        public string ValueSelector { get; set; }
+
+        [TypeConverter(typeof(BaseAxisConverter))]
+        [Description("Specifies the axis from which the bars in the graph will be displayed.")]
+        public BarBase BaseAxis { get; set; }
+
+        [Description("Specifies how the different bars in the graph will be visually arranged.")]
+        public BarType BarType { get; set; }
+
+        [Description("The optional capacity used for rolling bar graphs. If no capacity is specified, all data points will be displayed.")]
+        public int? Capacity { get; set; }
+
+        internal VisualizerController Controller { get; set; }
+
+        internal class VisualizerController
+        {
+            internal int Capacity;
+            internal Type IndexType;
+            internal string IndexLabel;
+            internal string[] ValueLabels;
+            internal Action<object, BarGraphVisualizer> AddValues;
+            internal BarBase BaseAxis;
+        }
+
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            var source = arguments.First();
+            var parameterType = source.Type.GetGenericArguments()[0];
+            var valueParameter = Expression.Parameter(typeof(object));
+            var viewParameter = Expression.Parameter(typeof(BarGraphVisualizer));
+            var elementVariable = Expression.Variable(parameterType);
+            Controller = new VisualizerController();
+            Controller.Capacity = Capacity.GetValueOrDefault();
+            Controller.BaseAxis = BaseAxis;
+
+            var selectedIndex = GraphHelper.SelectIndexMember(elementVariable, IndexSelector, out Controller.IndexLabel);
+            Controller.IndexType = selectedIndex.Type;
+            if (selectedIndex.Type != typeof(string))
+            {
+                selectedIndex = Expression.Call(selectedIndex, nameof(ToString), null);
+            }
+
+            var baseAxis = BaseAxis;
+            var ordinalValue = Expression.Default(typeof(double));
+            var baseX = Expression.Constant(baseAxis <= BarBase.X2);
+            var pointConstructor = typeof(PointPair).GetConstructor(new[] { typeof(double), typeof(double), typeof(string) });
+            var selectedValues = GraphHelper.SelectDataValues(elementVariable, ValueSelector, out Controller.ValueLabels);
+            var addValuesBody = Expression.Block(new[] { elementVariable },
+                Expression.Assign(elementVariable, Expression.Convert(valueParameter, parameterType)),
+                Expression.Call(viewParameter, nameof(BarGraphVisualizer.AddValues), null, selectedIndex, selectedValues));
+            Controller.AddValues = Expression.Lambda<Action<object, BarGraphVisualizer>>(addValuesBody, valueParameter, viewParameter).Compile();
+            return Expression.Call(typeof(BarGraphBuilder), nameof(Process), new[] { parameterType }, source);
+        }
+
+        static IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return source;
+        }
+
+        class BaseAxisConverter : EnumConverter
+        {
+            public BaseAxisConverter(Type type)
+                : base(type)
+            {
+            }
+
+            public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                return new StandardValuesCollection(new[] { BarBase.X, BarBase.Y });
+            }
+        }
+    }
+}
---
+++ Bonsai.Design.Visualizers/BarGraphVisualizer.cs
@@ -0,0 +1,138 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Bonsai.Design.Visualizers
+{
+    public class BarGraphVisualizer : DialogTypeVisualizer
+    {
+        GraphControl graph;
+        BarGraphBuilder.VisualizerController controller;
+        IPointListEdit[] barSeries;
+        bool labelBars;
+
+        static void GetBarGraphAxes(BarBase barBase, GraphControl graph, out Axis indexAxis, out Axis valueAxis)
+        {
+            switch (barBase)
+            {
+                case BarBase.X:
+                case BarBase.X2:
+                    indexAxis = graph.GraphPane.XAxis;
+                    valueAxis = graph.GraphPane.YAxis;
+                    break;
+                case BarBase.Y:
+                case BarBase.Y2:
+                    indexAxis = graph.GraphPane.YAxis;
+                    valueAxis = graph.GraphPane.XAxis;
+                    break;
+                default: throw new ArgumentOutOfRangeException(nameof(barBase));
+            }
+        }
+
+        internal void AddValues(string index, double[] values)
+        {
+            EnsureSeries(values.Length);
+            if (values.Length > 0)
+            {
+                var count = barSeries[0].Count;
+                var updateLast = count > 0 && index.Equals(barSeries[0][count - 1].Tag);
+                if (updateLast && controller.BaseAxis <= BarBase.X2) UpdateLastBaseX();
+                else if (updateLast) UpdateLastBaseY();
+                else if (controller.BaseAxis <= BarBase.X2) AddBaseX();
+                else AddBaseY();
+
+                void UpdateLastBaseX()
+                {
+                    for (int i = 0; i < barSeries.Length; i++)
+                        barSeries[i][count - 1].Y = values[i];
+                }
+
+                void UpdateLastBaseY()
+                {
+                    for (int i = 0; i < barSeries.Length; i++)
+                        barSeries[i][count - 1].X = values[i];
+                }
+
+                void AddBaseX()
+                {
+                    for (int i = 0; i < barSeries.Length; i++)
+                        barSeries[i].Add(new PointPair(0, values[i], index));
+                }
+
+                void AddBaseY()
+                {
+                    for (int i = 0; i < barSeries.Length; i++)
+                        barSeries[i].Add(new PointPair(values[i], 0, index));
+                }
+            }
+        }
+
+        void EnsureSeries(int count)
+        {
+            if (barSeries == null || barSeries.Length != count)
+            {
+                graph.ResetColorCycle();
+                graph.GraphPane.CurveList.Clear();
+                barSeries = new IPointListEdit[count];
+                for (int i = 0; i < barSeries.Length; i++)
+                {
+                    var color = graph.GetNextColor();
+                    var values = controller.Capacity > 0
+                        ? (IPointListEdit)new RollingPointPairList(controller.Capacity)
+                        : new PointPairList();
+                    var barItem = new BarItem(labelBars ? controller.ValueLabels[i] : null, values, color);
+                    barItem.Bar.Fill.Brush = new SolidBrush(color);
+                    barItem.Bar.Border.IsVisible = false;
+                    graph.GraphPane.CurveList.Add(barItem);
+                    barSeries[i] = values;
+                }
+            }
+        }
+
+        public override void Load(IServiceProvider provider)
+        {
+            var context = (ITypeVisualizerContext)provider.GetService(typeof(ITypeVisualizerContext));
+            var barChartBuilder = (BarGraphBuilder)ExpressionBuilder.GetVisualizerElement(context.Source).Builder;
+            controller = barChartBuilder.Controller;
+
+            graph = new GraphControl();
+            graph.Dock = DockStyle.Fill;
+            graph.GraphPane.BarSettings.Base = barChartBuilder.BaseAxis;
+            graph.GraphPane.BarSettings.Type = barChartBuilder.BarType;
+            GetBarGraphAxes(barChartBuilder.BaseAxis, graph, out Axis indexAxis, out Axis valueAxis);
+            GraphHelper.FormatOrdinalAxis(indexAxis, typeof(string));
+            GraphHelper.SetAxisLabel(indexAxis, controller.IndexLabel);
+            indexAxis.Scale.IsReverse = controller.BaseAxis == BarBase.Y;
+            indexAxis.MajorTic.IsInside = false;
+
+            var hasLabels = controller.ValueLabels != null;
+            var labelAxis = hasLabels && controller.ValueLabels.Length == 1;
+            if (labelAxis) GraphHelper.SetAxisLabel(valueAxis, controller.ValueLabels[0]);
+            labelBars = hasLabels && !labelAxis;
+            EnsureSeries(hasLabels ? controller.ValueLabels.Length : 0);
+
+            var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
+            if (visualizerService != null)
+            {
+                visualizerService.AddControl(graph);
+            }
+        }
+
+        public override void Show(object value)
+        {
+            controller.AddValues(value, this);
+            graph.Invalidate();
+        }
+
+        public override void Unload()
+        {
+            graph.Dispose();
+            graph = null;
+            controller = null;
+            barSeries = null;
+        }
+    }
+}
