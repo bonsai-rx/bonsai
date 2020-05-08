5dfd2dad493a6413f2b659707331fceb7fc7c490
glopesdev <glopesdev@users.noreply.github.com>
2020-05-08 10:17:16 +0100

    Ensure tooltip displays only when text is clipped

--- Bonsai.Editor/ToolStripConstrainedStatusLabel.cs
+++ Bonsai.Editor/ToolStripConstrainedStatusLabel.cs
@@ -11,9 +11,11 @@ namespace Bonsai.Editor
         public override Size GetPreferredSize(Size constrainingSize)
         {
             var maximumSize = MaximumSize;
-            var preferredSize = base.GetPreferredSize(constrainingSize);
+            var basePreferredSize = base.GetPreferredSize(constrainingSize);
+            var preferredSize = basePreferredSize;
             if (maximumSize.Width > 0) preferredSize.Width = Math.Min(preferredSize.Width, maximumSize.Width);
             if (maximumSize.Height > 0) preferredSize.Height = Math.Min(preferredSize.Height, maximumSize.Height);
+            AutoToolTip = preferredSize != basePreferredSize;
             return preferredSize;
         }
     }
