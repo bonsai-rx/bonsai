90d3d7c2aa96608ee479872a8ff91601f04c29d9
glopesdev <glopesdev@users.noreply.github.com>
2020-11-12 20:35:36 +0000

    Add backwards compatible main form class

---
+++ Bonsai.Editor/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Bonsai.Editor
+{
+    [Obsolete]
+    public class MainForm : EditorForm
+    {
+        public MainForm(
+            IObservable<IGrouping<string, WorkflowElementDescriptor>> elementProvider,
+            IObservable<TypeVisualizerDescriptor> visualizerProvider)
+            : base(elementProvider, visualizerProvider)
+        {
+        }
+
+        public MainForm(
+            IObservable<IGrouping<string, WorkflowElementDescriptor>> elementProvider,
+            IObservable<TypeVisualizerDescriptor> visualizerProvider,
+            IServiceProvider provider)
+            : base(elementProvider, visualizerProvider, provider)
+        {
+        }
+
+        public MainForm(
+            IObservable<IGrouping<string, WorkflowElementDescriptor>> elementProvider,
+            IObservable<TypeVisualizerDescriptor> visualizerProvider,
+            IServiceProvider provider,
+            float editorScale)
+            : base(elementProvider, visualizerProvider, provider, editorScale)
+        {
+        }
+    }
+}
