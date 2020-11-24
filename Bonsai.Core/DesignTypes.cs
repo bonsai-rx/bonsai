1be169d8b2f5b4d114771834d52a5d8c340bff8b
glopesdev <glopesdev@users.noreply.github.com>
2020-11-24 20:40:37 +0000

    Add design type constants for file path editors

--- Bonsai.Core/DesignTypes.cs
+++ Bonsai.Core/DesignTypes.cs
@@ -24,6 +24,24 @@
         public const string MultilineStringEditor = "System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
 
         /// <summary>
+        /// The assembly qualified name of a UI editor that can edit folder name paths
+        /// using a folder browser dialog. This field is read-only.
+        /// </summary>
+        public const string FolderNameEditor = "Bonsai.Design.FolderNameEditor, Bonsai.Design";
+
+        /// <summary>
+        /// The assembly qualified name of a UI editor that can edit file name paths
+        /// using an open file dialog. This field is read-only.
+        /// </summary>
+        public const string OpenFileNameEditor = "Bonsai.Design.OpenFileNameEditor, Bonsai.Design";
+
+        /// <summary>
+        /// The assembly qualified name of a UI editor that can edit file name paths
+        /// using a save file dialog. This field is read-only.
+        /// </summary>
+        public const string SaveFileNameEditor = "Bonsai.Design.SaveFileNameEditor, Bonsai.Design";
+
+        /// <summary>
         /// The assembly qualified name of the UI editor base class. This field is read-only.
         /// </summary>
         public const string UITypeEditor = "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
