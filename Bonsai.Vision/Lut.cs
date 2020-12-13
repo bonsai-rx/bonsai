448ea3ea7636b08763f9b950d11e8305015af856
glopesdev <glopesdev@users.noreply.github.com>
2020-12-13 20:10:59 +0000

    Add support for LUT operator

---
+++ Bonsai.Vision/Lut.cs
@@ -0,0 +1,26 @@
+﻿using OpenCV.Net;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Vision
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Performs a look-up table transformation of the input image pixels.")]
+    public class Lut
+    {
+        public IObservable<IplImage> Process<TArray>(IObservable<Tuple<IplImage, TArray>> source) where TArray : Arr
+        {
+            return source.Select(input =>
+            {
+                var image = input.Item1;
+                var lut = input.Item2;
+                var output = new IplImage(image.Size, image.Depth, image.Channels);
+                CV.LUT(image, output, lut);
+                return output;
+            });
+        }
+    }
+}
