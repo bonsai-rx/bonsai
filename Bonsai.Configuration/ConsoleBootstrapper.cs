a82ac112933e01b3763271334b8b8f256f69fdf1
glopesdev <glopesdev@users.noreply.github.com>
2020-12-29 22:55:59 +0000

    Ensure console bootstrapper error logging

--- Bonsai.Configuration/ConsoleBootstrapper.cs
+++ Bonsai.Configuration/ConsoleBootstrapper.cs
@@ -1,4 +1,6 @@
 ﻿using Bonsai.NuGet;
+using System;
+using System.Threading.Tasks;
 
 namespace Bonsai.Configuration
 {
@@ -12,6 +14,21 @@ namespace Bonsai.Configuration
             packageManager.Logger = ConsoleLogger.Default;
             packageManager.RequiringLicenseAcceptance += (sender, e) => e.LicenseAccepted = true;
             return packageManager;
+        }
+
+        protected override async Task RunPackageOperationAsync(LicenseAwarePackageManager packageManager, Func<Task> operationFactory)
+        {
+            await operationFactory().ContinueWith(task =>
+            {
+                if (task.Exception is AggregateException ex)
+                {
+                    foreach (var error in ex.InnerExceptions)
+                    {
+                        packageManager.Logger.LogError(error.Message);
+                    }
+                }
+                else packageManager.Logger.LogError(task.Exception.Message);
+            }, TaskContinuationOptions.OnlyOnFaulted);
         }
     }
 }
