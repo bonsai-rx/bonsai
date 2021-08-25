6f0e6a9a676b13fbdf09ada0b42c3f8f7921b14b
glopesdev <glopesdev@users.noreply.github.com>
2021-08-26 00:40:34 +0100

    Add operator for pre-computing normal matrix
    Fixes #816

--- Bonsai.Shaders.Design/Configuration/ShaderScriptComponentEditor.cs
+++ Bonsai.Shaders.Design/Configuration/ShaderScriptComponentEditor.cs
@@ -75,6 +75,7 @@ void main()
                     Source = @"#version 400
 uniform mat4 modelview;
 uniform mat4 projection;
+uniform mat4 normalMatrix;
 layout(location = 0) in vec3 vp;
 layout(location = 1) in vec2 vt;
 layout(location = 2) in vec3 vn;
@@ -84,12 +85,11 @@ out vec3 normal;
 
 void main()
 {
-  mat4 normalmat = transpose(inverse(modelview));
   vec4 v = modelview * vec4(vp, 1.0);
   gl_Position = projection * v;
   position = vec3(v);
   texCoord = vt;
-  normal = normalize(vec3(normalmat * vec4(vn, 0.0)));
+  normal = normalize(vec3(normalMatrix * vec4(vn, 0.0)));
 }
 "
                 },
---
+++ Bonsai.Shaders/NormalMatrix.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Shaders
+{
+    [Description("Computes the normal matrix for the specified input modelview matrix.")]
+    public class NormalMatrix : Transform<Matrix4, Matrix4>
+    {
+        public IObservable<Matrix3> Process(IObservable<Matrix3> source)
+        {
+            return source.Select(input =>
+            {
+                if (input.Determinant == 0)
+                {
+                    return Matrix3.Zero;
+                }
+                input.Invert();
+                input.Transpose();
+                return input;
+            });
+        }
+
+        public override IObservable<Matrix4> Process(IObservable<Matrix4> source)
+        {
+            return source.Select(input =>
+            {
+                if (input.Determinant == 0)
+                {
+                    return Matrix4.Zero;
+                }
+                input.Invert();
+                input.Transpose();
+                return input;
+            });
+        }
+    }
+}
