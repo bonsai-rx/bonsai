82e6ddb9a728ab2dec6ef8f20eb6e52a9701642e
glopesdev <glopesdev@users.noreply.github.com>
2021-08-25 13:23:37 +0100

    Add support for transposing shader matrices

---
+++ Bonsai.Shaders/Transpose.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Shaders
+{
+    [Description("Transposes the input matrix.")]
+    public class Transpose : Transform<Matrix4, Matrix4>
+    {
+        public IObservable<Matrix3> Process(IObservable<Matrix3> source)
+        {
+            return source.Select(Matrix3.Transpose);
+        }
+
+        public override IObservable<Matrix4> Process(IObservable<Matrix4> source)
+        {
+            return source.Select(Matrix4.Transpose);
+        }
+    }
+}
