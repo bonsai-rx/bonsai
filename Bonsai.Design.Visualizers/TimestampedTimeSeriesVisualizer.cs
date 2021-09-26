5efe9725de877fd77af657fc0659ac0679427eb0
glopesdev <glopesdev@users.noreply.github.com>
2021-09-26 20:57:01 +0100

    Add support for timestamped array series

--- Bonsai.Design.Visualizers/TimestampedTimeSeriesVisualizer.cs
+++ Bonsai.Design.Visualizers/TimestampedTimeSeriesVisualizer.cs
@@ -12,6 +12,15 @@ using Bonsai.Design.Visualizers;
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<ushort>), Target = typeof(Timestamped<ushort>))]
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<float>), Target = typeof(Timestamped<float>))]
 [assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<double>), Target = typeof(Timestamped<double>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<byte[]>), Target = typeof(Timestamped<byte[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<int[]>), Target = typeof(Timestamped<int[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<uint[]>), Target = typeof(Timestamped<uint[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<long[]>), Target = typeof(Timestamped<long[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<ulong[]>), Target = typeof(Timestamped<ulong[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<short[]>), Target = typeof(Timestamped<short[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<ushort[]>), Target = typeof(Timestamped<ushort[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<float[]>), Target = typeof(Timestamped<float[]>))]
+[assembly: TypeVisualizer(typeof(TimestampedTimeSeriesVisualizer<double[]>), Target = typeof(Timestamped<double[]>))]
 
 namespace Bonsai.Design.Visualizers
 {
@@ -20,7 +29,11 @@ namespace Bonsai.Design.Visualizers
         public override void Show(object value)
         {
             var timestamped = (Timestamped<T>)value;
-            AddValue(timestamped.Timestamp.DateTime, Convert.ToDouble(timestamped.Value));
+            if (timestamped.Value is Array array)
+            {
+                AddValue(timestamped.Timestamp.DateTime, array);
+            }
+            else AddValue(timestamped.Timestamp.DateTime, Convert.ToDouble(timestamped.Value));
         }
     }
 }
