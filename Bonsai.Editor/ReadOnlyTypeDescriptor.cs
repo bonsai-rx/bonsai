a164adc0bb60c72076bcd82a92d86336c5b8647f
glopesdev <glopesdev@users.noreply.github.com>
2022-05-26 13:37:04 +0100

    Disable editing of objects from included workflows
    Fixes #899

---
+++ Bonsai.Editor/ReadOnlyTypeDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Bonsai.Editor
+{
+    class ReadOnlyTypeDescriptor : CustomTypeDescriptor
+    {
+        public ReadOnlyTypeDescriptor(object instance, ICustomTypeDescriptor parent)
+            : base(parent)
+        {
+            PropertyOwner = instance;
+        }
+
+        object PropertyOwner { get; }
+
+        public override AttributeCollection GetAttributes()
+        {
+            var baseAttributes = base.GetAttributes();
+            return AttributeCollection.FromExisting(baseAttributes, ReadOnlyAttribute.Yes);
+        }
+
+        public override object GetPropertyOwner(PropertyDescriptor pd)
+        {
+            return PropertyOwner;
+        }
+    }
+}
