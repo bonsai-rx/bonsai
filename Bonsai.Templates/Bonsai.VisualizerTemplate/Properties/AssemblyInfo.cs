44da8e95f93a16f2ca38c49a7b1fb2936a90c806
glopesdev <glopesdev@users.noreply.github.com>
2022-06-09 05:34:23 +0100

    Update project and item templates for VS 2022
    Fixes #832

--- Bonsai.Templates/Bonsai.VisualizerTemplate/Properties/AssemblyInfo.cs
+++ Bonsai.Templates/Bonsai.VisualizerTemplate/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gonçalo Lopes")]
 [assembly: AssemblyProduct("Bonsai.VisualizerTemplate")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2021")]
+[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
