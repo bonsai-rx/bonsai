1b41ff685be80ffed6e7ba3f54164c7b5ab4b6ad
glopesdev <glopesdev@users.noreply.github.com>
2022-07-05 23:50:53 +0100

    Mapping assignment must precede target evaluation

--- Bonsai.Core/Expressions/VisualizerMappingBuilder.cs
+++ Bonsai.Core/Expressions/VisualizerMappingBuilder.cs
@@ -33,10 +33,9 @@ namespace Bonsai.Expressions
         bool IArgumentBuilder.BuildArgument(Expression source, Edge<ExpressionBuilder, ExpressionBuilderArgument> successor, out Expression argument)
         {
             if (successor.Target.Value is InspectBuilder targetBuilder &&
-                GetVisualizerElement(targetBuilder) is InspectBuilder targetVisualizer &&
                 InspectBuilder.GetInspectBuilder(source) is InspectBuilder sourceVisualizer)
             {
-                targetVisualizer.AddVisualizerSource(successor.Label.Index, sourceVisualizer);
+                targetBuilder.AddVisualizerSource(successor.Label.Index, sourceVisualizer);
             }
 
             argument = source;
