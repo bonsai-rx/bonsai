210466178697c5e4e7c76a1bf6c5b0b0488aff47
glopesdev <glopesdev@users.noreply.github.com>
2022-08-23 12:12:06 +0100

    Ensure continuation does not corrupt editor state

--- Bonsai.Editor/GraphModel/WorkflowEditor.cs
+++ Bonsai.Editor/GraphModel/WorkflowEditor.cs
@@ -1,4 +1,4 @@
-﻿using Bonsai.Dag;
+using Bonsai.Dag;
 using Bonsai.Design;
 using Bonsai.Editor.Properties;
 using Bonsai.Expressions;
@@ -1459,8 +1459,17 @@ namespace Bonsai.Editor.GraphModel
             if (replacementNode != null) DeleteGraphNode(replacementNode);
             if (continuation != null)
             {
-                var groupNode = workflow.Single(node => ExpressionBuilder.Unwrap(node.Value) == workflowExpressionBuilder);
-                updateSelectedNode = continuation(groupNode) ?? updateSelectedNode;
+                try
+                {
+                    var groupNode = workflow.Single(node => ExpressionBuilder.Unwrap(node.Value) == workflowExpressionBuilder);
+                    updateSelectedNode = continuation(groupNode) ?? updateSelectedNode;
+                }
+                catch (Exception)
+                {
+                    commandExecutor.EndCompositeCommand();
+                    commandExecutor.Undo(allowRedo: false);
+                    throw;
+                }
             }
             commandExecutor.Execute(() =>
             {
