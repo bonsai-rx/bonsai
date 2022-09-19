2b94f61593334af68271973c72837582e5d93c66
glopesdev <glopesdev@users.noreply.github.com>
2022-09-19 17:27:17 +0100

    Add support for replacing nested operators

--- Bonsai.Core/ProxyTypeAttribute.cs
+++ Bonsai.Core/ProxyTypeAttribute.cs
@@ -2,14 +2,30 @@
 
 namespace Bonsai
 {
+    /// <summary>
+    /// Specifies a type used to replace the class this attribute is bound to.
+    /// This attribute is meant for internal use only.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
-    sealed class ProxyTypeAttribute : Attribute
+    public sealed class ProxyTypeAttribute : Attribute
     {
-        public ProxyTypeAttribute(Type type)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProxyTypeAttribute"/> class
+        /// with the specified destination type.
+        /// </summary>
+        /// <param name="destination">
+        /// The <see cref="Type"/> that should be used to replace the class this
+        /// attribute is bound to.
+        /// </param>
+        public ProxyTypeAttribute(Type destination)
         {
-            Type = type;
+            Destination = destination;
         }
 
-        public Type Type { get; set; }
+        /// <summary>
+        /// Gets the <see cref="Type"/> that should be used to replace the class this
+        /// attribute is bound to.
+        /// </summary>
+        public Type Destination { get; private set; }
     }
 }
