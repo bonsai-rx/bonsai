e0573fcdb215524e5f89934e77bbd7953bff41c7
glopesdev <glopesdev@users.noreply.github.com>
2022-09-25 11:27:18 +0100

    Refactor TimedGate functionality into Gate

--- Bonsai.Core/ObservableCombinators.cs
+++ Bonsai.Core/ObservableCombinators.cs
@@ -48,124 +48,156 @@ namespace Bonsai
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence every time the specified
-        /// interval elapses.
+        /// Allows a single element from an observable sequence to pass through every
+        /// time the specified time interval elapses.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
+        /// <typeparam name="TSource">
+        /// The type of the elements in the <paramref name="source"/> sequence.
+        /// </typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
         /// <param name="interval">
-        /// The time interval after which a new element of the sequence is allowed to propagate.
+        /// The time interval after which a new element of the sequence is allowed to pass
+        /// through the gate.
         /// </param>
-        /// <returns>The gated observable sequence.</returns>
+        /// <returns>
+        /// The filtered observable sequence. Every time the specified time interval
+        /// elapses, the next element from the <paramref name="source"/> sequence
+        /// will be allowed through.
+        /// </returns>
         public static IObservable<TSource> Gate<TSource>(this IObservable<TSource> source, TimeSpan interval)
         {
             return Gate(source, interval, Scheduler.Default);
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence every time the specified
-        /// interval elapses, using the specified scheduler to run gating timers.
+        /// Allows a single element from an observable sequence to pass through every
+        /// time the specified time interval elapses, using the specified scheduler to
+        /// run gate timers.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
+        /// <typeparam name="TSource">
+        /// The type of the elements in the <paramref name="source"/> sequence.
+        /// </typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
         /// <param name="interval">
-        /// The time interval after which a new element of the sequence is allowed to propagate.
+        /// The time interval after which a new element of the sequence is allowed to pass
+        /// through the gate.
         /// </param>
-        /// <param name="scheduler">The scheduler to run the gating timer on.</param>
-        /// <returns>The gated observable sequence.</returns>
+        /// <param name="scheduler">The scheduler to run the gate timer on.</param>
+        /// <returns>
+        /// The filtered observable sequence. Every time the specified time interval
+        /// elapses, the next element from the <paramref name="source"/> sequence
+        /// will be allowed through.
+        /// </returns>
         public static IObservable<TSource> Gate<TSource>(this IObservable<TSource> source, TimeSpan interval, IScheduler scheduler)
         {
             return Gate(source, Observable.Timer(interval, scheduler));
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence every time the gate
-        /// produces an element.
+        /// Allows a single element from an observable sequence to pass through every
+        /// time a second sequence emits a notification.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TGate">The type of the elements in the sequence of gate events.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
-        /// <param name="gate">
-        /// The sequence of gate events. Every time a new gate event is received, the single
-        /// next element from <paramref name="source"/> is allowed to propagate.
-        /// </param>
-        /// <returns>The gated observable sequence.</returns>
-        public static IObservable<TSource> Gate<TSource, TGate>(this IObservable<TSource> source, IObservable<TGate> gate)
+        /// <typeparam name="TSource">
+        /// The type of the elements in the <paramref name="source"/> sequence.
+        /// </typeparam>
+        /// <typeparam name="TGateOpening">
+        /// The type of the elements in the <paramref name="gateOpenings"/> sequence.
+        /// </typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
+        /// <param name="gateOpenings">The sequence of gate opening events.</param>
+        /// <returns>
+        /// The filtered observable sequence. Every time the <paramref name="gateOpenings"/>
+        /// sequence produces a notification, the next element from the
+        /// <paramref name="source"/> sequence will be allowed through.
+        /// </returns>
+        public static IObservable<TSource> Gate<TSource, TGateOpening>(
+            this IObservable<TSource> source,
+            IObservable<TGateOpening> gateOpenings)
         {
-            return source.Window(() => gate)
+            return source.Window(() => gateOpenings)
                          .SelectMany(window => window.Take(1));
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence if this element is produced
-        /// within a specified time interval after the gate produces an element.
+        /// Allows an element from an observable sequence to pass through if this element
+        /// is produced after a gate opening event and before the specified time interval
+        /// elapses.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TGate">The type of the elements in the sequence of gate events.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
-        /// <param name="gate">
-        /// The sequence of gate events. Every time a new gate event is received, the single
-        /// next element from <paramref name="source"/> is allowed to propagate if it is
-        /// produced before the maximum <paramref name="timeSpan"/> elapses.
-        /// </param>
+        /// <typeparam name="TSource">The type of the elements in the <paramref name="source"/> sequence.</typeparam>
+        /// <typeparam name="TGateOpening">The type of the elements in the <paramref name="gateOpenings"/> sequence.</typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
+        /// <param name="gateOpenings">The sequence of gate opening events.</param>
         /// <param name="timeSpan">
         /// After receiving a gate event, the maximum interval that can elapse before an
         /// element from the source sequence is produced. If the element arrives after the
-        /// interval elapsed, it is dropped.
+        /// interval elapsed, it is dropped from the result sequence.
         /// </param>
-        /// <returns>The gated observable sequence.</returns>
-        public static IObservable<TSource> Gate<TSource, TGate>(this IObservable<TSource> source, IObservable<TGate> gate, TimeSpan timeSpan)
+        /// <returns>
+        /// The filtered observable sequence. Every time the <paramref name="gateOpenings"/>
+        /// sequence produces a notification, the next element from the <paramref name="source"/>
+        /// sequence will be allowed through if it is emitted before the specified time interval
+        /// elapses.
+        /// </returns>
+        public static IObservable<TSource> Gate<TSource, TGateOpening>(
+            this IObservable<TSource> source,
+            IObservable<TGateOpening> gateOpenings,
+            TimeSpan timeSpan)
         {
-            return Gate(source, gate, timeSpan, Scheduler.Default);
+            return Gate(source, gateOpenings, timeSpan, Scheduler.Default);
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence if this element is produced
-        /// within a specified time interval after the gate produces an element, using
-        /// the specified scheduler to run gate closing timers.
+        /// Allows an element from an observable sequence to pass through if this element
+        /// is produced after a gate opening event and before the specified time interval
+        /// elapses, using the specified scheduler to run gate closing timers.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TGate">The type of the elements in the sequence of gate events.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
-        /// <param name="gate">
-        /// The sequence of gate events. Every time a new gate event is received, the single
-        /// next element from <paramref name="source"/> is allowed to propagate if it is
-        /// produced before the maximum <paramref name="timeSpan"/> elapses.
-        /// </param>
+        /// <typeparam name="TSource">The type of the elements in the <paramref name="source"/> sequence.</typeparam>
+        /// <typeparam name="TGateOpening">The type of the elements in the <paramref name="gateOpenings"/> sequence.</typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
+        /// <param name="gateOpenings">The sequence of gate opening events.</param>
         /// <param name="timeSpan">
         /// After receiving a gate event, the maximum interval that can elapse before an
         /// element from the source sequence is produced. If the element arrives after the
-        /// interval elapsed, it is dropped.
+        /// interval elapsed, it is dropped from the result sequence.
         /// </param>
         /// <param name="scheduler">The scheduler to run the gate closing timer on.</param>
-        /// <returns>The gated observable sequence.</returns>
-        public static IObservable<TSource> Gate<TSource, TGate>(this IObservable<TSource> source, IObservable<TGate> gate, TimeSpan timeSpan, IScheduler scheduler)
+        /// <returns>
+        /// The filtered observable sequence. Every time the <paramref name="gateOpenings"/>
+        /// sequence produces a notification, the next element from the <paramref name="source"/>
+        /// sequence will be allowed through if it is emitted before the specified time interval
+        /// elapses.
+        /// </returns>
+        public static IObservable<TSource> Gate<TSource, TGateOpening>(
+            this IObservable<TSource> source,
+            IObservable<TGateOpening> gateOpenings,
+            TimeSpan timeSpan,
+            IScheduler scheduler)
         {
-            return Gate(source, gate, Observable.Timer(timeSpan, scheduler));
+            return Gate(source, gateOpenings, Observable.Timer(timeSpan, scheduler));
         }
 
         /// <summary>
-        /// Takes the single next element from the sequence if this element is produced
-        /// between a gate opening and gate closing event.
+        /// Allows an element from an observable sequence to pass through if this element
+        /// is produced between a gate opening and gate closing event.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
-        /// <typeparam name="TGateOpening">The type of the elements in the sequence of gate opening events.</typeparam>
-        /// <typeparam name="TGateClosing">The type of the elements in the sequence of gate closing events.</typeparam>
-        /// <param name="source">The observable sequence to be gated.</param>
-        /// <param name="openGate">
-        /// The sequence of gate opening events. Every time a new gate event is received,
-        /// the single next element from <paramref name="source"/> is allowed to propagate
-        /// if it is produced before the next gate closing event.
-        /// </param>
-        /// <param name="closeGate">
-        /// The sequence of gate closing events. Every time a new gate event is received,
-        /// the single next element from <paramref name="source"/> is allowed to propagate
-        /// if it is produced before the next gate closing event.
-        /// </param>
-        /// <returns>The gated observable sequence.</returns>
-        public static IObservable<TSource> Gate<TSource, TGateOpening, TGateClosing>(this IObservable<TSource> source, IObservable<TGateOpening> openGate, IObservable<TGateClosing> closeGate)
+        /// <typeparam name="TSource">The type of the elements in the <paramref name="source"/> sequence.</typeparam>
+        /// <typeparam name="TGateOpening">The type of the elements in the <paramref name="gateOpenings"/> sequence.</typeparam>
+        /// <typeparam name="TGateClosing">The type of the elements in the <paramref name="gateClosings"/> sequence.</typeparam>
+        /// <param name="source">The observable sequence to filter.</param>
+        /// <param name="gateOpenings">The sequence of gate opening events.</param>
+        /// <param name="gateClosings">The sequence of gate closing events.</param>
+        /// <returns>
+        /// The filtered observable sequence. Every time the <paramref name="gateOpenings"/>
+        /// sequence produces a notification, the next element from the <paramref name="source"/>
+        /// sequence will be allowed through if it is emitted before the next notification
+        /// from the <paramref name="gateClosings"/> sequence.
+        /// </returns>
+        public static IObservable<TSource> Gate<TSource, TGateOpening, TGateClosing>(
+            this IObservable<TSource> source,
+            IObservable<TGateOpening> gateOpenings,
+            IObservable<TGateClosing> gateClosings)
         {
-            return source.Window(openGate, window => closeGate)
+            return source.Window(gateOpenings, window => gateClosings)
                          .Select(window => window.Take(1))
                          .Switch();
         }
--- Bonsai.Core/Reactive/Gate.cs
+++ Bonsai.Core/Reactive/Gate.cs
@@ -1,37 +1,73 @@
 ﻿using System;
 using System.Xml.Serialization;
 using System.ComponentModel;
+using System.Xml;
 
 namespace Bonsai.Reactive
 {
     /// <summary>
-    /// Represents an operator that takes the next element from the first sequence
-    /// whenever the second sequence emits a notification.
+    /// Represents an operator that allows a single element from the first sequence
+    /// to pass through every time a second sequence emits a notification.
     /// </summary>
     [Combinator]
     [XmlType(Namespace = Constants.XmlNamespace)]
-    [Description("Takes the next element from the first sequence whenever the second sequence emits a notification.")]
+    [Description("Allows a single element from the first sequence to pass through every time a second sequence emits a notification.")]
     public class Gate
     {
         /// <summary>
-        /// Takes the next element from the first observable sequence whenever
-        /// the second sequence emits a notification.
+        /// Gets or sets a value specifying the maximum time the gate stays open.
+        /// </summary>
+        /// <remarks>
+        /// If no value is specified, the gate stays open indefinitely until an element
+        /// arrives. If a maximum time span is specified, however, then if an element
+        /// from the first sequence arrives after this interval elapses, that element
+        /// will not be allowed through and will be dropped from the result sequence.
+        /// </remarks>
+        [XmlIgnore]
+        [Description("The maximum time the gate stays open.")]
+        public TimeSpan? TimeSpan { get; set; }
+
+        /// <summary>
+        /// Gets or sets an XML representation of the gate time span for serialization.
+        /// </summary>
+        [Browsable(false)]
+        [XmlElement(nameof(TimeSpan))]
+        public string TimeSpanXml
+        {
+            get
+            {
+                var timeSpan = TimeSpan;
+                if (timeSpan.HasValue) return XmlConvert.ToString(timeSpan.Value);
+                else return null;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) TimeSpan = XmlConvert.ToTimeSpan(value);
+                else TimeSpan = null;
+            }
+        }
+
+        /// <summary>
+        /// Allows a single element from an observable sequence to pass through every
+        /// time a second sequence emits a notification.
         /// </summary>
         /// <typeparam name="TSource">
         /// The type of the elements in the <paramref name="source"/> sequence.
         /// </typeparam>
-        /// <typeparam name="TOther">
-        /// The type of the elements in the <paramref name="other"/> sequence.
+        /// <typeparam name="TGateOpening">
+        /// The type of the elements in the <paramref name="gateOpenings"/> sequence.
         /// </typeparam>
-        /// <param name="source">The observable sequence to take elements from.</param>
-        /// <param name="other">
-        /// The sequence of gate events. Every time this sequence produces a notification,
-        /// the next element from the <paramref name="source"/> sequence is taken.
-        /// </param>
-        /// <returns>The gated observable sequence.</returns>
-        public IObservable<TSource> Process<TSource, TOther>(IObservable<TSource> source, IObservable<TOther> other)
+        /// <param name="source">The observable sequence to filter.</param>
+        /// <param name="gateOpenings">The sequence of gate opening events.</param>
+        /// <returns>
+        /// The filtered observable sequence. Every time the <paramref name="gateOpenings"/>
+        /// sequence produces a notification, the next element from the
+        /// <paramref name="source"/> sequence will be allowed through.
+        /// </returns>
+        public IObservable<TSource> Process<TSource, TGateOpening>(IObservable<TSource> source, IObservable<TGateOpening> gateOpenings)
         {
-            return source.Gate(other);
+            var timeSpan = TimeSpan;
+            return timeSpan.HasValue ? source.Gate(gateOpenings, timeSpan.Value) : source.Gate(gateOpenings);
         }
     }
 }
