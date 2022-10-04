9e8aa3f872d0cfbb863d4c73e36b61a72ee8f4f5
glopesdev <glopesdev@users.noreply.github.com>
2022-10-04 19:18:22 +0100

    Simplify test syntax

--- Bonsai.Core.Tests/TypeMappingTests.cs
+++ Bonsai.Core.Tests/TypeMappingTests.cs
@@ -16,18 +16,18 @@ namespace Bonsai.Core.Tests
         {
             var workflow = new WorkflowBuilder();
             var builder = new StringBuilder();
-            workflow.Workflow.Add(new CombinatorBuilder() { Combinator = new MappingCombinator() });
-            workflow.Workflow.Add(new CombinatorBuilder() { Combinator = new GenericMappingCombinator<int>() });
+            workflow.Workflow.Add(new CombinatorBuilder { Combinator = new MappingCombinator() });
+            workflow.Workflow.Add(new CombinatorBuilder { Combinator = new GenericMappingCombinator<int>() });
             workflow.Workflow.Add(new AddBuilder { Operand = new WorkflowProperty<int>() });
 #pragma warning disable CS0612 // Type or member is obsolete
             workflow.Workflow.Add(new ExternalizedTimeSpan<int>());
 #pragma warning restore CS0612 // Type or member is obsolete
-            workflow.Workflow.Add(new PropertySource<Bonsai.Reactive.WindowCount, int>());
+            workflow.Workflow.Add(new PropertySource<Reactive.WindowCount, int>());
             workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<Tuple<int, int>>() });
             workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<Tuple<Tuple<int, int, int>>>() });
             workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<Tuple<Tuple<int, int>, int>>() });
-            workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<List<Bonsai.Core.Tests.MappingNamespace1.Vector2>>() });
-            workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<List<Bonsai.Core.Tests.MappingNamespace2.Vector2>>() });
+            workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<List<MappingNamespace1.Vector2>>() });
+            workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<List<MappingNamespace2.Vector2>>() });
             workflow.Workflow.Add(new InputMappingBuilder { TypeMapping = new TypeMapping<System.Diagnostics.Stopwatch>() });
             using (var writer = XmlWriter.Create(builder, new XmlWriterSettings { Indent = true }))
             {
