f884b0b51c56975cf92624c4131e9d4246543a0c
glopesdev <glopesdev@users.noreply.github.com>
2023-02-13 23:21:40 +0000

    Add template for creating a new bonsai environment

---
+++ Bonsai.Templates/Bonsai.EnvironmentTemplate/Setup.ps1
@@ -0,0 +1,19 @@
+if (!(Test-Path "./Bonsai.exe")) {
+    $release = "https://github.com/bonsai-rx/bonsai/releases/latest/download/Bonsai.zip"
+    $configPath = "./Bonsai.config"
+    if (Test-Path $configPath) {
+        [xml]$config = Get-Content $configPath
+        $bootstrapper = $config.PackageConfiguration.Packages.Package.where{$_.id -eq 'Bonsai'}
+        if ($bootstrapper) {
+            $version = $bootstrapper.version
+            $release = "https://github.com/bonsai-rx/bonsai/releases/download/$version/Bonsai.zip"
+        }
+    }
+    Invoke-WebRequest $release -OutFile "temp.zip"
+    Move-Item -Path "NuGet.config" "temp.config"
+    Expand-Archive "temp.zip" -DestinationPath "." -Force
+    Move-Item -Path "temp.config" "NuGet.config" -Force
+    Remove-Item -Path "temp.zip"
+	Remove-Item -Path "Bonsai32.exe"
+}
+& .\Bonsai.exe --no-editor
