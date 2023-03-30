58ccf04594d79e51f451e997b49a2234167ed9ec
glopesdev <glopesdev@users.noreply.github.com>
2023-03-30 23:31:42 +0100

    Enable advanced markdown extensions

--- Bonsai.Editor/GraphView/MarkdownConvert.cs
+++ Bonsai.Editor/GraphView/MarkdownConvert.cs
@@ -5,11 +5,15 @@ namespace Bonsai.Editor.GraphView
 {
     class MarkdownConvert
     {
+        static readonly MarkdownPipeline Pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .Build();
+
         public static string ToHtml(Font font, string text)
         {
             if (!string.IsNullOrEmpty(text))
             {
-                var html = Markdown.ToHtml(text);
+                var html = Markdown.ToHtml(text, Pipeline);
                 return $@"<div style=""font-family: '{font.Name}'; line-height: 1em;"">{html}</div>";
             }
 
