8f22aae35c582c2306c4862dbc3a394141364487
glopesdev <glopesdev@users.noreply.github.com>
2023-05-17 10:04:46 +0100

    Add UI type editor for annotation text property

---
+++ Bonsai.Design/AnnotationTextEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace Bonsai.Design
+{
+    /// <summary>
+    /// Provides a user interface editor that displays a dialog box for editing
+    /// the annotation text.
+    /// </summary>
+    public class AnnotationTextEditor : UITypeEditor
+    {
+        /// <inheritdoc/>
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+        /// <inheritdoc/>
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (provider != null)
+            {
+                var editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+                if (editorService != null)
+                {
+                    using var editorDialog = new AnnotationBuilderEditorDialog();
+                    editorDialog.Annotation = value as string;
+                    if (editorService.ShowDialog(editorDialog) == DialogResult.OK)
+                    {
+                        return editorDialog.Annotation;
+                    }
+                }
+            }
+
+            return base.EditValue(context, provider, value);
+        }
+    }
+}
