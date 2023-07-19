b5c54e29312e0a08a6606442044b082c494dc772
glopesdev <glopesdev@users.noreply.github.com>
2023-07-19 09:35:47 +0100

    Pack player project as a dotnet tool

--- Bonsai.Player/Program.cs
+++ Bonsai.Player/Program.cs
@@ -1,42 +1,45 @@
-﻿using System;
-using System.IO;
-using System.Xml;
+﻿using System.Xml;
 using Bonsai.Expressions;
-using System.Threading;
 using System.Reactive.Linq;
 using Bonsai.Configuration;
+using System.Threading.Tasks;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
 
 namespace Bonsai.Player
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            if (args.Length < 1)
+            var filePath = new Argument<FileInfo>(
+                name: "WORKFLOW",
+                description: "The workflow file to run.",
+                parse: result =>
+                {
+                    var filePath = result.Tokens.Single().Value;
+                    if (!File.Exists(filePath))
+                    {
+                        result.ErrorMessage = "The specified workflow file path does not exist.";
+                        return null;
+                    }
+                    else return new FileInfo(filePath);
+                });
+            var rootCommand = new RootCommand("Run Bonsai workflows from the command-line.");
+            rootCommand.AddArgument(filePath);
+            rootCommand.SetHandler(async filePath =>
             {
-                Console.WriteLine("usage: Bonsai.Player <workflowFileName>");
-                return;
-            }
+                WorkflowBuilder workflowBuilder;
+                ConfigurationHelper.SetAssemblyResolve();
+                using (var reader = XmlReader.Create(filePath.FullName))
+                {
+                    workflowBuilder = (WorkflowBuilder)WorkflowBuilder.Serializer.Deserialize(reader);
+                }
 
-            var fileName = args[0];
-            if (!File.Exists(fileName))
-            {
-                throw new ArgumentException("Specified workflow file does not exist.");
-            }
-
-            WorkflowBuilder workflowBuilder;
-            ConfigurationHelper.SetAssemblyResolve();
-            using (var reader = XmlReader.Create(fileName))
-            {
-                workflowBuilder = (WorkflowBuilder)WorkflowBuilder.Serializer.Deserialize(reader);
-            }
-
-            var workflowCompleted = new ManualResetEvent(false);
-            workflowBuilder.Workflow.BuildObservable().Subscribe(
-                unit => { },
-                ex => { Console.Error.WriteLine(ex); workflowCompleted.Set(); },
-                () => workflowCompleted.Set());
-            workflowCompleted.WaitOne();
+                await workflowBuilder.Workflow.BuildObservable();
+            }, filePath);
+            await rootCommand.InvokeAsync(args);
         }
     }
 }
