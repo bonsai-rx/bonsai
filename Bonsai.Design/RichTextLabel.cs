8f098b76f0a4a137eb9af7ed7dfeeb4ab7b89681
glopesdev <glopesdev@users.noreply.github.com>
2023-07-20 22:26:10 +0100

    Disable caret selection from text visualizer

---
+++ Bonsai.Design/RichTextLabel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace Bonsai.Design
+{
+    internal class RichTextLabel : RichTextBox
+    {
+        public RichTextLabel()
+        {
+            ReadOnly = true;
+            TabStop = false;
+            SetStyle(ControlStyles.Selectable, false);
+            SetStyle(ControlStyles.UserMouse, true);
+        }
+    }
+}
