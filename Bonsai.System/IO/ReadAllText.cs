3e8a2c05478a5822860e3c71bb5bfbbba66c2d09
glopesdev <glopesdev@users.noreply.github.com>
2023-07-24 22:33:33 +0100

    Add ReadAllText and WriteAllText operators

---
+++ Bonsai.System/IO/ReadAllText.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace Bonsai.IO
+{
+    /// <summary>
+    /// Represents an operator that opens a text file, returns a single string with all
+    /// lines in the file, and then closes the file.
+    /// </summary>
+    [DefaultProperty(nameof(Path))]
+    [Description("Opens a text file, returns a single string with all lines in the file, and then closes the file.")]
+    public class ReadAllText : Source<string>
+    {
+        /// <summary>
+        /// Gets or sets the relative or absolute path of the file to open for reading.
+        /// </summary>
+        [Description("The relative or absolute path of the file to open for reading.")]
+        [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Generates an observable sequence that opens the text file, returns a single string
+        /// with all lines in the file, and then closes the file.
+        /// </summary>
+        /// <returns>
+        /// A sequence containing a single string with all lines in the file.
+        /// </returns>
+        public override IObservable<string> Generate()
+        {
+            var path = Path;
+            return Observable.Defer(() => Observable.Return(File.ReadAllText(path)));
+        }
+    }
+}
---
+++ Bonsai.System/IO/WriteAllText.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace Bonsai.IO
+{
+    /// <summary>
+    /// Represents an operator that opens a text file, writes the source string to
+    /// the file, and then closes the file.
+    /// </summary>
+    [DefaultProperty(nameof(Path))]
+    [Description("Creates a new file, writes the source string to the file, and then closes the file.")]
+    public class WriteAllText : Sink<string>
+    {
+        /// <summary>
+        /// Gets or sets the relative or absolute path of the file to open for writing.
+        /// </summary>
+        [Description("The relative or absolute path of the file to open for writing. If the specified file already exists, it is overwritten.")]
+        [Editor("Bonsai.Design.SaveFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Creates a new file, writes the string in the observable sequence to the file,
+        /// and then closes the file.
+        /// </summary>
+        /// <param name="source">
+        /// The sequence containing the string to write to the file.
+        /// </param>
+        /// <returns>
+        /// An observable sequence that is identical to the <paramref name="source"/> sequence
+        /// but where there is an additional side effect of writing the string to the file.
+        /// </returns>
+        public override IObservable<string> Process(IObservable<string> source)
+        {
+            var path = Path;
+            return source.Do(contents =>
+            {
+                using var writer = new StreamWriter(path, append: false);
+                writer.Write(contents);
+            });
+        }
+    }
+}
