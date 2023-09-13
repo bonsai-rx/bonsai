e3e39f99aaa9d0dc8f2d1a8b2216b5a1519283c7
glopesdev <glopesdev@users.noreply.github.com>
2023-09-14 00:11:47 +0100

    Add library folders to DLL search path on windows

---
+++ Bonsai.Configuration/NativeMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Bonsai.Configuration
+{
+    static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern int AddDllDirectory(string NewDirectory);
+    }
+}
