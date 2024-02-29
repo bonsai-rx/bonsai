13a2e0765466992d74b74e495d3b0b4dc55f2da0
glopesdev <glopesdev@users.noreply.github.com>
2024-02-29 16:09:29 +0000

    Replace tab with spaces

--- Bonsai.Templates/Bonsai.EnvironmentTemplate/Setup.ps1
+++ Bonsai.Templates/Bonsai.EnvironmentTemplate/Setup.ps1
@@ -14,6 +14,6 @@ if (!(Test-Path "./Bonsai.exe")) {
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Move-Item -Path "temp.config" "NuGet.config" -Force
     Remove-Item -Path "temp.zip"
-	Remove-Item -Path "Bonsai32.exe"
+    Remove-Item -Path "Bonsai32.exe"
 }
 & .\Bonsai.exe --no-editor
