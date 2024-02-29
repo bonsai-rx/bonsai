b4dce5046258007a10ec79dcc782835d6fb98129
glopesdev <glopesdev@users.noreply.github.com>
2024-02-29 14:03:32 +0000

    Improve detection of word boundaries in node names

--- Bonsai.Editor/StringExtensions.cs
+++ Bonsai.Editor/StringExtensions.cs
@@ -5,14 +5,42 @@ namespace Bonsai.Editor
 {
     internal static class StringExtensions
     {
+        static bool IsLowerToUpperCase(char a, char b)
+        {
+            return char.IsLower(a) && char.IsUpper(b);
+        }
+
+        static bool IsUpperToLowerCase(char a, char b)
+        {
+            return char.IsUpper(a) && char.IsLower(b);
+        }
+
+        static bool IsWordSeparator(char c)
+        {
+            return char.IsWhiteSpace(c) || c switch
+            {
+                '_' or '-' => true,
+                _ => false
+            };
+        }
+
+        static bool IsWordBreak(string text, int index, char current)
+        {
+            var previous = text[index - 1];
+            return IsLowerToUpperCase(previous, current)
+                || IsWordSeparator(previous)
+                || index < text.Length - 1 && IsUpperToLowerCase(current, text[index + 1]);
+        }
+
         public static string[] SplitOnWordBoundaries(this string text)
         {
             var wordCount = 0;
             var words = new string[text.Length];
             var builder = new StringBuilder(text.Length);
-            foreach (var c in text)
+            for (int i = 0; i < text.Length; i++)
             {
-                if (builder.Length > 0 && (Char.IsUpper(c) || Char.IsWhiteSpace(c)))
+                var c = text[i];
+                if (i > 0 && IsWordBreak(text, i, c))
                 {
                     words[wordCount++] = builder.ToString();
                     builder.Clear();
