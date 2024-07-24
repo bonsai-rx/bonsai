5bf950ecf7c351c6f88d15f9c2273aed8563b4e0
glopesdev <glopesdev@users.noreply.github.com>
2024-07-24 07:03:00 +0100

    Remove unused method

--- Bonsai.NuGet.Design/PackageViewController.cs
+++ Bonsai.NuGet.Design/PackageViewController.cs
@@ -204,19 +204,6 @@ namespace Bonsai.NuGet.Design
                 : new SearchQuery(repository, searchTerm, pageSize, includePrerelease, packageTypes);
         }
 
-        static Bitmap ResizeImage(Image image, Size newSize)
-        {
-            var result = new Bitmap(newSize.Width, newSize.Height);
-            using (var graphics = Graphics.FromImage(result))
-            {
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                graphics.DrawImage(image, 0, 0, newSize.Width, newSize.Height);
-            }
-            return result;
-        }
-
         IObservable<Image> GetPackageIcon(Uri iconUrl)
         {
             return iconReader.GetAsync(iconUrl).ToObservable();
