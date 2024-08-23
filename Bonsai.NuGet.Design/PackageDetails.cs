26feabc61c2cae6877dfa1ece4cf0b711e104aea
glopesdev <glopesdev@users.noreply.github.com>
2024-08-23 09:08:42 +0100

    Add multi-version selector and package status icon

--- Bonsai.NuGet.Design/PackageDetails.cs
+++ Bonsai.NuGet.Design/PackageDetails.cs
@@ -10,13 +10,13 @@ using System.IO;
 using NuGet.Frameworks;
 using NuGet.Packaging.Core;
 using System.ComponentModel;
+using NuGet.Versioning;
 
 namespace Bonsai.NuGet.Design
 {
     internal partial class PackageDetails : UserControl
     {
         PackageViewItem selectedItem;
-        PackageOperationType operation;
         const int TextHeightMargin = 7;
         static readonly object OperationClickEvent = new();
 
@@ -24,18 +24,11 @@ namespace Bonsai.NuGet.Design
         {
             InitializeComponent();
             ProjectFramework = NuGetFramework.AnyFramework;
+            versionComboBox.DisplayMember = nameof(VersionInfo.Version);
             SetPackage(null);
         }
 
-        public PackageOperationType Operation
-        {
-            get => operation;
-            set
-            {
-                operation = value;
-                operationButton.Text = value.ToString();
-            }
-        }
+        public PackageOperationType Operation { get; set; }
 
         [Category("Action")]
         public event PackageViewEventHandler OperationClick
@@ -67,16 +60,49 @@ namespace Bonsai.NuGet.Design
                 return;
             }
 
-            var package = item.Package;
+            var package = item.SelectedPackage;
             packageIdLabel.ImageList = item.ImageList;
             packageIdLabel.ImageIndex = item.ImageIndex;
             packageIdLabel.Text = package.Identity.Id;
 
+            installedVersionLayoutPanel.Visible =
+                (Operation == PackageOperationType.Install ||
+                Operation == PackageOperationType.Update) &&
+                selectedItem.LocalPackage != null;
+            if (installedVersionLayoutPanel.Visible)
+            {
+                installedVersionTextBox.Text = selectedItem.LocalPackage.Identity.Version.ToString();
+            }
+
+            var operation = Operation == PackageOperationType.Install && selectedItem.LocalPackage != null
+                ? PackageOperationType.Update
+                : Operation;
+            operationButton.Text = operation.ToString();
+
+            versionComboBox.Items.Clear();
+            foreach (var version in item.PackageVersions
+                                        .OrderByDescending(v => v.Version, VersionComparer.VersionRelease))
+            {
+                versionComboBox.Items.Add(version);
+                if (version.Version == package.Identity.Version)
+                {
+                    version.PackageSearchMetadata = package;
+                    versionComboBox.SelectedItem = version;
+                }
+            }
+
+            var selectedVersion = (VersionInfo)versionComboBox.SelectedItem;
+            SetPackageVersion(selectedVersion);
+            ResumeLayout();
+        }
+
+        void SetPackageVersion(VersionInfo versionInfo)
+        {
+            var package = versionInfo.PackageSearchMetadata;
             createdByLabel.Text = string.Join(CultureInfo.CurrentCulture.TextInfo.ListSeparator, package.Authors);
             SetLinkLabelUri(detailsLinkLabel, package.PackageDetailsUrl, true);
-            versionLabel.Text = package.Identity.Version.ToString();
             lastPublishedLabel.Text = package.Published.HasValue ? package.Published.Value.Date.ToShortDateString() : Resources.UnpublishedLabel;
-            downloadsLabel.Text = package.DownloadCount.ToString();
+            downloadsLabel.Text = versionInfo.DownloadCount.ToString();
             SetLinkLabelLicense(licenseLinkLabel, package, true);
             SetLinkLabelUri(projectLinkLabel, package.ProjectUrl, true);
             SetLinkLabelUri(reportAbuseLinkLabel, package.ReportAbuseUrl, false);
@@ -108,7 +134,6 @@ namespace Bonsai.NuGet.Design
                 dependenciesTextBox.Visible = false;
                 dependencyWarningLabel.Text = Resources.NoDependenciesLabel;
             }
-            ResumeLayout();
         }
 
         void SetLinkLabelLicense(LinkLabel linkLabel, IPackageSearchMetadata package, bool hideEmptyLink)
@@ -166,7 +191,40 @@ namespace Bonsai.NuGet.Design
 
         private void operationButton_Click(object sender, EventArgs e)
         {
-            OnOperationClick(new PackageViewEventArgs(selectedItem.Package, Operation));
+            var selectedVersion = (VersionInfo)versionComboBox.SelectedItem;
+            if (selectedVersion != null)
+            {
+                OnOperationClick(new PackageViewEventArgs(selectedVersion.PackageSearchMetadata, Operation));
+            }
+        }
+
+        private void uninstallButton_Click(object sender, EventArgs e)
+        {
+            var metadataBuilder = PackageSearchMetadataBuilder.FromIdentity(selectedItem.LocalPackage.Identity);
+            OnOperationClick(new PackageViewEventArgs(metadataBuilder.Build(), PackageOperationType.Uninstall));
+        }
+
+        private async void versionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var currentPackage = selectedItem;
+            var selectedVersion = (VersionInfo)versionComboBox.SelectedItem;
+            selectedVersion.PackageSearchMetadata ??= await selectedItem.GetVersionMetadataAsync(selectedVersion);
+
+            // cancel the update if selected package changes before version metadata is extracted
+            if (currentPackage != selectedItem || selectedVersion.PackageSearchMetadata is null)
+                return;
+
+            SuspendLayout();
+            SetPackageVersion(selectedVersion);
+            ResumeLayout();
+        }
+
+        private void versionComboBox_TextChanged(object sender, EventArgs e)
+        {
+            if (versionComboBox.SelectedIndex < 0)
+            {
+                operationButton.Enabled = false;
+            }
         }
     }
 }
