a4df614094561928e4e621bbd591985732013c55
glopesdev <glopesdev@users.noreply.github.com>
2024-09-03 17:43:06 +0100

    Add dedicated watch stream to track cancellations

---
+++ Bonsai.Core/Expressions/WatchNotification.cs
@@ -0,0 +1,33 @@
+﻿namespace Bonsai.Expressions
+{
+    /// <summary>
+    /// Indicates the type of an inspector watch notification.
+    /// </summary>
+    public enum WatchNotification
+    {
+        /// <summary>
+        /// Indicates the sequence was subscribed to.
+        /// </summary>
+        Subscribe,
+
+        /// <summary>
+        /// Indicates the sequence has emitted a value.
+        /// </summary>
+        OnNext,
+
+        /// <summary>
+        /// Indicates the sequence has terminated with an error.
+        /// </summary>
+        OnError,
+
+        /// <summary>
+        /// Indicates the sequence has terminated successfully.
+        /// </summary>
+        OnCompleted,
+
+        /// <summary>
+        /// Indicates the subscription to the sequence has been disposed.
+        /// </summary>
+        Unsubscribe
+    }
+}
