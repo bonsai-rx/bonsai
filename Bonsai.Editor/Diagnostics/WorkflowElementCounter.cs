a747f9f64a4ce84d90c95fc69e8d62e83a5faaf1
glopesdev <glopesdev@users.noreply.github.com>
2024-09-03 17:43:06 +0100

    Add dedicated watch stream to track cancellations

--- Bonsai.Editor/Diagnostics/WorkflowElementCounter.cs
+++ Bonsai.Editor/Diagnostics/WorkflowElementCounter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Threading;
 using Bonsai.Expressions;
 
 namespace Bonsai.Editor.Diagnostics
@@ -9,70 +8,69 @@ namespace Bonsai.Editor.Diagnostics
     internal class WorkflowElementCounter : IDisposable
     {
         readonly IDisposable subscription;
-        private long subscribeCount;
-        private long onNextCount;
-        private long onCompletedCount;
-        private long onErrorCount;
-        private long disposeCount;
-        private long lastNotificationCount;
+        private NotificationCounter subscribeCounter;
+        private NotificationCounter onNextCounter;
+        private NotificationCounter onCompletedCounter;
+        private NotificationCounter onErrorCounter;
+        private NotificationCounter unsubscribeCounter;
+        private WorkflowElementStatus lastInactiveStatus;
 
         public WorkflowElementCounter(InspectBuilder inspectBuilder)
         {
             InspectBuilder = inspectBuilder ?? throw new ArgumentNullException(nameof(inspectBuilder));
-            subscription = InspectBuilder.Output.SelectMany(source =>
+            subscription = InspectBuilder.Watch.SelectMany(source => source.Do(notification =>
             {
-                Interlocked.Increment(ref subscribeCount);
-                return source
-                    .Do(value => Interlocked.Increment(ref onNextCount),
-                        error => Interlocked.Increment(ref onErrorCount),
-                        () => Interlocked.Increment(ref onCompletedCount))
-                    .IgnoreElements()
-                    .Finally(() => Interlocked.Increment(ref disposeCount));
-            }).Subscribe();
+                switch (notification)
+                {
+                    case WatchNotification.Subscribe: subscribeCounter.Increment(); break;
+                    case WatchNotification.OnNext: onNextCounter.Increment(); break;
+                    case WatchNotification.OnError: onErrorCounter.Increment(); break;
+                    case WatchNotification.OnCompleted: onCompletedCounter.Increment(); break;
+                    case WatchNotification.Unsubscribe: unsubscribeCounter.Increment(); break;
+                }
+            }).IgnoreElements()).Subscribe();
         }
 
         public InspectBuilder InspectBuilder { get; }
 
-        public long SubscribeCount => subscribeCount;
+        public long SubscribeCount => subscribeCounter.Read();
 
-        public long OnNextCount => onNextCount;
+        public long OnNextCount => onNextCounter.Read();
 
-        public long OnErrorCount => onErrorCount;
+        public long OnErrorCount => onErrorCounter.Read();
 
-        public long OnCompletedCount => onCompletedCount;
+        public long OnCompletedCount => onCompletedCounter.Read();
 
-        public long DisposeCount => disposeCount;
+        public long UnsubscribeCount => unsubscribeCounter.Read();
 
         public long TotalCount =>
-            Interlocked.Read(ref subscribeCount) +
-            Interlocked.Read(ref onNextCount) +
-            Interlocked.Read(ref onErrorCount) +
-            Interlocked.Read(ref onCompletedCount) +
-            Interlocked.Read(ref disposeCount);
+            SubscribeCount +
+            OnNextCount +
+            OnErrorCount +
+            OnCompletedCount +
+            UnsubscribeCount;
 
         public WorkflowElementStatus GetStatus()
         {
-            var notificationCount = Interlocked.Read(ref onNextCount);
-            var activeSubscriptions = Interlocked.Read(ref subscribeCount) - Interlocked.Read(ref disposeCount);
+            var notificationDelta = onNextCounter.ReadDelta(out long notificationCount);
+            var unsubscribeDelta = unsubscribeCounter.ReadDelta(out long unsubscribeCount);
+            var activeSubscriptions = SubscribeCount - unsubscribeCount;
             if (activeSubscriptions > 0)
             {
-                if (notificationCount > lastNotificationCount)
-                {
-                    lastNotificationCount = notificationCount;
-                    return WorkflowElementStatus.Notifying;
-                }
-
-                return WorkflowElementStatus.Active;
+                return notificationDelta > 0
+                    ? WorkflowElementStatus.Notifying
+                    : WorkflowElementStatus.Active;
             }
-            else
+            else if (unsubscribeDelta > 0)
             {
-                lastNotificationCount = notificationCount;
-                var errorCount = Interlocked.Read(ref onErrorCount);
-                var completedCount = Interlocked.Read(ref onCompletedCount);
-                if (errorCount > 0) return WorkflowElementStatus.Error;
-                else if (completedCount > 0) return WorkflowElementStatus.Completed;
-                return WorkflowElementStatus.Ready;
+                var errorDelta = onErrorCounter.ReadDelta(out long _);
+                var completedDelta = onCompletedCounter.ReadDelta(out long _);
+                if (errorDelta > 0) lastInactiveStatus = WorkflowElementStatus.Error;
+                else if (completedDelta > 0) lastInactiveStatus = WorkflowElementStatus.Completed;
+                else lastInactiveStatus = WorkflowElementStatus.Canceled;
             }
+
+            return lastInactiveStatus;
         }
 
         public void Dispose()
