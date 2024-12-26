cbbd445ee9d707a2bcbefb9935d8e750eefdf02d
glopesdev <glopesdev@users.noreply.github.com>
2024-12-26 16:14:54 +0000

    Allow restore docking state up to first level

--- Bonsai.Editor/DockPanelHelper.cs
+++ Bonsai.Editor/DockPanelHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
 using Bonsai.Design;
 using WeifenLuo.WinFormsUI.Docking;
 
@@ -17,6 +18,10 @@ namespace Bonsai.Editor
             int contentIndex = -1;
             Rectangle? floatWindowBounds = null;
             DockContent dockContent = default;
+            DockAlignment? dockAlignment = default;
+            double dockProportion = default;
+            IntPtr dockPaneHandle = default;
+            IntPtr previousPaneHandle = default;
             var isUndoAction = false;
             Action close = () =>
             {
@@ -38,9 +43,14 @@ namespace Bonsai.Editor
                 dockContent.HideOnClose = false;
                 dockContent.FormClosed += (sender, e) =>
                 {
+                    var nestedDockingStatus = dockContent.Pane.NestedDockingStatus;
                     dockState = dockContent.DockState;
+                    dockAlignment = nestedDockingStatus.Alignment;
+                    dockProportion = nestedDockingStatus.Proportion;
+                    dockPaneHandle = dockContent.Pane.Handle;
+                    previousPaneHandle = (nestedDockingStatus.PreviousPane?.Handle).GetValueOrDefault();
                     floatWindowBounds = dockState == DockState.Float
-                        ? dockContent.Pane.ParentForm.Bounds
+                        ? dockContent.Pane.FloatWindow.Bounds
                         : null;
                     contentIndex = dockContent.Pane.Contents.IndexOf(dockContent);
                     if (!isUndoAction)
@@ -58,19 +68,41 @@ namespace Bonsai.Editor
                     }
                 };
 
-                if (floatWindowBounds.HasValue)
+                var dockControl = dockPaneHandle != IntPtr.Zero ? Control.FromHandle(dockPaneHandle) : null;
+                if (dockControl is DockPane dockPane)
+                {
+                    dockContent.Show(dockPane, contentIndex);
+                    return;
+                }
+
+                dockControl = previousPaneHandle != IntPtr.Zero ? Control.FromHandle(previousPaneHandle) : null;
+                if (dockControl is DockPane previousPane)
+                {
+                    dockContent.Show(previousPane, dockAlignment.Value, dockProportion);
+                }
+                else if (floatWindowBounds.HasValue)
+                {
                     dockContent.Show(dockPanel, floatWindowBounds.GetValueOrDefault());
+                }
                 else
                 {
                     dockContent.Show(dockPanel, dockState);
-                    if (contentIndex >= 0 && dockContent.PanelPane is not null)
-                        dockContent.PanelPane.SetContentIndex(dockContent, contentIndex);
                 }
             };
 
             commandExecutor.Execute(
                 createAndShow,
                 close);
+        }
+
+        public static void Show(this DockContent dockContent, DockPane pane, int contentIndex)
+        {
+            pane.DockPanel.SuspendLayout(allWindows: true);
+            dockContent.DockPanel = pane.DockPanel;
+            dockContent.Pane = pane;
+            pane.SetContentIndex(dockContent, contentIndex);
+            dockContent.Show();
+            pane.DockPanel.ResumeLayout(performLayout: true, allWindows: true);
         }
     }
 }
