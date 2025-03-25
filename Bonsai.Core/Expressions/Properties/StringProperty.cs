9a7d2666b3ad0ca45a7e79da50b6d4769b26b61f
glopesdev <glopesdev@users.noreply.github.com>
2025-03-25 11:35:49 +0000

    Add multiline string editor for string properties

--- Bonsai.Core/Expressions/Properties/StringProperty.cs
+++ Bonsai.Core/Expressions/Properties/StringProperty.cs
@@ -11,5 +11,14 @@ namespace Bonsai.Expressions
     [Description("Represents a workflow property containing Unicode text.")]
     public class StringProperty : WorkflowProperty<string>
     {
+        /// <summary>
+        /// Gets or sets the value of the property.
+        /// </summary>
+        [Editor(DesignTypes.MultilineStringEditor, DesignTypes.UITypeEditor)]
+        public new string Value
+        {
+            get => base.Value;
+            set => base.Value = value;
+        }
     }
 }
