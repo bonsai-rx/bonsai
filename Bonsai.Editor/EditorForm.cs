f516d66693afc5ac45c6a5411e33445386108c91
glopesdev <glopesdev@users.noreply.github.com>
2025-03-29 00:39:25 +0000

    Offer to delete file on visualizer layout error

--- Bonsai.Editor/EditorForm.cs
+++ Bonsai.Editor/EditorForm.cs
@@ -843,13 +843,22 @@ namespace Bonsai.Editor
             var layoutPath = LayoutHelper.GetCompatibleLayoutPath(settingsDirectory, fileName);
             if (File.Exists(layoutPath))
             {
-                using var reader = XmlReader.Create(layoutPath);
                 try
                 {
+                    using var reader = XmlReader.Create(layoutPath);
                     var visualizerLayout = (VisualizerLayout)VisualizerLayout.Serializer.Deserialize(reader);
                     visualizerSettings.SetVisualizerLayout(workflowBuilder, visualizerLayout);
                 }
-                catch (InvalidOperationException) { }
+                catch (Exception ex) when (ex is ArgumentException || ex is InvalidOperationException)
+                {
+                    var icon = MessageBoxIcon.Error;
+                    var caption = Resources.VisualizerLayout_Caption;
+                    var message = string.Format(Resources.VisualizerLayoutCorrupt_Question, ex.Message);
+                    if (MessageBox.Show(this, message, caption, MessageBoxButtons.YesNo, icon) == DialogResult.Yes)
+                    {
+                        File.Delete(layoutPath);
+                    }
+                }
             }
 
             ResetProjectStatus();
