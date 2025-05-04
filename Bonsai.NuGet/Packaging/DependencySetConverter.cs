0a49392ab618159579fcc25253ad49b047007012
glopesdev <glopesdev@users.noreply.github.com>
2025-05-04 21:30:31 +0100

    Move PackageBuilder converters to core package
    To improve future refactoring and decoupling from UI frameworks the
    previously internal type description classes are moved away from the
    dialog and into the core platform agnostic package.

---
+++ Bonsai.NuGet/Packaging/DependencySetConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using NuGet.Packaging;
+
+namespace Bonsai.NuGet.Packaging
+{
+    public class DependencySetConverter : TypeConverter
+    {
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return "(Collection)";
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+
+        public override bool GetPropertiesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            var dependencyGroups = value as Collection<PackageDependencyGroup>;
+            if (dependencyGroups != null)
+            {
+                var properties = from dependencyGroup in dependencyGroups
+                                 from dependency in dependencyGroup.Packages
+                                 select new ConstantPropertyDescriptor(dependency.Id, dependency.VersionRange);
+                return new PropertyDescriptorCollection(properties.ToArray());
+            }
+
+            return base.GetProperties(context, value, attributes);
+        }
+    }
+}
---
+++ Bonsai.NuGet/Packaging/NuGetVersionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using NuGet.Versioning;
+
+namespace Bonsai.NuGet.Packaging
+{
+    public class NuGetVersionConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string)) return true;
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var text = value as string;
+            if (text != null) return NuGetVersion.Parse(text);
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
