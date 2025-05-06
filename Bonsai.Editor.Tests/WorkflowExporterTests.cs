9bcba696e05babab2cf3c4f38f2c1953e9d31692
glopesdev <glopesdev@users.noreply.github.com>
2025-05-06 11:31:01 +0100

    Add test for exporting with missing subjects

--- Bonsai.Editor.Tests/WorkflowExporterTests.cs
+++ Bonsai.Editor.Tests/WorkflowExporterTests.cs
@@ -8,17 +8,27 @@ namespace Bonsai.Editor.Tests
     [DoNotParallelize] // Tests involve filesystem at current working directory and must not be ran concurrently
     public class WorkflowExporterTests
     {
-        [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void ExportImage_WorkflowContainsMissingTypes_InvalidOperationException()
+        static void ExportImage(string fileName)
         {
-            var fileName = "MissingTypes.bonsai";
             EditorHelper.SaveEmbeddedResource(fileName, fileName);
             try
             {
                 WorkflowExporter.ExportImage(fileName, Path.ChangeExtension(fileName, ".svg"));
             }
             finally { File.Delete(fileName); }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ExportImage_WorkflowContainsMissingTypes_InvalidOperationException()
+        {
+            ExportImage("MissingTypes.bonsai");
+        }
+
+        [TestMethod]
+        public void ExportImage_WorkflowContainsMissingSubjects_ExportSuccessful()
+        {
+            ExportImage("MissingSubject.bonsai");
         }
     }
 }
