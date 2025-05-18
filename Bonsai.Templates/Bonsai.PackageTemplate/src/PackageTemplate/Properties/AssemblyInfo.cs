5f6cf51a1613b3241637daea9cfdee9c7d413b27
glopesdev <glopesdev@users.noreply.github.com>
2025-05-18 20:46:40 +0100

    Add minimal package template contents
    The new package template is to provide a complete repository structure
    with the recommended layout and project settings for new packages.

---
+++ Bonsai.Templates/Bonsai.PackageTemplate/src/PackageTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Bonsai;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: XmlNamespacePrefix("clr-namespace:$projectname$", null)]
